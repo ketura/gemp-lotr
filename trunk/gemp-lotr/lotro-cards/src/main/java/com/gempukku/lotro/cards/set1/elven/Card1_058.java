a9fab2fd2c5da39622574122b70af7885b453ce4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-29 15:14:37 +0000

    "The Seen and the Unseen"

---
+++ trunk/gemp-lotr/lotro-cards/src/main/java/com/gempukku/lotro/cards/set1/elven/Card1_058.java
@@ -0,0 +1,67 @@
+package com.gempukku.lotro.cards.set1.elven;
+
+import com.gempukku.lotro.cards.AbstractLotroCardBlueprint;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventFromHandAction;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Fellowship: Exert 2 Elves to discard a condition.
+ */
+public class Card1_058 extends AbstractLotroCardBlueprint {
+    public Card1_058() {
+        super(Side.FREE_PEOPLE, CardType.EVENT, Culture.ELVEN, "The Seen and the Unseen", "1_58");
+        addKeyword(Keyword.FELLOWSHIP);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canPlayFromHandDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && Filters.countActive(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.ELF), Filters.canExert()) >= 2) {
+            final PlayEventFromHandAction action = new PlayEventFromHandAction(self);
+            action.addCost(
+                    new ChooseActiveCardEffect(playerId, "Exert first Elf", Filters.keyword(Keyword.ELF)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, final PhysicalCard firstElf) {
+                            action.addCost(new ExertCharacterEffect(firstElf));
+                            action.addCost(
+                                    new ChooseActiveCardEffect(playerId, "Exert second Elf", Filters.keyword(Keyword.ELF), Filters.not(Filters.sameCard(firstElf))) {
+                                        @Override
+                                        protected void cardSelected(LotroGame game, PhysicalCard secondElf) {
+                                            action.addCost(new ExertCharacterEffect(secondElf));
+                                        }
+                                    });
+                        }
+                    });
+            action.addEffect(
+                    new ChooseActiveCardEffect(playerId, "Choose condition", Filters.type(CardType.CONDITION)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard condition) {
+                            action.addEffect(new DiscardCardFromPlayEffect(condition));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
