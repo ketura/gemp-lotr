479a2bcb3eebeb4b775ac4293463de77ca108856
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-29 13:29:44 +0000

    Simplifying choice effect

--- gemp-lotr/lotro-cards/src/main/java/com/gempukku/lotro/cards/effects/EffectPreCondition.java
+++
@@ -1,7 +0,0 @@
-package com.gempukku.lotro.cards.effects;
-
-import com.gempukku.lotro.game.state.LotroGame;
-
-public interface EffectPreCondition {
-    public boolean getResult(LotroGame game);
-}
