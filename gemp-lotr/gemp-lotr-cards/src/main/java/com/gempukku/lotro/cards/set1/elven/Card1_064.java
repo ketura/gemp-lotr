8e6400e5721269ecf072e4614e86c8450b070354
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-30 11:04:31 +0000

    "Support of the Last Homely House"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/elven/Card1_064.java
@@ -0,0 +1,60 @@
+package com.gempukku.lotro.cards.set1.elven;
+
+import com.gempukku.lotro.cards.AbstractLotroCardBlueprint;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.cards.modifiers.AllyOnCurrentSiteModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Maneuver: Exert an Elf ally whose home is site 3. Until the regroup phase, that ally is strength +3 and
+ * participates in archery fire and skirmishes.
+ */
+public class Card1_064 extends AbstractLotroCardBlueprint {
+    public Card1_064() {
+        super(Side.FREE_PEOPLE, CardType.EVENT, Culture.ELVEN, "Support of the Last Homely House", "1_64");
+        addKeyword(Keyword.MANEUVER);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canPlayDuringPhase(game.getGameState(), Phase.MANEUVER, self)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.ELF), Filters.type(CardType.ALLY), Filters.siteNumber(3), Filters.canExert())) {
+            final CostToEffectAction action = new CostToEffectAction(self, "Exert an Elf ally whose home is site 3. Until the regroup phase, that ally is strength +3 and participates in archery fire and skirmishes.");
+            action.addCost(
+                    new ChooseActiveCardEffect(playerId, "Choose and Elf ally", Filters.keyword(Keyword.ELF), Filters.type(CardType.ALLY), Filters.siteNumber(3), Filters.canExert()) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard elfAlly) {
+                            action.addCost(new ExertCharacterEffect(elfAlly));
+                            action.addEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new AllyOnCurrentSiteModifier(self, Filters.sameCard(elfAlly)), Phase.REGROUP));
+                        }
+                    }
+            );
+
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
