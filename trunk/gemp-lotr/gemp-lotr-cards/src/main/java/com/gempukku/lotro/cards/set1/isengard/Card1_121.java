148609c788e54ccbac91ecab40f2f0985da6f922
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-30 13:12:58 +0000

    "Bred for Battle"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_121.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractLotroCardBlueprint;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Exert an Uruk-hai to make it strength +3.
+ */
+public class Card1_121 extends AbstractLotroCardBlueprint {
+    public Card1_121() {
+        super(Side.SHADOW, CardType.EVENT, Culture.ISENGARD, "Bred for Battle", "1_121");
+        addKeyword(Keyword.SKIRMISH);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canPlayShadowCardDuringPhase(game.getGameState(), game.getModifiersQuerying(), Phase.SKIRMISH, self)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.URUK_HAI), Filters.canExert())) {
+            final PlayEventAction action = new PlayEventAction(self);
+            action.addCost(
+                    new ChooseActiveCardEffect(playerId, "Choose an Uruk-hai", Filters.keyword(Keyword.URUK_HAI), Filters.canExert()) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard urukHai) {
+                            action.addCost(new ExertCharacterEffect(urukHai));
+                            action.addEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, Filters.sameCard(urukHai), 3), Phase.SKIRMISH));
+                        }
+                    }
+            );
+
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
