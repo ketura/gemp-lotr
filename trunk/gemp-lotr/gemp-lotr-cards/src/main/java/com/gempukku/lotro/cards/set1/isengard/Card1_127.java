2ca323a4924db46cd4a1dd32b6a2c837d0896914
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-30 14:50:47 +0000

    "Lurtz"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_127.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 7
+ * Type: Minion • Uruk-Hai
+ * Strength: 13
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Archer. Damage +1. Maneuver: Spot another Uruk-hai to make Lurtz fierce until the regroup phase.
+ */
+public class Card1_127 extends AbstractMinion {
+    public Card1_127() {
+        super(7, 13, 3, 5, Culture.ISENGARD, "Lurtz", "1_127", true);
+        addKeyword(Keyword.ARCHER);
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 0)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.URUK_HAI), Filters.not(Filters.sameCard(self)))) {
+            CostToEffectAction action = new CostToEffectAction(self, "Spot another Uruk-hai to make Lurtz fierce until the regroup phase.");
+            action.addEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new KeywordModifier(self, Filters.sameCard(self), Keyword.FIERCE), Phase.REGROUP));
+
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
