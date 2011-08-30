4b29b92866e54b866afb3b2a730a3ace545895d8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-30 15:55:14 +0000

    "Orthanc Assassin"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_131.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 5
+ * Type: Minion • Uruk-Hai
+ * Strength: 10
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Archer. Damage +1.
+ */
+public class Card1_131 extends AbstractMinion {
+    public Card1_131() {
+        super(5, 10, 3, 5, Culture.ISENGARD, "Orthanc Assassin", "1_131", true);
+        addKeyword(Keyword.ARCHER);
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        List<Action> actions = new LinkedList<Action>();
+
+        appendPlayMinionAction(actions, game, self);
+
+        return actions;
+    }
+}
