0278372cbc831edfa6f7d27a0d118822e6013af5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 11:11:01 +0000

    "Uruk Savage"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_151.java
@@ -0,0 +1,37 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 2
+ * Type: Minion • Uruk-Hai
+ * Strength: 5
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Damage +1.
+ */
+public class Card1_151 extends AbstractMinion {
+    public Card1_151() {
+        super(2, 5, 3, 5, Culture.ISENGARD, "Uruk Savage", "1_151");
+        addKeyword(Keyword.URUK_HAI);
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        List<Action> actions = new LinkedList<Action>();
+        appendPlayMinionAction(actions, game, self);
+        return actions;
+    }
+}
