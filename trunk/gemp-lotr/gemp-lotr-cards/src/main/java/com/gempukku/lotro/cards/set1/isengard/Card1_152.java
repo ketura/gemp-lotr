852e47be890fb7730a4fcf61616ddc0ba6a8b499
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 11:19:13 +0000

    "Uruk Shaman"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_152.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.HealCharacterEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 3
+ * Type: Minion • Uruk-Hai
+ * Strength: 8
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. Maneuver: Remove (2) to heal an Uruk-hai.
+ */
+public class Card1_152 extends AbstractMinion {
+    public Card1_152() {
+        super(3, 8, 2, 5, Culture.ISENGARD, "Uruk Shaman", "1_152");
+        addKeyword(Keyword.URUK_HAI);
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        List<Action> actions = new LinkedList<Action>();
+        appendPlayMinionAction(actions, game, self);
+
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 2)) {
+            final CostToEffectAction action = new CostToEffectAction(self, "Remove (2) to heal an Uruk-hai");
+            action.addCost(new RemoveTwilightEffect(2));
+            action.addEffect(
+                    new ChooseActiveCardEffect(playerId, "Choose an Uruk-hai", Filters.keyword(Keyword.URUK_HAI)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard urukHai) {
+                            action.addEffect(new HealCharacterEffect(urukHai));
+                        }
+                    });
+
+            actions.add(action);
+        }
+        return actions;
+    }
+}
