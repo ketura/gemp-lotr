3201a3911dbbeba23c352722c3de08c3425377db
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 22:48:31 +0000

    "Midgewater Moors"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_333.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardFromHandEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Twilight Cost: 1
+ * Type: Site
+ * Site: 2
+ * Game Text: Plains. Each time a minion is played, the Free Peoples player discards a card from hand.
+ */
+public class Card1_333 extends AbstractSite {
+    public Card1_333() {
+        super("Midgewater Moors", 2, 1, Direction.LEFT);
+        addKeyword(Keyword.PLAINS);
+    }
+
+    @Override
+    public List<? extends Action> getRequiredWhenActions(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.type(CardType.MINION))) {
+            CostToEffectAction action = new CostToEffectAction(self, null, "Free Peoples player discards a card from hand");
+            action.addEffect(
+                    new ChooseAndDiscardCardFromHandEffect(action, game.getGameState().getCurrentPlayerId(), false));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
