141dc5fddbb0de4ceefd733ca41aa97fea83990d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 12:32:27 +0000

    "Uruk Soldier"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_154.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.DiscardTopCardFromDeckEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 2
+ * Type: Minion • Uruk-Hai
+ * Strength: 7
+ * Vitality: 1
+ * Site: 5
+ * Game Text: Damage +1. When you play this minion, you may make the Free Peoples player discard the top card of his
+ * draw deck.
+ */
+public class Card1_154 extends AbstractMinion {
+    public Card1_154() {
+        super(2, 7, 1, 5, Culture.ISENGARD, "Uruk Soldier", "1_154");
+        addKeyword(Keyword.URUK_HAI);
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        List<Action> actions = new LinkedList<Action>();
+        appendPlayMinionAction(actions, game, self);
+        return actions;
+    }
+
+    @Override
+    public List<? extends Action> getRequiredWhenActions(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.sameCard(self))) {
+            CostToEffectAction action = new CostToEffectAction(self, "The Free Peoples player discard the top card of his draw deck.");
+            action.addEffect(new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
