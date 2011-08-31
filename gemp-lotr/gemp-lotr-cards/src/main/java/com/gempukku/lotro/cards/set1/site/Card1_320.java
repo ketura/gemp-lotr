5e8790528f0e49aca0980003a61fba5b74aa5e5e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 14:58:28 +0000

    "East Road"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_320.java
@@ -0,0 +1,25 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Type: Site
+ * Site: 1
+ * Game Text: Each companion's twilight cost is +2
+ */
+public class Card1_320 extends AbstractSite {
+    public Card1_320() {
+        super("East Road", "1_320", 1, 0, Direction.LEFT);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new TwilightCostModifier(self, Filters.type(CardType.COMPANION), 2);
+    }
+}
