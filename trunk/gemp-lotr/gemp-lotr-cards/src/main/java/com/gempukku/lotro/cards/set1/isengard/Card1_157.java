ca34b8604de68bc2d75a55b6c470204913b71af6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 12:49:35 +0000

    "Uruk-hai Armory"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/isengard/Card1_157.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set1.isengard;
+
+import com.gempukku.lotro.cards.AbstractLotroCardBlueprint;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayPermanentAction;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: Plays to your support area. While you can spot an Uruk-hai, the fellowship archery total is -1.
+ */
+public class Card1_157 extends AbstractLotroCardBlueprint {
+    public Card1_157() {
+        super(Side.SHADOW, CardType.CONDITION, Culture.ISENGARD, "Uruk-hai Armory", "1_157");
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canPlayShadowCardDuringPhase(game, Phase.SHADOW, self)) {
+            PlayPermanentAction action = new PlayPermanentAction(self, Zone.SHADOW_SUPPORT);
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new AbstractModifier(self, "While you can spot an Uruk-hai, the fellowship archery total is -1.", null) {
+            @Override
+            public int getArcheryTotal(GameState gameState, ModifiersQuerying modifiersLogic, Side side, int result) {
+                if (side == Side.FREE_PEOPLE
+                        && Filters.canSpot(gameState, modifiersLogic, Filters.keyword(Keyword.URUK_HAI)))
+                    return result - 1;
+                return result;
+            }
+        };
+    }
+}
