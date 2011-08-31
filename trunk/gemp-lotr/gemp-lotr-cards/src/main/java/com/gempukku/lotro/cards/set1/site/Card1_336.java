ca55e522cd981a1b363df5fc486003b417384b6f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 22:59:25 +0000

    "Weathertop"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_336.java
@@ -0,0 +1,38 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Twilight Cost: 3
+ * Type: Site
+ * Site: 2
+ * Game Text: Each Nazgul at Weathertop is fierce.
+ */
+public class Card1_336 extends AbstractSite {
+    public Card1_336() {
+        super("Weathertop", 2, 3, Direction.LEFT);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(final PhysicalCard self) {
+        return new KeywordModifier(self,
+                Filters.and(
+                        Filters.keyword(Keyword.NAZGUL),
+                        new Filter() {
+                            @Override
+                            public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                                return gameState.getCurrentSite() == self;
+                            }
+                        }
+                ), Keyword.FIERCE);
+    }
+}
