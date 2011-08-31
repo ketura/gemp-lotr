643ecb27212897e247a0ebec017919a3aabec98b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-08-31 15:05:13 +0000

    "Green Hill Country"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_323.java
@@ -0,0 +1,25 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Type: Site
+ * Site: 1
+ * Game Text: The twilight cost of each Hobbit is -1.
+ */
+public class Card1_323 extends AbstractSite {
+    public Card1_323() {
+        super("Green Hill Country", "1_323", 1, 0, Direction.LEFT);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new TwilightCostModifier(self, Filters.keyword(Keyword.HOBBIT), -1);
+    }
+}
