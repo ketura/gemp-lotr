d449246661dd69b8058ba7deaa1cc1593b5c6def
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-01 01:01:40 +0000

    "Pillars of the Kings"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_358.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.HealCharacterEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.ChooseCardsFromHandEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardFromHandEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Twilight Cost: 6
+ * Type: Site
+ * Site: 8
+ * Game Text: River. Fellowship: Discard a [GONDOR] card from hand to heal a [GONDOR] companion.
+ */
+public class Card1_358 extends AbstractSite {
+    public Card1_358() {
+        super("Pillars of the Kings", 8, 6, Direction.RIGHT);
+        addKeyword(Keyword.RIVER);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && Filters.filter(game.getGameState().getHand(playerId), game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.GONDOR)).size() > 0) {
+            final CostToEffectAction action = new CostToEffectAction(self, Keyword.FELLOWSHIP, "Discard a GONDOR card from hand to heal a GONDOR companion.");
+            action.addCost(
+                    new ChooseCardsFromHandEffect(playerId, "Choose a GONDOR card", 1, 1, Filters.culture(Culture.GONDOR)) {
+                        @Override
+                        protected void cardsSelected(List<PhysicalCard> selectedCards) {
+                            action.addCost(new DiscardCardFromHandEffect(selectedCards.get(0)));
+                        }
+                    });
+            action.addEffect(
+                    new ChooseActiveCardEffect(playerId, "Choose a GONDOR companion", Filters.type(CardType.COMPANION), Filters.culture(Culture.GONDOR)) {
+                        @Override
+                        protected void cardSelected(PhysicalCard gondonCompanion) {
+                            action.addEffect(new HealCharacterEffect(gondonCompanion));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
