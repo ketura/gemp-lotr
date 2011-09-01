4f9333cdb5dc2c8b279cf7dbc72fbb48d7c9e177
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-01 01:08:36 +0000

    "Emyn Muil"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_360.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Twilight Cost: 9
+ * Type: Site
+ * Site: 9
+ * Game Text: Maneuver: Exert your minion to make that minion fierce until the regroup phase.
+ */
+public class Card1_360 extends AbstractSite {
+    public Card1_360() {
+        super("Emyn Muil", 9, 9, Direction.LEFT);
+    }
+
+    @Override
+    public List<? extends Action> getPlayablePhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.MANEUVER, self)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.owner(playerId), Filters.type(CardType.MINION), Filters.canExert())) {
+            final CostToEffectAction action = new CostToEffectAction(self, Keyword.MANEUVER, "Exert your minion to make that minion fierce until the regroup phase.");
+            action.addCost(
+                    new ChooseActiveCardEffect(playerId, "Choose your minion", Filters.owner(playerId), Filters.type(CardType.MINION), Filters.canExert()) {
+                        @Override
+                        protected void cardSelected(PhysicalCard minion) {
+                            action.addCost(new ExertCharacterEffect(minion));
+                            action.addEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, Filters.sameCard(minion), Keyword.FIERCE), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
