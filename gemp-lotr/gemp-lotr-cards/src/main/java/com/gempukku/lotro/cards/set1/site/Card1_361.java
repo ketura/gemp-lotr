fdc03f4ef33c7b460fd67806478ea1d0974d8ab8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-01 01:10:14 +0000

    "Slopes of Amon Hen"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_361.java
@@ -0,0 +1,27 @@
+package com.gempukku.lotro.cards.set1.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Twilight Cost: 9
+ * Type: Site
+ * Site: 9
+ * Game Text: The twilight cost of each [ISENGARD] minion is -1.
+ */
+public class Card1_361 extends AbstractSite {
+    public Card1_361() {
+        super("Slopes of Amon Hen", 9, 9, Direction.LEFT);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new TwilightCostModifier(self, Filters.and(Filters.culture(Culture.ISENGARD), Filters.type(CardType.MINION)), -1);
+    }
+}
