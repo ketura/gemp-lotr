31b29e07a1a22b0903ea0ccdcc80396597088da5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-01 00:32:21 +0000

    "Dimrill Dale"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_350.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_350.java
@@ -2,6 +2,7 @@ package com.gempukku.lotro.cards.set1.site;
 
 import com.gempukku.lotro.cards.AbstractSite;
 import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.filters.Filters;
@@ -18,15 +19,14 @@ import java.util.List;
 
 /**
  * Set: The Fellowship of the Ring
- * Twilight Cost: 0
+ * Twilight Cost: 3
  * Type: Site
- * Site: 3
- * Game Text: River. Sanctuary. The twilight cost of the first Nazgul played to Ford of Bruinen each turn is -5.
+ * Site: 6
+ * Game Text: Sanctuary. The twilight cost of the first [MORIA] Orc played each Shadow phase is -2.
  */
-public class Card1_338 extends AbstractSite {
-    public Card1_338() {
-        super("Ford of Bruinen", 3, 0, Direction.RIGHT);
-        addKeyword(Keyword.RIVER);
+public class Card1_350 extends AbstractSite {
+    public Card1_350() {
+        super("Dimrill Dale", 6, 3, Direction.LEFT);
         addKeyword(Keyword.SANCTUARY);
     }
 
@@ -34,19 +34,19 @@ public class Card1_338 extends AbstractSite {
     public Modifier getAlwaysOnEffect(final PhysicalCard self) {
         return new TwilightCostModifier(self,
                 Filters.and(
-                        Filters.keyword(Keyword.NAZGUL),
+                        Filters.culture(Culture.MORIA),
+                        Filters.keyword(Keyword.ORC),
                         new Filter() {
                             @Override
                             public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
                                 return (self.getData() == null);
                             }
-                        }), -5);
+                        }), -2);
     }
 
     @Override
     public List<? extends Action> getRequiredWhenActions(LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.keyword(Keyword.NAZGUL))
-                && game.getGameState().getCurrentSite() == self)
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.and(Filters.culture(Culture.MORIA), Filters.keyword(Keyword.ORC))))
             self.storeData(new Object());
         if (effectResult.getType() == EffectResult.Type.END_OF_TURN
                 && self.getData() != null)
