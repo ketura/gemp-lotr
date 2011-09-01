454579d0dc385ddc6c2c5a8f2f1615d0e35ed5a1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-01 00:52:24 +0000

    "Anduin Banks"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_356.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_356.java
@@ -1,6 +1,15 @@
 package com.gempukku.lotro.cards.set1.site;
 
 import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 /**
  * Set: The Fellowship of the Ring
@@ -12,5 +21,21 @@ import com.gempukku.lotro.cards.AbstractSite;
 public class Card1_356 extends AbstractSite {
     public Card1_356() {
         super("Anduin Banks", 8, 6, Direction.RIGHT);
+        addKeyword(Keyword.RIVER);
     }
-}
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new AbstractModifier(self, "For each companion in the fellowship over 4, add 2 to the minion archery total.", null) {
+            @Override
+            public int getArcheryTotal(GameState gameState, ModifiersQuerying modifiersLogic, Side side, int result) {
+                if (side == Side.SHADOW) {
+                    int bonus = Math.max(0, Filters.countActive(gameState, modifiersLogic, Filters.type(CardType.COMPANION)) - 4);
+                    return result + bonus;
+                }
+                return result;
+            }
+
+            );
+        }
+    }
