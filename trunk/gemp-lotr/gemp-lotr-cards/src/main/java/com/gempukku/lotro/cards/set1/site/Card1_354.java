4d08261062757c2520401bc518f6a1b1de5b9da8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-01 00:46:40 +0000

    "Anduin Wilderland", "Anduin Confluence"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_354.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_354.java
@@ -1,6 +1,13 @@
 package com.gempukku.lotro.cards.set1.site;
 
 import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 /**
  * Set: The Fellowship of the Ring
@@ -12,5 +19,20 @@ import com.gempukku.lotro.cards.AbstractSite;
 public class Card1_354 extends AbstractSite {
     public Card1_354() {
         super("Anduin Wilderland", 7, 6, Direction.RIGHT);
+        addKeyword(Keyword.FOREST);
+        addKeyword(Keyword.RIVER);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(final PhysicalCard self) {
+        return new AbstractModifier(self, "Skip archery phase", null) {
+            @Override
+            public boolean shouldSkipPhase(GameState gameState, ModifiersQuerying modifiersQuerying, Phase phase, boolean result) {
+                if (phase == Phase.ARCHERY
+                        && gameState.getCurrentSite() == self)
+                    return true;
+                return result;
+            }
+        };
     }
 }
---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/site/Card1_355.java
@@ -0,0 +1,4 @@
+package com.gempukku.lotro.cards.set1.site;
+
+public class Card1_355 {
+}
