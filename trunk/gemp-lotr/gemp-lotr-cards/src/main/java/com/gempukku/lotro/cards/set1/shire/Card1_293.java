a261e720ce865ff60395d4e7e509b7b6b70065c4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-04 23:46:44 +0000

    "Halfling Deftness"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/shire/Card1_293.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set1.shire;
+
+import com.gempukku.lotro.cards.AbstractLotroCardBlueprint;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Exert a Hobbit to make him strength +3.
+ */
+public class Card1_293 extends AbstractLotroCardBlueprint {
+    public Card1_293() {
+        super(Side.FREE_PEOPLE, CardType.EVENT, Culture.SHIRE, "Halfling Deftness");
+        addKeyword(Keyword.SKIRMISH);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canPlayFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.HOBBIT), Filters.canExert())) {
+            final PlayEventAction action = new PlayEventAction(self);
+            action.addCost(
+                    new ChooseActiveCardEffect(playerId, "Choose a Hobbit", Filters.keyword(Keyword.HOBBIT), Filters.canExert()) {
+                        @Override
+                        protected void cardSelected(PhysicalCard hobbit) {
+                            action.addCost(new ExertCharacterEffect(hobbit));
+                            action.addEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, Filters.sameCard(hobbit), 3), Phase.SKIRMISH));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
