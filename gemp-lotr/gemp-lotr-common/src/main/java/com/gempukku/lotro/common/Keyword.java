a6bf7266554bb15bd617a9f2571ae1627383f841
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-06 10:03:26 +0000

    "Gandalf"

--- gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Keyword.java
+++ gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Keyword.java
@@ -1,7 +1,7 @@
 package com.gempukku.lotro.common;
 
 public enum Keyword {
-    ELF("Elf"), HOBBIT("Hobbit"), DWARF("Dwarf"), MAN("Man"),
+    ELF("Elf"), HOBBIT("Hobbit"), DWARF("Dwarf"), MAN("Man"), WIZARD("Wizard"),
     URUK_HAI("Uruk-Hai"), NAZGUL("Nazgul"), ORC("Orc"), TROLL("Troll"),
 
     SUPPORT_AREA("Support Area"),
