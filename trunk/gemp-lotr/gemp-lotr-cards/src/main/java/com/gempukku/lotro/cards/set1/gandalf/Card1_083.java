a7fa079b23ab8c7e3012bfa06fa7574b0279ce0e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-06 11:45:43 +0000

    "Servant of the Secret Fire"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/gandalf/Card1_083.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set1.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Spell. Skirmish: Spot Gandalf to make a minion strength -3.
+ */
+public class Card1_083 extends AbstractEvent {
+    public Card1_083() {
+        super(Side.FREE_PEOPLE, Culture.GANDALF, "Servant of the Secret Fire", Phase.SKIRMISH);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.name("Gandalf"));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.addEffect(
+                new ChooseActiveCardEffect(playerId, "Choose a minion", Filters.type(CardType.MINION)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard minion) {
+                        action.addEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(minion), -3), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+}
