061491865007c5e5f13674c4f683faf4cc581b27
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-06 16:03:33 +0000

    "An Able Guide"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/gondor/Card1_088.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set1.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Regroup: Spot a ranger to remove (4).
+ */
+public class Card1_088 extends AbstractEvent {
+    public Card1_088() {
+        super(Side.FREE_PEOPLE, Culture.GONDOR, "An Able Guide", Phase.REGROUP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.RANGER));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.addEffect(
+                new RemoveTwilightEffect(4));
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+}
