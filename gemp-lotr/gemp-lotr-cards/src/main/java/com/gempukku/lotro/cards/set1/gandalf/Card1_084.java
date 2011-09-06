f49cfbc48fa210524cc55bfe2fcdb21e410368c9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-06 13:38:14 +0000

    "Sleep Caradhras"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/gandalf/Card1_084.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set1.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 3
+ * Type: Event
+ * Game Text: Spell. Fellowship: Exert Gandalf to discard every condition.
+ */
+public class Card1_084 extends AbstractEvent {
+    public Card1_084() {
+        super(Side.FREE_PEOPLE, Culture.GANDALF, "Sleep Caradhras", Phase.FELLOWSHIP);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.name("Gandalf"), Filters.canExert());
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        PhysicalCard gandalf = Filters.findFirstActive(game.getGameState(), game.getModifiersQuerying(), Filters.name("Gandalf"), Filters.canExert());
+        action.addCost(new ExertCharacterEffect(gandalf));
+        action.addEffect(
+                new DiscardCardsFromPlayEffect(self, Filters.type(CardType.CONDITION)));
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 3;
+    }
+}
