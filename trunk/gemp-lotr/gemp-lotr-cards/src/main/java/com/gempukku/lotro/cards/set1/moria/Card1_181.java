52ec9389c2bebf4df73ec263982e8b62a8f28c90
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-08 20:48:31 +0000

    "Goblin Sneak"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/moria/Card1_181.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set1.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChooseArbitraryCardsEffect;
+import com.gempukku.lotro.cards.effects.PutCardFromDiscardOnBottomOfDeckEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 1
+ * Type: Minion • Orc
+ * Strength: 5
+ * Vitality: 1
+ * Site: 4
+ * Game Text: When you play this minion, you may place a [MORIA] Orc from your discard pile beneath your draw deck.
+ */
+public class Card1_181 extends AbstractMinion {
+    public Card1_181() {
+        super(1, 5, 1, 4, Keyword.ORC, Culture.MORIA, "Goblin Sneak");
+    }
+
+    @Override
+    public List<? extends Action> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.sameCard(self))) {
+            final DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, null, "Place a MORIA Orc from your discard pile beneath your draw deck.");
+            action.addEffect(
+                    new ChooseArbitraryCardsEffect(playerId, "Choose MORIA Orc", game.getGameState().getDiscard(playerId), Filters.and(Filters.culture(Culture.MORIA), Filters.keyword(Keyword.ORC)), 1, 1) {
+                        @Override
+                        protected void cardsSelected(List<PhysicalCard> selectedCards) {
+                            action.addEffect(
+                                    new PutCardFromDiscardOnBottomOfDeckEffect(selectedCards.get(0)));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
