528674819776ebb38df56c41def67ae4a96b4743
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-08 18:36:00 +0000

    "Goblin Patrol Troop"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/moria/Card1_177.java
@@ -0,0 +1,21 @@
+package com.gempukku.lotro.cards.set1.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 6
+ * Type: Minion • Orc
+ * Strength: 13
+ * Vitality: 3
+ * Site: 4
+ */
+public class Card1_177 extends AbstractMinion {
+    public Card1_177() {
+        super(6, 13, 3, 4, Keyword.ORC, Culture.MORIA, "Goblin Patrol Troop");
+    }
+}
