02b9c91269b4bd9ed37427a013a863f16559ebd3
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-08 23:03:04 +0000

    "Troll's Keyward"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/moria/Card1_199.java
@@ -0,0 +1,31 @@
+package com.gempukku.lotro.cards.set1.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Cave Troll of Moria's twilight cost is -2.
+ */
+public class Card1_199 extends AbstractMinion {
+    public Card1_199() {
+        super(3, 8, 3, 4, Keyword.ORC, Culture.MORIA, "Troll's Keyward", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new TwilightCostModifier(self, Filters.name("Cave Troll of Moria"), -2);
+    }
+}
