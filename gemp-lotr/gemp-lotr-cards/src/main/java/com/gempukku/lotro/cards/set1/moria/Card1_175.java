db28c45179f520d0cbd50947eaf98b877a540009
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-08 18:33:37 +0000

    "Goblin Domain"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/moria/Card1_175.java
@@ -0,0 +1,70 @@
+package com.gempukku.lotro.cards.set1.moria;
+
+import com.gempukku.lotro.cards.AbstractLotroCardBlueprint;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayPermanentAction;
+import com.gempukku.lotro.cards.effects.ExhaustCharacterEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.PlayCardResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: Search. To play, spot a [MORIA] minion. Plays to your support area. Each time a companion is played to
+ * site 4 or higher, that companion comes into play exhausted.
+ */
+public class Card1_175 extends AbstractLotroCardBlueprint {
+    public Card1_175() {
+        super(Side.SHADOW, CardType.CONDITION, Culture.MORIA, "Goblin Domain");
+        addKeyword(Keyword.SEARCH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return PlayConditions.canPayForShadowCard(game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.MORIA), Filters.type(CardType.MINION));
+    }
+
+    @Override
+    public Action getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return new PlayPermanentAction(self, Zone.FREE_SUPPORT);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canPlayCardDuringPhase(game, Phase.SHADOW, self)
+                && checkPlayRequirements(playerId, game, self, 0))
+            return Collections.singletonList(getPlayCardAction(playerId, game, self, 0));
+        return null;
+    }
+
+    @Override
+    public List<? extends Action> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.type(CardType.COMPANION))
+                && game.getGameState().getCurrentSiteNumber() >= 4) {
+            PlayCardResult playCardResult = (PlayCardResult) effectResult;
+            DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, null, "Exhaust the played companion.");
+            action.addEffect(
+                    new ExhaustCharacterEffect(action, false, playCardResult.getPlayedCard()));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
