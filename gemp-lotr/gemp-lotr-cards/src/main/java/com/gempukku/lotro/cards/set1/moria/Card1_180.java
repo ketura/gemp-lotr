fa54f714749f9180b5eb8bf67ec4e15efc67c96c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-08 20:44:45 +0000

    "Goblin Scimitar"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/moria/Card1_180.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set1.moria;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 0
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be a [MORIA] Orc. When you play this possession, you may draw a card.
+ */
+public class Card1_180 extends AbstractAttachable {
+    public Card1_180() {
+        super(Side.SHADOW, CardType.POSSESSION, 0, Culture.MORIA, "Goblin Scimitar");
+        addKeyword(Keyword.HAND_WEAPON);
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Filters.culture(Culture.MORIA), Filters.keyword(Keyword.ORC), Filters.not(Filters.hasAttached(Filters.keyword(Keyword.HAND_WEAPON))));
+    }
+
+    @Override
+    public List<? extends Action> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.sameCard(self))) {
+            DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, null, "Draw a card");
+            action.addEffect(new DrawCardEffect(playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
