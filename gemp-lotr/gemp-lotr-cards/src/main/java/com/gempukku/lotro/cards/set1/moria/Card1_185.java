9c3a513119e6a90acabe0ffbea258788a207a988
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-08 21:39:53 +0000

    "Goblin Warrior"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/moria/Card1_185.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set1.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 2
+ * Type: Minion • Orc
+ * Strength: 6
+ * Vitality: 1
+ * Site: 4
+ * Game Text: When you play this minion, you may discard a [DWARVEN] ally or [DWARVEN] condition.
+ */
+public class Card1_185 extends AbstractMinion {
+    public Card1_185() {
+        super(2, 6, 1, 4, Keyword.ORC, Culture.MORIA, "Goblin Warrior");
+    }
+
+    @Override
+    public List<? extends Action> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.sameCard(self))) {
+            final DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, null, "Discard a DWARVEN ally or DWARVEN condition.");
+            action.addEffect(
+                    new ChooseActiveCardEffect(playerId, "Choose DWARVEN ally or DWARVEN condition", Filters.culture(Culture.DWARVEN), Filters.or(Filters.type(CardType.ALLY), Filters.type(CardType.CONDITION))) {
+                        @Override
+                        protected void cardSelected(PhysicalCard dwarvenCard) {
+                            action.addEffect(new DiscardCardFromPlayEffect(self, dwarvenCard));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
