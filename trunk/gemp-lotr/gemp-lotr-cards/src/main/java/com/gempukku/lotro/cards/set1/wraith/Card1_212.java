c46bbaf2029411e8ac184f8177427929999ec87b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-09 14:07:58 +0000

    "Fear"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/wraith/Card1_212.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set1.wraith;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardFromPlayEffect;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Search. Maneuver: Exert your Nazgul to discard an ally.
+ */
+public class Card1_212 extends AbstractEvent {
+    public Card1_212() {
+        super(Side.SHADOW, Culture.WRAITH, "Fear", Phase.MANEUVER);
+        addKeyword(Keyword.SEARCH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.NAZGUL), Filters.canExert());
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.addEffect(
+                new ChooseActiveCardEffect(playerId, "Choose an ally", Filters.type(CardType.ALLY)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard ally) {
+                        action.addEffect(
+                                new DiscardCardFromPlayEffect(self, ally));
+                    }
+                });
+
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+}
