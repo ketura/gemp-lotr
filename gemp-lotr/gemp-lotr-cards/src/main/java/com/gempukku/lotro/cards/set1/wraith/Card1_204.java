8a6f16f50e45703bf4ecb623499c154b3fe5eb59
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-09 10:34:38 +0000

    "All Veils Removed"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/wraith/Card1_204.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set1.wraith;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Skirmish: Make a Nazgul strength +2 (or +5 if the Ring-bearer wears The One Ring).
+ */
+public class Card1_204 extends AbstractEvent {
+    public Card1_204() {
+        super(Side.SHADOW, Culture.WRAITH, "All Veils Removed", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, final LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.addEffect(
+                new ChooseActiveCardEffect(playerId, "Choose a Nazgul", Filters.keyword(Keyword.NAZGUL)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard nazgul) {
+                        int bonus = (game.getGameState().isWearingRing()) ? 5 : 2;
+                        action.addEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(nazgul), bonus), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+}
