db4b6d78beb44dbb397ac754143e861c2ce76ba2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-10 11:41:26 +0000

    "Band of the Eye"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/sauron/Card1_240.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set1.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.DiscardCardAtRandomFromHandEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 4
+ * Type: Minion • Orc
+ * Strength: 12
+ * Vitality: 3
+ * Site: 6
+ * Game Text: Response: If this minion wins a skirmish, remove (2) to make the Free Peoples player discard a card
+ * at random from hand.
+ */
+public class Card1_240 extends AbstractMinion {
+    public Card1_240() {
+        super(4, 12, 3, 6, Keyword.ORC, Culture.SAURON, "Band of the Eye");
+    }
+
+    @Override
+    public List<? extends Action> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(effectResult, self)
+                && PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), null, self, 2)) {
+            DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, null, "Remove (2) to make the Free Peoples player discard a card at random from hand.");
+            action.addCost(new RemoveTwilightEffect(2));
+            action.addEffect(
+                    new DiscardCardAtRandomFromHandEffect(game.getGameState().getCurrentPlayerId()));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
