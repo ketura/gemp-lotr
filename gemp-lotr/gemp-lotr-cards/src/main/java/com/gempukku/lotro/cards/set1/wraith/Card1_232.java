658e763da8f0f9dc7609ee4078dad45e699a9fd7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-10 01:17:37 +0000

    "Ulaire Lemenya"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/wraith/Card1_232.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set1.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 4
+ * Type: Minion • Nazgul
+ * Strength: 9
+ * Vitality: 2
+ * Site: 3
+ * Game Text: Each companion or ally who bears a [WRAITH] condition is strength -2.
+ */
+public class Card1_232 extends AbstractMinion {
+    public Card1_232() {
+        super(4, 9, 2, 3, Keyword.NAZGUL, Culture.WRAITH, "Ulaire Lemenya", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new StrengthModifier(self,
+                Filters.and(
+                        Filters.or(
+                                Filters.type(CardType.ALLY),
+                                Filters.type(CardType.COMPANION)
+                        ),
+                        Filters.hasAttached(
+                                Filters.and(
+                                        Filters.culture(Culture.WRAITH),
+                                        Filters.type(CardType.CONDITION)
+                                )
+                        )
+                ), -2);
+    }
+}
