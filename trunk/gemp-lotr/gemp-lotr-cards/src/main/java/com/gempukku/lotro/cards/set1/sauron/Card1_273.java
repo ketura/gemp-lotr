1b8649e8526e377edf5ff4f17eace51073750930
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-10 21:53:38 +0000

    "The Ring's Oppression"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/sauron/Card1_273.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/sauron/Card1_273.java
@@ -3,7 +3,7 @@ package com.gempukku.lotro.cards.set1.sauron;
 import com.gempukku.lotro.cards.AbstractResponseEvent;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
-import com.gempukku.lotro.cards.effects.DiscardTopCardFromDeckEffect;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
@@ -20,19 +20,18 @@ import java.util.List;
  * Set: The Fellowship of the Ring
  * Side: Shadow
  * Culture: Sauron
- * Twilight Cost: 0
+ * Twilight Cost: 3
  * Type: Event
- * Game Text: Response: If a companion is killed by a [SAURON] Orc, the Free Peoples player must discard 3 cards from
- * the top of his draw deck for each card in the dead pile.
+ * Game Text: Response: If a [SAURON] Orc kills a companion, add 1 burden (or 3 if the Ring-bearer wears The One Ring).
  */
-public class Card1_265 extends AbstractResponseEvent {
-    public Card1_265() {
-        super(Side.SHADOW, Culture.SAURON, "Orc Butchery");
+public class Card1_273 extends AbstractResponseEvent {
+    public Card1_273() {
+        super(Side.SHADOW, Culture.SAURON, "The Ring's Oppression");
     }
 
     @Override
     public int getTwilightCost() {
-        return 0;
+        return 3;
     }
 
     @Override
@@ -45,9 +44,10 @@ public class Card1_265 extends AbstractResponseEvent {
                     && skirmish != null
                     && Filters.filter(skirmish.getShadowCharacters(), game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.SAURON), Filters.keyword(Keyword.ORC)).size() > 0) {
                 PlayEventAction action = new PlayEventAction(self);
-                int deadPileCount = game.getGameState().getDeadPile(game.getGameState().getCurrentPlayerId()).size();
-                for (int i = 0; i < deadPileCount * 3; i++)
-                    action.addEffect(new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+                int burdens = (game.getGameState().isWearingRing()) ? 3 : 1;
+                for (int i = 0; i < burdens; i++)
+                    action.addEffect(
+                            new AddBurdenEffect(game.getGameState().getCurrentPlayerId()));
                 return Collections.singletonList(action);
             }
         }
