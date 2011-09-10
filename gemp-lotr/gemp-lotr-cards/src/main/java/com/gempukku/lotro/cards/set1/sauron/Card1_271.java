de4fc1e8fa4fa8a9d856def45c1affe5ea377f8e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-10 21:48:16 +0000

    "Orc Soldier"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/sauron/Card1_271.java
@@ -0,0 +1,53 @@
+package com.gempukku.lotro.cards.set1.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharacterEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.game.state.Skirmish;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.effects.WoundCharacterEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 2
+ * Type: Minion • Orc
+ * Strength: 7
+ * Vitality: 2
+ * Site: 6
+ * Game Text: Skirmish: Exert this minion to wound a character he is skirmishing.
+ */
+public class Card1_271 extends AbstractMinion {
+    public Card1_271() {
+        super(2, 7, 2, 6, Keyword.ORC, Culture.SAURON, "Orc Soldier");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
+                && PlayConditions.canExert(game.getGameState(), game.getModifiersQuerying(), self)) {
+            DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, Keyword.SKIRMISH, "Exert this minion to wound a character he is skirmishing.");
+            action.addCost(
+                    new ExertCharacterEffect(self));
+            Skirmish skirmish = game.getGameState().getSkirmish();
+            if (skirmish != null && skirmish.getShadowCharacters().contains(self)) {
+                PhysicalCard fpChar = skirmish.getFellowshipCharacter();
+                if (fpChar != null)
+                    action.addEffect(new WoundCharacterEffect(fpChar));
+            }
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+}
