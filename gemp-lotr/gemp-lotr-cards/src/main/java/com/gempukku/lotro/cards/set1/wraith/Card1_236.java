980df91ddc49a71993621ad103a2c60aace6f824
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-10 01:37:28 +0000

    "Ulaire Toldea"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/wraith/Card1_236.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set1.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.DefaultCostToEffectAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 6
+ * Type: Minion • Nazgul
+ * Strength: 12
+ * Vitality: 3
+ * Site: 3
+ * Game Text: Fierce. Assignment: Spot 4 burdens to assign a companion (except the Ring-bearer) to skirmish
+ * Ulaire Toldea.
+ */
+public class Card1_236 extends AbstractMinion {
+    public Card1_236() {
+        super(6, 12, 3, 3, Keyword.NAZGUL, Culture.WRAITH, "Ulaire Toldea", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, final LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.ASSIGNMENT, self, 0)
+                && game.getGameState().getBurdens(game.getGameState().getCurrentPlayerId()) >= 4) {
+            DefaultCostToEffectAction action = new DefaultCostToEffectAction(self, Keyword.ASSIGNMENT, "Assign a companion (except the Ring-bearer) to skirmish Ulaire Toldea.");
+            if (Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.sameCard(self), Filters.notAssigned())) {
+                action.addEffect(
+                        new ChooseActiveCardEffect(playerId, "Choose a companion (except the Ring-Bearer", Filters.type(CardType.COMPANION), Filters.not(Filters.keyword(Keyword.RING_BEARER))) {
+                            @Override
+                            protected void cardSelected(PhysicalCard companion) {
+                                game.getGameState().assignToSkirmishes(companion, Collections.singletonList(self));
+                            }
+                        });
+            }
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
