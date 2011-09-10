0d14551c0c931affe45eee6aca95ba78c2434e43
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-10 17:12:08 +0000

    "Morgul Warden"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/sauron/Card1_259.java
@@ -0,0 +1,40 @@
+package com.gempukku.lotro.cards.set1.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifierEffect;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * Set: The Fellowship of the Ring
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 9
+ * Vitality: 3
+ * Site: 6
+ * Game Text: For each other [SAURON] Orc you can spot, Morgul Warden is strength +1.
+ */
+public class Card1_259 extends AbstractMinion {
+    public Card1_259() {
+        super(3, 9, 3, 6, Keyword.ORC, Culture.SAURON, "Morgul Warden", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(final PhysicalCard self) {
+        return new AbstractModifier(self, "For each other [SAURON] Orc you can spot, Morgul Warden is strength +1.", Filters.sameCard(self), new ModifierEffect[]{ModifierEffect.STRENGTH_MODIFIER}) {
+            @Override
+            public int getStrength(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard, int result) {
+                int otherSauronOrcs = Filters.countActive(gameState, modifiersQuerying, Filters.culture(Culture.SAURON), Filters.keyword(Keyword.ORC), Filters.not(Filters.sameCard(self)));
+                return otherSauronOrcs + result;
+            }
+        };
+    }
+}
