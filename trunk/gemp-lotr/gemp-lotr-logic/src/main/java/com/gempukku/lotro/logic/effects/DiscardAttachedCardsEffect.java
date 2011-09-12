17a365574d190ad0f9d8c3b9505a5d8cd1f81b54
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-12 13:26:54 +0000

    Cards discarded because of killed character are also triggering discard_from_play effects.

--- trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/effects/DiscardAttachedCardsEffect.java
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/effects/DiscardAttachedCardsEffect.java
@@ -4,11 +4,14 @@ import com.gempukku.lotro.common.Zone;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.timing.UnrespondableEffect;
+import com.gempukku.lotro.logic.timing.AbstractEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.DiscardCardsFromPlayResult;
 
+import java.util.LinkedList;
 import java.util.List;
 
-public class DiscardAttachedCardsEffect extends UnrespondableEffect {
+public class DiscardAttachedCardsEffect extends AbstractEffect {
     private PhysicalCard _card;
 
     public DiscardAttachedCardsEffect(PhysicalCard card) {
@@ -16,13 +19,32 @@ public class DiscardAttachedCardsEffect extends UnrespondableEffect {
     }
 
     @Override
-    public void doPlayEffect(LotroGame game) {
+    public boolean canPlayEffect(LotroGame game) {
+        return true;
+    }
+
+    @Override
+    public EffectResult.Type getType() {
+        return EffectResult.Type.DISCARD_FROM_PLAY;
+    }
+
+    @Override
+    public String getText() {
+        return "Discard attached cards";
+    }
+
+    @Override
+    public EffectResult playEffect(LotroGame game) {
+        List<PhysicalCard> discardedCards = new LinkedList<PhysicalCard>();
         GameState gameState = game.getGameState();
         List<PhysicalCard> attachedCards = gameState.getAttachedCards(_card);
         for (PhysicalCard attachedCard : attachedCards) {
             gameState.stopAffecting(attachedCard);
             gameState.removeCardFromZone(attachedCard);
             gameState.addCardToZone(attachedCard, Zone.DISCARD);
+            discardedCards.add(attachedCard);
         }
+
+        return new DiscardCardsFromPlayResult(discardedCards);
     }
 }
