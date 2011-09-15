94418d34aef6116d7b3542e98eb7fe2fc6cab4f4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-15 23:59:27 +0000

    Characters should not die immediatelly when you play them, maximum vitality is not 0. haha

--- trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/ModifiersLogic.java
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/ModifiersLogic.java
@@ -179,7 +179,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
             if (affectsCardWithSkipSet(gameState, physicalCard, modifier))
                 result = modifier.getKeywordCount(gameState, this, physicalCard, keyword, result);
         }
-        return Math.min(0, result);
+        return Math.max(0, result);
     }
 
     @Override
@@ -200,7 +200,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
         for (Modifier modifier : getModifiers(ModifierEffect.ALL_MODIFIER))
             result = modifier.getMoveLimit(gameState, this, result);
 
-        return Math.min(1, result);
+        return Math.max(1, result);
     }
 
     @Override
@@ -216,7 +216,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
                     && appliesStrengthModifier(gameState, modifier.getSource()))
                 result = modifier.getStrength(gameState, this, physicalCard, result);
         }
-        return Math.min(0, result);
+        return Math.max(0, result);
     }
 
     private boolean appliesStrengthModifier(GameState gameState, PhysicalCard modifierSource) {
@@ -245,7 +245,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
             if (affectsCardWithSkipSet(gameState, physicalCard, modifier))
                 result = modifier.getVitality(gameState, this, physicalCard, result);
         }
-        return Math.min(0, result);
+        return Math.max(0, result);
     }
 
     @Override
@@ -259,7 +259,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
             if (affectsCardWithSkipSet(gameState, physicalCard, modifier))
                 result = modifier.getTwilightCost(gameState, this, physicalCard, result);
         }
-        return Math.min(0, result);
+        return Math.max(0, result);
     }
 
     @Override
@@ -273,7 +273,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
             if (affectsCardWithSkipSet(gameState, physicalCard, modifier))
                 result = modifier.getPlayOnTwilightCost(gameState, this, physicalCard, target, result);
         }
-        return Math.min(0, result);
+        return Math.max(0, result);
     }
 
     @Override
@@ -287,7 +287,7 @@ public class ModifiersLogic implements ModifiersEnvironment, ModifiersQuerying {
             if (affectsCardWithSkipSet(gameState, physicalCard, modifier))
                 result = modifier.getRoamingPenalty(gameState, this, physicalCard, result);
         }
-        return Math.min(0, result);
+        return Math.max(0, result);
     }
 
     @Override
