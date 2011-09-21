235af5fe1512480debdc1b47d37f2a4643e770ed
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-21 14:56:28 +0000

    Effective stats display.

--- gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Keyword.java
+++ gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Keyword.java
@@ -5,26 +5,46 @@ public enum Keyword {
 
     SKIRMISH("Skirmish"), FELLOWSHIP("Fellowship"), RESPONSE("Response"), MANEUVER("Maneuver"), ARCHERY("Archery"), SHADOW("Shadow"), ASSIGNMENT("Assignment"), REGROUP("Regroup"),
 
-    RING_BOUND("Ring-Bound"), RING_BEARER("Ring-Bearer"),
+    RING_BOUND("Ring-Bound", true), RING_BEARER("Ring-Bearer", true),
 
-    ROAMING("Roaming"),
+    ROAMING("Roaming", true),
 
     WEATHER("Weather"), TALE("Tale"), SPELL("Spell"), SEARCH("Search"), STEALTH("Stealth"),
 
     RIVER("River"), PLAINS("Plains"), UNDERGROUND("Underground"), SANCTUARY("Sanctuary"), FOREST("Forest"), MARSH("Marsh"), MOUNTAIN("Mountain"),
 
-    DAMAGE("Damage"), DEFENDER("Defender"), FIERCE("Fierce"), ARCHER("Archer"), RANGER("Ranger"), TRACKER("Tracker"),
+    DAMAGE("Damage", true, true), DEFENDER("Defender", true, true), FIERCE("Fierce", true), ARCHER("Archer", true), RANGER("Ranger", true), TRACKER("Tracker", true),
 
     HAND_WEAPON("Hand Weapon"), ARMOR("Armor"), HELM("Helm"), MOUNT("Mount"), RANGED_WEAPON("Ranged Weapon"), CLOAK("Cloak"), PIPE("Pipe"),
     PIPEWEED("Pipeweed"), SHIELD("Shield");
 
     private String _humanReadable;
+    private boolean _infoDisplayable;
+    private boolean _multiples;
 
     private Keyword(String humanReadable) {
+        this(humanReadable, false);
+    }
+
+    private Keyword(String humanReadable, boolean infoDisplayable) {
+        this(humanReadable, infoDisplayable, false);
+    }
+
+    private Keyword(String humanReadable, boolean infoDisplayable, boolean multiples) {
         _humanReadable = humanReadable;
+        _infoDisplayable = infoDisplayable;
+        _multiples = multiples;
     }
 
     public String getHumanReadable() {
         return _humanReadable;
+    }
+
+    public boolean isInfoDisplayable() {
+        return _infoDisplayable;
+    }
+
+    public boolean isMultiples() {
+        return _multiples;
     }
 }
