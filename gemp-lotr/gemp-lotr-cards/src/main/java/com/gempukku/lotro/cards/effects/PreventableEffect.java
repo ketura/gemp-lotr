4200e859d9ee00b2c2b2c2add4e4249c686fccaa
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-26 15:41:35 +0000

    "Dismay Our Enemiers"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/PreventableEffect.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/PreventableEffect.java
@@ -3,7 +3,7 @@ package com.gempukku.lotro.cards.effects;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.CostToEffectAction;
 import com.gempukku.lotro.logic.decisions.MultipleChoiceAwaitingDecision;
-import com.gempukku.lotro.logic.timing.Cost;
+import com.gempukku.lotro.logic.timing.ChooseableCost;
 import com.gempukku.lotro.logic.timing.CostResolution;
 import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.EffectResult;
@@ -15,9 +15,9 @@ public class PreventableEffect implements Effect {
     private CostToEffectAction _action;
     private Effect _effectToExecute;
     private Iterator<String> _choicePlayers;
-    private Cost _preventionCost;
+    private ChooseableCost _preventionCost;
 
-    public PreventableEffect(CostToEffectAction action, Effect effectToExecute, List<String> choicePlayers, Cost preventionCost) {
+    public PreventableEffect(CostToEffectAction action, Effect effectToExecute, List<String> choicePlayers, ChooseableCost preventionCost) {
         _action = action;
         _effectToExecute = effectToExecute;
         _choicePlayers = choicePlayers.iterator();
@@ -41,7 +41,7 @@ public class PreventableEffect implements Effect {
     }
 
     private void sendDecisionToNextPlayerOrExecute(LotroGame game) {
-        if (_choicePlayers.hasNext()) {
+        if (_choicePlayers.hasNext() && _preventionCost.canPlayCost(game)) {
             sendDecisionToNextPlayer(game);
         } else {
             _action.insertEffect(_effectToExecute);
