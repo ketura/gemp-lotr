f074f21757eb67f270934519c9ec93ce87f5d649
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-26 15:55:27 +0000

    "Release the Angry Flood"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/elven/Card2_019.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set2.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.costs.ChooseAndExertCharactersCost;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.WoundCharacterEffect;
+
+/**
+ * Set: Mines of Moria
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Maneuver: Exert an Elf to wound each Nazgul once (or twice if at a river).
+ */
+public class Card2_019 extends AbstractEvent {
+    public Card2_019() {
+        super(Side.FREE_PEOPLE, Culture.ELVEN, "Release the Angry Flood", Phase.MANEUVER);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.ELF), Filters.canExert());
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersCost(action, playerId, 1, 1, Filters.race(Race.ELF)));
+        action.appendEffect(
+                new WoundCharacterEffect(playerId, Filters.race(Race.NAZGUL)));
+        if (game.getModifiersQuerying().hasKeyword(game.getGameState(), game.getGameState().getCurrentSite(), Keyword.RIVER))
+            action.appendEffect(
+                    new WoundCharacterEffect(playerId, Filters.race(Race.NAZGUL)));
+        return action;
+    }
+}
