45eec0452ec0acc8ab480d2ffadf98450c68c9d6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-26 17:22:20 +0000

    "Jarnsmid"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/gandalf/Card2_025.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set2.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAlly;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+
+/**
+ * Set: Mines of Moria
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 1
+ * Type: Ally • Home 3 • Man
+ * Strength: 4
+ * Vitality: 2
+ * Site: 3
+ * Game Text: To play, spot Gandalf. Each time you play a shield, armor, helm, or hand weapon, its twilight cost is -1.
+ */
+public class Card2_025 extends AbstractAlly {
+    public Card2_025() {
+        super(1, 3, 4, 2, Race.MAN, Culture.GANDALF, "Jarnsmid", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.name("Gandalf"));
+    }
+
+    @Override
+    public Modifier getAlwaysOnEffect(PhysicalCard self) {
+        return new TwilightCostModifier(self,
+                Filters.or(
+                        Filters.keyword(Keyword.SHIELD),
+                        Filters.keyword(Keyword.ARMOR),
+                        Filters.keyword(Keyword.HELM),
+                        Filters.keyword(Keyword.HAND_WEAPON)
+                ), 1);
+    }
+}
