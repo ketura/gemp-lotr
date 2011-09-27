12a4bf450b84399389470afbca74d143ca8d7221
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-27 11:01:27 +0000

    "You Cannot Pass!"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/gandalf/Card2_030.java
@@ -0,0 +1,53 @@
+package com.gempukku.lotro.cards.set2.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.RemoveKeywordModifier;
+
+/**
+ * Set: Mines of Moria
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Spell. Maneuver: Spot Gandalf bearing a staff to prevent a minion from being fierce until the regroup
+ * phase.
+ */
+public class Card2_030 extends AbstractEvent {
+    public Card2_030() {
+        super(Side.FREE_PEOPLE, Culture.GANDALF, "You Cannot Pass!", Phase.MANEUVER);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.name("Gandalf"), Filters.hasAttached(Filters.keyword(Keyword.STAFF)));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(playerId, "Choose a minion", Filters.type(CardType.MINION)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard card) {
+                        action.insertEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new RemoveKeywordModifier(self, Filters.sameCard(card), Keyword.FIERCE), Phase.REGROUP));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+}
