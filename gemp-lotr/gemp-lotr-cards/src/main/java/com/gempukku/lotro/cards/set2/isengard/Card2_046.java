b0c0934728b8677ce7eceda9376f65411f51fd1a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-27 23:36:58 +0000

    "Uruk Captain"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/isengard/Card2_046.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set2.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.costs.ExertCharactersCost;
+import com.gempukku.lotro.cards.effects.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 3
+ * Type: Minion • Uruk-Hai
+ * Strength: 9
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. Shadow: Remove (1) and exert Uruk Captain to play an Uruk-hai from your discard pile.
+ */
+public class Card2_046 extends AbstractMinion {
+    public Card2_046() {
+        super(3, 9, 2, 5, Race.URUK_HAI, Culture.ISENGARD, "Uruk Captain", true);
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SHADOW, self, 1)
+                && PlayConditions.canExert(game.getGameState(), game.getModifiersQuerying(), self)
+                // There has to be playable Uruk-hai in discard pile
+                && Filters.filter(game.getGameState().getDiscard(playerId), game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.URUK_HAI), Filters.playable(game)).size() > 0) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.SHADOW);
+            action.appendCost(
+                    new RemoveTwilightEffect(1));
+            action.appendCost(
+                    new ExertCharactersCost(playerId, self));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Filters.race(Race.URUK_HAI)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
