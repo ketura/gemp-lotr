198517be712025697458eeae9664477919091ba6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-27 23:59:45 +0000

    "Archer Commander"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/moria/Card2_049.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set2.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 4
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Archer. Each other [MORIA] archer is strength +2.
+ */
+public class Card2_049 extends AbstractMinion {
+    public Card2_049() {
+        super(4, 8, 2, 4, Race.ORC, Culture.MORIA, "Archer Commander", true);
+        addKeyword(Keyword.ARCHER);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self,
+                        Filters.and(
+                                Filters.not(Filters.sameCard(self)),
+                                Filters.culture(Culture.MORIA),
+                                Filters.keyword(Keyword.ARCHER)
+                        ), 2)
+        );
+    }
+}
