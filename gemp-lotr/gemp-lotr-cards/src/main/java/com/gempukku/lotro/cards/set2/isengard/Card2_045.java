8fb9c4276177547a57415be231cb7f7be1909e02
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-27 23:32:37 +0000

    "Too Much Attention"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/isengard/Card2_045.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set2.isengard;
+
+import com.gempukku.lotro.cards.AbstractResponseEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.DiscardTopCardFromDeckEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Response: If the Ring-bearer puts on The One Ring, spot an [ISENGARD] minion to make the Free Peoples
+ * player discard the top 5 cards of his or her draw deck.
+ */
+public class Card2_045 extends AbstractResponseEvent {
+    public Card2_045() {
+        super(Side.SHADOW, Culture.ISENGARD, "Too Much Attention");
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public List<PlayEventAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.PUT_ON_THE_ONE_RING
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.ISENGARD), Filters.type(CardType.MINION))
+                && checkPlayRequirements(playerId, game, self, 0)) {
+            PlayEventAction action = new PlayEventAction(self);
+            action.appendEffect(
+                    new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+            action.appendEffect(
+                    new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+            action.appendEffect(
+                    new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+            action.appendEffect(
+                    new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+            action.appendEffect(
+                    new DiscardTopCardFromDeckEffect(game.getGameState().getCurrentPlayerId()));
+
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
