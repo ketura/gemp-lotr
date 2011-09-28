23ad6b0e659ba2ca7281fd3ce4209a59ba12d4d7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-28 22:04:45 +0000

    "Goblin Scrabbler"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/moria/Card2_064.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set2.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.modifiers.RoamingPenaltyModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 1
+ * Type: Minion • Orc
+ * Strength: 4
+ * Vitality: 1
+ * Site: 4
+ * Game Text: When you play this minion, you may discard an [ELVEN] condition. The roaming penalty for each [MORIA] Orc
+ * you play is -1.
+ */
+public class Card2_064 extends AbstractMinion {
+    public Card2_064() {
+        super(1, 4, 1, 4, Race.ORC, Culture.MORIA, "Goblin Scrabbler");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new RoamingPenaltyModifier(self,
+                        Filters.and(
+                                Filters.culture(Culture.MORIA),
+                                Filters.race(Race.ORC),
+                                Filters.owner(self.getOwner())
+                        ), -1));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.sameCard(self))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Filters.culture(Culture.ELVEN), Filters.type(CardType.CONDITION)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
