1bff5b9db4b74ab73712dfb1c985212ee6430f6e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-28 21:53:39 +0000

    "Foul Things"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/moria/Card2_059.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set2.moria;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 2
+ * Type: Event
+ * Game Text: Shadow: Play a [MORIA] minion from your discard pile.
+ */
+public class Card2_059 extends AbstractEvent {
+    public Card2_059() {
+        super(Side.SHADOW, Culture.MORIA, "Foul Things", Phase.SHADOW);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 2;
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                // There has to be playable MORIA Orc in discard pile to be able to use "Foul Things"
+                && Filters.filter(game.getGameState().getDiscard(playerId), game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.MORIA), Filters.playable(game)).size() > 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Filters.culture(Culture.MORIA)));
+
+        return action;
+    }
+}
