0a853b320a66e9ce6764e54b931f0b29852fbdf4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-28 21:38:11 +0000

    "Fill With Fear"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/ShouldSkipPhaseModifier.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/ShouldSkipPhaseModifier.java
@@ -4,10 +4,12 @@ import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Condition;
 import com.gempukku.lotro.logic.modifiers.ModifierEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 public class ShouldSkipPhaseModifier extends AbstractModifier {
+    private Condition _condition;
     private Phase _phase;
     private String _playerId;
 
@@ -16,9 +18,15 @@ public class ShouldSkipPhaseModifier extends AbstractModifier {
         _phase = phase;
     }
 
+    public ShouldSkipPhaseModifier(PhysicalCard source, Condition condition, Phase phase) {
+        super(source, "Skip " + phase.toString() + " phase", null, new ModifierEffect[]{ModifierEffect.ACTION_MODIFIER});
+        _condition = condition;
+        _phase = phase;
+    }
+
     @Override
     public boolean shouldSkipPhase(GameState gameState, ModifiersQuerying modifiersQuerying, Phase phase, String playerId, boolean result) {
-        if (phase == _phase && (_playerId == null || _playerId.equals(playerId)))
+        if (phase == _phase && (_playerId == null || _playerId.equals(playerId)) && (_condition == null || _condition.isFullfilled(gameState, modifiersQuerying)))
             return true;
         return result;
     }
