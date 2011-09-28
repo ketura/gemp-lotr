d8222bc73f49ae756e14f5a54746775d310d6428
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-28 21:57:09 +0000

    "Goblin Flankers"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/moria/Card2_061.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set2.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 5
+ * Type: Minion • Orc
+ * Strength: 11
+ * Vitality: 3
+ * Site: 4
+ * Game Text: When you play this minion, you may add (3) if the fellowship has moved more than once this turn.
+ */
+public class Card2_061 extends AbstractMinion {
+    public Card2_061() {
+        super(5, 11, 3, 4, Race.ORC, Culture.MORIA, "Goblin Flankers");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.sameCard(self))
+                && game.getGameState().getMoveCount() > 1) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new AddTwilightEffect(3));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
