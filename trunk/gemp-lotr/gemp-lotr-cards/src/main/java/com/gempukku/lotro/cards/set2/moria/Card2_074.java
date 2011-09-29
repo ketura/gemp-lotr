8b1405ee8bc78985b73a9aacf41e3d4d527ccf05
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-29 22:32:34 +0000

    "Whip of Many Thongs"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/moria/Card2_074.java
@@ -0,0 +1,65 @@
+package com.gempukku.lotro.cards.set2.moria;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.costs.ExertCharactersCost;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Moria
+ * Twilight Cost: 1
+ * Type: Artifact
+ * Strength: +1
+ * Game Text: Bearer must be The Balrog. Skirmish: Exert The Balrog to discard a helm, shield, or armor borne by
+ * a character The Balrog is skirmishing.
+ */
+public class Card2_074 extends AbstractAttachable {
+    public Card2_074() {
+        super(Side.SHADOW, CardType.ARTIFACT, 1, Culture.MORIA, Keyword.HAND_WEAPON, "Whip of Many Thongs", true);
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("The Balrog");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.hasAttached(self), 1));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self.getAttachedTo())) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.SKIRMISH);
+            action.appendCost(
+                    new ExertCharactersCost(self, self.getAttachedTo()));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1,
+                            Filters.or(
+                                    Filters.keyword(Keyword.HELM),
+                                    Filters.keyword(Keyword.SHIELD),
+                                    Filters.keyword(Keyword.ARMOR)),
+                            Filters.attachedTo(
+                                    Filters.inSkirmishAgainst(Filters.hasAttached(self)))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
