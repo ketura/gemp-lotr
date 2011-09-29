fbed2cb2d9e8c54b26da89c434dd957d5cbd923c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-29 23:44:51 +0000

    "It Wants to be Found"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/wraith/Card2_078.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set2.wraith;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 2
+ * Type: Event
+ * Game Text: Maneuver: Spot a twilight Nazgul to add a burden.
+ */
+public class Card2_078 extends AbstractEvent {
+    public Card2_078() {
+        super(Side.SHADOW, Culture.WRAITH, "It Wants to be Found", Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.NAZGUL), Filters.keyword(Keyword.TWILIGHT));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 2;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new AddBurdenEffect(playerId));
+        return action;
+    }
+}
