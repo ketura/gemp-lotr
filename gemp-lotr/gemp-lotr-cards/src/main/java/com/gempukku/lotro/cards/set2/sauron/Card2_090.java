17ba41e0c6035ddf806c171fd98a28c370eaf881
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 00:53:35 +0000

    "Orc Taskmaster"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/sauron/Card2_090.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set2.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.costs.ExertCharactersCost;
+import com.gempukku.lotro.cards.effects.AddTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 3
+ * Site: 6
+ * Game Text: Shadow: Exert this minion to add (1).
+ */
+public class Card2_090 extends AbstractMinion {
+    public Card2_090() {
+        super(3, 8, 3, 6, Race.ORC, Culture.SAURON, "Orc Taskmaster");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SHADOW, self, 0)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.SHADOW);
+            action.appendCost(
+                    new ExertCharactersCost(self, self));
+            action.appendEffect(
+                    new AddTwilightEffect(1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
