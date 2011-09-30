a5a2d3ed9ab55e6cf9122863243ca9c6109cfaec
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 01:30:49 +0000

    "Bilbo"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/shire/Card2_096.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set2.shire;
+
+import com.gempukku.lotro.cards.AbstractAlly;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.costs.ChooseAndDiscardCardsFromPlayCost;
+import com.gempukku.lotro.cards.costs.ExertCharactersCost;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 2
+ * Type: Ally • Home 1 • Hobbit
+ * Strength: 2
+ * Vitality: 3
+ * Site: 1
+ * Game Text: Fellowship: Exert Bilbo and discard a tale to discard a Shadow condition from an opponent's support area.
+ */
+public class Card2_096 extends AbstractAlly {
+    public Card2_096() {
+        super(2, 1, 2, 3, Race.HOBBIT, Culture.SHIRE, "Bilbo", true);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.TALE))) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.FELLOWSHIP);
+            action.appendCost(
+                    new ExertCharactersCost(self, self));
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromPlayCost(action, playerId, 1, 1, Filters.keyword(Keyword.TALE)));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Filters.side(Side.SHADOW), Filters.type(CardType.CONDITION), Filters.zone(Zone.SHADOW_SUPPORT)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
