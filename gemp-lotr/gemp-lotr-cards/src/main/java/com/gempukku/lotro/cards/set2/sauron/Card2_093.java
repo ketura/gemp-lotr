85152986942084612a17dd5616fdc48764fa5e0f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 01:08:30 +0000

    "Tower Assassin"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/sauron/Card2_093.java
@@ -0,0 +1,53 @@
+package com.gempukku.lotro.cards.set2.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.costs.ExertCharactersCost;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.WoundCharacterEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 4
+ * Type: Minion • Orc
+ * Strength: 11
+ * Vitality: 3
+ * Site: 6
+ * Game Text: Maneuver: Exert Tower Assassin to wound an ally.
+ */
+public class Card2_093 extends AbstractMinion {
+    public Card2_093() {
+        super(4, 11, 3, 6, Race.ORC, Culture.SAURON, "Tower Assassin", true);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 0)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
+            final ActivateCardAction action = new ActivateCardAction(self, Keyword.MANEUVER);
+            action.appendCost(
+                    new ExertCharactersCost(self, self));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(playerId, "Choose an ally", Filters.type(CardType.ALLY)) {
+                        @Override
+                        protected void cardSelected(PhysicalCard card) {
+                            action.insertEffect(
+                                    new WoundCharacterEffect(playerId, card));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
