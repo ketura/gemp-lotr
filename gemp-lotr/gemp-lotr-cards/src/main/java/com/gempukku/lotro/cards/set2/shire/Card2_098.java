dfbfb090ced306f5e53834545c8bea1c211f51e3
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 01:45:02 +0000

    "Dear Friends"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/shire/Card2_098.java
@@ -0,0 +1,67 @@
+package com.gempukku.lotro.cards.set2.shire;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.costs.ChooseAndExertCharactersCost;
+import com.gempukku.lotro.cards.effects.ChooseCardsFromDiscardEffect;
+import com.gempukku.lotro.cards.effects.PutCardFromDiscardOnBottomOfDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+import java.util.Collection;
+
+/**
+ * Set: Mines of Moria
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Fellowship: Exert a companion to place a Free Peoples card from your discard pile beneath your draw deck
+ * (or 2 Free Peoples cards if that companion has the Frodo signet).
+ */
+public class Card2_098 extends AbstractEvent {
+    public Card2_098() {
+        super(Side.FREE_PEOPLE, Culture.SHIRE, "Dear Friends", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.type(CardType.COMPANION));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersCost(action, playerId, 1, 1, Filters.type(CardType.COMPANION)) {
+                    @Override
+                    protected void cardsSelected(Collection<PhysicalCard> characters, boolean success) {
+                        super.cardsSelected(characters, success);
+                        if (success) {
+                            Signet signet = characters.iterator().next().getBlueprint().getSignet();
+                            int count = (signet == Signet.FRODO) ? 2 : 1;
+                            action.appendEffect(
+                                    new ChooseCardsFromDiscardEffect(playerId, count, count, Filters.side(Side.FREE_PEOPLE)) {
+                                        @Override
+                                        protected void cardsSelected(Collection<PhysicalCard> cards) {
+                                            for (PhysicalCard card : cards) {
+                                                action.insertEffect(
+                                                        new PutCardFromDiscardOnBottomOfDeckEffect(card));
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+        return action;
+    }
+}
