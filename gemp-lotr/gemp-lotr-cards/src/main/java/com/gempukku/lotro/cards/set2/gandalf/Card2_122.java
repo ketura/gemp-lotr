423abbcf5855a2d1e6edc36851c724bd0948fbb0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 03:39:17 +0000

    "Gandalf"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/gandalf/Card2_122.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set2.gandalf;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Signet;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 4
+ * Type: Companion • Wizard
+ * Strength: 7
+ * Vitality: 4
+ * Resistance: 6
+ * Signet: Aragorn
+ * Game Text: At the start of each of your turns, you may draw a card.
+ */
+public class Card2_122 extends AbstractCompanion {
+    public Card2_122() {
+        super(4, 7, 4, Culture.GANDALF, Race.WIZARD, Signet.ARAGORN, "Gandalf", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.START_OF_TURN) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new DrawCardEffect(playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
