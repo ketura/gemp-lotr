31fc076874c3d8d66b0d2a96ebb627990fcc3417
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 03:11:32 +0000

    "Sam"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/shire/Card2_114.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/shire/Card2_114.java
@@ -1,10 +1,10 @@
-package com.gempukku.lotro.cards.set1.shire;
+package com.gempukku.lotro.cards.set2.shire;
 
 import com.gempukku.lotro.cards.AbstractCompanion;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.costs.ExertCharactersCost;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
 import com.gempukku.lotro.cards.effects.MakeRingBearerEffect;
-import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
@@ -12,6 +12,7 @@ import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
 import com.gempukku.lotro.logic.effects.KillEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
 import com.gempukku.lotro.logic.timing.Action;
 import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.EffectResult;
@@ -20,7 +21,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Set: The Fellowship of the Ring
+ * Set: Mines of Moria
  * Side: Free
  * Culture: Shire
  * Twilight Cost: 2
@@ -28,12 +29,13 @@ import java.util.List;
  * Strength: 3
  * Vitality: 4
  * Resistance: 6
- * Signet: Aragorn
- * Game Text: Fellowship: Exert Sam to remove a burden. Response: If Frodo dies, make Sam the Ring-bearer (resistance 5).
+ * Signet: Gandalf
+ * Game Text: Maneuver: Exert Sam twice to make him defender +1 until the regroup phase. Response: If Frodo dies, make
+ * Sam the Ring-bearer (resistance 5).
  */
-public class Card1_311 extends AbstractCompanion {
-    public Card1_311() {
-        super(2, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.ARAGORN, "Sam", true);
+public class Card2_114 extends AbstractCompanion {
+    public Card2_114() {
+        super(2, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.GANDALF, "Sam", true);
     }
 
     @Override
@@ -43,11 +45,16 @@ public class Card1_311 extends AbstractCompanion {
 
     @Override
     protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
-                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
-            ActivateCardAction action = new ActivateCardAction(self, Keyword.FELLOWSHIP);
-            action.appendCost(new ExertCharactersCost(self, self));
-            action.appendEffect(new RemoveBurdenEffect(playerId));
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.MANEUVER, self)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), 2, Filters.sameCard(self))) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.MANEUVER);
+            action.appendCost(
+                    new ExertCharactersCost(self, self));
+            action.appendCost(
+                    new ExertCharactersCost(self, self));
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new KeywordModifier(self, Filters.sameCard(self), Keyword.DEFENDER), Phase.REGROUP));
             return Collections.singletonList(action);
         }
         return null;
