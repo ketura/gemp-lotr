8a8957eee432d734f1691ef4636073926b408866
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 12:21:27 +0000

    "Gift of the Evenstar"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/elven/Card3_019.java
@@ -0,0 +1,75 @@
+package com.gempukku.lotro.cards.set3.elven;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.costs.ChoiceCost;
+import com.gempukku.lotro.cards.costs.ChooseAndExertCharactersCost;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.ChooseableCost;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: To play, spot Arwen. Bearer must be Aragorn. Skirmish: Exert Aragorn or discard 2 cards from hand to make
+ * a minion skirmishing Aragorn strength -1.
+ */
+public class Card3_019 extends AbstractAttachable {
+    public Card3_019() {
+        super(Side.FREE_PEOPLE, CardType.CONDITION, 1, Culture.ELVEN, null, "Gift of the Evenstar", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, Filter additionalAttachmentFilter, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, additionalAttachmentFilter, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.name("Arwen"));
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("Aragorn");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && (PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.name("Aragorn"))
+                || game.getGameState().getHand(playerId).size() >= 2)) {
+            final ActivateCardAction action = new ActivateCardAction(self, Keyword.SKIRMISH);
+            List<ChooseableCost> possibleCosts = new LinkedList<ChooseableCost>();
+            possibleCosts.add(
+                    new ChooseAndExertCharactersCost(action, playerId, 1, 1, Filters.name("Aragorn")));
+            possibleCosts.add(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, 2, 2, Filters.any()));
+            action.appendCost(
+                    new ChoiceCost(action, playerId, possibleCosts));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(playerId, "Choose a minion", Filters.inSkirmishAgainst(Filters.name("Aragorn"))) {
+                        @Override
+                        protected void cardSelected(PhysicalCard card) {
+                            action.insertEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, Filters.sameCard(card), -1), Phase.SKIRMISH));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
