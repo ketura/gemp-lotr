ef737bd859c2cf70c648afdebf8d9dd28bcecd67
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 13:53:58 +0000

    "Narya"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/gandalf/Card3_034.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set3.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.effects.AddTwilightEffect;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 0
+ * Type: Artifact
+ * Vitality: +1
+ * Game Text: Bearer must be Gandalf. At the start of each of your turns, you may add (3) to remove a burden.
+ */
+public class Card3_034 extends AbstractAttachableFPPossession {
+    public Card3_034() {
+        super(0, 0, 1, Culture.GANDALF, CardType.ARTIFACT, Keyword.RING, "Narya", true);
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("Gandalf");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.START_OF_TURN) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new AddTwilightEffect(3));
+            action.appendEffect(
+                    new RemoveBurdenEffect(playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
