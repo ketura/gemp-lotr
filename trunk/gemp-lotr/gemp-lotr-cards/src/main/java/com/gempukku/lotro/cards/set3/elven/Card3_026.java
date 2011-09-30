8e9fe350cd01abf1a193b0ed429d9e9401acde7d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 12:52:40 +0000

    "Something Draws Near"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/elven/Card3_026.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set3.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.costs.ChooseAndExertCharactersCost;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Fellowship or Maneuver: Exert an Elf to discard an [ISENGARD] possession or [ISENGARD] artifact.
+ */
+public class Card3_026 extends AbstractEvent {
+    public Card3_026() {
+        super(Side.FREE_PEOPLE, Culture.ELVEN, "Something Draws Near", Phase.FELLOWSHIP, Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.ELF));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersCost(action, playerId, 1, 1, Filters.race(Race.ELF)));
+        action.appendEffect(
+                new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Filters.culture(Culture.ISENGARD), Filters.or(Filters.type(CardType.POSSESSION), Filters.type(CardType.ARTIFACT))));
+        return action;
+    }
+}
