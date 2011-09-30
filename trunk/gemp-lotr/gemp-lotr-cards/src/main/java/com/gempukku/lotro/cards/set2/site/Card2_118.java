5c26193a2faee81b88b17e91b3b174b0da3b0ee1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-09-30 03:28:44 +0000

    "Great Chasm"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set2/site/Card2_118.java
@@ -0,0 +1,62 @@
+package com.gempukku.lotro.cards.set2.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mines of Moria
+ * Twilight Cost: 4
+ * Type: Site
+ * Site: 4
+ * Game Text: Underground. The twilight cost of the first [MORIA] archer played each Shadow phase is -2.
+ */
+public class Card2_118 extends AbstractSite {
+    public Card2_118() {
+        super("Great Chasm", 4, 4, Direction.RIGHT);
+        addKeyword(Keyword.UNDERGROUND);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(final PhysicalCard self) {
+        return Collections.singletonList(
+                new TwilightCostModifier(self,
+                        Filters.and(
+                                Filters.culture(Culture.MORIA),
+                                Filters.keyword(Keyword.ARCHER),
+                                new Filter() {
+                                    @Override
+                                    public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                                        return (self.getData() == null);
+                                    }
+                                }
+                        ), -2));
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.and(Filters.culture(Culture.MORIA), Filters.keyword(Keyword.ARCHER)))
+                && game.getGameState().getCurrentSite() == self)
+            self.storeData(new Object());
+        if (effectResult.getType() == EffectResult.Type.END_OF_PHASE
+                && game.getGameState().getCurrentPhase() == Phase.SHADOW
+                && self.getData() != null)
+            self.removeData();
+        return null;
+    }
+}
