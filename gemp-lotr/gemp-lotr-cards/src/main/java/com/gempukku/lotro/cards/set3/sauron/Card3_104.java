6d62ebaaecdd7732c1193ee816c560405e30c5ae
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-01 12:32:30 +0000

    Rest of Sauron cards.

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/sauron/Card3_104.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set3.sauron;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayPermanentAction;
+import com.gempukku.lotro.cards.costs.ChooseAndExertCharactersCost;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: To play, exert a [SAURON] minion. Plays to your support area. Each time the fellowship moves,
+ * the Free Peoples player must discard a card from hand.
+ */
+public class Card3_104 extends AbstractPermanent {
+    public Card3_104() {
+        super(Side.SHADOW, 1, CardType.CONDITION, Culture.SAURON, Zone.SHADOW_SUPPORT, "Tower of Barad-dur");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.SAURON), Filters.type(CardType.MINION));
+    }
+
+    @Override
+    public PlayPermanentAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayPermanentAction action = super.getPlayCardAction(playerId, game, self, twilightModifier);
+        action.appendCost(
+                new ChooseAndExertCharactersCost(action, playerId, 1, 1, Filters.culture(Culture.SAURON), Filters.type(CardType.MINION)));
+        return action;
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.WHEN_FELLOWSHIP_MOVES) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromHandEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, Filters.any()));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/sauron/Card3_105.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set3.sauron;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.costs.ChooseAndDiscardCardsFromPlayCost;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+import java.util.Collections;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Maneuver: Spot a [SAURON] minion to discard Bilbo. The Free Peoples player may discard 2 Free Peoples
+ * conditions to prevent this.
+ */
+public class Card3_105 extends AbstractEvent {
+    public Card3_105() {
+        super(Side.SHADOW, Culture.SAURON, "Why Shouldn't I Keep It?", Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.SAURON), Filters.type(CardType.MINION));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new PreventableEffect(action,
+                        new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Filters.name("Bilbo")) {
+                            @Override
+                            public String getText(LotroGame game) {
+                                return "Discard Bilbo";
+                            }
+                        }, Collections.singletonList(game.getGameState().getCurrentPlayerId()),
+                        new ChooseAndDiscardCardsFromPlayCost(action, game.getGameState().getCurrentPlayerId(), 2, 2, Filters.side(Side.FREE_PEOPLE), Filters.type(CardType.CONDITION))));
+        return action;
+    }
+}
