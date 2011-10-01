9cd9046e494a1b6a404ab0793d67584d6e86f241
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-01 01:16:01 +0000

    "Citadel of Minas Tirith"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/gondor/Card3_040.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set3.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: Plays to your support area. You may heal a [GONDOR] companion at the end of each turn during which
+ * no companion or ally lost a skirmish.
+ */
+public class Card3_040 extends AbstractPermanent {
+    public Card3_040() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.GONDOR, Zone.FREE_SUPPORT, "Citadel of Minas Tirith");
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.losesSkirmish(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.and(Filters.culture(Culture.GONDOR), Filters.type(CardType.COMPANION)))) {
+            self.storeData(new Object());
+        }
+        return null;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.END_OF_TURN) {
+            if (self.getData() != null) {
+                self.removeData();
+            } else {
+                OptionalTriggerAction action = new OptionalTriggerAction(self);
+                action.appendEffect(
+                        new ChooseAndHealCharactersEffect(action, playerId, Filters.culture(Culture.GONDOR), Filters.type(CardType.COMPANION)));
+                return Collections.singletonList(action);
+            }
+        }
+        return null;
+    }
+}
