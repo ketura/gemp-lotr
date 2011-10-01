6ff72d180673121b07b1bffd5981c93316ce322c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-01 05:34:59 +0000

    "Voice of Rauros" "We Must Go Warily"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set3/gondor/Card3_047.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set3.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.costs.ChooseAndExertCharactersCost;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+
+/**
+ * Set: Realms of Elf-lords
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Maneuver: Exert Aragorn to make each companion with the Aragorn signet damage +1 until the regroup phase.
+ */
+public class Card3_047 extends AbstractEvent {
+    public Card3_047() {
+        super(Side.FREE_PEOPLE, Culture.GONDOR, "Voice of Rauros", Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.name("Aragorn"));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersCost(action, playerId, 1, 1, Filters.name("Aragorn")));
+        action.appendEffect(
+                new AddUntilStartOfPhaseModifierEffect(
+                        new KeywordModifier(self, Filters.and(Filters.type(CardType.COMPANION), Filters.signet(Signet.ARAGORN)), Keyword.DAMAGE), Phase.REGROUP));
+        return action;
+    }
+}
