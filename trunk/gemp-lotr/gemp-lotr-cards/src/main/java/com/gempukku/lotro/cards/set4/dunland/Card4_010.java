c5bd76dc2e686b395f736ac4c2f336a0fe71c001
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-05 15:24:39 +0000

    "Dunlending Brigand"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_010.java
@@ -0,0 +1,21 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 2
+ * Type: Minion • Man
+ * Strength: 8
+ * Vitality: 1
+ * Site: 3
+ */
+public class Card4_010 extends AbstractMinion {
+    public Card4_010() {
+        super(2, 8, 1, 3, Race.MAN, Culture.DUNLAND, "Dunlending Brigand");
+    }
+}
