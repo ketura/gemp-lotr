ad9e2dd62b337664b0cb14b866f7353078cc2517
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-05 15:01:14 +0000

    "Burn Every Village"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_005.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Make a [DUNLAND] Man strength +2 (or +4 if you control a site).
+ */
+public class Card4_005 extends AbstractEvent {
+    public Card4_005() {
+        super(Side.SHADOW, Culture.DUNLAND, "Burn Every Village", Phase.SKIRMISH);
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        final int bonus = (Filters.countActive(game.getGameState(), game.getModifiersQuerying(), Filters.siteControlled(playerId)) > 0) ? 4 : 2;
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose DUNLAND Man", Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard card) {
+                        action.appendEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(card), bonus), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+}
