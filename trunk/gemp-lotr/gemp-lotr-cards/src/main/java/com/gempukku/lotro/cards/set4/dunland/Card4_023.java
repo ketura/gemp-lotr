9fca3285d461fee480493f386b43a0a61ade1bf5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-05 20:41:36 +0000

    "Hillman Mob"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_023.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 12
+ * Vitality: 1
+ * Site: 3
+ * Game Text: Each time this minion wins a skirmish, you may play a [DUNLAND] Man from hand; his twilight cost is -2.
+ * That Man is fierce and damage +1 until the regroup phase.
+ */
+public class Card4_023 extends AbstractMinion {
+    public Card4_023() {
+        super(5, 12, 1, 3, Race.MAN, Culture.DUNLAND, "Hillman Mob");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(effectResult, self)
+                && Filters.filter(game.getGameState().getHand(playerId), game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN), Filters.playable(game, -2)).size() > 0) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game.getGameState().getHand(playerId), Filters.and(Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)), -2) {
+                        @Override
+                        protected void cardChosenCallback(PhysicalCard cardChosenToPlay) {
+                            action.appendEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, Filters.sameCard(cardChosenToPlay), Keyword.FIERCE), Phase.REGROUP));
+                            action.appendEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, Filters.sameCard(cardChosenToPlay), Keyword.DAMAGE), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
