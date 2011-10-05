18ba678ce3f44b4cdaeee5726d41f768c42dadad
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-05 19:46:09 +0000

    "Dunlending Savage"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_017.java
@@ -0,0 +1,21 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 4
+ * Type: Minion • Man
+ * Strength: 11
+ * Vitality: 1
+ * Site: 3
+ */
+public class Card4_017 extends AbstractMinion {
+    public Card4_017() {
+        super(4, 11, 1, 3, Race.MAN, Culture.DUNLAND, "Dunlending Savage");
+    }
+}
