9142314dc1f0f9b2a26be2649749c2a7324a90f9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-05 20:21:59 +0000

    "Hill Chief"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_020.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 4
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 2
+ * Site: 3
+ * Game Text: Shadow: Spot a site you control and exert Hill Chief to draw 3 cards.
+ */
+public class Card4_020 extends AbstractMinion {
+    public Card4_020() {
+        super(4, 10, 2, 3, Race.MAN, Culture.DUNLAND, "Hill Chief", true);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SHADOW, self, 0)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.siteControlled(playerId))
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.SHADOW);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new DrawCardEffect(playerId, 3));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
