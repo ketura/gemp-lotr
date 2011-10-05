02993a9a62e26a9fb44dd8307d986ba78ce0899a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-05 20:47:28 +0000

    "Iron Axe"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_026.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +3
+ * Game Text: Bearer must be a [DUNLAND] Man.
+ */
+public class Card4_026 extends AbstractAttachable {
+    public Card4_026() {
+        super(Side.SHADOW, CardType.POSSESSION, 1, Culture.DUNLAND, Keyword.HAND_WEAPON, "Iron Axe");
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN));
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.hasAttached(self), 3));
+    }
+}
