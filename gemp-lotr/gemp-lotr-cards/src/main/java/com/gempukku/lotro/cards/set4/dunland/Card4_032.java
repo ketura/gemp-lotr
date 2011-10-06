80302ed56585c6966de34a7d5dd4c2249a3952a5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 09:33:14 +0000

    "Ravage the Defeated"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_032.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractResponseEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.SkirmishResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Response: If your [DUNLAND] Man wins a skirmish, discard all Free Peoples cards borne by the companion
+ * or ally he was skirmishing.
+ */
+public class Card4_032 extends AbstractResponseEvent {
+    public Card4_032() {
+        super(Side.SHADOW, Culture.DUNLAND, "Ravage the Defeated");
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public List<PlayEventAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.canPlayCardDuringPhase(game, (Phase) null, self)
+                && PlayConditions.winsSkirmish(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.and(Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)))) {
+            SkirmishResult skirmishResult = (SkirmishResult) effectResult;
+            final List<PhysicalCard> losers = skirmishResult.getLosers();
+            PlayEventAction action = new PlayEventAction(self);
+            action.appendEffect(
+                    new DiscardCardsFromPlayEffect(self, Filters.and(Filters.attachedTo(Filters.in(losers)), Filters.side(Side.FREE_PEOPLE))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
