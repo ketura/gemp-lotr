9105ab7ee67364ad441b30064bd1bc086affd03f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 21:46:51 +0000

    "Ferevellon"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/elven/Card4_068.java
@@ -0,0 +1,64 @@
+package com.gempukku.lotro.cards.set4.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 2
+ * Type: Companion • Elf
+ * Strength: 5
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: To play, spot an Elf. Skirmish: Exert Ferevellon to make a minion skirmishing him strength -2.
+ */
+public class Card4_068 extends AbstractCompanion {
+    public Card4_068() {
+        super(2, 5, 3, Culture.ELVEN, Race.ELF, null, "Ferevellon", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.ELF));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
+            final ActivateCardAction action = new ActivateCardAction(self, Keyword.SKIRMISH);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a minion", Filters.inSkirmishAgainst(Filters.sameCard(self))) {
+                        @Override
+                        protected void cardSelected(PhysicalCard card) {
+                            action.insertEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, Filters.sameCard(card), -2), Phase.SKIRMISH));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
