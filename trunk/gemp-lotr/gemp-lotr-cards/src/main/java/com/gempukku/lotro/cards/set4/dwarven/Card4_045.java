9eea0d227a6d1ff25377f814ab4c9df0f527b14c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 16:19:53 +0000

    "Dwarven Foresight"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dwarven/Card4_045.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set4.dwarven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChooseStackedCardsEffect;
+import com.gempukku.lotro.cards.effects.PutCardFromStackedIntoHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+import java.util.Collection;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Dwarven
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Fellowship: Spot a Dwarf to take a Free Peoples card stacked on a [DWARVEN] condition into hand.
+ */
+public class Card4_045 extends AbstractEvent {
+    public Card4_045() {
+        super(Side.FREE_PEOPLE, Culture.DWARVEN, "Dwarven Foresight", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.DWARF));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseStackedCardsEffect(action, playerId, 1, 1, Filters.and(Filters.culture(Culture.DWARVEN), Filters.type(CardType.CONDITION)), Filters.side(Side.FREE_PEOPLE)) {
+                    @Override
+                    protected void cardsChosen(Collection<PhysicalCard> stackedCards) {
+                        for (PhysicalCard card : stackedCards)
+                            action.insertEffect(
+                                    new PutCardFromStackedIntoHandEffect(card));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+}
