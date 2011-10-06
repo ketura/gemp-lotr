1386d9c124d62f444597ea0836b2e4c79971fd69
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 19:28:39 +0000

    "Erethon"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/DoesNotAddToArcheryTotalModifier.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/DoesNotAddToArcheryTotalModifier.java
@@ -4,12 +4,17 @@ import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Condition;
 import com.gempukku.lotro.logic.modifiers.ModifierEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 public class DoesNotAddToArcheryTotalModifier extends AbstractModifier {
     public DoesNotAddToArcheryTotalModifier(PhysicalCard source, Filter affectFilter) {
-        super(source, "Does not add to archery total", affectFilter, new ModifierEffect[]{ModifierEffect.ARCHERY_MODIFIER});
+        this(source, affectFilter, null);
+    }
+
+    public DoesNotAddToArcheryTotalModifier(PhysicalCard source, Filter affectFilter, Condition condition) {
+        super(source, "Does not add to archery total", affectFilter, condition, new ModifierEffect[]{ModifierEffect.ARCHERY_MODIFIER});
     }
 
     @Override
