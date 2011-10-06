8538b51a4064588334132bcd067204f03673e6a9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 10:53:23 +0000

    "Wulf"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_040.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.TakeControlOfASiteEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 4
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 2
+ * Site: 3
+ * Game Text: Fierce. Response: If another [DUNLAND] Man wins a skirmish, exert Wulf to take control of a site.
+ */
+public class Card4_040 extends AbstractMinion {
+    public Card4_040() {
+        super(4, 10, 2, 3, Race.MAN, Culture.DUNLAND, "Wulf", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public List<? extends Action> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.and(Filters.not(Filters.sameCard(self)), Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)))
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.RESPONSE);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new TakeControlOfASiteEffect(self, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
