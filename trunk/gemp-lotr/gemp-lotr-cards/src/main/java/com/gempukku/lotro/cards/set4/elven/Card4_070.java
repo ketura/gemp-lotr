e275b7d32a485fdc062e57bd0b8c58b4e8509906
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 22:02:29 +0000

    "Flashing Steel"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/elven/Card4_070.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set4.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Exert an Elf to make a minion skirmishing that Elf strength -3.
+ */
+public class Card4_070 extends AbstractEvent {
+    public Card4_070() {
+        super(Side.FREE_PEOPLE, Culture.ELVEN, "Flashing Steel", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.ELF));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.race(Race.ELF)) {
+                    @Override
+                    protected void forEachCardExertedCallback(PhysicalCard character) {
+                        action.appendEffect(
+                                new ChooseActiveCardEffect(self, playerId, "Choose a minion", Filters.type(CardType.MINION), Filters.inSkirmishAgainst(Filters.sameCard(character))) {
+                                    @Override
+                                    protected void cardSelected(PhysicalCard card) {
+                                        action.insertEffect(
+                                                new AddUntilEndOfPhaseModifierEffect(
+                                                        new StrengthModifier(self, Filters.sameCard(card), -3), Phase.SKIRMISH));
+                                    }
+                                });
+                    }
+                });
+        return action;
+    }
+}
