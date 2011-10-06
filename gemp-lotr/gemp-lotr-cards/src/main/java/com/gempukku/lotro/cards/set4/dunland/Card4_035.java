9f6eddf44e2c71b00ca3115b0f7571502892129a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 10:18:36 +0000

    "Wake of Destruction"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_035.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Maneuver: Spot 2 sites you control to make each of your [DUNLAND] Men fierce until the regroup phase.
+ */
+public class Card4_035 extends AbstractEvent {
+    public Card4_035() {
+        super(Side.SHADOW, Culture.DUNLAND, "Wake of Destruction", Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return Filters.countSpottable(game.getGameState(), game.getModifiersQuerying(), Filters.siteControlled(playerId)) >= 2;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new AddUntilStartOfPhaseModifierEffect(
+                        new KeywordModifier(self, Filters.and(Filters.owner(playerId), Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)), Keyword.FIERCE), Phase.REGROUP));
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+}
