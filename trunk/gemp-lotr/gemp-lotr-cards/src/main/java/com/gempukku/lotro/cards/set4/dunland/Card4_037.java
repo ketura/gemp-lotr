3d723856c1b2b07ca5422512782e1d0d1fd5a818
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-06 10:35:37 +0000

    "War Cry of Dunland"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/dunland/Card4_037.java
@@ -0,0 +1,60 @@
+package com.gempukku.lotro.cards.set4.dunland;
+
+import com.gempukku.lotro.cards.AbstractResponseEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.SkirmishResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Response: If a [DUNLAND] Man wins a skirmish, make him fierce and strength +4 until the regroup phase.
+ */
+public class Card4_037 extends AbstractResponseEvent {
+    public Card4_037() {
+        super(Side.SHADOW, Culture.DUNLAND, "War Cry of Dunland");
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public List<PlayEventAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (PlayConditions.canPlayCardDuringPhase(game, (Phase) null, self)
+                && PlayConditions.winsSkirmish(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.and(Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)))) {
+            final List<PhysicalCard> winners = ((SkirmishResult) effectResult).getWinners();
+            final PlayEventAction action = new PlayEventAction(self);
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose DUNLAND Man", Filters.in(winners), Filters.culture(Culture.DUNLAND), Filters.race(Race.MAN)) {
+                        @Override
+                        protected void cardSelected(PhysicalCard card) {
+                            action.insertEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new StrengthModifier(self, Filters.sameCard(card), 4), Phase.REGROUP));
+                            action.insertEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, Filters.sameCard(card), Keyword.FIERCE), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
