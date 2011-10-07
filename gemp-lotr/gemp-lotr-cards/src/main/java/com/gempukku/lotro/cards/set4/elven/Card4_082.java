7d929c9df7e01be42a33c169c381de3d30a44cec
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-07 09:23:02 +0000

    "Strength of Arms"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/elven/Card4_082.java
@@ -0,0 +1,68 @@
+package com.gempukku.lotro.cards.set4.elven;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddTokenEffect;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: Plays to your support area.
+ * Each time an Elf wins a skirmish, you may place an [ELVEN] token on this card.
+ * Skirmish: Make a minion skirmishing an Elf strength -1 for each [ELVEN] token here (limit -3). Discard this condition.
+ */
+public class Card4_082 extends AbstractPermanent {
+    public Card4_082() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.ELVEN, Zone.SUPPORT, "Strength of Arms", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(game.getGameState(), game.getModifiersQuerying(), effectResult, Filters.race(Race.ELF))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new AddTokenEffect(self, self, Token.ELVEN));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, final LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)) {
+            final ActivateCardAction action = new ActivateCardAction(self, Keyword.SKIRMISH);
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose minion", Filters.type(CardType.MINION), Filters.inSkirmishAgainst(Filters.race(Race.ELF))) {
+                        @Override
+                        protected void cardSelected(PhysicalCard card) {
+                            int bonus = -game.getGameState().getTokenCount(self, Token.ELVEN);
+                            action.insertEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, Filters.sameCard(card), bonus), Phase.SKIRMISH));
+                            action.appendEffect(
+                                    new DiscardCardsFromPlayEffect(self, self));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
