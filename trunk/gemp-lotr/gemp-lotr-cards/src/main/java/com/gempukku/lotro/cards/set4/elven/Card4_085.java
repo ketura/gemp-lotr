9b5943657a37553eeff047577449e7394f316def
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-07 09:32:45 +0000

    "Thandronen"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/elven/Card4_085.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set4.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardStackedCardsEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 2
+ * Type: Companion • Elf
+ * Strength: 5
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: To play, spot an Elf.
+ * Regroup: Exert Thandronen to discard a minion stacked on a condition or stacked on a site.
+ */
+public class Card4_085 extends AbstractCompanion {
+    public Card4_085() {
+        super(2, 5, 3, Culture.ELVEN, Race.ELF, null, "Thandronen", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.ELF));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.REGROUP, self)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), self)) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.REGROUP);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndDiscardStackedCardsEffect(action, playerId, 1, 1,
+                            Filters.or(Filters.type(CardType.CONDITION), Filters.type(CardType.SITE)),
+                            Filters.type(CardType.MINION)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
