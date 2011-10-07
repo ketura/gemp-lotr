e3b04eae74c334e60ab7c17f82a08ed4d618b04f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-07 14:57:07 +0000

    "Stump and Bramble"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gandalf/Card4_101.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set4.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.AllyOnCurrentSiteModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Maneuver: Spot an [ISENGARD] minion and an Ent ally. Until the regroup phase, that ally is damage +2
+ * and participates in archery fire and skirmishes.
+ */
+public class Card4_101 extends AbstractEvent {
+    public Card4_101() {
+        super(Side.FREE_PEOPLE, Culture.GANDALF, "Stump and Bramble", Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.ISENGARD), Filters.type(CardType.MINION))
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.ENT), Filters.type(CardType.ALLY));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "choose an Ent ally", Filters.race(Race.ENT), Filters.type(CardType.ALLY)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard card) {
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new KeywordModifier(self, Filters.sameCard(card), Keyword.DAMAGE, 2), Phase.REGROUP));
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new AllyOnCurrentSiteModifier(self, Filters.sameCard(card)), Phase.REGROUP));
+                    }
+                });
+        return action;
+    }
+}
--- trunk/gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Race.java
+++ trunk/gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Race.java
@@ -3,7 +3,7 @@ package com.gempukku.lotro.common;
 public enum Race {
     BALROG("Balrog"), CREATURE("Creature"),
     ELF("Elf"), HOBBIT("Hobbit"), DWARF("Dwarf"), MAN("Man"), WIZARD("Wizard"),
-    URUK_HAI("Uruk-Hai"), NAZGUL("Nazgul"), ORC("Orc"), TROLL("Troll");
+    URUK_HAI("Uruk-Hai"), NAZGUL("Nazgul"), ORC("Orc"), TROLL("Troll"), ENT("Ent");
 
     private String _humanReadable;
 
