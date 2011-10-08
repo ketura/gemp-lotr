ea6fd4b422157f49be21ff2fa66b8a8426ef1571
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 14:07:58 +0000

    "Arrows Thick in the Air"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_110.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set4.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 3
+ * Type: Event
+ * Game Text: Archery or Skirmish: Exert 2 Ring-bound Men to wound each roaming minion.
+ */
+public class Card4_110 extends AbstractEvent {
+    public Card4_110() {
+        super(Side.FREE_PEOPLE, Culture.GONDOR, "Arrows Thick in the Air", Phase.ARCHERY, Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExertMultiple(self, game.getGameState(), game.getModifiersQuerying(), 1, 2, Filters.keyword(Keyword.RING_BOUND), Filters.race(Race.MAN));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 3;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Filters.keyword(Keyword.RING_BOUND), Filters.race(Race.MAN)));
+        action.appendEffect(
+                new WoundCharactersEffect(self, Filters.and(Filters.type(CardType.MINION), Filters.keyword(Keyword.ROAMING))));
+        return action;
+    }
+}
