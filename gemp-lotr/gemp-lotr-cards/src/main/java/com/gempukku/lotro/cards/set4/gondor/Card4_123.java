ba2d1e9634cf0f3635645cc03907d6508a8afed1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 15:46:07 +0000

    "Hard Choice"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_123.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set4.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Fellowship: Spot Aragorn to heal a companion who has the Aragorn signet twice.
+ */
+public class Card4_123 extends AbstractEvent {
+    public Card4_123() {
+        super(Side.FREE_PEOPLE, Culture.GONDOR, "Hard Choice", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.name("Aragorn"));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndHealCharactersEffect(action, playerId, 1, 1, 2, Filters.type(CardType.COMPANION), Filters.signet(Signet.ARAGORN)));
+        return action;
+    }
+}
