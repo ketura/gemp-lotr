086ed910867407637a4510ec1df1e92271274116
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 14:04:15 +0000

    "Aragorn"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_109.java
@@ -0,0 +1,26 @@
+package com.gempukku.lotro.cards.set4.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Signet;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 4
+ * Type: Companion • Man
+ * Strength: 8
+ * Vitality: 4
+ * Resistance: 6
+ * Signet: Gandalf
+ * Game Text: Defender +1.
+ */
+public class Card4_109 extends AbstractCompanion {
+    public Card4_109() {
+        super(4, 8, 4, Culture.GONDOR, Race.MAN, Signet.GANDALF, "Aragorn", true);
+        addKeyword(Keyword.DEFENDER, 1);
+    }
+}
