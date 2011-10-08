373fa45d1f20fc0ecd5cd3309399e581a6592483
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 14:24:25 +0000

    "Curse Them"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_113.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set4.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.CancelSkirmishEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Exert a Ring-bound Man to cancel a skirmish involving a roaming minion.
+ */
+public class Card4_113 extends AbstractEvent {
+    public Card4_113() {
+        super(Side.FREE_PEOPLE, Culture.GONDOR, "Curse Them", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.MAN), Filters.keyword(Keyword.RING_BOUND));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.race(Race.MAN), Filters.keyword(Keyword.RING_BOUND)));
+        action.appendEffect(
+                new CancelSkirmishEffect(Filters.and(Filters.type(CardType.MINION), Filters.keyword(Keyword.ROAMING))));
+        return action;
+    }
+}
