56311e20c8d12f46fe5a6b5546116cb7a611d0e0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 00:05:02 +0000

    "Task Was Not Done"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gandalf/Card4_102.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set4.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 3
+ * Type: Event
+ * Game Text: Spell. Regroup: Exert Gandalf to discard up to 2 wounded minions.
+ */
+public class Card4_102 extends AbstractEvent {
+    public Card4_102() {
+        super(Side.FREE_PEOPLE, Culture.GANDALF, "Task Was Not Done", Phase.REGROUP);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game.getGameState(), game.getModifiersQuerying(), Filters.name("Gandalf"));
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 3;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.name("Gandalf")));
+        action.appendEffect(
+                new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 0, 2, Filters.type(CardType.MINION), Filters.wounded()));
+        return action;
+    }
+}
