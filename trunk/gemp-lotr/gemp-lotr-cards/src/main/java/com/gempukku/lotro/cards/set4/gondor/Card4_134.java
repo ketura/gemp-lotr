52be878f18d6f848da390ce85d2da79e6c16f52a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 21:25:15 +0000

    "Sword of Gondor"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_134.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set4.gondor;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be a [GONDOR] Man. While skirmishing a Man or a roaming minion, bearer is damage +1.
+ */
+public class Card4_134 extends AbstractAttachableFPPossession {
+    public Card4_134() {
+        super(1, 2, 0, Culture.GONDOR, Keyword.HAND_WEAPON, "Sword of Gondor");
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Filters.culture(Culture.GONDOR), Filters.race(Race.MAN));
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self,
+                        Filters.and(
+                                Filters.hasAttached(self),
+                                Filters.inSkirmishAgainst(
+                                        Filters.or(
+                                                Filters.race(Race.MAN),
+                                                Filters.roamingMinion()
+                                        )
+                                )
+                        ), Keyword.DAMAGE));
+    }
+}
