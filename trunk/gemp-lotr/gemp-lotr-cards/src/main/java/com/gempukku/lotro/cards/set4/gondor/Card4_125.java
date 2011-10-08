2e6d1c7b1ff323ef2b09fa6197fc9d1a576bfe51
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-08 15:57:29 +0000

    "Henneth Annun"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_125.java
@@ -0,0 +1,72 @@
+package com.gempukku.lotro.cards.set4.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Condition
+ * Game Text: Plays to your support area. While the fellowship is at site 6T, each roaming minion skirmishing
+ * a Ring-bound companion is strength -2. Skirmish: Spot a [GONDOR] Man and discard this condition to wound
+ * a roaming minion.
+ */
+public class Card4_125 extends AbstractPermanent {
+    public Card4_125() {
+        super(Side.FREE_PEOPLE, 0, CardType.CONDITION, Culture.GONDOR, Zone.SUPPORT, "Henneth Annun", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self,
+                        Filters.and(
+                                Filters.type(CardType.MINION),
+                                Filters.keyword(Keyword.ROAMING),
+                                Filters.inSkirmishAgainst(
+                                        Filters.and(
+                                                Filters.type(CardType.COMPANION),
+                                                Filters.keyword(Keyword.RING_BOUND)
+                                        )
+                                )
+                        ),
+                        new Condition() {
+                            @Override
+                            public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                                return gameState.getCurrentSiteNumber() == 6 && gameState.getCurrentSiteBlock() == Block.TWO_TOWERS;
+                            }
+                        }, -2));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.culture(Culture.GONDOR), Filters.race(Race.MAN))) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.SKIRMISH);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, Filters.type(CardType.MINION), Filters.keyword(Keyword.ROAMING)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
