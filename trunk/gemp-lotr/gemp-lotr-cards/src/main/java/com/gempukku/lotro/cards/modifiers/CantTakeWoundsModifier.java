58be991e79bd5bf5f9239195bb57c09e7585345f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-09 10:29:53 +0000

    "Broad-bladed Sword"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/CantTakeWoundsModifier.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/CantTakeWoundsModifier.java
@@ -4,12 +4,17 @@ import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Condition;
 import com.gempukku.lotro.logic.modifiers.ModifierEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 public class CantTakeWoundsModifier extends AbstractModifier {
     public CantTakeWoundsModifier(PhysicalCard source, Filter affectFilter) {
         super(source, "Can't take wounds", affectFilter, new ModifierEffect[]{ModifierEffect.WOUND_MODIFIER});
+    }
+
+    public CantTakeWoundsModifier(PhysicalCard source, Condition condition, Filter affectFilter) {
+        super(source, "Can't take wounds", affectFilter, condition, new ModifierEffect[]{ModifierEffect.WOUND_MODIFIER});
     }
 
     @Override
