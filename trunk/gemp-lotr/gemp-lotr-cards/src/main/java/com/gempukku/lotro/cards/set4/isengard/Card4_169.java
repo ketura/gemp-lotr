d10868cb07fe24a13e4cfea14cde6a922522b2ec
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-10 18:31:58 +0000

    "Ranged Commander"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_169.java
@@ -0,0 +1,64 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.DoesNotAddToArcheryTotalModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 8
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Archer. The twilight cost of each [ISENGARD] archer is -1. Archery: Exert Ranged Commander to make
+ * the Free Peoples player wound an unbound companion; this minion does not add to the minion archery total.
+ */
+public class Card4_169 extends AbstractMinion {
+    public Card4_169() {
+        super(4, 8, 2, 5, Race.URUK_HAI, Culture.ISENGARD, "Ranged Commander", true);
+        addKeyword(Keyword.ARCHER);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new TwilightCostModifier(self, Filters.and(Filters.culture(Culture.ISENGARD), Filters.keyword(Keyword.ARCHER)), -1));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.ARCHERY, self, 0)
+                && PlayConditions.canExert(self, game, Filters.sameCard(self))) {
+            ActivateCardAction action = new ActivateCardAction(self, Keyword.ARCHERY);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, Filters.unboundCompanion()));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new DoesNotAddToArcheryTotalModifier(self, Filters.sameCard(self)), Phase.ARCHERY));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
