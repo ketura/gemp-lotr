b8601a7de3488f679d7fe75ee5809fbed8a6bcd8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-10 19:26:32 +0000

    "Still They Came"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_175.java
@@ -0,0 +1,62 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Skirmish: Exert an Uruk-hai who is not assigned to a skirmish to make another Uruk-hai strength +3.
+ */
+public class Card4_175 extends AbstractEvent {
+    public Card4_175() {
+        super(Side.SHADOW, Culture.ISENGARD, "Still They Came", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game, Filters.race(Race.URUK_HAI), Filters.notAssignedToSkirmish());
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.race(Race.URUK_HAI), Filters.notAssignedToSkirmish()) {
+                    @Override
+                    protected void forEachCardExertedCallback(PhysicalCard character) {
+                        action.appendEffect(
+                                new ChooseActiveCardEffect(self, playerId, "Choose Uruk-hai", Filters.not(Filters.sameCard(character)), Filters.race(Race.URUK_HAI)) {
+                                    @Override
+                                    protected void cardSelected(PhysicalCard card) {
+                                        action.insertEffect(
+                                                new AddUntilEndOfPhaseModifierEffect(
+                                                        new StrengthModifier(self, Filters.sameCard(card), 3), Phase.SKIRMISH));
+                                    }
+                                });
+                    }
+                });
+        return action;
+    }
+}
