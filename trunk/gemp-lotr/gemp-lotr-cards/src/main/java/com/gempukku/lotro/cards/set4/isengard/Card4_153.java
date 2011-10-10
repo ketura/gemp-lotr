19f440d16c523bb7bfb08a7c7f4c659da441c623
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-10 13:20:02 +0000

    "Grima"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_153.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 2
+ * Type: Minion • Man
+ * Strength: 4
+ * Vitality: 3
+ * Site: 3
+ * Game Text: Each unbound companion (or ally) bearing a Shadow condition is strength -1.
+ */
+public class Card4_153 extends AbstractMinion {
+    public Card4_153() {
+        super(2, 4, 3, 3, Race.MAN, Culture.ISENGARD, "Grima", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self,
+                        Filters.and(
+                                Filters.hasAttached(Filters.and(Filters.side(Side.SHADOW), Filters.type(CardType.CONDITION))),
+                                Filters.or(
+                                        Filters.unboundCompanion(),
+                                        Filters.type(CardType.ALLY)
+                                )), -1));
+    }
+}
