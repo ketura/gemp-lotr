90d360e3352d8654cacd85787cc320d0213f3ae5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 08:46:37 +0000

    "Uruk Fanatic"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_185.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 9
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. If there are at least 3 cards in the dead pile, this minion is fierce.
+ */
+public class Card4_185 extends AbstractMinion {
+    public Card4_185() {
+        super(4, 9, 2, 5, Race.URUK_HAI, Culture.ISENGARD, "Uruk Fanatic");
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.sameCard(self),
+                        new Condition() {
+                            @Override
+                            public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                                return gameState.getDeadPile(gameState.getCurrentPlayerId()).size() >= 3;
+                            }
+                        }, Keyword.FIERCE, 1));
+    }
+}
