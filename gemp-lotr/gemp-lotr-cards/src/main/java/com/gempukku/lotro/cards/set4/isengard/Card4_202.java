f74cc41d550f6458cf4b7f4e83ae05f11118d87d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 09:56:00 +0000

    "Uruk-hai Band"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_202.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 5
+ * Type: Minion • Uruk-Hai
+ * Strength: 10
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Damage +1. This minion is strength +2 for each site you control.
+ */
+public class Card4_202 extends AbstractMinion {
+    public Card4_202() {
+        super(5, 10, 3, 5, Race.URUK_HAI, Culture.ISENGARD, "Uruk-hai Band");
+        addKeyword(Keyword.DAMAGE);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.sameCard(self), null, new Evaluator() {
+                    @Override
+                    public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+                        return 2 * Filters.countSpottable(gameState, modifiersQuerying, Filters.siteControlled(self.getOwner()));
+                    }
+                }));
+    }
+}
