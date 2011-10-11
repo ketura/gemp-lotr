afe439b52a8cada457033bc70ee2ee3136bdda35
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 10:15:09 +0000

    "We Are the Fighting Uruk-hai"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_210.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Skirmish: Make an Uruk-hai strength +2 (and damage +1 if you control a site).
+ */
+public class Card4_210 extends AbstractEvent {
+    public Card4_210() {
+        super(Side.SHADOW, Culture.ISENGARD, "We Are the Fighting Uruk-hai", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, final LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose an Uruk-hai", Filters.race(Race.URUK_HAI)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard card) {
+                        boolean controlsSite = Filters.countActive(game.getGameState(), game.getModifiersQuerying(), Filters.siteControlled(playerId)) > 0;
+                        action.insertEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(self), 2), Phase.SKIRMISH));
+                        if (controlsSite)
+                            action.appendEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new KeywordModifier(self, Filters.sameCard(self), Keyword.DAMAGE), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+}
