9d1e234e080e74b89705f1f4455b173b8f515e8f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 09:36:59 +0000

    "Uruk Searcher"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_194.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 9
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Tracker. Fierce. While skirmishing a character bearing a search card, this minion is strength +2
+ * and damage +1.
+ */
+public class Card4_194 extends AbstractMinion {
+    public Card4_194() {
+        super(4, 9, 2, 5, Race.URUK_HAI, Culture.ISENGARD, "Uruk Searcher");
+        addKeyword(Keyword.TRACKER);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, Filters.and(Filters.sameCard(self), Filters.inSkirmishAgainst(Filters.hasAttached(Filters.keyword(Keyword.SEARCH)))), 2));
+        modifiers.add(
+                new KeywordModifier(self, Filters.and(Filters.sameCard(self), Filters.inSkirmishAgainst(Filters.hasAttached(Filters.keyword(Keyword.SEARCH)))), Keyword.DAMAGE));
+        return modifiers;
+    }
+}
