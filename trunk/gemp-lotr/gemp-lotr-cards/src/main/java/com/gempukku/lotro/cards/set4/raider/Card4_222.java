10b92e88a77326504b6d67bd7cedb26b8d3f6027
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 13:52:46 +0000

    "Desert Warrior"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/raider/Card4_222.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set4.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 2
+ * Type: Minion • Man
+ * Strength: 6
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Southron. At the start of each skirmish involving this minion, you may remove (3) to wound a companion
+ * or ally he is skirmishing.
+ */
+public class Card4_222 extends AbstractMinion {
+    public Card4_222() {
+        super(2, 6, 2, 4, Race.MAN, Culture.RAIDER, "Desert Warrior");
+        addKeyword(Keyword.SOUTHRON);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.START_OF_PHASE
+                && game.getGameState().getCurrentPhase() == Phase.SKIRMISH
+                && game.getGameState().getSkirmish().getShadowCharacters().contains(self)
+                && game.getGameState().getTwilightPool() >= 3) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new RemoveTwilightEffect(3));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, Filters.inSkirmishAgainst(Filters.sameCard(self))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
