6e529d5118c16bea36da6901de22f3555230fb75
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 10:08:51 +0000

    "Vengeance"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_208.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Make an [ISENGARD] tracker strength +2 for each card borne by the character it is skirmishing.
+ */
+public class Card4_208 extends AbstractEvent {
+    public Card4_208() {
+        super(Side.SHADOW, Culture.ISENGARD, "Vengeance", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, final LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose an ISENGARD tracker", Filters.culture(Culture.ISENGARD), Filters.keyword(Keyword.TRACKER)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard card) {
+                        int bonus = 2 * Filters.countSpottable(game.getGameState(), game.getModifiersQuerying(), Filters.attachedTo(Filters.inSkirmishAgainst(Filters.sameCard(self))));
+                        action.insertEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(card), bonus), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+}
