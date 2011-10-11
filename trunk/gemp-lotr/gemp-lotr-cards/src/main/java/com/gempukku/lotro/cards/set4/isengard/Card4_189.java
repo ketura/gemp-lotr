061b89553eb7aa1ac77fcd7860fff9cc1295c1ac
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-11 09:06:16 +0000

    "Uruk Plains Runner"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/isengard/Card4_189.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set4.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 2
+ * Type: Minion • Uruk-Hai
+ * Strength: 7
+ * Vitality: 1
+ * Site: 5
+ * Game Text: Tracker. Fierce. During a fierce skirmish involving this minion, it is strength +3 and damage +1.
+ */
+public class Card4_189 extends AbstractMinion {
+    public Card4_189() {
+        super(2, 7, 1, 5, Race.URUK_HAI, Culture.ISENGARD, "Uruk Plains Runner");
+        addKeyword(Keyword.TRACKER);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, Filters.and(Filters.sameCard(self), Filters.inSkirmish()),
+                        new Condition() {
+                            @Override
+                            public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                                return gameState.isFierceSkirmishes();
+                            }
+                        }, 3));
+        modifiers.add(
+                new KeywordModifier(self, Filters.and(Filters.sameCard(self), Filters.inSkirmish()),
+                        new Condition() {
+                            @Override
+                            public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                                return gameState.isFierceSkirmishes();
+                            }
+                        }, Keyword.DAMAGE, 1));
+
+        return modifiers;
+    }
+}
