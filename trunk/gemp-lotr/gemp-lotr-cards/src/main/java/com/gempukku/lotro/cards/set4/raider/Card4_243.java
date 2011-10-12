5f25cd81d2509bcaf91f10186e50c1a9a3c86e0b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-12 16:08:05 +0000

    "Rapid Fire"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/raider/Card4_243.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set4.raider;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 6
+ * Type: Event
+ * Game Text: Archery: Exert a [RAIDER] archer to make the minion archery total +1 for each burden (limit +5).
+ */
+public class Card4_243 extends AbstractEvent {
+    public Card4_243() {
+        super(Side.SHADOW, Culture.RAIDER, "Rapid Fire", Phase.ARCHERY);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game, Filters.culture(Culture.RAIDER), Filters.keyword(Keyword.ARCHER));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.culture(Culture.RAIDER), Filters.keyword(Keyword.ARCHER)));
+        int total = Math.min(5, game.getGameState().getBurdens());
+        action.appendEffect(
+                new AddUntilEndOfPhaseModifierEffect(
+                        new ArcheryTotalModifier(self, Side.SHADOW, total), Phase.ARCHERY));
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 6;
+    }
+}
