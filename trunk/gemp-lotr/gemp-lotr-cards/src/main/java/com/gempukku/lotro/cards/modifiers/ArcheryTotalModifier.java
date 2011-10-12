6bd6882b6fa21f8e15805870d06742c72541037b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-12 16:16:05 +0000

    "Southron Archer"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/ArcheryTotalModifier.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/ArcheryTotalModifier.java
@@ -7,27 +7,39 @@ import com.gempukku.lotro.logic.modifiers.AbstractModifier;
 import com.gempukku.lotro.logic.modifiers.Condition;
 import com.gempukku.lotro.logic.modifiers.ModifierEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.evaluator.ConstantEvaluator;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 
 public class ArcheryTotalModifier extends AbstractModifier {
     private Side _side;
     private Condition _condition;
-    private int _modifier;
+    private Evaluator _evaluator;
 
     public ArcheryTotalModifier(PhysicalCard source, Side side, int modifier) {
         this(source, side, null, modifier);
     }
 
     public ArcheryTotalModifier(PhysicalCard source, Side side, Condition condition, int modifier) {
-        super(source, ((side == Side.FREE_PEOPLE) ? "Fellowship" : "Minion") + " archery total " + ((modifier < 0) ? modifier : ("+" + modifier)), null, new ModifierEffect[]{ModifierEffect.ARCHERY_MODIFIER});
+        this(source, side, condition, new ConstantEvaluator(modifier));
+    }
+
+    public ArcheryTotalModifier(PhysicalCard source, Side side, Condition condition, Evaluator evaluator) {
+        super(source, null, null, new ModifierEffect[]{ModifierEffect.ARCHERY_MODIFIER});
         _side = side;
         _condition = condition;
-        _modifier = modifier;
+        _evaluator = evaluator;
+    }
+
+    @Override
+    public String getText(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        int modifier = _evaluator.evaluateExpression(gameState, modifiersQuerying, self);
+        return ((_side == Side.FREE_PEOPLE) ? "Fellowship" : "Minion") + " archery total " + ((modifier < 0) ? modifier : ("+" + modifier));
     }
 
     @Override
     public int getArcheryTotal(GameState gameState, ModifiersQuerying modifiersQuerying, Side side, int result) {
         if (side == _side && ((_condition == null) || (_condition.isFullfilled(gameState, modifiersQuerying))))
-            return result + _modifier;
+            return result + _evaluator.evaluateExpression(gameState, modifiersQuerying, null);
         else
             return result;
     }
