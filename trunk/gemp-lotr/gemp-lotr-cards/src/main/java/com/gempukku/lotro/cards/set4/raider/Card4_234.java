e7ec07ab20e35adf20b53c7ec16a3a93baa0f038
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-12 15:16:05 +0000

    "Flanking Attack"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/raider/Card4_234.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set4.raider;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Maneuver or Skirmish: Exert a Southron to add (3).
+ */
+public class Card4_234 extends AbstractEvent {
+    public Card4_234() {
+        super(Side.SHADOW, Culture.RAIDER, "Flanking Attack", Phase.MANEUVER, Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game, Filters.keyword(Keyword.SOUTHRON));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.keyword(Keyword.SOUTHRON)));
+        action.appendEffect(
+                new AddTwilightEffect(self, 3));
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 0;
+    }
+}
