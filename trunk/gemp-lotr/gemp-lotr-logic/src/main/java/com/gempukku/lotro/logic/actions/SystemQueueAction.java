0f7fb8ef52eac5a0e0b7604d9ae088ff2470d4c5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-12 18:55:45 +0000

    Fixing two major problems.

---
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/actions/SystemQueueAction.java
@@ -0,0 +1,31 @@
+package com.gempukku.lotro.logic.actions;
+
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Effect;
+
+public class SystemQueueAction extends AbstractCostToEffectAction {
+    @Override
+    public PhysicalCard getActionSource() {
+        return null;
+    }
+
+    @Override
+    public String getText(LotroGame game) {
+        return null;
+    }
+
+    @Override
+    public Effect nextEffect() {
+        if (!isCostFailed()) {
+            Effect cost = getNextCost();
+            if (cost != null)
+                return cost;
+
+            Effect effect = getNextEffect();
+            if (effect != null)
+                return effect;
+        }
+        return null;
+    }
+}
