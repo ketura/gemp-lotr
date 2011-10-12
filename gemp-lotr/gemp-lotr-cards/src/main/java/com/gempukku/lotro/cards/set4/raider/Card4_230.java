9c22d8e4699a9c41ae5d51e6da9a5bf6eafce42d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-12 12:59:01 +0000

    "Easterling Trooper"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/raider/Card4_230.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set4.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotBurdensCondition;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 4
+ * Type: Minion • Man
+ * Strength: 9
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Easterling. While you can spot 3 burdens, this minion is fierce and damage +1.
+ */
+public class Card4_230 extends AbstractMinion {
+    public Card4_230() {
+        super(4, 9, 2, 4, Race.MAN, Culture.RAIDER, "Easterling Trooper");
+        addKeyword(Keyword.EASTERLING);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new KeywordModifier(self, Filters.sameCard(self), new SpotBurdensCondition(3), Keyword.FIERCE, 1));
+        modifiers.add(
+                new KeywordModifier(self, Filters.sameCard(self), new SpotBurdensCondition(3), Keyword.DAMAGE, 1));
+        return modifiers;
+    }
+}
