05ac60a2a1f393f44a5244065f0b00dac7770c38
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-13 14:57:11 +0000

    "Southron Troop"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/raider/Card4_256.java
@@ -0,0 +1,62 @@
+package com.gempukku.lotro.cards.set4.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AssignmentEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 7
+ * Type: Minion • Man
+ * Strength: 14
+ * Vitality: 4
+ * Site: 4
+ * Game Text: Southron. Archer. Ambush (1). To play, spot a Southron. Assignment: Spot 7 companions to assign this
+ * minion to the Ringbearer. The Free Peoples player may make you discard a companion (except the Ring-bearer)
+ * to prevent this.
+ */
+public class Card4_256 extends AbstractMinion {
+    public Card4_256() {
+        super(7, 14, 4, 4, Race.MAN, Culture.RAIDER, "Southron Troop");
+        addKeyword(Keyword.SOUTHRON);
+        addKeyword(Keyword.ARCHER);
+        addKeyword(Keyword.AMBUSH, 1);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, Filters.keyword(Keyword.SOUTHRON));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canSpot(game, 7, Filters.type(CardType.COMPANION))
+                && PlayConditions.canCardAssignToSkirmish(self, game, self)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new PreventableEffect(action,
+                            new AssignmentEffect(playerId, Filters.findFirstActive(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.RING_BEARER)), self),
+                            game.getGameState().getCurrentPlayerId(),
+                            new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Filters.type(CardType.COMPANION), Filters.not(Filters.keyword(Keyword.RING_BEARER)))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
