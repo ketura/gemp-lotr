5809ee24fc1b749e2f3ea8d8075b120cd91cf8b8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-13 21:51:54 +0000

    You can't transfer card to the same bearer ("Horn of Boromir")

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/AbstractAttachableFPPossession.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/AbstractAttachableFPPossession.java
@@ -61,6 +61,7 @@ public abstract class AbstractAttachableFPPossession extends AbstractAttachable 
             }
 
             validTransferFilter = Filters.and(validTransferFilter,
+                    Filters.not(Filters.sameCard(self.getAttachedTo())),
                     new Filter() {
                         @Override
                         public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
