44682d47fb01b88d461ea1447d231cbd055382d0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-13 15:04:12 +0000

    "Southron Wanderer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/raider/Card4_258.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set4.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.effects.AssignmentEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 3
+ * Type: Minion • Man
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Southron. Ambush (1). Assignment: Exert this minion and spot 5 companions to assign this minion
+ * to the Ringbearer. The Free Peoples player may add (4) to prevent this.
+ */
+public class Card4_258 extends AbstractMinion {
+    public Card4_258() {
+        super(3, 8, 2, 4, Race.MAN, Culture.RAIDER, "Southron Wanderer");
+        addKeyword(Keyword.SOUTHRON);
+        addKeyword(Keyword.AMBUSH, 1);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.ASSIGNMENT, self, 0)
+                && PlayConditions.canExert(self, game, Filters.sameCard(self))
+                && PlayConditions.canSpot(game, 5, Filters.type(CardType.COMPANION))) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new PreventableEffect(action,
+                            new AssignmentEffect(playerId, Filters.findFirstActive(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.RING_BEARER)), self),
+                            game.getGameState().getCurrentPlayerId(),
+                            new AddTwilightEffect(self, 4)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
