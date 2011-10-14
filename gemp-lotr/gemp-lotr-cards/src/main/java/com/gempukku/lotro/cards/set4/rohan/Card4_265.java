072755f8cbd884305818fe60f7b4361d75f4816b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-14 10:06:59 +0000

    "Elite Rider"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/rohan/Card4_265.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set4.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: When this companion is in your starting fellowship, his twilight cost is -1. While skirmishing a wounded
+ * minion, this companion is strength +2.
+ */
+public class Card4_265 extends AbstractCompanion {
+    public Card4_265() {
+        super(3, 6, 3, Culture.ROHAN, Race.MAN, null, "Elite Rider");
+    }
+
+    @Override
+    public int getCompanionStartingFellowshipModifier() {
+        return -1;
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self,
+                        Filters.and(
+                                Filters.sameCard(self),
+                                Filters.inSkirmishAgainst(Filters.type(CardType.MINION), Filters.wounded())), 2));
+    }
+}
