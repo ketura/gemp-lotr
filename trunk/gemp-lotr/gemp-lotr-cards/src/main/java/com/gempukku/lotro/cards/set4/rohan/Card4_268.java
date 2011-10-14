74dd9f0ece3e2d98e5323e9c90e00ff80d9fdc6a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-14 10:18:51 +0000

    "Eomer's Spear"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/rohan/Card4_268.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set4.rohan;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be a [ROHAN] Man. If bearer is Eomer and he is mounted, Eomer is damage +2.
+ */
+public class Card4_268 extends AbstractAttachableFPPossession {
+    public Card4_268() {
+        super(1, 2, 0, Culture.ROHAN, Keyword.HAND_WEAPON, "Eomer's Spear", true);
+    }
+
+    @Override
+    protected Filter getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Filters.culture(Culture.ROHAN), Filters.race(Race.MAN));
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.and(Filters.hasAttached(self), Filters.name("Eomer")), Keyword.DAMAGE, 2));
+    }
+}
