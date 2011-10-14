5f1d6d6b2656816e0ebacf4e12f87cc97a770fbb
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-14 14:54:16 +0000

    "Fight for the Villagers"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/rohan/Card4_273.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set4.rohan;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Skirmish: Make a [ROHAN] Man strength +2 (or +3 if you spot a villager).
+ */
+public class Card4_273 extends AbstractEvent {
+    public Card4_273() {
+        super(Side.FREE_PEOPLE, Culture.ROHAN, "Fight for the Villagers", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, final LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose ROHAN Man", Filters.culture(Culture.ROHAN), Filters.race(Race.MAN)) {
+                    @Override
+                    protected void cardSelected(PhysicalCard card) {
+                        int bonus = Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.VILLAGER)) ? 3 : 2;
+                        action.insertEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(card), bonus), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+
+    @Override
+    public int getTwilightCost() {
+        return 1;
+    }
+}
