f7770909f42995fcb400aeb6f550930e1c4d79c5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-15 21:23:25 +0000

    "Simbelmyne"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/rohan/Card4_289.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set4.rohan;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Fellowship: Spot 2 [ROHAN] Men (or 1 valiant [ROHAN] Man) to play a [ROHAN] character
+ * or [ROHAN] possession from your draw deck.
+ */
+public class Card4_289 extends AbstractEvent {
+    public Card4_289() {
+        super(Side.FREE_PEOPLE, 1, Culture.ROHAN, "Simbelmyne", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && (
+                PlayConditions.canSpot(game, 2, Culture.ROHAN, Race.MAN)
+                        || PlayConditions.canSpot(game, Culture.ROHAN, Race.MAN, Keyword.VALIANT));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndPlayCardFromDeckEffect(playerId,
+                        Filters.and(
+                                Culture.ROHAN,
+                                Filters.or(
+                                        CardType.ALLY,
+                                        CardType.COMPANION,
+                                        CardType.POSSESSION
+                                )
+                        )));
+        return action;
+    }
+}
