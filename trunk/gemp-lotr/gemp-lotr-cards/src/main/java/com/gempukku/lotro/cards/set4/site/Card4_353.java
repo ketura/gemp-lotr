d8dcbe9416ef3f5dfa73b07e2c670fcb4bd58285
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 21:36:54 +0000

    "Great Hall"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_353.java
@@ -0,0 +1,36 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.modifiers.AllyParticipatesInArcheryFireAndSkirmishesModifier;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Twilight Cost: 3
+ * Type: Site
+ * Site: 6T
+ * Game Text: Sanctuary. Battleground. Each [ROHAN] ally may participate in archery fire and skirmishes at Great Hall.
+ */
+public class Card4_353 extends AbstractSite {
+    public Card4_353() {
+        super("Great Hall", Block.TWO_TOWERS, 6, 3, Direction.LEFT);
+        addKeyword(Keyword.SANCTUARY);
+        addKeyword(Keyword.BATTLEGROUND);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new AllyParticipatesInArcheryFireAndSkirmishesModifier(self, Filters.and(Culture.ROHAN, CardType.ALLY)));
+    }
+}
