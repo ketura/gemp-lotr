0adaa3c08a497791d6a2122145b9ba74081e1835
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 18:36:56 +0000

    "Get On and Get Away"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/shire/Card4_304.java
@@ -0,0 +1,38 @@
+package com.gempukku.lotro.cards.set4.shire;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.cards.effects.PlaySiteEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Fellowship: Add 2 burdens to play the fellowship's next 2 sites (replacing opponent's sites if necessary).
+ */
+public class Card4_304 extends AbstractEvent {
+    public Card4_304() {
+        super(Side.FREE_PEOPLE, 0, Culture.SHIRE, "Get On and Get Away");
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        int siteNumber = game.getGameState().getCurrentSiteNumber();
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new AddBurdenEffect(self, 2));
+        action.appendEffect(
+                new PlaySiteEffect(playerId, null, siteNumber + 1));
+        if (siteNumber < 8)
+            action.appendEffect(
+                    new PlaySiteEffect(playerId, null, siteNumber + 2));
+        return action;
+    }
+}
