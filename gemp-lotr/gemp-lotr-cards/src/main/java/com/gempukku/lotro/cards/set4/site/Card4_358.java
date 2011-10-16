5f231d7d87e66beec42fa0e6bc6907db937fe77c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 21:48:58 +0000

    "Ring of Isengard"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_358.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Twilight Cost: 7
+ * Type: Site
+ * Site: 8T
+ * Game Text: Shadow: Play a [DUNLAND] minion to draw a card.
+ */
+public class Card4_358 extends AbstractSite {
+    public Card4_358() {
+        super("Ring of Isengard", Block.TWO_TOWERS, 8, 7, Direction.RIGHT);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.SHADOW, self)
+                && PlayConditions.canPlayFromHand(playerId, game, Culture.DUNLAND, CardType.MINION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game.getGameState().getHand(playerId), Culture.DUNLAND, CardType.MINION));
+            action.appendEffect(
+                    new DrawCardEffect(playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
