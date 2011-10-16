2cf28e3feb83a7e9fc8fd3c679c50f41ee662e81
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 20:16:24 +0000

    "Eastemnet Gullies"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_325.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Type: Site
+ * Site: 1T
+ * Game Text: Fellowship: Exert 2 unbound companions to play Legolas from your draw deck.
+ */
+public class Card4_325 extends AbstractSite {
+    public Card4_325() {
+        super("Eastemnet Gullies", Block.TWO_TOWERS, 1, 0, Direction.LEFT);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && PlayConditions.canExertMultiple(self, game.getGameState(), game.getModifiersQuerying(), 1, 2, Filters.unboundCompanion)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Filters.unboundCompanion));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Filters.name("Legolas")));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
