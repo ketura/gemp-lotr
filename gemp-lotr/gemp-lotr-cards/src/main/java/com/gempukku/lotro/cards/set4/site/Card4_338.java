11acaea085533b7cb9c4f42fbf366b32c4c08598
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 20:44:25 +0000

    "Golden Hall"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_338.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Twilight Cost: 1
+ * Type: Site
+ * Site: 3T
+ * Game Text: Sanctuary. When the fellowship moves to Golden Hall, all Free Peoples weapons must be discarded.
+ */
+public class Card4_338 extends AbstractSite {
+    public Card4_338() {
+        super("Golden Hall", Block.TWO_TOWERS, 3, 1, Direction.RIGHT);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.WHEN_MOVE_TO
+                && game.getGameState().getCurrentSite() == self) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new DiscardCardsFromPlayEffect(self, Filters.and(Side.FREE_PEOPLE, Filters.weapon)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
