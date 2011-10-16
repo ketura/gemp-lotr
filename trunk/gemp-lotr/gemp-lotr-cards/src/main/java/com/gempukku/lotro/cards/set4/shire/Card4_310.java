dd89b9484a59867b31457fa90792c6c4d62d2132
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 19:02:38 +0000

    "Merry"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/shire/Card4_310.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set4.shire;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Companion • Hobbit
+ * Strength: 3
+ * Vitality: 4
+ * Resistance: 6
+ * Signet: Frodo
+ * Game Text: Skirmish: Exert the Ring-bearer to discard Merry. Any Shadow player may remove (2) to prevent this.
+ */
+public class Card4_310 extends AbstractCompanion {
+    public Card4_310() {
+        super(1, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.FRODO, "Merry", true);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && PlayConditions.canExert(self, game, Keyword.RING_BEARER)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Keyword.RING_BEARER));
+            action.appendEffect(
+                    new PreventableEffect(action,
+                            new DiscardCardsFromPlayEffect(self, self),
+                            GameUtils.getOpponents(game, playerId),
+                            new RemoveTwilightEffect(2)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
