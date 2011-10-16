5cf0bde053e82a30ca1225b58c9e3fa84c512b98
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 20:21:05 +0000

    "The Riddermark"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_328.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Type: Site
+ * Site: 1T
+ * Game Text: Plains. Fellowship: Exert 2 unbound companions to play Aragorn from your draw deck.
+ */
+public class Card4_328 extends AbstractSite {
+    public Card4_328() {
+        super("The Riddermark", Block.TWO_TOWERS, 1, 0, Direction.LEFT);
+        addKeyword(Keyword.PLAINS);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && PlayConditions.canExertMultiple(self, game.getGameState(), game.getModifiersQuerying(), 1, 2, Filters.unboundCompanion)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Filters.unboundCompanion));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Filters.name("Aragorn")));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
