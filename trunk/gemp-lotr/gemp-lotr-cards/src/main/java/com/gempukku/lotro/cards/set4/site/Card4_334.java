ddbaf3b1372c5b1f4510a5ff5cbd972fdeb16636
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 20:33:50 +0000

    "Rohirrim Village"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_334.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Twilight Cost: 3
+ * Type: Site
+ * Site: 2T
+ * Game Text: Each [DUNLAND] Man may not take wounds during the archery phase.
+ */
+public class Card4_334 extends AbstractSite {
+    public Card4_334() {
+        super("Rohirrim Village", Block.TWO_TOWERS, 2, 3, Direction.LEFT);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new CantTakeWoundsModifier(self,
+                        new Condition() {
+                            @Override
+                            public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                                return gameState.getCurrentPhase() == Phase.ARCHERY;
+                            }
+                        }, Filters.and(Culture.DUNLAND, Race.MAN)));
+    }
+}
