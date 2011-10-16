17593927d370dec6e3ad61a0d1d60fdb06ed5fd2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-16 20:47:35 +0000

    "Stables"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/site/Card4_339.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set4.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.CheckLimitEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Two Towers
+ * Twilight Cost: 0
+ * Type: Site
+ * Site: 3T
+ * Game Text: Sanctuary. Fellowship: Exert a [ROHAN] Man to play a [ROHAN] mount from your draw deck (limit once
+ * per turn).
+ */
+public class Card4_339 extends AbstractSite {
+    public Card4_339() {
+        super("Stables", Block.TWO_TOWERS, 3, 0, Direction.RIGHT);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && PlayConditions.canExert(self, game, Culture.ROHAN, Race.MAN)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Culture.ROHAN, Race.MAN));
+            action.appendEffect(
+                    new CheckLimitEffect(action, self, 1, Phase.FELLOWSHIP,
+                            new ChooseAndPlayCardFromDeckEffect(playerId, Culture.ROHAN, PossessionClass.MOUNT)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
