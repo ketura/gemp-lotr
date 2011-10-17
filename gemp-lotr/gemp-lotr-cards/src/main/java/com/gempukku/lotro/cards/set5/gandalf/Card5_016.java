2bc3b2fb4522634a3da408bb4b40552001942bfe
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 12:58:25 +0000

    "Down From The Hills"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gandalf/Card5_016.java
@@ -0,0 +1,63 @@
+package com.gempukku.lotro.cards.set5.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 5
+ * Type: Event
+ * Game Text: Spell. Maneuver: Exert Gandalf 3 times to make an unbound companion with the Gandalf signet strength +5,
+ * defender +1, damage +2, and unable to take wounds until the regroup phase.
+ */
+public class Card5_016 extends AbstractEvent {
+    public Card5_016() {
+        super(Side.FREE_PEOPLE, 5, Culture.GANDALF, "Down From The Hills", Phase.MANEUVER);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game, 3, Filters.name("Gandalf"));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, 3, Filters.name("Gandalf")));
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose unbound companion", Filters.unboundCompanion, Signet.GANDALF) {
+                    @Override
+                    protected void cardSelected(LotroGame game, PhysicalCard card) {
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new StrengthModifier(self, Filters.sameCard(card), 5), Phase.REGROUP));
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new KeywordModifier(self, Filters.sameCard(card), Keyword.DEFENDER, 1), Phase.REGROUP));
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new KeywordModifier(self, Filters.sameCard(card), Keyword.DAMAGE, 2), Phase.REGROUP));
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new CantTakeWoundsModifier(self, Filters.sameCard(card)), Phase.REGROUP));
+                    }
+                });
+        return action;
+    }
+}
