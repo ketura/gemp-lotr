5961911bd683b6b383146e1d54c9ee3f26a71734
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 10:34:13 +0000

    "Wild Men of the Hills"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/dunland/Card5_003.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set5.dunland;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.UnrespondableEffect;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Dunland
+ * Twilight Cost: 1
+ * Type: Event
+ * Game Text: Shadow: Spot 3 [DUNLAND] minions to shuffle your hand into your draw deck and draw 8 cards.
+ * The Free Peoples player may discard 3 cards from hand to prevent this.
+ */
+public class Card5_003 extends AbstractEvent {
+    public Card5_003() {
+        super(Side.SHADOW, 1, Culture.DUNLAND, "Leaping Blaze", Phase.SHADOW);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, 3, Culture.DUNLAND, CardType.MINION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new PreventableEffect(action,
+                        new UnrespondableEffect() {
+                            @Override
+                            protected void doPlayEffect(LotroGame game) {
+                                game.getGameState().shuffleCardsIntoDeck(game.getGameState().getHand(playerId), playerId);
+                                action.appendEffect(
+                                        new DrawCardEffect(playerId, 8));
+                            }
+
+                            @Override
+                            public String getText(LotroGame game) {
+                                return "Shuffle hand into draw deck and draw 8 cards";
+                            }
+                        }, game.getGameState().getCurrentPlayerId(),
+                        new ChooseAndDiscardCardsFromHandEffect(action, game.getGameState().getCurrentPlayerId(), false, 3)));
+        return action;
+    }
+}
