27d709556aafd5cf4933ca4a8b3a947dc7155759
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 22:39:28 +0000

    "Wolves of Isengard"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/isengard/Card5_069.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set5.isengard;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 2
+ * Type: Condition
+ * Game Text: Plays on a plains site you control. Regroup: Exert an [ISENGARD] Orc to make the Free Peoples player
+ * wound a companion.
+ */
+public class Card5_069 extends AbstractAttachable {
+    public Card5_069() {
+        super(Side.SHADOW, CardType.CONDITION, 2, Culture.ISENGARD, null, "Wolves of Isengard");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Filters.siteControlled(playerId), Keyword.PLAINS);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.REGROUP, self, 0)
+                && PlayConditions.canExert(self, game, Culture.ISENGARD, Race.ORC)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Culture.ISENGARD, Race.ORC));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
