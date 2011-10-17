47786f6cde8e26b6e425a02596fd0e57f8af27af
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 22:45:11 +0000

    "Company of Haradrim"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/raider/Card5_071.java
@@ -0,0 +1,53 @@
+package com.gempukku.lotro.cards.set5.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.cards.modifiers.MoveLimitModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 6
+ * Type: Minion • Man
+ * Strength: 13
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Southron. Regroup: Remove (9) and spot antother Southron to make the move limit -1 for this turn ( to a
+ * minimum of 1 ).
+ */
+public class Card5_071 extends AbstractMinion {
+    public Card5_071() {
+        super(6, 13, 3, 4, Race.MAN, Culture.RAIDER, "Company of Haradrim");
+        addKeyword(Keyword.SOUTHRON);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.REGROUP, self, 9)
+                && PlayConditions.canSpot(game, Filters.not(self), Keyword.SOUTHRON)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveTwilightEffect(9));
+            action.appendEffect(
+                    new AddUntilEndOfTurnModifierEffect(
+                            new MoveLimitModifier(self, -1)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
