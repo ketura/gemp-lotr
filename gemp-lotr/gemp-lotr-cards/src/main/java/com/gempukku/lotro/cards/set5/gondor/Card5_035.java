9258b5fd3040281e6420b68cf6c522eb5b8feb3e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 14:34:22 +0000

    "Gondorian Knight"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gondor/Card5_035.java
@@ -0,0 +1,24 @@
+package com.gempukku.lotro.cards.set5.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Knight.
+ */
+public class Card5_035 extends AbstractCompanion {
+    public Card5_035() {
+        super(2, 6, 3, Culture.GONDOR, Race.MAN, null, "Gondorian Knight");
+        addKeyword(Keyword.KNIGHT);
+    }
+}
