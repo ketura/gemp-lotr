04a9175c812fd037ce9ccc07af914ffe707a05d7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 16:31:58 +0000

    "War Must Be"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gondor/Card5_043.java
@@ -0,0 +1,33 @@
+package com.gempukku.lotro.cards.set5.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CardMatchesEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Make a [GONDOR] Man strength +2 (or +4 if skirmishing a minion bearing a [GONDOR] fortification).
+ */
+public class Card5_043 extends AbstractEvent {
+    public Card5_043() {
+        super(Side.FREE_PEOPLE, 0, Culture.GONDOR, "War Must Be", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId,
+                        new CardMatchesEvaluator(2, 4, Filters.inSkirmishAgainst(CardType.MINION, Filters.hasAttached(Culture.GONDOR, Keyword.FORTIFICATION))), Culture.GONDOR, Race.MAN));
+        return action;
+    }
+}
