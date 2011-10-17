f39059fad3ef95bb68eaee9a3c5bc007fb4b52f3
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 12:39:58 +0000

    "That Is No Orc Horn"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/elven/Card5_014.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set5.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.LiberateASiteEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 2
+ * Type: Event
+ * Game Text: Regroup: Spot 3 Elf companions to heal a companion and liberate a site.
+ */
+public class Card5_014 extends AbstractEvent {
+    public Card5_014() {
+        super(Side.FREE_PEOPLE, 2, Culture.ELVEN, "That Is No Orc Horn", Phase.REGROUP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, 3, Race.ELF, CardType.COMPANION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        List<Effect> possibleEffects = new LinkedList<Effect>();
+        possibleEffects.add(
+                new ChooseAndHealCharactersEffect(action, playerId, 1, 1, CardType.COMPANION));
+        possibleEffects.add(
+                new LiberateASiteEffect(self));
+
+        action.appendEffect(
+                new ChoiceEffect(action, playerId, possibleEffects));
+        return action;
+    }
+}
