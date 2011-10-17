e329e766d8ae89e7f81272aff613bc179b3923c2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 22:42:33 +0000

    "Army of Haradrim"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/raider/Card5_070.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set5.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 7
+ * Type: Minion • Man
+ * Strength: 14
+ * Vitality: 4
+ * Site: 4
+ * Game Text: Southron. Skirmish: Remove (3) to play a [RAIDER] mount from your discard pile.
+ */
+public class Card5_070 extends AbstractMinion {
+    public Card5_070() {
+        super(7, 14, 4, 4, Race.MAN, Culture.RAIDER, "Army of Haradrim", true);
+        addKeyword(Keyword.SOUTHRON);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 3)
+                && PlayConditions.canPlayFromDiscard(playerId, game, 3, Culture.RAIDER, PossessionClass.MOUNT)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveTwilightEffect(3));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Culture.RAIDER, PossessionClass.MOUNT));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
