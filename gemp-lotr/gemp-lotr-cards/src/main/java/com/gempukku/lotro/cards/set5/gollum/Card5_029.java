7c3498fdcfa2ef757e14ad5eb3202cd5c00e96bb
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 13:55:52 +0000

    "Smeagol"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gollum/Card5_029.java
@@ -0,0 +1,65 @@
+package com.gempukku.lotro.cards.set5.gollum;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayPermanentAction;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Signet;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Gollum
+ * Twilight Cost: 0
+ * Type: Companion
+ * Strength: 3
+ * Vitality: 4
+ * Resistance: 6
+ * Signet: Frodo
+ * Game Text: Ring-bound. To play, add a burden. Skirmish: Add a burden to make Smeagol strength +2 and takes no wound.
+ */
+public class Card5_029 extends AbstractCompanion {
+    public Card5_029() {
+        super(0, 3, 4, Culture.GOLLUM, null, Signet.FRODO, "Smeagol", true);
+        addKeyword(Keyword.RING_BOUND);
+    }
+
+    @Override
+    public PlayPermanentAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        final PlayPermanentAction playCardAction = super.getPlayCardAction(playerId, game, self, twilightModifier);
+        playCardAction.appendCost(
+                new AddBurdenEffect(self, 1));
+        return playCardAction;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new AddBurdenEffect(self, 1));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new StrengthModifier(self, Filters.sameCard(self), 2), Phase.SKIRMISH));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new CantTakeWoundsModifier(self, Filters.sameCard(self)), Phase.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
