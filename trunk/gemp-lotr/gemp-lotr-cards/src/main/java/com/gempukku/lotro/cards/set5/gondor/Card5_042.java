3ea2e93776088592314872087198a44372483aa6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-17 16:29:35 +0000

    "Turgon"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gondor/Card5_042.java
@@ -0,0 +1,53 @@
+package com.gempukku.lotro.cards.set5.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Knight. To play, spot a knight. Maneuver: Exert Turgon to wound a minion bearing a [GONDOR] fortification.
+ */
+public class Card5_042 extends AbstractCompanion {
+    public Card5_042() {
+        super(2, 6, 3, Culture.GONDOR, Race.MAN, null, "Turgon", true);
+        addKeyword(Keyword.KNIGHT);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, Keyword.KNIGHT);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.MANEUVER, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, CardType.MINION, Filters.hasAttached(Culture.GONDOR, Keyword.FORTIFICATION)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
