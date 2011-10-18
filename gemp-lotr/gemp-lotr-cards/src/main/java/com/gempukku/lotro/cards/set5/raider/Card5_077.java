cea142c6b273971559f1c5aec6080509c29110b1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-18 22:06:39 +0000

    "Strength in Numbers"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/raider/Card5_077.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set5.raider;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ForEachYouSpotEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 0
+ * Type: Condition
+ * Game Text: To play, spot a Southron. Plays to your support area. Maneuver: Discard this condition to add (1) for
+ * each Man with ambush you spot.
+ */
+public class Card5_077 extends AbstractPermanent {
+    public Card5_077() {
+        super(Side.SHADOW, 0, CardType.CONDITION, Culture.RAIDER, Zone.SUPPORT, "Strength in Numbers");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, Keyword.SOUTHRON);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 0)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ForEachYouSpotEffect(playerId, Keyword.AMBUSH) {
+                        @Override
+                        protected void spottedCards(int spotCount) {
+                            action.insertEffect(
+                                    new AddTwilightEffect(self, spotCount));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
