77c9c68c5400429ceb0c14b919c48b0d69acb171
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-18 20:26:05 +0000

    "Mumak"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/raider/Card5_073.java
@@ -0,0 +1,53 @@
+package com.gempukku.lotro.cards.set5.raider;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 2
+ * Type: Possession • Mount
+ * Strength: +3
+ * Game Text: Bearer must be a Southron. Bearer is fierce. Each time bearer wins a skirmish, you may add (2).
+ */
+public class Card5_073 extends AbstractAttachable {
+    public Card5_073() {
+        super(Side.SHADOW, CardType.POSSESSION, 2, Culture.RAIDER, PossessionClass.MOUNT, "Mumak");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Keyword.SOUTHRON;
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.hasAttached(self), Keyword.FIERCE));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(effectResult, self.getAttachedTo())) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new AddTwilightEffect(self, 2));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
