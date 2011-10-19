b9ad3ec580d1de10b6249a88ea9344d543d5ce46
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-19 14:49:12 +0000

    "Orc Infantry"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/rohan/Card5_106.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set5.rohan;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.TakeControlOfASiteEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 1
+ * Type: Minion • Orc
+ * Strength: 5
+ * Vitality: 2
+ * Site: 6
+ * Game Text: Regroup: Spot a [SAURON] condition and discard this minion to take control of a site.
+ */
+public class Card5_106 extends AbstractMinion {
+    public Card5_106() {
+        super(1, 5, 2, 6, Race.ORC, Culture.SAURON, "Orc Infantry");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.REGROUP, self, 0)
+                && PlayConditions.canSpot(game, Culture.SAURON, CardType.CONDITION)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new TakeControlOfASiteEffect(self, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
