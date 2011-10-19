d1901106fe8ca4cec1446b81100a03f9b20e277c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-19 15:23:08 +0000

    "Sting"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/shire/Card5_116.java
@@ -0,0 +1,66 @@
+package com.gempukku.lotro.cards.set5.shire;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.PossessionClass;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be Frodo. Skirmish: Exert Frodo to make Smeagol strength +2 or Gollum strength -2.
+ */
+public class Card5_116 extends AbstractAttachableFPPossession {
+    public Card5_116() {
+        super(1, 2, 0, Culture.SHIRE, PossessionClass.HAND_WEAPON, "Sting", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("Frodo");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && PlayConditions.canExert(self, game, Filters.name("Frodo"))) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.name("Frodo")));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose Smeagol or Gollum", Filters.or(Filters.name("Smeagol"), Filters.name("Gollum"))) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            if (card.getBlueprint().getName().equals("Smeagol"))
+                                action.insertEffect(
+                                        new AddUntilEndOfPhaseModifierEffect(
+                                                new StrengthModifier(self, Filters.sameCard(card), 2), Phase.SKIRMISH));
+                            else
+                                action.insertEffect(
+                                        new AddUntilEndOfPhaseModifierEffect(
+                                                new StrengthModifier(self, Filters.sameCard(card), -2), Phase.SKIRMISH));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
