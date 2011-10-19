37179c52f9afbad42e8fa1e965109821f2f5d57c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-19 15:26:06 +0000

    "You Must Help Us"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/shire/Card5_117.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set5.shire;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Fellowship: Play Smeagol from your discard pile to heal each Ring-bound Hobbit twice.
+ */
+public class Card5_117 extends AbstractEvent {
+    public Card5_117() {
+        super(Side.FREE_PEOPLE, 0, Culture.SHIRE, "You Must Help Us", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Filters.name("Smeagol"));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Filters.name("Smeagol")));
+        action.appendEffect(
+                new HealCharactersEffect(playerId, Filters.and(Race.HOBBIT, Keyword.RING_BOUND)));
+        action.appendEffect(
+                new HealCharactersEffect(playerId, Filters.and(Race.HOBBIT, Keyword.RING_BOUND)));
+        return action;
+    }
+}
