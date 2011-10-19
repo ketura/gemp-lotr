a7ec9595227f7873e44d081193bf033a72749558
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-19 13:21:17 +0000

    "Dead Marshes"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/rohan/Card5_095.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set5.rohan;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Battle of Helm's Deep
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 0
+ * Type: Condition
+ * Game Text: Plays to your support area. While the fellowship is at site 4T, Ring-bound companions skirmishing [SAURON]
+ * orcs are strength -1. Maneuver: Spot 2 [SAURON] Orcs to play up to 2 [SAURON] conditions from your discard pile.
+ * Discard this condition.
+ */
+public class Card5_095 extends AbstractPermanent {
+    public Card5_095() {
+        super(Side.SHADOW, 0, CardType.CONDITION, Culture.SAURON, Zone.SUPPORT, "Dead Marshes", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self,
+                        Filters.and(CardType.COMPANION, Keyword.RING_BOUND, Filters.inSkirmishAgainst(Culture.SAURON, Race.ORC)),
+                        new LocationCondition(Filters.siteNumber(4), Filters.siteBlock(Block.TWO_TOWERS)), -1));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 0)
+                && PlayConditions.canSpot(game, 2, Culture.SAURON, Race.ORC)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Culture.SAURON, CardType.CONDITION));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Culture.SAURON, CardType.CONDITION));
+            action.appendEffect(
+                    new DiscardCardsFromPlayEffect(self, self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
