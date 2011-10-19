84d58b0ec1bd19e230f1df53032af250d9f3cb65
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-19 21:34:15 +0000

    "Dunlending Elder"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/StrengthModifier.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/StrengthModifier.java
@@ -1,6 +1,6 @@
 package com.gempukku.lotro.cards.modifiers;
 
-import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
@@ -13,15 +13,15 @@ import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 public class StrengthModifier extends AbstractModifier {
     private Evaluator _evaluator;
 
-    public StrengthModifier(PhysicalCard source, Filter affectFilter, int modifier) {
+    public StrengthModifier(PhysicalCard source, Filterable affectFilter, int modifier) {
         this(source, affectFilter, null, modifier);
     }
 
-    public StrengthModifier(PhysicalCard source, Filter affectFilter, Condition condition, int modifier) {
+    public StrengthModifier(PhysicalCard source, Filterable affectFilter, Condition condition, int modifier) {
         this(source, affectFilter, condition, new ConstantEvaluator(modifier));
     }
 
-    public StrengthModifier(PhysicalCard source, Filter affectFilter, Condition condition, Evaluator evaluator) {
+    public StrengthModifier(PhysicalCard source, Filterable affectFilter, Condition condition, Evaluator evaluator) {
         super(source, null, affectFilter, condition, new ModifierEffect[]{ModifierEffect.STRENGTH_MODIFIER});
         _evaluator = evaluator;
     }
