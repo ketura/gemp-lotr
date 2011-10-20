5f0a709e9b52aa5172bea6760b5f24ed4af11e36
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-20 19:14:46 +0000

    "Master Broke His Promise"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/gollum/Card6_041.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set6.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.MoveLimitModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 5
+ * Type: Event
+ * Game Text: Regroup: Exert Gollum 3 times to wound each companion. The move limit for this turn is -1.
+ */
+public class Card6_041 extends AbstractEvent {
+    public Card6_041() {
+        super(Side.SHADOW, 5, Culture.GOLLUM, "Master Broke His Promise", Phase.REGROUP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canExert(self, game, 3, Filters.name("Gollum"));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, 3, Filters.name("Gollum")));
+        action.appendEffect(
+                new WoundCharactersEffect(self, CardType.COMPANION));
+        action.appendEffect(
+                new AddUntilEndOfTurnModifierEffect(
+                        new MoveLimitModifier(self, -1)));
+        return action;
+    }
+}
