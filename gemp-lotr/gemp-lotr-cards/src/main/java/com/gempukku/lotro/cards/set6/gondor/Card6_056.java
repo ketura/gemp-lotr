039667d4906b7b2bd231261e11677fe28ad08303
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-20 20:48:15 +0000

    "Trust"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/gondor/Card6_056.java
@@ -0,0 +1,35 @@
+package com.gempukku.lotro.cards.set6.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Skirmish: Make a [GONDOR] Man strength +2 (and heal a companion if you spot an [ELVEN] token).
+ */
+public class Card6_056 extends AbstractEvent {
+    public Card6_056() {
+        super(Side.FREE_PEOPLE, 0, Culture.GONDOR, "Trust", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 2, Culture.GONDOR, Race.MAN));
+        if (Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), Filters.hasToken(Token.ELVEN)))
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, CardType.COMPANION));
+        return action;
+    }
+}
