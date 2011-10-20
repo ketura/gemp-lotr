b57f1561eb3b60c7af48917d450040cf2dba00b0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-20 16:15:00 +0000

    "Glamdring"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/gandalf/Card6_031.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set6.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be Gandalf. He is damage +1. Each time you play a spell during a skirmish, you may make
+ * Gandalf damage +1 until the end of that skirmish.
+ */
+public class Card6_031 extends AbstractAttachableFPPossession {
+    public Card6_031() {
+        super(2, 2, 0, Culture.GANDALF, PossessionClass.HAND_WEAPON, "Glamdring", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("Gandalf");
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.hasAttached(self), Keyword.DAMAGE, 1));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game, effectResult, Filters.owner(playerId), Keyword.SPELL)
+                && game.getGameState().getCurrentPhase() == Phase.SKIRMISH) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new KeywordModifier(self, self.getAttachedTo(), Keyword.DAMAGE), Phase.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
