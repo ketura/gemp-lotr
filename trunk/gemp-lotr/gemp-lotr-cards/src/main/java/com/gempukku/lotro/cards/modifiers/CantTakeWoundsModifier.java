2f5d17991e6401ef1d6730e1622205205b2cd396
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-20 21:34:10 +0000

    "Berserk Butcher"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/CantTakeWoundsModifier.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/CantTakeWoundsModifier.java
@@ -1,6 +1,6 @@
 package com.gempukku.lotro.cards.modifiers;
 
-import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
@@ -9,11 +9,11 @@ import com.gempukku.lotro.logic.modifiers.ModifierEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 public class CantTakeWoundsModifier extends AbstractModifier {
-    public CantTakeWoundsModifier(PhysicalCard source, Filter affectFilter) {
+    public CantTakeWoundsModifier(PhysicalCard source, Filterable affectFilter) {
         super(source, "Can't take wounds", affectFilter, new ModifierEffect[]{ModifierEffect.WOUND_MODIFIER});
     }
 
-    public CantTakeWoundsModifier(PhysicalCard source, Condition condition, Filter affectFilter) {
+    public CantTakeWoundsModifier(PhysicalCard source, Condition condition, Filterable affectFilter) {
         super(source, "Can't take wounds", affectFilter, condition, new ModifierEffect[]{ModifierEffect.WOUND_MODIFIER});
     }
 
---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/isengard/Card6_060.java
@@ -0,0 +1,78 @@
+package com.gempukku.lotro.cards.set6.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveTokensFromCardEffect;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 6
+ * Type: Minion • Uruk-Hai
+ * Strength: 12
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Damage +1. Berserk Butcher is strength +1 for each wound on each character in its skirmish.
+ * Maneuver: Remove an [ISENGARD] token from a machine and exert Berserk Butcher twice to prevent all wounds
+ * to Uruk-hai until the assignment phase.
+ */
+public class Card6_060 extends AbstractMinion {
+    public Card6_060() {
+        super(6, 12, 3, 5, Race.URUK_HAI, Culture.ISENGARD, "Berserk Butcher", true);
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.and(Filters.sameCard(self), Filters.inSkirmish),
+                        null,
+                        new Evaluator() {
+                            @Override
+                            public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+                                int wounds = 0;
+                                for (PhysicalCard physicalCard : Filters.filterActive(gameState, modifiersQuerying, Filters.inSkirmish))
+                                    wounds += gameState.getWounds(physicalCard);
+                                return wounds;
+                            }
+                        }));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 0)
+                && PlayConditions.canRemoveTokens(game, Token.ISENGARD, 1, Keyword.MACHINE)
+                && PlayConditions.canExert(self, game, 2, self)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndRemoveTokensFromCardEffect(self, playerId, Token.ISENGARD, 1, Keyword.MACHINE));
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new CantTakeWoundsModifier(self, Race.URUK_HAI), Phase.ASSIGNMENT));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
