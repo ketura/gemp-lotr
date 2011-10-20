9ab17a8b70dd51ee9bf7dc089beee720d1d92459
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-20 19:21:13 +0000

    "Not Listening"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/gollum/Card6_043.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set6.gollum;
+
+import com.gempukku.lotro.cards.AbstractResponseEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Free
+ * Culture: Gollum
+ * Twilight Cost: 0
+ * Type: Event
+ * Game Text: Response: If Smeagol wins a skirmish, heal him and remove 2 burdens.
+ */
+public class Card6_043 extends AbstractResponseEvent {
+    public Card6_043() {
+        super(Side.FREE_PEOPLE, 0, Culture.GOLLUM, "Not Listening");
+    }
+
+    @Override
+    public List<PlayEventAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(game, effectResult, Filters.name("Smeagol"))) {
+            PlayEventAction action = new PlayEventAction(self);
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, Filters.name("Smeagol")));
+            action.appendEffect(
+                    new RemoveBurdenEffect(self));
+            action.appendEffect(
+                    new RemoveBurdenEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
