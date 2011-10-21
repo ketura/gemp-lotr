94797cc77ac0bf05611a5a35c09581b2f757522e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-21 11:20:00 +0000

    "Isengard Builder"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/isengard/Card6_066.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set6.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.effects.DiscardStackedCardsEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 7
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Regroup: If stacked on an [ISENGARD] card, discard this card to add (2).
+ */
+public class Card6_066 extends AbstractMinion {
+    public Card6_066() {
+        super(3, 7, 3, 4, Race.ORC, Culture.ISENGARD, "Isengard Builder");
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActionsFromStacked(String playerId, LotroGame game, PhysicalCard self) {
+        if (self.getStackedOn().getBlueprint().getCulture() == Culture.ISENGARD) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardStackedCardsEffect(self, Collections.singleton(self)));
+            action.appendEffect(
+                    new AddTwilightEffect(self, 2));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
