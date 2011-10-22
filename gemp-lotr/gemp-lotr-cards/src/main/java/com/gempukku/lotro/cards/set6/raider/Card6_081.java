123015fb42e1de730e7e4aa1619937f5f3976602
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 16:39:04 +0000

    "Southron Invaders"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/raider/Card6_081.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set6.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Southron. Ambush (1). When you play this minion, you may spot another Southron to play a minion with
+ * ambush from your discard pile.
+ */
+public class Card6_081 extends AbstractMinion {
+    public Card6_081() {
+        super(5, 10, 3, 4, Race.MAN, Culture.RAIDER, "Southron Invaders");
+        addKeyword(Keyword.SOUTHRON);
+        addKeyword(Keyword.AMBUSH, 1);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.played(game, effectResult, self)
+                && PlayConditions.canSpot(game, Filters.not(self), Keyword.SOUTHRON)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Keyword.AMBUSH)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Keyword.AMBUSH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
