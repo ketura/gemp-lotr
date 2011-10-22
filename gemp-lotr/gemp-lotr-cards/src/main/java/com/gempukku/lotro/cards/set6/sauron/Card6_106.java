d51f1a4fa8a8d4fc9d75945ec38bd88ba07e5be8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 20:09:50 +0000

    "Troll of Udun"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/sauron/Card6_106.java
@@ -0,0 +1,62 @@
+package com.gempukku.lotro.cards.set6.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.cards.modifiers.StrengthModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 10
+ * Type: Minion • Troll
+ * Strength: 14
+ * Vitality: 5
+ * Site: 6
+ * Game Text: Fierce. While you can spot a Troll, this minion's twilight cost is -4. Skirmish: Discard a [SAURON] card
+ * from hand to make this minion strength +1.
+ */
+public class Card6_106 extends AbstractMinion {
+    public Card6_106() {
+        super(10, 14, 5, 6, Race.TROLL, Culture.SAURON, "Troll of Udun");
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        if (Filters.canSpot(gameState, modifiersQuerying, Race.TROLL))
+            return -4;
+        return 0;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, Culture.SAURON)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, Culture.SAURON));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new StrengthModifier(self, self, 1), Phase.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
