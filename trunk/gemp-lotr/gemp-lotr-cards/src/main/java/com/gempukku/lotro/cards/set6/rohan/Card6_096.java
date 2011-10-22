8b83c45ccb12e8b9fe0b5562f2118180a15b8ced
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 17:43:20 +0000

    "News From the Mark"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/rohan/Card6_096.java
@@ -0,0 +1,60 @@
+package com.gempukku.lotro.cards.set6.rohan;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.lotro.cards.modifiers.MoveLimitModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.decisions.MultipleChoiceAwaitingDecision;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.effects.PlayoutDecisionEffect;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 2
+ * Type: Event
+ * Game Text: Fellowship: Spot 3 mounted [ROHAN] Men to make the move limit for this turn +2. Each Shadow player may
+ * draw 6 cards.
+ */
+public class Card6_096 extends AbstractEvent {
+    public Card6_096() {
+        super(Side.FREE_PEOPLE, 2, Culture.ROHAN, "News From the Mark", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, 3, Culture.ROHAN, Race.MAN, Filters.mounted);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new AddUntilEndOfTurnModifierEffect(
+                        new MoveLimitModifier(self, 2)));
+        for (final String opponentId : GameUtils.getOpponents(game, playerId)) {
+            action.appendEffect(
+                    new PlayoutDecisionEffect(game.getUserFeedback(), opponentId,
+                            new MultipleChoiceAwaitingDecision(1, "Do you want to draw 6 cards?", new String[]{"Yes", "No"}) {
+                                @Override
+                                protected void validDecisionMade(int index, String result) {
+                                    if (index == 0)
+                                        action.insertEffect(
+                                                new DrawCardEffect(opponentId, 6));
+                                }
+                            }));
+        }
+        return action;
+    }
+}
