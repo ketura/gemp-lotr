cecc2b4408881c6a8f1aa8d161fb35f5bc5b7de2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 18:47:36 +0000

    "Gate Sentry"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/sauron/Card6_102.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set6.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 9
+ * Vitality: 3
+ * Site: 6
+ * Game Text: Maneuver: Exert this minion twice and spot another [SAURON] minion to discard a condition.
+ */
+public class Card6_102 extends AbstractMinion {
+    public Card6_102() {
+        super(3, 9, 3, 6, Race.ORC, Culture.SAURON, "Gate Sentry");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.MANEUVER, self, 0)
+                && PlayConditions.canSelfExert(self, 2, game)
+                && PlayConditions.canSpot(game, Culture.SAURON, CardType.MINION, Filters.not(self))) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.MINION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
