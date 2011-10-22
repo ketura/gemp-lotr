73009632eee0c7add37b241889583f9f09343a19
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 20:48:52 +0000

    "Rocks of Emyn Muil"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/site/Card6_115.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set6.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Signet;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Type: Site
+ * Site: 1T
+ * Game Text: Fellowship: Play a companion with the Frodo signet to remove a burden.
+ */
+public class Card6_115 extends AbstractSite {
+    public Card6_115() {
+        super("Rocks of Emyn Muil", Block.TWO_TOWERS, 1, 0, Direction.LEFT);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && PlayConditions.canPlayFromHand(playerId, game, CardType.COMPANION, Signet.FRODO)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game.getGameState().getHand(playerId), CardType.COMPANION, Signet.FRODO));
+            action.appendEffect(
+                    new RemoveBurdenEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
