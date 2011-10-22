3561e02230ccf362a5b664fff34b7a48bcc1564e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 20:43:43 +0000

    "Merry"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/shire/Card6_113.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set6.shire;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Companion • Hobbit
+ * Strength: 3
+ * Vitality: 4
+ * Resistance: 6
+ * Signet: Aragorn
+ * Game Text: Regroup: Discard Merry to heal a [GANDALF] companion.
+ */
+public class Card6_113 extends AbstractCompanion {
+    public Card6_113() {
+        super(1, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.ARAGORN, "Merry", true);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.REGROUP, self)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, Culture.GANDALF, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
