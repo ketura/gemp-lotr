28de90133b7c8e7222750f7de0b760df1c7766ae
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 21:10:26 +0000

    "Valley of Saruman"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/site/Card6_119.java
@@ -0,0 +1,38 @@
+package com.gempukku.lotro.cards.set6.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Twilight Cost: 8
+ * Type: Site
+ * Site: 8T
+ * Game Text: When the fellowship moves to this site, discard all allies.
+ */
+public class Card6_119 extends AbstractSite {
+    public Card6_119() {
+        super("Valley of Saruman", Block.TWO_TOWERS, 8, 8, Direction.RIGHT);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.movesTo(game, effectResult, self)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new DiscardCardsFromPlayEffect(self, CardType.ALLY));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
