0fb9f41d85443727c1aeacbfcca25f2513f39bcd
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-22 17:54:24 +0000

    "Corpse Lights"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/sauron/Card6_099.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set6.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ents of Fangorn
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 3
+ * Type: Minion • Wraith
+ * Strength: 7
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Twilight. Skirmish: Exert this minion to make a [SAURON] or twilight minion strength +1 for each twilight
+ * minion you can spot.
+ */
+public class Card6_099 extends AbstractMinion {
+    public Card6_099() {
+        super(3, 7, 2, 4, Race.WRAITH, Culture.SAURON, "Corpse Lights");
+        addKeyword(Keyword.TWILIGHT);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId,
+                            new CountSpottableEvaluator(CardType.MINION, Keyword.TWILIGHT),
+                            CardType.MINION, Filters.or(Culture.SAURON, Keyword.TWILIGHT)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
--- trunk/gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Race.java
+++ trunk/gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Race.java
@@ -1,7 +1,7 @@
 package com.gempukku.lotro.common;
 
 public enum Race implements Filterable {
-    BALROG("Balrog"), CREATURE("Creature"),
+    BALROG("Balrog"), CREATURE("Creature"), WRAITH("Wraith"),
     ELF("Elf"), HOBBIT("Hobbit"), DWARF("Dwarf"), MAN("Man"), WIZARD("Wizard"),
     URUK_HAI("Uruk-Hai"), NAZGUL("Nazgul"), ORC("Orc"), TROLL("Troll"), ENT("Ent");
 
