824af9e421c623d77ffdeb0d4deaea31f940eec1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-27 16:10:50 +0000

    "Arwen"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/dwarven/Card7_016.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set7.dwarven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Signet;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 2
+ * Type: Companion • Elf
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Signet: Gandalf
+ * Game Text: While skirmishing a fierce minion, Arwen is strength +3.
+ */
+public class Card7_016 extends AbstractCompanion {
+    public Card7_016() {
+        super(2, 6, 3, Culture.ELVEN, Race.ELF, Signet.GANDALF, "Arwen", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.and(self, Filters.inSkirmishAgainst(Keyword.FIERCE)), 3));
+    }
+}
