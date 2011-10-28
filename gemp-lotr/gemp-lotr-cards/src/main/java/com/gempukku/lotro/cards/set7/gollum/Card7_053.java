f4672f0d9ef8c78c329dff3dbc6fe6adfa4ecfc6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-28 16:11:56 +0000

    "Captured by the Ring"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gollum/Card7_053.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set7.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 0
+ * Type: Event • Shadow
+ * Game Text: Play Gollum from your draw deck or discard pile to add a threat.
+ */
+public class Card7_053 extends AbstractEvent {
+    public Card7_053() {
+        super(Side.SHADOW, 0, Culture.GOLLUM, "Captured by the Ring", Phase.SHADOW);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        PlayEventAction action = new PlayEventAction(self);
+        List<Effect> possibleCosts = new LinkedList<Effect>();
+        possibleCosts.add(
+                new ChooseAndPlayCardFromDeckEffect(playerId, Filters.name("Gollum")) {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return "Play Gollum from your deck";
+                    }
+                });
+        possibleCosts.add(
+                new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Filters.name("Gollum")) {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return "Play Gollum from your discard pile";
+                    }
+                });
+        action.appendCost(
+                new ChoiceEffect(action, playerId, possibleCosts));
+        action.appendEffect(
+                new AddThreatsEffect(playerId, self, 1));
+        return action;
+    }
+}
