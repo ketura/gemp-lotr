c6a37531d6ead4b39b069efc50d44bc0a259f961
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-28 11:05:51 +0000

    "Glamdring"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gandalf/Card7_039.java
@@ -0,0 +1,76 @@
+package com.gempukku.lotro.cards.set7.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseOpponentEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be Gandalf. He is damage +1. Skirmish: If you have more cards in hand than each opponent,
+ * discard 2 cards from hand to make an opponent exert a minion.
+ */
+public class Card7_039 extends AbstractAttachableFPPossession {
+    public Card7_039() {
+        super(2, 2, 0, Culture.GANDALF, PossessionClass.HAND_WEAPON, "Glamdring", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("Gandalf");
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.hasAttached(self), Keyword.DAMAGE, 1));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && hasMoreCardsThanEachOpponent(playerId, game)
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2));
+            action.appendEffect(
+                    new ChooseOpponentEffect(playerId) {
+                        @Override
+                        protected void opponentChosen(String opponentId) {
+                            action.insertEffect(
+                                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.MINION));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    private boolean hasMoreCardsThanEachOpponent(String playerId, LotroGame game) {
+        int cardsInHand = game.getGameState().getHand(playerId).size();
+        for (String opponentId : GameUtils.getOpponents(game, playerId)) {
+            if (game.getGameState().getHand(opponentId).size() >= cardsInHand)
+                return false;
+        }
+        return true;
+    }
+}
