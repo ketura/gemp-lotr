c59b718173307056d5c676ce6a79f8b39094caba
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-10-28 09:28:32 +0000

    "Elrond"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/elven/Card7_021.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set7.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 4
+ * Type: Companion • Elf
+ * Strength: 8
+ * Vitality: 4
+ * Resistance: 6
+ * Game Text: To play, spot Aragorn or an Elf. At the start of each turn, you may exert Elrond to heal a character
+ * bearing an artifact.
+ */
+public class Card7_021 extends AbstractCompanion {
+    public Card7_021() {
+        super(4, 8, 4, Culture.ELVEN, Race.ELF, null, "Elrond", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, Filters.or(Filters.name("Aragorn"), Race.ELF));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.START_OF_TURN
+                && PlayConditions.canSelfExert(self, game)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, Filters.hasAttached(CardType.ARTIFACT)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
