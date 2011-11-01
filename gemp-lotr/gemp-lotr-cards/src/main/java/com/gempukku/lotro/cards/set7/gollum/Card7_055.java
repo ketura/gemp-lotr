c6f5a93db5f008aec6740479bcab3afbb5d0c002
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-01 12:04:05 +0000

    "Days Growing Dark"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gollum/Card7_055.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set7.gollum;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Gollum
+ * Twilight Cost: 2
+ * Type: Condition • Support Area
+ * Game Text: Fellowship: Discard 3 cards from hand to play Smeagol from your discard pile.
+ */
+public class Card7_055 extends AbstractPermanent {
+    public Card7_055() {
+        super(Side.FREE_PEOPLE, 2, CardType.CONDITION, Culture.GOLLUM, Zone.SUPPORT, "Days Growing Dark");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.FELLOWSHIP, self)
+                && PlayConditions.canDiscardFromHand(game, playerId, 3, Filters.any)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Filters.name("Smeagol"))) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 3));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Filters.name("Smeagol")));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
