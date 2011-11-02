1288d16cba593191594c5582ce73111da5b6fd4d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 23:48:13 +0000

    "Targon"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gondor/Card7_124.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set7.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Ranger. Skirmish: If you have initiative, discard 2 cards from hand to wound a Man Targon is skirmishing.
+ */
+public class Card7_124 extends AbstractCompanion {
+    public Card7_124() {
+        super(2, 6, 3, Culture.GONDOR, Race.MAN, null, "Targon", true);
+        addKeyword(Keyword.RANGER);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && PlayConditions.hasInitiative(game, Side.FREE_PEOPLE)
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, Race.MAN, Filters.inSkirmishAgainst(self)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
