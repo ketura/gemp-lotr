20e3373270a0bc87a1c56b98f4144e5a6f7ee8c7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 22:11:49 +0000

    "Aegnor"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/elven/Card10_004.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/elven/Card10_004.java
@@ -2,13 +2,15 @@ package com.gempukku.lotro.cards.set10.elven;
 
 import com.gempukku.lotro.cards.AbstractCompanion;
 import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.NegativeEvaluator;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
-import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,11 +20,11 @@ import java.util.List;
  * Side: Free
  * Culture: Elven
  * Twilight Cost: 2
- * Type: Companion • Elf
+ * Type: Companion ï¿½ Elf
  * Strength: 6
  * Vitality: 3
  * Resistance: 6
- * Game Text: Skirmish: Exert Aegnor to make a minion skirmishing an unbound [ELVEN] companion strength -1 for each archer you spot. 
+ * Game Text: Skirmish: Exert Aegnor to make a minion skirmishing an unbound [ELVEN] companion strength -1 for each archer you spot.
  * Rarity: U
  */
 public class Card10_004 extends AbstractCompanion {
@@ -32,20 +34,17 @@ public class Card10_004 extends AbstractCompanion {
 
     @Override
     protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self) {
-            
-        	if (PlayConditions.canSelfExert(self, game)) {
-        		ActivateCardAction action = new ActivateCardAction(self);
-        		action.setText("Make an minion skirmishing an unbound Elf compaion strength -1 for each archer you spot");
-        		action.appendCost(
-        				new ExertCharactersEffect(self, self));
-        		action.appendEffect(
-        				int archerTotal = Filters.countSpottable(game.getGameState(), game.getModifiersQuerying(), Filters.keyword(Keyword.ARCHER));
-        				// TODO Need to make it so it targets minion skirmishing unbound elf and not just any minion, also does it spot minions?
-        				new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, -archerTotal, Filters.type(CardType.MINION)));
-        		actions.add(action);
-        	}
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.setText("Make an minion skirmishing an unbound Elf compaion strength -1 for each archer you spot");
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, new NegativeEvaluator(new CountSpottableEvaluator(Keyword.ARCHER)), CardType.MINION, Filters.inSkirmishAgainst(Culture.ELVEN, Filters.unboundCompanion)));
+            return Collections.singletonList(action);
         }
+
         return null;
     }
 }
