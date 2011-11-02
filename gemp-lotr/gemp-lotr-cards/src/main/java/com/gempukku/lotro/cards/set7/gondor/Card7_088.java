0ca2f81b85939a22486110ada86a42bca1991616
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 14:34:48 +0000

    "Dervorin"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gondor/Card7_088.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set7.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: At the start of the regroup phase, if you have only 4 cards in hand, you may spot another [GONDOR] Man
+ * to discard a minion.
+ */
+public class Card7_088 extends AbstractCompanion {
+    public Card7_088() {
+        super(2, 6, 3, Culture.GONDOR, Race.MAN, null, "Dervorin", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.startOfPhase(game, effectResult, Phase.REGROUP)
+                && game.getGameState().getHand(playerId).size() == 4
+                && PlayConditions.canSpot(game, Culture.GONDOR, Race.MAN, Filters.not(self))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.MINION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
