93b55f60e3d68bfd87178307df148510c195a55d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 14:14:24 +0000

    "City of Men"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gondor/Card7_083.java
@@ -0,0 +1,65 @@
+package com.gempukku.lotro.cards.set7.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseActionProxyEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.AbstractActionProxy;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Event • Skirmish
+ * Game Text: Make a [GONDOR] Man strength +2. If that Man is skirmishing a [SAURON] minion, discard that minion
+ * at the end of the skirmish.
+ */
+public class Card7_083 extends AbstractEvent {
+    public Card7_083() {
+        super(Side.FREE_PEOPLE, 1, Culture.GONDOR, "City of Men", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, final PhysicalCard self, int twilightModifier) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose a GONDOR Man", Culture.GONDOR, Race.MAN) {
+                    @Override
+                    protected void cardSelected(LotroGame game, final PhysicalCard card) {
+                        game.getModifiersEnvironment().addUntilEndOfPhaseModifier(
+                                new StrengthModifier(self, card, 2), Phase.SKIRMISH);
+                        if (Filters.inSkirmishAgainst(Culture.SAURON, CardType.MINION).accepts(game.getGameState(), game.getModifiersQuerying(), card)) {
+                            action.appendEffect(
+                                    new AddUntilEndOfPhaseActionProxyEffect(
+                                            new AbstractActionProxy() {
+                                                @Override
+                                                public List<? extends Action> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult) {
+                                                    if (PlayConditions.endOfPhase(game, effectResult, Phase.SKIRMISH)) {
+                                                        RequiredTriggerAction action = new RequiredTriggerAction(self);
+                                                        action.appendEffect(
+                                                                new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Culture.SAURON, CardType.MINION, Filters.inSkirmishAgainst(card)));
+                                                        return Collections.singletonList(action);
+                                                    }
+                                                    return null;
+                                                }
+                                            }, Phase.SKIRMISH));
+                        }
+                    }
+                });
+        return action;
+    }
+}
--- trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/turn/general/EndOfPhaseGameProcess.java
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/turn/general/EndOfPhaseGameProcess.java
@@ -2,11 +2,17 @@ package com.gempukku.lotro.logic.timing.processes.turn.general;
 
 import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.game.state.actions.DefaultActionsEnvironment;
+import com.gempukku.lotro.logic.actions.SystemQueueAction;
 import com.gempukku.lotro.logic.effects.TriggeringResultEffect;
+import com.gempukku.lotro.logic.modifiers.ModifiersLogic;
+import com.gempukku.lotro.logic.timing.AbstractSuccessfulEffect;
 import com.gempukku.lotro.logic.timing.Effect;
-import com.gempukku.lotro.logic.timing.actions.SimpleEffectAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
 import com.gempukku.lotro.logic.timing.processes.GameProcess;
 import com.gempukku.lotro.logic.timing.results.EndOfPhaseResult;
+
+import java.util.Collection;
 
 public class EndOfPhaseGameProcess implements GameProcess {
     private LotroGame _game;
@@ -21,7 +27,34 @@ public class EndOfPhaseGameProcess implements GameProcess {
 
     @Override
     public void process() {
-        _game.getActionsEnvironment().addActionToStack(new SimpleEffectAction(new TriggeringResultEffect(Effect.Type.END_OF_PHASE, new EndOfPhaseResult(_phase), "End of " + _phase + " phase"), "End of " + _phase + " phase"));
+        SystemQueueAction action = new SystemQueueAction() {
+            @Override
+            public String getText(LotroGame game) {
+                return "End of " + _phase + " phase";
+            }
+        };
+        action.appendEffect(
+                new TriggeringResultEffect(Effect.Type.END_OF_PHASE, new EndOfPhaseResult(_phase), "End of " + _phase + " phase"));
+        action.appendEffect(
+                new AbstractSuccessfulEffect() {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return null;
+                    }
+
+                    @Override
+                    public Type getType() {
+                        return null;
+                    }
+
+                    @Override
+                    public Collection<? extends EffectResult> playEffect(LotroGame game) {
+                        ((ModifiersLogic) game.getModifiersEnvironment()).removeEndOfPhase(_phase);
+                        ((DefaultActionsEnvironment) game.getActionsEnvironment()).removeEndOfPhaseActionProxies(_phase);
+                        return null;
+                    }
+                });
+        _game.getActionsEnvironment().addActionToStack(action);
     }
 
     @Override
