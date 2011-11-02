f9f21088e2fe8ca8d4feab0977a874d974e9aa38
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 16:04:06 +0000

    "Gondor Still Stands"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/choose/PutCardFromDiscardOnTopOfDeckEffect.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.effects.choose;
+
+import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.timing.AbstractEffect;
+
+import java.util.Collections;
+
+public class PutCardFromDiscardOnTopOfDeckEffect extends AbstractEffect {
+    private PhysicalCard _physicalCard;
+
+    public PutCardFromDiscardOnTopOfDeckEffect(PhysicalCard physicalCard) {
+        _physicalCard = physicalCard;
+    }
+
+    @Override
+    public boolean isPlayableInFull(LotroGame game) {
+        return _physicalCard.getZone() == Zone.DISCARD;
+    }
+
+    @Override
+    protected FullEffectResult playEffectReturningResult(LotroGame game) {
+        if (isPlayableInFull(game)) {
+            GameState gameState = game.getGameState();
+            gameState.sendMessage(_physicalCard.getOwner() + " puts " + GameUtils.getCardLink(_physicalCard) + " from discard on the top of deck");
+            gameState.removeCardsFromZone(_physicalCard.getOwner(), Collections.singleton(_physicalCard));
+            gameState.putCardOnTopOfDeck(_physicalCard);
+            return new FullEffectResult(null, true, true);
+        }
+        return new FullEffectResult(null, false, false);
+    }
+
+    @Override
+    public String getText(LotroGame game) {
+        return "Put " + GameUtils.getCardLink(_physicalCard) + " from discard on top of deck";
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+}
