b749cfec077c9e020b98c4b36b51e42896aa0ba5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 16:56:40 +0000

    Removing unused Effect types (start/end of phase and end of turn).

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/turn/EndOfTurnGameProcess.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/turn/EndOfTurnGameProcess.java
@@ -6,7 +6,6 @@ import com.gempukku.lotro.logic.actions.SystemQueueAction;
 import com.gempukku.lotro.logic.effects.TriggeringResultEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersLogic;
 import com.gempukku.lotro.logic.timing.AbstractSuccessfulEffect;
-import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.EffectResult;
 import com.gempukku.lotro.logic.timing.processes.GameProcess;
 import com.gempukku.lotro.logic.timing.results.EndOfTurnResult;
@@ -29,7 +28,7 @@ public class EndOfTurnGameProcess implements GameProcess {
             }
         };
         action.appendEffect(
-                new TriggeringResultEffect(Effect.Type.END_OF_TURN, new EndOfTurnResult(), "End of turn"));
+                new TriggeringResultEffect(null, new EndOfTurnResult(), "End of turn"));
         action.appendEffect(
                 new AbstractSuccessfulEffect() {
                     @Override
