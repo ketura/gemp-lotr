6d483359592669c56f017cdf7ea348dc7387db32
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 11:22:52 +0000

    "Smeagol"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/OverwhelmedByMultiplierModifier.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/OverwhelmedByMultiplierModifier.java
@@ -1,6 +1,6 @@
 package com.gempukku.lotro.cards.modifiers;
 
-import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
@@ -11,11 +11,11 @@ import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 public class OverwhelmedByMultiplierModifier extends AbstractModifier {
     private int _multiplier;
 
-    public OverwhelmedByMultiplierModifier(PhysicalCard source, Filter affectFilter, int multiplier) {
+    public OverwhelmedByMultiplierModifier(PhysicalCard source, Filterable affectFilter, int multiplier) {
         this(source, affectFilter, null, multiplier);
     }
 
-    public OverwhelmedByMultiplierModifier(PhysicalCard source, Filter affectFilter, Condition condition, int multiplier) {
+    public OverwhelmedByMultiplierModifier(PhysicalCard source, Filterable affectFilter, Condition condition, int multiplier) {
         super(source, "Cannot be overwhelmed unless his strength is *" + multiplier, affectFilter, condition, ModifierEffect.OVERWHELM_MODIFIER);
         _multiplier = multiplier;
     }
