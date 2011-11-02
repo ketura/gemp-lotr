b21a3da0e97caca9887fae81850057921a32f868
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-02 16:04:06 +0000

    "Gondor Still Stands"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/choose/ChooseAndPutCardFromDiscardOnTopOfDeckEffect.java
@@ -0,0 +1,28 @@
+package com.gempukku.lotro.cards.effects.choose;
+
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.SubAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collection;
+
+public class ChooseAndPutCardFromDiscardOnTopOfDeckEffect extends ChooseCardsFromDiscardEffect {
+    private Action _action;
+
+    public ChooseAndPutCardFromDiscardOnTopOfDeckEffect(Action action, String playerId, int minimum, int maximum, Filterable... filters) {
+        super(playerId, minimum, maximum, filters);
+        _action = action;
+    }
+
+    @Override
+    protected void cardsSelected(LotroGame game, Collection<PhysicalCard> cards) {
+        if (cards.size() > 0) {
+            SubAction subAction = new SubAction(_action);
+            for (PhysicalCard card : cards)
+                subAction.appendEffect(new PutCardFromDiscardOnTopOfDeckEffect(card));
+            game.getActionsEnvironment().addActionToStack(subAction);
+        }
+    }
+}
