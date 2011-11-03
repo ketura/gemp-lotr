9a760cd3025b7c4aaa2716284d7544518ad3cdb7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-03 15:14:00 +0000

    "Arwen"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/elven/Card10_005.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/elven/Card10_005.java
@@ -2,16 +2,15 @@ package com.gempukku.lotro.cards.set10.elven;
 
 import com.gempukku.lotro.cards.AbstractCompanion;
 import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifier;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
 import com.gempukku.lotro.cards.effects.DiscardBottomCardFromDeckEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
-import com.gempukku.lotro.cards.modifiers.evaluator.NegativeEvaluator;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,7 +25,8 @@ import java.util.List;
  * Vitality: 3
  * Resistance: 6
  * Signet: Aragorn
- * Game Text: Skirmish: Discard a card from hand to discard the bottom card of your draw deck. If the bottom card was an [ELVEN] card, each minion skirmishing Arwen is strength -3. 
+ * Game Text: Skirmish: Discard a card from hand to discard the bottom card of your draw deck. If the bottom card was
+ * an [ELVEN] card, each minion skirmishing Arwen is strength -3.
  * Rarity: U
  */
 public class Card10_005 extends AbstractCompanion {
@@ -35,23 +35,23 @@ public class Card10_005 extends AbstractCompanion {
     }
 
     @Override
-    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
         if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self)
-                && PlayConditions.canDiscardFromHand(game, self, 1)) {
-            ActivateCardAction action = new ActivateCardAction(self);
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, Filters.any)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
             action.setText("Discard the bottom card of your draw deck. If the bottom card was an Elven card, each minion skirmishing Arwen is strength -3.");
             action.appendCost(
                     new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1));
             action.appendEffect(
-            		new DiscardBottomCardFromDeckEffect(playerId) {
-            			@Override
+                    new DiscardBottomCardFromDeckEffect(playerId) {
+                        @Override
                         protected void discardedCardCallback(PhysicalCard card) {
-                            if (card.getCulture() == Culture.ELVEN) {
-                            	action.appendEffect(
-                            			new AddUntilEndOfPhaseModifier(new StrengthModifier(self, Fitlers.in(CardType.MINION, Filters.inSkirmishAgainst(self)), -3), Phase.SKIRMISH);
+                            if (card.getBlueprint().getCulture() == Culture.ELVEN) {
+                                action.appendEffect(
+                                        new AddUntilEndOfPhaseModifierEffect(new StrengthModifier(self, Filters.and(CardType.MINION, Filters.inSkirmishAgainst(self)), -3), Phase.SKIRMISH));
                             }
-            			}
-            		}
+                        }
+                    });
             return Collections.singletonList(action);
         }
         return null;
