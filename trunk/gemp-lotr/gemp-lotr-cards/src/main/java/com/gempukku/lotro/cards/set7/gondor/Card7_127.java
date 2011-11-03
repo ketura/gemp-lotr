b40bf95d714e6f82889b801c2101278480a651de
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-03 00:07:00 +0000

    "Vorondil"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/gondor/Card7_127.java
@@ -0,0 +1,84 @@
+package com.gempukku.lotro.cards.set7.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.LiberateASiteEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 5
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: While no opponent controls a site, Vorondil is strength +2. Regroup: Exert 3 [GONDOR] Men or 2 knights
+ * to liberate a site.
+ */
+public class Card7_127 extends AbstractCompanion {
+    public Card7_127() {
+        super(2, 5, 3, Culture.GONDOR, Race.MAN, null, "Vorondil", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, final PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, self,
+                        new Condition() {
+                            @Override
+                            public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                                return Filters.countActive(gameState, modifiersQuerying, Filters.siteControlledByShadowPlayer(self.getOwner())) == 0;
+                            }
+                        }, 2));
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.REGROUP, self)
+                && (PlayConditions.canExertMultiple(self, game, 1, 3, Culture.GONDOR, Race.MAN) || PlayConditions.canExertMultiple(self, game, 1, 2, Keyword.KNIGHT))) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            List<Effect> possibleCosts = new LinkedList<Effect>();
+            possibleCosts.add(
+                    new ChooseAndExertCharactersEffect(action, playerId, 3, 3, Culture.GONDOR, Race.MAN) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Exert 3 GONDOR Men";
+                        }
+                    });
+            possibleCosts.add(
+                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Keyword.KNIGHT) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Exert 2 knights";
+                        }
+                    });
+            action.appendCost(
+                    new ChoiceEffect(action, playerId, possibleCosts));
+            action.appendEffect(
+                    new LiberateASiteEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
