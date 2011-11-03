b5923ba35ff8016562946cc8ffd8464289878cdf
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-03 11:09:15 +0000

    "Easterling Regiment"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/raider/Card7_145.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set7.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 11
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Easterling. Fierce. Each time Easterling Regiment wins a skirmish, you may add (1) for each burden.
+ */
+public class Card7_145 extends AbstractMinion {
+    public Card7_145() {
+        super(5, 11, 3, 4, Race.MAN, Culture.RAIDER, "Easterling Regiment", true);
+        addKeyword(Keyword.EASTERLING);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.winsSkirmish(game, effectResult, self)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            int burdens = game.getGameState().getBurdens();
+            action.appendEffect(
+                    new AddTwilightEffect(self, burdens));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
