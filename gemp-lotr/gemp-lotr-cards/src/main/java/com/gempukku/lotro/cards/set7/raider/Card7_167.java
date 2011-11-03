f846c40928b5a6758804669e9dde8d80d86b0ce2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-03 21:27:54 +0000

    "Southron Marksmen"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/raider/Card7_167.java
@@ -0,0 +1,61 @@
+package com.gempukku.lotro.cards.set7.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.RemoveThreatsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Southron. Archer. Fierce. To play, spot a [RAIDER] Man. Archery: Exert this minion and remove a threat
+ * to make the minion archery total +1.
+ */
+public class Card7_167 extends AbstractMinion {
+    public Card7_167() {
+        super(5, 10, 3, 4, Race.MAN, Culture.RAIDER, "Southron Marksmen");
+        addKeyword(Keyword.SOUTHRON);
+        addKeyword(Keyword.ARCHER);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, Culture.RAIDER, Race.MAN);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.ARCHERY, self, 0)
+                && PlayConditions.canSelfExert(self, game)
+                && PlayConditions.canRemoveThreat(game, self, 1)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendCost(
+                    new RemoveThreatsEffect(self, 1));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new ArcheryTotalModifier(self, Side.SHADOW, 1), Phase.ARCHERY));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
