27bbea322d9a49df1d93addba16985ba64330c10
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-03 10:52:45 +0000

    "Easterling Aggressor"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/raider/Card7_139.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set7.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 3
+ * Type: Minion • Man
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Easterling. Skirmish: If you have initiative or you spot 3 Easterlings, discard 2 cards from hand
+ * to heal another Easterling.
+ */
+public class Card7_139 extends AbstractMinion {
+    public Card7_139() {
+        super(3, 8, 2, 4, Race.MAN, Culture.RAIDER, "Easterling Aggressor");
+        addKeyword(Keyword.EASTERLING);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
+                && (PlayConditions.hasInitiative(game, Side.SHADOW) || PlayConditions.canSpot(game, 3, Keyword.EASTERLING))
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, 1, 1, Filters.not(self), Keyword.EASTERLING));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
