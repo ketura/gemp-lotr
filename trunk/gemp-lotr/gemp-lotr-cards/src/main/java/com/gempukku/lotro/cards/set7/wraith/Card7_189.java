d971946864dad4e6a722e1f7a4bbb1681638ea55
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-06 16:30:01 +0000

    "Morgul Cur"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/wraith/Card7_189.java
@@ -0,0 +1,36 @@
+package com.gempukku.lotro.cards.set7.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: While you can spot a Nazgul, this minion is strength +2.
+ */
+public class Card7_189 extends AbstractMinion {
+    public Card7_189() {
+        super(3, 8, 2, 4, Race.ORC, Culture.WRAITH, "Morgul Cur");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, self, new SpotCondition(Race.NAZGUL), 2));
+    }
+}
