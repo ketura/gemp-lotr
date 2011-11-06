8d2bb942c1c2dae2eaf87100634dc395395bcd76
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-06 23:29:42 +0000

    "Eomer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/rohan/Card7_227.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set7.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPutCardFromDiscardIntoHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 8
+ * Vitality: 3
+ * Resistance: 6
+ * Signet: Aragorn
+ * Game Text: Valiant. While you can spot a [ROHAN] Man, Eomer's twilight cost is -1. Fellowship: Play a [ROHAN]
+ * companion to take a [ROHAN] possession or [ROHAN] skirmish event into hand from your discard pile.
+ */
+public class Card7_227 extends AbstractCompanion {
+    public Card7_227() {
+        super(3, 8, 3, Culture.ROHAN, Race.MAN, Signet.ARAGORN, "Eomer", true);
+        addKeyword(Keyword.VALIANT);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        if (Filters.canSpot(gameState, modifiersQuerying, Culture.ROHAN, Race.MAN))
+            return -1;
+        return 0;
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)
+                && PlayConditions.canPlayFromHand(playerId, game, Culture.ROHAN, CardType.COMPANION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game.getGameState().getHand(playerId), Culture.ROHAN, CardType.COMPANION));
+            action.appendEffect(
+                    new ChooseAndPutCardFromDiscardIntoHandEffect(action, playerId, 1, 1, Culture.ROHAN, Filters.or(CardType.POSSESSION, Filters.and(CardType.EVENT, Keyword.SKIRMISH))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
