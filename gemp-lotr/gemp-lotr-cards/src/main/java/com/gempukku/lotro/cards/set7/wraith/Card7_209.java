486ff735f76d051ae1ac4f19f2c568c90ab8e56a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-06 17:55:38 +0000

    "Too Late"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/wraith/Card7_209.java
@@ -0,0 +1,61 @@
+package com.gempukku.lotro.cards.set7.wraith;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.TransferPermanentEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.ExertResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: Skirmish: Transfer this condition from your support area to a character skirmishing a Nazgul. Each time
+ * bearer exerts, add (1).
+ */
+public class Card7_209 extends AbstractPermanent {
+    public Card7_209() {
+        super(Side.SHADOW, 1, CardType.CONDITION, Culture.WRAITH, Zone.SUPPORT, "Too Late");
+    }
+
+    @Override
+    public List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
+                && self.getZone() == Zone.SUPPORT
+                && Filters.filter(game.getGameState().getSkirmish().getShadowCharacters(), game.getGameState(), game.getModifiersQuerying(), Filters.race(Race.NAZGUL)).size() > 0
+                && game.getGameState().getSkirmish().getFellowshipCharacter() != null) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new TransferPermanentEffect(self, game.getGameState().getSkirmish().getFellowshipCharacter()));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.EXERT) {
+            ExertResult exertResult = (ExertResult) effectResult;
+            if (exertResult.getExertedCards().contains(self.getAttachedTo())) {
+                RequiredTriggerAction action = new RequiredTriggerAction(self);
+                action.appendEffect(
+                        new AddTwilightEffect(self, 1));
+                return Collections.singletonList(action);
+            }
+        }
+        return null;
+    }
+}
