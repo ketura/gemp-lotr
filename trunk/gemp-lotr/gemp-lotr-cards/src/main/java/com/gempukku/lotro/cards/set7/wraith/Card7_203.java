cbc908a30886008bd654444c3b386b8828cb9fd2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-06 17:21:09 +0000

    "Nazgul Scimitar"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/wraith/Card7_203.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set7.wraith;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.modifiers.conditions.MinThreatCondition;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +1
+ * Game Text: Bearer must be a [WRAITH] minion. While you can spot 5 threats, bearer is damage +1.
+ */
+public class Card7_203 extends AbstractAttachable {
+    public Card7_203() {
+        super(Side.SHADOW, CardType.POSSESSION, 1, Culture.WRAITH, PossessionClass.HAND_WEAPON, "Nazgul Scimitar");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(CardType.MINION, Culture.WRAITH);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, Filters.hasAttached(self), 1));
+        modifiers.add(
+                new KeywordModifier(self, Filters.hasAttached(self), new MinThreatCondition(5), Keyword.DAMAGE, 1));
+        return modifiers;
+    }
+}
