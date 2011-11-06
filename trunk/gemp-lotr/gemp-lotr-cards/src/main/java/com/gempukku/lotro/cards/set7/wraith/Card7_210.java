726062964c9b2b9b957591388d83d93a94f779d2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-06 18:07:21 +0000

    "Ulaire Attea"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/wraith/Card7_210.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set7.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 6
+ * Type: Minion • Nazgul
+ * Strength: 12
+ * Vitality: 3
+ * Site: 3
+ * Game Text: Fierce. Shadow: If you have initiative, exert Ulaire Attea and spot another [WRAITH] minion to add
+ * a burden.
+ */
+public class Card7_210 extends AbstractMinion {
+    public Card7_210() {
+        super(6, 12, 3, 3, Race.NAZGUL, Culture.WRAITH, "Ulaire Attea", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
+                && PlayConditions.hasInitiative(game, Side.SHADOW)
+                && PlayConditions.canSelfExert(self, game)
+                && PlayConditions.canSpot(game, Filters.not(self), Culture.WRAITH, CardType.MINION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new AddBurdenEffect(self, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
