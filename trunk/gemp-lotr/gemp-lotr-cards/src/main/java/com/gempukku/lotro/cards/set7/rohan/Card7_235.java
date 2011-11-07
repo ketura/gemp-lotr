0bf53ae8bd20115bceeaaf2a5e64d71f44da4814
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-07 10:28:02 +0000

    "Guthlaf"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/rohan/Card7_235.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set7.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Skirmish: Discard a [ROHAN] condition to make Guthlaf strength +1.
+ */
+public class Card7_235 extends AbstractCompanion {
+    public Card7_235() {
+        super(2, 6, 3, Culture.ROHAN, Race.MAN, null, "Guthlaf", true);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canDiscardFromPlay(self, game, Culture.ROHAN, CardType.CONDITION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Culture.ROHAN, CardType.CONDITION));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new StrengthModifier(self, self, 1), Phase.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
