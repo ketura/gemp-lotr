46bbc8da3b9725cdb2b8b1627f78d270c69d3e58
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-07 12:09:37 +0000

    "With Strength to Fight"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/rohan/Card7_261.java
@@ -0,0 +1,76 @@
+package com.gempukku.lotro.cards.set7.rohan;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 0
+ * Type: Condition • Support Area
+ * Game Text: To play, spot a [ROHAN] companion. Each time the fellowship moves, add a threat or discard this condition.
+ * Regroup: Discard this condition to heal up to 2 [ROHAN] companions.
+ */
+public class Card7_261 extends AbstractPermanent {
+    public Card7_261() {
+        super(Side.FREE_PEOPLE, 0, CardType.CONDITION, Culture.ROHAN, Zone.SUPPORT, "With Strength to Fight");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier)
+                && PlayConditions.canSpot(game, Culture.ROHAN, CardType.COMPANION);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.WHEN_FELLOWSHIP_MOVES) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            List<Effect> possibleEffects = new LinkedList<Effect>();
+            possibleEffects.add(
+                    new AddThreatsEffect(self.getOwner(), self, 1));
+            possibleEffects.add(
+                    new DiscardCardsFromPlayEffect(self, self) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Discard this condition";
+                        }
+                    });
+            action.appendEffect(
+                    new ChoiceEffect(action, self.getOwner(), possibleEffects));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, 0, 2, Culture.ROHAN, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
