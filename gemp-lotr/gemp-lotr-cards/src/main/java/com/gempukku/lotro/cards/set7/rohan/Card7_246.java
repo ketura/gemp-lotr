c19bf1a0296c1fbf895aee0864f2c88c7be5445a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-07 11:07:26 +0000

    "Rohirrim Guard"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/rohan/Card7_246.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set7.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Skirmish: Discard a card from hand to exert a minion skirmishing this companion.
+ */
+public class Card7_246 extends AbstractCompanion {
+    public Card7_246() {
+        super(2, 6, 3, Culture.ROHAN, Race.MAN, null, "Rohirrim Guard");
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, Filters.any)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1));
+            action.appendEffect(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.MINION, Filters.inSkirmishAgainst(self)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
