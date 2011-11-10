9c3b534b396f4bccfd2775b755365cc093be8b2a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 01:11:33 +0000

    "Orc Rager"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/sauron/Card7_304.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set7.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 9
+ * Vitality: 3
+ * Site: 6
+ * Game Text: Skirmish: Discard this minion to make another [SAURON] Orc strength +4.
+ */
+public class Card7_304 extends AbstractMinion {
+    public Card7_304() {
+        super(3, 9, 3, 6, Race.ORC, Culture.SAURON, "Orc Rager");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 4, Filters.not(self), Culture.SAURON, Race.ORC));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
