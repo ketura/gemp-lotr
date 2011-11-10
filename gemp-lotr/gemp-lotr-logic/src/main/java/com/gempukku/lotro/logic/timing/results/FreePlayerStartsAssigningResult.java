3451f2db7b1c178ffae5b90f92638e2ff6ab8742
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 15:24:00 +0000

    Introducing an extra sub-step for cards that require cost to be able to assign them by player.

---
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/results/FreePlayerStartsAssigningResult.java
@@ -0,0 +1,9 @@
+package com.gempukku.lotro.logic.timing.results;
+
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+public class FreePlayerStartsAssigningResult extends EffectResult {
+    public FreePlayerStartsAssigningResult() {
+        super(Type.FREE_PEOPLE_PLAYER_STARTS_ASSIGNING);
+    }
+}
