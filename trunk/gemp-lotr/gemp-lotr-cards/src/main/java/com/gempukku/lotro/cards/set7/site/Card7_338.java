fb76cb33c1b66d102ee3a225e9688454defaf056
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 22:37:05 +0000

    "Beacon of Minas Tirith"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/site/Card7_338.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set7.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Twilight Cost: 0
+ * Type: Site
+ * Site: 3K
+ * Game Text: Sanctuary. Fellowship: If you cannot spot 3 threats, add a threat to heal a companion.
+ */
+public class Card7_338 extends AbstractSite {
+    public Card7_338() {
+        super("Beacon of Minas Tirith", Block.KING, 3, 0, Direction.RIGHT);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.FELLOWSHIP, self)
+                && !PlayConditions.canSpotThreat(game, 3)
+                && PlayConditions.canAddThreat(game, self, 1)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new AddThreatsEffect(playerId, self, 1));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, 1, 1, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
