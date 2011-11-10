a4f85df7115e5bdf068f704064fcbf835677d4c5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 23:00:50 +0000

    "Minas Tirith Sixth Circle"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/site/Card7_351.java
@@ -0,0 +1,40 @@
+package com.gempukku.lotro.cards.set7.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Twilight Cost: 3
+ * Type: Site
+ * Site: 6K
+ * Game Text: Sanctuary. If the fellowship moves from this site during the regroup phase, heal each [GONDOR] companion.
+ */
+public class Card7_351 extends AbstractSite {
+    public Card7_351() {
+        super("Minas Tirith Sixth Circle", Block.KING, 6, 3, Direction.LEFT);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.movesFrom(game, effectResult, self)
+                && PlayConditions.isPhase(game, Phase.REGROUP)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new HealCharactersEffect(self, Culture.GONDOR, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+}
