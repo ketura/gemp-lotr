3c49c8ac41b82379d3c41c16e9084d5f9576b9cf
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 22:23:57 +0000

    "Rohirrim Road"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/site/Card7_332.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set7.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Type: Site
+ * Site: 1K
+ * Game Text: Fellowship: Exert an Elf to make the twilight cost of each condition and possession +2 until the end of
+ * the turn.
+ */
+public class Card7_332 extends AbstractSite {
+    public Card7_332() {
+        super("Rohirrim Road", Block.KING, 1, 0, Direction.LEFT);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.FELLOWSHIP, self)
+                && PlayConditions.canExert(self, game, Race.ELF)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Race.ELF));
+            action.appendEffect(
+                    new AddUntilEndOfTurnModifierEffect(
+                            new TwilightCostModifier(self, Filters.or(CardType.CONDITION, CardType.POSSESSION), 2)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
