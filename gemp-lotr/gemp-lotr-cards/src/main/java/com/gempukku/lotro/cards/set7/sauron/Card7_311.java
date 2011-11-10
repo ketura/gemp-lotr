627eb9149d6ec666c7f5b52309b6e305beed98fb
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 13:04:44 +0000

    "Siege Commander"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/sauron/Card7_311.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set7.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Return of the King
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 5
+ * Type: Minion • Orc
+ * Strength: 12
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Besieger. Skirmish: Discard 2 cards from hand to make a besieger fierce until the regroup phase.
+ */
+public class Card7_311 extends AbstractMinion {
+    public Card7_311() {
+        super(5, 12, 3, 5, Race.ORC, Culture.SAURON, "Siege Commander", true);
+        addKeyword(Keyword.BESIEGER);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a besieger", Keyword.BESIEGER) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.insertEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, card, Keyword.FIERCE), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
