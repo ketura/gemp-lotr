21a386ebb4e0311d46a6680e5f7123afedf43143
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 16:36:04 +0000

    "Merry"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/shire/Card7_321.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/shire/Card7_321.java
@@ -43,7 +43,7 @@ public class Card7_321 extends AbstractCompanion {
                     new ReturnCardsToHandEffect(self, self));
             action.appendEffect(
                     new PlayoutDecisionEffect(game.getUserFeedback(), playerId,
-                            new IntegerAwaitingDecision(1, "How many ROHAN possessions do you wish to play?", 0, 2) {
+                            new IntegerAwaitingDecision(1, "How many ROHAN possessions do you wish to play from your discard pile?", 0, 2) {
                                 @Override
                                 public void decisionMade(String result) throws DecisionResultInvalidException {
                                     int count = getValidatedResult(result);
