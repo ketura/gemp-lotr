4bffec2bc53eb1ef255d445d2b1ba5575f6481f6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-10 17:45:55 +0000

    "Sam"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/shire/Card7_327.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/shire/Card7_327.java
@@ -3,14 +3,14 @@ package com.gempukku.lotro.cards.set7.shire;
 import com.gempukku.lotro.cards.AbstractCompanion;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.effects.MakeRingBearerEffect;
-import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
 import com.gempukku.lotro.logic.timing.Effect;
 
 import java.util.Collections;
@@ -25,13 +25,13 @@ import java.util.List;
  * Strength: 3
  * Vitality: 4
  * Resistance: 6
- * Signet: Aragorn
- * Game Text: Ring-bound. Fellowship: Play a Hobbit companion to remove a burden. Response: If Frodo is killed, make
- * Sam the Ring-bearer (resistance 5).
+ * Signet: Frodo
+ * Game Text: Ring-bound. Fellowship: If you have initiative, discard a card from hand to heal Sam. Response: If Frodo
+ * is killed, make Sam the Ring-bearer (resistance 5).
  */
-public class Card7_326 extends AbstractCompanion {
-    public Card7_326() {
-        super(2, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.ARAGORN, "Sam", true);
+public class Card7_327 extends AbstractCompanion {
+    public Card7_327() {
+        super(2, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.FRODO, "Sam", true);
         addKeyword(Keyword.RING_BOUND);
     }
 
@@ -43,12 +43,13 @@ public class Card7_326 extends AbstractCompanion {
     @Override
     protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
         if (PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)
-                && PlayConditions.canPlayFromHand(playerId, game, CardType.COMPANION, Race.HOBBIT)) {
+                && PlayConditions.hasInitiative(game, Side.FREE_PEOPLE)
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, Filters.any)) {
             ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
-                    new ChooseAndPlayCardFromHandEffect(playerId, game.getGameState().getHand(playerId), CardType.COMPANION, Race.HOBBIT));
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1));
             action.appendEffect(
-                    new RemoveBurdenEffect(self));
+                    new HealCharactersEffect(self, self));
             return Collections.singletonList(action);
         }
         return null;
@@ -63,4 +64,5 @@ public class Card7_326 extends AbstractCompanion {
         }
         return null;
     }
+
 }
