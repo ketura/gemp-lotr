087aa04ffe6267770920de7ce6195561e5aca145
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-11 23:57:11 +0000

    - "Morgul Blade" now allows to transfer the Blade Tip from support zone and discard to the character skirmishing the bearer.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/wraith/Card1_216.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/wraith/Card1_216.java
@@ -2,20 +2,25 @@ package com.gempukku.lotro.cards.set1.wraith;
 
 import com.gempukku.lotro.cards.AbstractAttachable;
 import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AttachCardEffect;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
 import com.gempukku.lotro.cards.effects.TransferPermanentEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.game.state.Skirmish;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
 import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
 import com.gempukku.lotro.logic.modifiers.Modifier;
 import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -50,22 +55,47 @@ public class Card1_216 extends AbstractAttachable {
 
     @Override
     protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)
-                && self.getZone() == Zone.SUPPORT) {
-            ActivateCardAction action = new ActivateCardAction(self);
+        if (PlayConditions.canUseShadowCardDuringPhase(game.getGameState(), Phase.SKIRMISH, self, 0)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
                     new DiscardCardsFromPlayEffect(self, self));
-            Skirmish skirmish = game.getGameState().getSkirmish();
-            if (skirmish != null && skirmish.getShadowCharacters().contains(self.getAttachedTo())) {
-                final PhysicalCard fpChar = skirmish.getFellowshipCharacter();
-                if (fpChar != null) {
-                    final PhysicalCard bladeTip = Filters.findFirstActive(game.getGameState(), game.getModifiersQuerying(), Filters.name("Blade Tip"), Filters.owner(playerId), Filters.zone(Zone.SUPPORT));
-                    if (bladeTip != null) {
-                        action.appendEffect(
-                                new TransferPermanentEffect(bladeTip, fpChar));
-                    }
-                }
+
+            final PhysicalCard bladeTipInSupport = Filters.findFirstActive(game.getGameState(), game.getModifiersQuerying(), Filters.name("Blade Tip"), Filters.owner(playerId), Filters.zone(Zone.SUPPORT));
+            final Collection<PhysicalCard> bladeTipsInDiscard = Filters.filter(game.getGameState().getDiscard(playerId), game.getGameState(), game.getModifiersQuerying(), Filters.name("Blade Tip"));
+
+            List<Effect> possibleEffects = new LinkedList<Effect>();
+            if (bladeTipInSupport != null) {
+                possibleEffects.add(
+                        new ChooseActiveCardEffect(self, playerId, "Choose a companion", CardType.COMPANION, Filters.inSkirmishAgainst(self.getAttachedTo())) {
+                            @Override
+                            protected void cardSelected(LotroGame game, PhysicalCard card) {
+                                action.insertEffect(
+                                        new TransferPermanentEffect(bladeTipInSupport, card));
+                            }
+
+                            @Override
+                            public String getText(LotroGame game) {
+                                return "Transfer Blade Tip from support area";
+                            }
+                        });
             }
+            if (bladeTipsInDiscard.size() > 0) {
+                possibleEffects.add(
+                        new ChooseActiveCardEffect(self, playerId, "Choose a companion", CardType.COMPANION, Filters.inSkirmishAgainst(self.getAttachedTo())) {
+                            @Override
+                            protected void cardSelected(LotroGame game, PhysicalCard card) {
+                                action.insertEffect(
+                                        new AttachCardEffect(bladeTipsInDiscard.iterator().next(), card));
+                            }
+
+                            @Override
+                            public String getText(LotroGame game) {
+                                return "Transfer Blade Tip from discard";
+                            }
+                        });
+            }
+            action.appendEffect(
+                    new ChoiceEffect(action, playerId, possibleEffects));
             return Collections.singletonList(action);
         }
         return null;
