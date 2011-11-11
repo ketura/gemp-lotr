4dfdf3f676aac7ba2cd0c42256fe0fef66f15e07
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-11 17:03:15 +0000

    "Hidden Event From Her"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/PutCardFromPlayOnTopOfDeckEffect.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.effects;
+
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.timing.AbstractEffect;
+
+import java.util.Collections;
+
+public class PutCardFromPlayOnTopOfDeckEffect extends AbstractEffect {
+    private PhysicalCard _physicalCard;
+
+    public PutCardFromPlayOnTopOfDeckEffect(PhysicalCard physicalCard) {
+        _physicalCard = physicalCard;
+    }
+
+    @Override
+    public boolean isPlayableInFull(LotroGame game) {
+        return _physicalCard.getZone().isInPlay();
+    }
+
+    @Override
+    protected FullEffectResult playEffectReturningResult(LotroGame game) {
+        if (isPlayableInFull(game)) {
+            GameState gameState = game.getGameState();
+            gameState.sendMessage(_physicalCard.getOwner() + " puts " + GameUtils.getCardLink(_physicalCard) + " from play on the top of deck");
+            gameState.removeCardsFromZone(_physicalCard.getOwner(), Collections.singleton(_physicalCard));
+            gameState.putCardOnTopOfDeck(_physicalCard);
+            return new FullEffectResult(null, true, true);
+        }
+        return new FullEffectResult(null, false, false);
+    }
+
+    @Override
+    public String getText(LotroGame game) {
+        return "Put " + GameUtils.getCardLink(_physicalCard) + " from play on top of deck";
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+}
---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/gollum/Card8_022.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set8.gollum;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.PutCardFromPlayOnTopOfDeckEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: Gollum is fierce. Shadow: Spot 6 companions to play Gollum from your discard pile and exert a companion.
+ * Place this condition on top of your draw deck.
+ */
+public class Card8_022 extends AbstractPermanent {
+    public Card8_022() {
+        super(Side.SHADOW, 1, CardType.CONDITION, Culture.GOLLUM, Zone.SUPPORT, "Hidden Event From Her");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.gollum, Keyword.FIERCE));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
+                && PlayConditions.canSpot(game, 6, CardType.COMPANION)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Filters.gollum)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Filters.gollum));
+            action.appendEffect(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.COMPANION));
+            action.appendEffect(
+                    new PutCardFromPlayOnTopOfDeckEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
