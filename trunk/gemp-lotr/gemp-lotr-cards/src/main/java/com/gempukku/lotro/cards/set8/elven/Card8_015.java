2dd8aa54276ee91e7ffde857052435e0ae7f6d31
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-11 15:23:02 +0000

    "Gandalf"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/elven/Card8_015.java
@@ -0,0 +1,35 @@
+package com.gempukku.lotro.cards.set8.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Signet;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 4
+ * Type: Companion • Wizard
+ * Strength: 7
+ * Vitality: 4
+ * Resistance: 6
+ * Signet: Aragorn
+ * Game Text: When Gandalf is in your starting fellowship, his twilight cost is -2.
+ */
+public class Card8_015 extends AbstractCompanion {
+    public Card8_015() {
+        super(4, 7, 4, Culture.GANDALF, Race.WIZARD, Signet.ARAGORN, "Gandalf", true);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        if (gameState.getCurrentPhase() == Phase.PLAY_STARTING_FELLOWSHIP)
+            return -2;
+        return 0;
+    }
+}
