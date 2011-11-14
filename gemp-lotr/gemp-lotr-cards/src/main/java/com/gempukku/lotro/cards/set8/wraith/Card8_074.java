8436b230c6e7c08c26c1f8dbc707b4bdc1bffec9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 16:47:28 +0000

    "Morgul Ambusher"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/wraith/Card8_074.java
@@ -0,0 +1,37 @@
+package com.gempukku.lotro.cards.set8.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.MinThreatCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: While you can spot 3 threats, the minion archery total is +2.
+ */
+public class Card8_074 extends AbstractMinion {
+    public Card8_074() {
+        super(3, 8, 2, 4, Race.ORC, Culture.WRAITH, "Morgul Ambusher");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new ArcheryTotalModifier(self, Side.SHADOW, new MinThreatCondition(3), 2));
+    }
+}
