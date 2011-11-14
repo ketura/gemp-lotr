dce5cbe43fa1e299c5cbd5255f0da68cd3b9cad0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 11:42:02 +0000

    "Black Numenorean"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/raider/Card8_049.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set8.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveTokensFromCardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 6
+ * Type: Minion • Man
+ * Strength: 12
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Corsair. Fierce. Skirmish: If you have initiative, remove a [RAIDER] token to discard a possession borne
+ * by a character skirmishing this minion.
+ */
+public class Card8_049 extends AbstractMinion {
+    public Card8_049() {
+        super(6, 12, 3, 4, Race.MAN, Culture.RAIDER, "Black Numenorean");
+        addKeyword(Keyword.CORSAIR);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.hasInitiative(game, Side.SHADOW)
+                && PlayConditions.canRemoveTokens(game, Token.RAIDER, 1, Filters.any)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndRemoveTokensFromCardEffect(self, playerId, Token.RAIDER, 1, Filters.any));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.POSSESSION, Filters.attachedTo(Filters.inSkirmishAgainst(self))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
