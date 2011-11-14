806b5a47a411a5652232efb28152a9a48663ead8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 17:16:38 +0000

    "Ulaire Nertea"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/wraith/Card8_080.java
@@ -0,0 +1,40 @@
+package com.gempukku.lotro.cards.set8.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.MinionSiteNumberModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 4
+ * Type: Minion • Nazgul
+ * Strength: 9
+ * Vitality: 2
+ * Site: 3
+ * Game Text: Enduring. Fierce. The site number of each [WRAITH] minion is -1.
+ */
+public class Card8_080 extends AbstractMinion {
+    public Card8_080() {
+        super(4, 9, 2, 3, Race.NAZGUL, Culture.WRAITH, "Ulaire Nertea", true);
+        addKeyword(Keyword.ENDURING);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new MinionSiteNumberModifier(self, Filters.and(Culture.WRAITH, CardType.MINION), null, -1));
+    }
+}
