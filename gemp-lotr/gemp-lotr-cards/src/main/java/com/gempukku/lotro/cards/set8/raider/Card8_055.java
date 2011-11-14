f2c14a20bf25754681ac6b14442cc32215d9841f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 12:07:00 +0000

    "Corsair Gunners"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/raider/Card8_055.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set8.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCultureTokensFromCardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Corsair. Skirmish: If you have initiative, remove a [RAIDER] token to make a corsair fierce until
+ * the regroup phase.
+ */
+public class Card8_055 extends AbstractMinion {
+    public Card8_055() {
+        super(5, 10, 3, 4, Race.MAN, Culture.RAIDER, "Corsair Gunners");
+        addKeyword(Keyword.CORSAIR);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.hasInitiative(game, Side.SHADOW)
+                && PlayConditions.canRemoveTokens(game, Token.RAIDER, 1, Filters.any)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndRemoveCultureTokensFromCardEffect(self, playerId, Token.RAIDER, 1, Filters.any));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose Corsair", Keyword.CORSAIR) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.insertEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, card, Keyword.FIERCE), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
