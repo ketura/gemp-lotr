896ea31f1685f42c69a4c5bafa61ae1a707a27f6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 11:22:52 +0000

    "Shadow Host"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/gondor/Card8_043.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set8.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayPermanentAction;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Signet;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 5
+ * Type: Companion • Wraith
+ * Strength: 9
+ * Vitality: 3
+ * Resistance: 6
+ * Signet: Aragorn
+ * Game Text: Enduring. To play, spot 2 exhausted [GONDOR] Wraiths and add 2 threats. While Shadow Host is exhausted,
+ * it is defender +1.
+ */
+public class Card8_043 extends AbstractCompanion {
+    public Card8_043() {
+        super(5, 9, 3, Culture.GONDOR, Race.WRAITH, Signet.ARAGORN, "Shadow Host", true);
+        addKeyword(Keyword.ENDURING);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier, ignoreRoamingPenalty)
+                && PlayConditions.canSpot(game, 2, Culture.GONDOR, Race.WRAITH, Filters.exhausted)
+                && PlayConditions.canAddThreat(game, self, 2);
+    }
+
+    @Override
+    public PlayPermanentAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayPermanentAction playCardAction = super.getPlayCardAction(playerId, game, self, twilightModifier, ignoreRoamingPenalty);
+        playCardAction.appendCost(
+                new AddThreatsEffect(playerId, self, 2));
+        return playCardAction;
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.and(self, Filters.exhausted), Keyword.DEFENDER, 1));
+    }
+}
