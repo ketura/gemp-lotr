62986e5e41121e9e1a2f8c26a4c884283ab71801
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 16:27:41 +0000

    "Black Dart"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/MayNotBearModifier.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/MayNotBearModifier.java
@@ -1,6 +1,8 @@
 package com.gempukku.lotro.cards.modifiers;
 
+import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
@@ -10,9 +12,9 @@ import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 public class MayNotBearModifier extends AbstractModifier {
     private Filter _unbearableCardFilter;
 
-    public MayNotBearModifier(PhysicalCard source, Filter affectFilter, Filter unbearableCardFilter) {
+    public MayNotBearModifier(PhysicalCard source, Filter affectFilter, Filterable... unbearableCardFilter) {
         super(source, "Affected by \"may not bear\" limitation", affectFilter, ModifierEffect.TARGET_MODIFIER);
-        _unbearableCardFilter = unbearableCardFilter;
+        _unbearableCardFilter = Filters.and(unbearableCardFilter);
     }
 
     @Override
