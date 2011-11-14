01847b8181dba8b823dc9e7e99de7cd718d2833f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 18:41:12 +0000

    "Eomer"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/choose/ChooseAndPlayCardFromDeckEffect.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/choose/ChooseAndPlayCardFromDeckEffect.java
@@ -5,12 +5,13 @@ import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
 import com.gempukku.lotro.logic.decisions.ArbitraryCardsSelectionDecision;
 import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
 import com.gempukku.lotro.logic.modifiers.ModifierFlag;
-import com.gempukku.lotro.logic.timing.Action;
 import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.UnrespondableEffect;
 
 import java.util.Collection;
 import java.util.LinkedList;
@@ -20,7 +21,7 @@ public class ChooseAndPlayCardFromDeckEffect implements Effect {
     private String _playerId;
     private Filterable[] _filter;
     private int _twilightModifier;
-    private Action _playCardAction;
+    private CostToEffectAction _playCardAction;
 
     public ChooseAndPlayCardFromDeckEffect(String playerId, Filterable... filter) {
         this(playerId, 0, filter);
@@ -57,14 +58,24 @@ public class ChooseAndPlayCardFromDeckEffect implements Effect {
                         public void decisionMade(String result) throws DecisionResultInvalidException {
                             List<PhysicalCard> selectedCards = getSelectedCardsByResponse(result);
                             if (selectedCards.size() > 0) {
-                                PhysicalCard selectedCard = selectedCards.get(0);
+                                final PhysicalCard selectedCard = selectedCards.get(0);
                                 _playCardAction = selectedCard.getBlueprint().getPlayCardAction(_playerId, game, selectedCard, _twilightModifier, false);
+                                _playCardAction.appendEffect(
+                                        new UnrespondableEffect() {
+                                            @Override
+                                            protected void doPlayEffect(LotroGame game) {
+                                                afterCardPlayed(selectedCard);
+                                            }
+                                        });
                                 game.getActionsEnvironment().addActionToStack(_playCardAction);
                             }
                         }
                     });
         }
         return null;
+    }
+
+    protected void afterCardPlayed(PhysicalCard cardPlayed) {
     }
 
     @Override
