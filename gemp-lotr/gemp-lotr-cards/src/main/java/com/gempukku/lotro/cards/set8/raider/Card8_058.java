4e5cd0f5a60bce4aad148ba2ba89b22d1801d66f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-14 12:15:07 +0000

    "Corsair Plunderer"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/raider/Card8_058.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set8.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddTokenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Token;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 2
+ * Type: Minion • Man
+ * Strength: 6
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Corsair. When you play this minion, you may discard 2 cards from hand to add 2 [RAIDER] tokens to a card
+ * that already has a [RAIDER] token on it.
+ */
+public class Card8_058 extends AbstractMinion {
+    public Card8_058() {
+        super(2, 6, 2, 4, Race.MAN, Culture.RAIDER, "Corsair Plunderer");
+        addKeyword(Keyword.CORSAIR);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (PlayConditions.played(game, effectResult, self)
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a card to add tokens to", Filters.hasToken(Token.RAIDER)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.insertEffect(
+                                    new AddTokenEffect(self, self, Token.RAIDER, 2));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
