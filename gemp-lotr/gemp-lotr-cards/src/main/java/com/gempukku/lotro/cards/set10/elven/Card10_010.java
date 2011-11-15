a977fd532c20e1bc1ae77441ae2a930bc517e633
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-15 16:35:17 +0000

    "Fleet-footed"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/elven/Card10_010.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set10.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.OptionalEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.UnrespondableEffect;
+
+/**
+ * Set: Mount Doom
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 1
+ * Type: Event • Skirmish
+ * Game Text: Make a minion skirmishing an Elf strength -2. Spot a site in a support area to place this event on top
+ * of your draw deck.
+ */
+public class Card10_010 extends AbstractEvent {
+    public Card10_010() {
+        super(Side.FREE_PEOPLE, 1, Culture.ELVEN, "Fleet-footed", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, -2, CardType.MINION, Filters.inSkirmishAgainst(Race.ELF)));
+        if (Filters.canSpot(game.getGameState(), game.getModifiersQuerying(), CardType.SITE, Zone.SUPPORT))
+            action.appendEffect(
+                    new OptionalEffect(action, playerId,
+                            new UnrespondableEffect() {
+                                @Override
+                                public String getText(LotroGame game) {
+                                    return "Place this event on top of your draw deck";
+                                }
+
+                                @Override
+                                protected void doPlayEffect(LotroGame game) {
+                                    action.skipDiscardPart();
+                                    game.getGameState().putCardOnTopOfDeck(self);
+                                }
+                            }));
+        return action;
+    }
+}
