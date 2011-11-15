afd8d5d3e30603eaa91728fa3c407c90dc39080a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-15 12:00:12 +0000

    "Gorgoroth Stormer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set8/sauron/Card8_101.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set8.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Siege of Gondor
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 4
+ * Type: Minion • Orc
+ * Strength: 10
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Besieger. Shadow: If you have initiative, exert this minion to add a burden and make the Free Peoples
+ * player draw a card.
+ */
+public class Card8_101 extends AbstractMinion {
+    public Card8_101() {
+        super(4, 10, 2, 5, Race.ORC, Culture.SAURON, "Gorgoroth Stormer");
+        addKeyword(Keyword.BESIEGER);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
+                && PlayConditions.hasInitiative(game, Side.SHADOW)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new AddBurdenEffect(self, 1));
+            action.appendEffect(
+                    new DrawCardEffect(game.getGameState().getCurrentPlayerId(), 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
