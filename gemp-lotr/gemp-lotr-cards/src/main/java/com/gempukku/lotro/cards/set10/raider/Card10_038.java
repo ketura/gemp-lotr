b7883f7901d3dfa17d45bebc08b0a6d1fabd3fb0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-16 16:51:26 +0000

    "Corsair Brute"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/raider/Card10_038.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set10.raider;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCultureTokensFromCardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Corsair. At the start of each skirmish involving this minion, you may remove a [RAIDER] token to wound
+ * a companion he is skirmishing.
+ */
+public class Card10_038 extends AbstractMinion {
+    public Card10_038() {
+        super(5, 10, 3, 4, Race.MAN, Culture.RAIDER, "Corsair Brute");
+        addKeyword(Keyword.CORSAIR);
+    }
+
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (PlayConditions.startOfPhase(game, effectResult, Phase.SKIRMISH)
+                && Filters.inSkirmish.accepts(game.getGameState(), game.getModifiersQuerying(), self)
+                && PlayConditions.canRemoveTokens(game, Token.RAIDER, 1, Filters.any)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ChooseAndRemoveCultureTokensFromCardEffect(self, playerId, Token.RAIDER, 1, Filters.any));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, CardType.COMPANION, Filters.inSkirmishAgainst(self)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
