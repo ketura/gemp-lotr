5cffa16e041a391479b4db289ade70a76a50c425
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-16 16:31:45 +0000

    "Fifth Level"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/gondor/Card10_032.java
@@ -0,0 +1,84 @@
+package com.gempukku.lotro.cards.set10.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.SpotEffect;
+import com.gempukku.lotro.cards.effects.TransferPermanentEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.NegativeEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: Fortification. Bearer is strength -1 for each Fortification you can spot. Maneuver: Exert 2 [GONDOR] Men
+ * or spot 3 knights to transfer this condition to a minion.
+ */
+public class Card10_032 extends AbstractPermanent {
+    public Card10_032() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.GONDOR, Zone.SUPPORT, "Fifth Level");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.hasAttached(self), null, new NegativeEvaluator(new CountSpottableEvaluator(Keyword.FORTIFICATION))));
+    }
+
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game.getGameState(), Phase.MANEUVER, self)
+                && self.getZone() == Zone.SUPPORT
+                && (
+                PlayConditions.canExertMultiple(self, game, 1, 2, Culture.GONDOR, Race.MAN)
+                        || PlayConditions.canSpot(game, 3, Keyword.KNIGHT))) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            List<Effect> possibleCosts = new LinkedList<Effect>();
+            possibleCosts.add(
+                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Culture.GONDOR, Race.MAN) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Exert 2 GONDOR Men";
+                        }
+                    });
+            possibleCosts.add(
+                    new SpotEffect(3, Keyword.KNIGHT) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Spot 3 Knights";
+                        }
+                    });
+            action.appendCost(
+                    new ChoiceEffect(action, playerId, possibleCosts));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose minion", CardType.MINION, Filters.inSkirmishAgainst(Culture.GONDOR, Race.MAN)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.insertEffect(
+                                    new TransferPermanentEffect(self, card));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
