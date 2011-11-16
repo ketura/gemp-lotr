85060955dd10eb0f5652f5dfa956e6d6ffc2a8c0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-16 13:06:29 +0000

    - "Get On and Get Away" is playable now in the Fellowship phase.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/shire/Card4_304.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/shire/Card4_304.java
@@ -4,6 +4,7 @@ import com.gempukku.lotro.cards.AbstractEvent;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
 import com.gempukku.lotro.cards.effects.AddBurdenEffect;
 import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.common.Side;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
@@ -19,7 +20,7 @@ import com.gempukku.lotro.logic.effects.PlaySiteEffect;
  */
 public class Card4_304 extends AbstractEvent {
     public Card4_304() {
-        super(Side.FREE_PEOPLE, 0, Culture.SHIRE, "Get On and Get Away");
+        super(Side.FREE_PEOPLE, 0, Culture.SHIRE, "Get On and Get Away", Phase.FELLOWSHIP);
     }
 
     @Override
