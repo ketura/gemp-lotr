6a076d18275bacfd12ba13dec72199ea73e11a6c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 15:30:26 +0000

    "Cirith Ungol Soldier"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/wraith/Card10_055.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set10.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Shadow: Spot a possession to make this minion fierce until the regroup phase.
+ */
+public class Card10_055 extends AbstractMinion {
+    public Card10_055() {
+        super(3, 8, 2, 4, Race.ORC, Culture.WRAITH, "Cirith Ungol Soldier");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
+                && PlayConditions.canSpot(game, CardType.POSSESSION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new KeywordModifier(self, self, Keyword.FIERCE), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
