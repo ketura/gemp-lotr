9fe3282e20e432d228229440c4b2965b59498e36
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 21:28:47 +0000

    "Frodo"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/shire/Card10_121.java
@@ -0,0 +1,31 @@
+package com.gempukku.lotro.cards.set10.shire;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Signet;
+
+/**
+ * Set: Mount Doom
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 0
+ * Type: Companion • Hobbit
+ * Strength: 3
+ * Vitality: 4
+ * Resistance: 12
+ * Signet: Frodo
+ * Game Text: Ring-bearer (resistance 12).
+ */
+public class Card10_121 extends AbstractCompanion {
+    public Card10_121() {
+        super(0, 3, 4, Culture.SHIRE, Race.HOBBIT, Signet.FRODO, "Frodo", true);
+        addKeyword(Keyword.CAN_START_WITH_RING);
+    }
+
+    @Override
+    public int getResistance() {
+        return 12;
+    }
+}
