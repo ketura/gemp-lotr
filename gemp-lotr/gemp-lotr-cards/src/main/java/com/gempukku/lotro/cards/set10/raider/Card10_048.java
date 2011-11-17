d974ac4f9e39e83824bb34ac2885012d51152115
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 10:58:46 +0000

    "Seasoned Leader"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/raider/Card10_048.java
@@ -0,0 +1,65 @@
+package com.gempukku.lotro.cards.set10.raider;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.cards.modifiers.VitalityModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.RemoveThreatsEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Shadow
+ * Culture: Raider
+ * Twilight Cost: 1
+ * Type: Condition
+ * Vitality: +1
+ * Game Text: Bearer must be a Southron. Bearer is enduring. Skirmish: Remove a threat to play a [RAIDER] condition from
+ * your discard pile.
+ */
+public class Card10_048 extends AbstractAttachable {
+    public Card10_048() {
+        super(Side.SHADOW, CardType.CONDITION, 1, Culture.RAIDER, null, "Seasoned Leader");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Keyword.SOUTHRON;
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new VitalityModifier(self, Filters.hasAttached(self), 1));
+        modifiers.add(
+                new KeywordModifier(self, Filters.hasAttached(self), Keyword.ENDURING));
+        return modifiers;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canRemoveThreat(game, self, 1)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Culture.RAIDER, CardType.CONDITION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveThreatsEffect(self, 1));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game.getGameState().getDiscard(playerId), Culture.RAIDER, CardType.CONDITION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
