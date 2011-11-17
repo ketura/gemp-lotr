f7ac5b3c819af506b8f56bde5429d91f1c7eae06
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 16:07:23 +0000

    "Swarming Like Beetles"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/wraith/Card10_065.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set10.wraith;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.effects.RemoveThreatsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 0
+ * Type: Condition • Support Area
+ * Game Text: Maneuver: Spot a [WRAITH] Orc and remove a threat to wound an unwounded companion.
+ */
+public class Card10_065 extends AbstractPermanent {
+    public Card10_065() {
+        super(Side.SHADOW, 0, CardType.CONDITION, Culture.WRAITH, Zone.SUPPORT, "Swarming Like Beetles");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.MANEUVER, self, 0)
+                && PlayConditions.canSpot(game, Culture.WRAITH, Race.ORC)
+                && PlayConditions.canRemoveThreat(game, self, 1)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveThreatsEffect(self, 1));
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, CardType.COMPANION, Filters.unwounded));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
