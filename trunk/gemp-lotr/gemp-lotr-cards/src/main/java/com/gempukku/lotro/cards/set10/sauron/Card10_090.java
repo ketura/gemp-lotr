d65babe1f2d437cb124aac2fb1fd2f1d1b221397
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 19:19:47 +0000

    "Mordor Brute"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/sauron/Card10_090.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set10.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.NotCondition;
+import com.gempukku.lotro.cards.modifiers.conditions.PhaseCondition;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 9
+ * Vitality: 3
+ * Site: 6
+ * Game Text: Each [SAURON] Orc bearing a possession cannot take wounds (except during skirmish phases).
+ */
+public class Card10_090 extends AbstractMinion {
+    public Card10_090() {
+        super(3, 9, 3, 6, Race.ORC, Culture.SAURON, "Mordor Brute");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        return Collections.singletonList(
+                new CantTakeWoundsModifier(self, new NotCondition(new PhaseCondition(Phase.SKIRMISH)),
+                        Filters.and(Culture.SAURON, Race.ORC, Filters.hasAttached(CardType.POSSESSION))));
+    }
+}
