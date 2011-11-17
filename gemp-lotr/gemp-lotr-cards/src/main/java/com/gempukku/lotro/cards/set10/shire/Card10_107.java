baa26cffd6bb5d9826a499a0b331b69a5dcece80
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 20:24:00 +0000

    "Great Heart"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/shire/Card10_107.java
@@ -0,0 +1,64 @@
+package com.gempukku.lotro.cards.set10.shire;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Condition
+ * Vitality: -1
+ * Game Text: Play this condition only during a skirmish. Bearer must be a Hobbit. When this condition is played, wound
+ * a minion skirmishing bearer.
+ */
+public class Card10_107 extends AbstractAttachable {
+    public Card10_107() {
+        super(Side.FREE_PEOPLE, CardType.CONDITION, 1, Culture.SHIRE, null, "Great Heart");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, Filter additionalAttachmentFilter, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, additionalAttachmentFilter, twilightModifier)
+                && PlayConditions.isPhase(game, Phase.SKIRMISH);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Race.HOBBIT;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canPlayCardDuringPhase(game, Phase.SKIRMISH, self)
+                && checkPlayRequirements(playerId, game, self, 0, false)) {
+            return Collections.singletonList(getPlayCardAction(playerId, game, self, 0, false));
+        }
+        return null;
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndWoundCharactersEffect(action, self.getOwner(), 1, 1, CardType.MINION, Filters.inSkirmishAgainst(Filters.hasAttached(self))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
