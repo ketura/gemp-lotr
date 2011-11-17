e76ca9061632c7c5c94c7eb055cd1589ece170c6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 16:14:23 +0000

    "Ulaire Cantea"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/CantExertWithCardModifier.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/CantExertWithCardModifier.java
@@ -1,23 +1,29 @@
 package com.gempukku.lotro.cards.modifiers;
 
-import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
+import com.gempukku.lotro.logic.modifiers.Condition;
 import com.gempukku.lotro.logic.modifiers.ModifierEffect;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 public class CantExertWithCardModifier extends AbstractModifier {
-    private Filter _preventExertWithFilter;
+    private Filterable _preventExertWithFilter;
 
-    public CantExertWithCardModifier(PhysicalCard source, Filter affectFilter, Filter preventExertWithFilter) {
-        super(source, "Affected by exertion preventing effect", affectFilter, ModifierEffect.WOUND_MODIFIER);
+    public CantExertWithCardModifier(PhysicalCard source, Filterable affectFilter, Filterable preventExertWithFilter) {
+        this(source, affectFilter, null, preventExertWithFilter);
+    }
+
+    public CantExertWithCardModifier(PhysicalCard source, Filterable affectFilter, Condition condition, Filterable preventExertWithFilter) {
+        super(source, "Affected by exertion preventing effect", affectFilter, condition, ModifierEffect.WOUND_MODIFIER);
         _preventExertWithFilter = preventExertWithFilter;
     }
 
     @Override
     public boolean canBeExerted(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard source, PhysicalCard card) {
-        if (_preventExertWithFilter.accepts(gameState, modifiersQuerying, source))
+        if (Filters.and(_preventExertWithFilter).accepts(gameState, modifiersQuerying, source))
             return false;
         return true;
     }
