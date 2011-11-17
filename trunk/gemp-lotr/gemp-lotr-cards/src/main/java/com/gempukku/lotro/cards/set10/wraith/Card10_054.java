bc0bf00b7a0f7371d28cd988a1c39ff2d797a232
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 15:26:57 +0000

    "Cirith Ungol Scavenger"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/wraith/Card10_054.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set10.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 3
+ * Type: Minion • Orc
+ * Strength: 8
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Each time this minion wins a skirmish, you may exert it to discard a possession borne by a character this
+ * minion is skirmishing.
+ */
+public class Card10_054 extends AbstractMinion {
+    public Card10_054() {
+        super(3, 8, 2, 4, Race.ORC, Culture.WRAITH, "Cirith Ungol Scavenger");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.POSSESSION, Filters.attachedTo(Filters.inSkirmishAgainst(self))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
