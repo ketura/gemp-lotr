3320764bcd3092169aa428f57ceba23b5945ab54
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-17 21:15:11 +0000

    "Base of Mindolluin"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set10/site/Card10_117.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set10.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Mount Doom
+ * Twilight Cost: 0
+ * Type: Site
+ * Site: 3K
+ * Game Text: Sanctuary. Fellowship: If you cannot spot 2 threats, add a threat to play a fortification from your
+ * draw deck.
+ */
+public class Card10_117 extends AbstractSite {
+    public Card10_117() {
+        super("Base of Mindolluin", Block.KING, 3, 0, Direction.RIGHT);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.FELLOWSHIP, self)
+                && !PlayConditions.canSpotThreat(game, 2)
+                && PlayConditions.canAddThreat(game, self, 1)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new AddThreatsEffect(playerId, self, 1));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Keyword.FORTIFICATION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
