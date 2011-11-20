7de8167d17d9463848622cd9bad96bb1029bfef3
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-20 03:30:38 +0000

    "Sauron"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set9/sauron/Card9_048.java
@@ -0,0 +1,36 @@
+package com.gempukku.lotro.cards.set9.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * Set: Reflections
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 18
+ * Type: Minion • Maia
+ * Strength: 24
+ * Vitality: 5
+ * Site: 6
+ * Game Text: Damage +2. Enduring. Fierce. For each burden you spot, threat you spot, and site you control, Sauron's
+ * twilight cost is -1.
+ */
+public class Card9_048 extends AbstractMinion {
+    public Card9_048() {
+        super(18, 24, 5, 6, Race.MAIA, Culture.SAURON, "Sauron", true);
+        addKeyword(Keyword.DAMAGE, 2);
+        addKeyword(Keyword.ENDURING);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        return -(gameState.getTwilightPool() + gameState.getThreats() + Filters.countSpottable(gameState, modifiersQuerying, Filters.siteControlled(self.getOwner())));
+    }
+}
