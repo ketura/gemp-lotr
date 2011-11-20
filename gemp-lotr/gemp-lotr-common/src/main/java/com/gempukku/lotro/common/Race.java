731f9b278c0ffaccc6f685e74dc9ea956bfa20a1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-20 01:23:54 +0000

    "Huorn"

--- gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Race.java
+++ gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Race.java
@@ -2,7 +2,7 @@ package com.gempukku.lotro.common;
 
 public enum Race implements Filterable {
     BALROG("Balrog"), CREATURE("Creature"), WRAITH("Wraith"),
-    ELF("Elf"), HOBBIT("Hobbit"), DWARF("Dwarf"), MAN("Man"), WIZARD("Wizard"),
+    ELF("Elf"), HOBBIT("Hobbit"), DWARF("Dwarf"), MAN("Man"), WIZARD("Wizard"), TREE("Tree"),
     URUK_HAI("Uruk-Hai"), NAZGUL("Nazgul"), ORC("Orc"), TROLL("Troll"), ENT("Ent"), SPIDER("Spider");
 
     private String _humanReadable;
