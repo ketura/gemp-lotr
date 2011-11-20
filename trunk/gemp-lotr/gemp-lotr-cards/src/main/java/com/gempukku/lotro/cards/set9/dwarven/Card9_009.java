3779be1c588f87afda681ded23e6192fa71a0b00
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-20 00:19:01 +0000

    "Ring of Retribution"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set9/dwarven/Card9_009.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set9.dwarven;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Reflections
+ * Side: Free
+ * Culture: Dwarven
+ * Twilight Cost: 0
+ * Type: Artifact • Ring
+ * Strength: +1
+ * Vitality: +1
+ * Game Text: Bearer must be a Dwarf. Response: If bearer wins a skirmish, discard this artifact to exert each Orc.
+ */
+public class Card9_009 extends AbstractAttachableFPPossession {
+    public Card9_009() {
+        super(0, 1, 1, Culture.DWARVEN, CardType.ARTIFACT, PossessionClass.RING, "Ring of Retribution", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Race.DWARF;
+    }
+
+    @Override
+    public List<? extends Action> getOptionalInPlayAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.hasAttached(self))
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ExertCharactersEffect(self, Race.ORC));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
