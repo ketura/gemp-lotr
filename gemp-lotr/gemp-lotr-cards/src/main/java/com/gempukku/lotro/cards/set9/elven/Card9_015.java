4f78c2c610a852057f1fae521b57c24d38e0e533
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-20 00:45:34 +0000

    "Gil-galad"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set9/elven/Card9_015.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set9.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Reflections
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 5
+ * Type: Companion • Elf
+ * Strength: 9
+ * Vitality: 4
+ * Resistance: 6
+ * Game Text: To play, spot 2 [ELVEN] companions. Regroup: Exert Gil-Galad to wound each wounded minion.
+ */
+public class Card9_015 extends AbstractCompanion {
+    public Card9_015() {
+        super(5, 9, 4, Culture.ELVEN, Race.ELF, null, "Gil-galad", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier, ignoreRoamingPenalty)
+                && PlayConditions.canSpot(game, 2, Culture.ELVEN, CardType.COMPANION);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self));
+            action.appendEffect(
+                    new WoundCharactersEffect(self, CardType.MINION, Filters.wounded));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
