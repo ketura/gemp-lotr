b71ff21419a50eee54b8fb6772c6e03695d5ed19
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-20 03:28:17 +0000

    "Ithil Stone"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set9/sauron/Card9_047.java
@@ -0,0 +1,78 @@
+package com.gempukku.lotro.cards.set9.sauron;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.actions.PlayPermanentAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
+import com.gempukku.lotro.logic.effects.DrawCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.StartOfPhaseResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Reflections
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 0
+ * Type: Artifact • Support Area
+ * Game Text: To play, exert a [WRAITH] or [SAURON] minion. At the start of each of your Shadow phases, you may draw
+ * a card. Shadow: Discard this artifact to play a [SAURON] minion. Its twilight cost is -3.
+ */
+public class Card9_047 extends AbstractPermanent {
+    public Card9_047() {
+        super(Side.SHADOW, 0, CardType.ARTIFACT, Culture.SAURON, Zone.SUPPORT, "Ithil Stone", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        return super.checkPlayRequirements(playerId, game, self, twilightModifier, ignoreRoamingPenalty)
+                && PlayConditions.canExert(self, game, CardType.MINION, Filters.or(Culture.WRAITH, Culture.SAURON));
+    }
+
+    @Override
+    public PlayPermanentAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayPermanentAction permanentAction = super.getPlayCardAction(playerId, game, self, twilightModifier, ignoreRoamingPenalty);
+        permanentAction.appendCost(
+                new ChooseAndExertCharactersEffect(permanentAction, playerId, 1, 1, CardType.MINION, Filters.or(Culture.WRAITH, Culture.SAURON)));
+        return permanentAction;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.SHADOW)
+                && (((StartOfPhaseResult) effectResult).getPlayerId().equals(playerId))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new DrawCardEffect(playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
+                && PlayConditions.canSelfDiscard(self, game)
+                && PlayConditions.canPlayFromHand(playerId, game, -3, Culture.SAURON, CardType.MINION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new DiscardCardsFromPlayEffect(self, self));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game, -3, Culture.SAURON, CardType.MINION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
