256018ae94fd67195baff0728c5c1ca46368cb25
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-21 02:27:42 +0000

    - "New Errand" now correctly allows to choose which minion to wound, if the minion loses that skirmish.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_128.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set4/gondor/Card4_128.java
@@ -11,11 +11,9 @@ import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.AbstractActionProxy;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
-import com.gempukku.lotro.logic.decisions.MultipleChoiceAwaitingDecision;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
 import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
-import com.gempukku.lotro.logic.effects.PlayoutDecisionEffect;
-import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -60,20 +58,13 @@ public class Card4_128 extends AbstractOldEvent {
                                 new AddUntilEndOfPhaseActionProxyEffect(
                                         new AbstractActionProxy() {
                                             @Override
-                                            public List<? extends RequiredTriggerAction> getRequiredAfterTriggers(LotroGame lotroGame, EffectResult effectResults) {
-                                                if (TriggerConditions.losesSkirmish(game, effectResults, Filters.sameCard(minion))) {
-                                                    final RequiredTriggerAction action = new RequiredTriggerAction(self);
+                                            public List<? extends OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult) {
+                                                if (TriggerConditions.losesSkirmish(game, effectResult, Filters.sameCard(minion))
+                                                        && playerId.equals(game.getGameState().getCurrentPlayerId())) {
+                                                    final OptionalTriggerAction action = new OptionalTriggerAction(self);
+                                                    action.setVirtualCardAction(true);
                                                     action.appendEffect(
-                                                            new PlayoutDecisionEffect(playerId,
-                                                                    new MultipleChoiceAwaitingDecision(1, "Would you like to wound " + minion.getBlueprint().getName(), new String[]{"Yes", "No"}) {
-                                                                        @Override
-                                                                        protected void validDecisionMade(int index, String result) {
-                                                                            if (result.equals("Yes")) {
-                                                                                action.insertEffect(
-                                                                                        new WoundCharactersEffect(self, minion));
-                                                                            }
-                                                                        }
-                                                                    }));
+                                                            new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, CardType.MINION));
                                                     return Collections.singletonList(action);
                                                 }
                                                 return null;
