511f34a4822f4dd6d18a8ccebd715193aec367f0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-11-23 20:42:02 +0000

    If a character is dying due to damage, the Sam has to be able to be used.

--- trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/rules/CharacterDeathRule.java
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/rules/CharacterDeathRule.java
@@ -15,24 +15,30 @@ import java.util.List;
 
 public class CharacterDeathRule {
     public List<RequiredTriggerAction> getKillEffects(LotroGame game) {
-        GameState gameState = game.getGameState();
+        if (game.getActionsEnvironment().findTopmostActionOfType(KillCharactersFromVitalityAction.class) == null) {
+            GameState gameState = game.getGameState();
 
-        List<PhysicalCard> deadCharacters = new LinkedList<PhysicalCard>();
+            List<PhysicalCard> deadCharacters = new LinkedList<PhysicalCard>();
 
-        Collection<PhysicalCard> characters = Filters.filterActive(gameState, game.getModifiersQuerying(),
-                Filters.or(CardType.ALLY, CardType.COMPANION, CardType.MINION));
-        for (PhysicalCard character : characters)
-            if (game.getModifiersQuerying().getVitality(gameState, character) <= 0)
-                deadCharacters.add(character);
+            Collection<PhysicalCard> characters = Filters.filterActive(gameState, game.getModifiersQuerying(),
+                    Filters.or(CardType.ALLY, CardType.COMPANION, CardType.MINION));
+            for (PhysicalCard character : characters)
+                if (game.getModifiersQuerying().getVitality(gameState, character) <= 0)
+                    deadCharacters.add(character);
 
-        if (deadCharacters.size() > 0) {
-            RequiredTriggerAction action = new RequiredTriggerAction(null);
-            action.setText("Character(s) death");
-            action.appendEffect(
-                    new KillEffect(deadCharacters, KillEffect.Cause.WOUNDS));
-            return Collections.singletonList(action);
+            if (deadCharacters.size() > 0)
+                return Collections.singletonList((RequiredTriggerAction) new KillCharactersFromVitalityAction(deadCharacters));
         }
 
         return null;
+    }
+
+    private class KillCharactersFromVitalityAction extends RequiredTriggerAction {
+        private KillCharactersFromVitalityAction(List<PhysicalCard> deadCharacters) {
+            super(null);
+            setText("Character(s) death");
+            appendEffect(
+                    new KillEffect(deadCharacters, KillEffect.Cause.WOUNDS));
+        }
     }
 }
