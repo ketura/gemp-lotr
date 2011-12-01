c91e6b3bbcfaca656da78bf86406d3d11e759609
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-01 22:06:11 +0000

    Changing order of possible costs to more intuitive.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/gondor/Card6_052.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set6/gondor/Card6_052.java
@@ -55,17 +55,17 @@ public class Card6_052 extends AbstractPermanent {
             ActivateCardAction action = new ActivateCardAction(self);
             List<Effect> possibleCosts = new LinkedList<Effect>();
             possibleCosts.add(
-                    new SelfDiscardEffect(self) {
-                        @Override
-                        public String getText(LotroGame game) {
-                            return "Discard this condition";
-                        }
-                    });
-            possibleCosts.add(
                     new RemoveTokenEffect(self, self, Token.GONDOR) {
                         @Override
                         public String getText(LotroGame game) {
                             return "Remove a GONDOR token from here";
+                        }
+                    });
+            possibleCosts.add(
+                    new SelfDiscardEffect(self) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Discard this condition";
                         }
                     });
             action.appendCost(
