f2ecf5d79e4a20915b700ab31c363bc2bdba5a70
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-02 12:16:19 +0000

    Adding archery and ambush tests.

--- gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/at/ArcheryAtTest.java
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/at/ArcheryAtTest.java
@@ -6,12 +6,13 @@ import com.gempukku.lotro.game.PhysicalCardImpl;
 import com.gempukku.lotro.logic.decisions.AwaitingDecision;
 import com.gempukku.lotro.logic.decisions.AwaitingDecisionType;
 import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
-import static junit.framework.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
 
 public class ArcheryAtTest extends AbstractAtTest {
     @Test
@@ -52,6 +53,51 @@ public class ArcheryAtTest extends AbstractAtTest {
 
         assertEquals(1, _game.getGameState().getWounds(legolas));
         assertEquals(1, _game.getGameState().getWounds(archerMinion));
+        assertEquals(Phase.ASSIGNMENT, _game.getGameState().getCurrentPhase());
+    }
+
+    @Test
+    public void archeryBonusesDontLeakToOtherSide() throws DecisionResultInvalidException {
+        Map<String, Collection<String>> extraCards = new HashMap<String, Collection<String>>();
+        initializeSimplestGame(extraCards);
+
+        PhysicalCardImpl legolas = new PhysicalCardImpl(100, "1_51", P1, _library.getLotroCardBlueprint("1_51"));
+        PhysicalCardImpl archerMinion = new PhysicalCardImpl(101, "4_138", P2, _library.getLotroCardBlueprint("4_138"));
+        PhysicalCardImpl doubleShot = new PhysicalCardImpl(102, "1_38", P1, _library.getLotroCardBlueprint("1_38"));
+
+        skipMulligans();
+
+        _game.getGameState().addCardToZone(_game, legolas, Zone.FREE_CHARACTERS);
+        _game.getGameState().addCardToZone(_game, doubleShot, Zone.HAND);
+
+        // End fellowship phase
+        assertEquals(Phase.FELLOWSHIP, _game.getGameState().getCurrentPhase());
+        playerDecided(P1, "");
+
+        _game.getGameState().addCardToZone(_game, archerMinion, Zone.SHADOW_CHARACTERS);
+
+        // End shadow phase
+        assertEquals(Phase.SHADOW, _game.getGameState().getCurrentPhase());
+        playerDecided(P2, "");
+
+        // End maneuver phase
+        playerDecided(P1, "");
+        playerDecided(P2, "");
+
+        // Play Double Shot
+        playerDecided(P1, "0");
+        // End archery phase
+        playerDecided(P2, "");
+        playerDecided(P1, "");
+
+        AwaitingDecision archeryWoundDecision = _userFeedback.getAwaitingDecision(P1);
+        assertEquals(AwaitingDecisionType.CARD_SELECTION, archeryWoundDecision.getDecisionType());
+        assertEquals(2, ((String[]) archeryWoundDecision.getDecisionParameters().get("cardId")).length);
+
+        playerDecided(P1, String.valueOf(legolas.getCardId()));
+
+        assertEquals(1, _game.getGameState().getWounds(legolas));
+        assertEquals(2, _game.getGameState().getWounds(archerMinion));
         assertEquals(Phase.ASSIGNMENT, _game.getGameState().getCurrentPhase());
     }
 }
