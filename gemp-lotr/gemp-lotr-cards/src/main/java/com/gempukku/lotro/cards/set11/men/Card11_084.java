d0a24c9164d4cb738dff8c51a799fe4b28b0cb4d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 23:59:32 +0000

    "Harad Standard-bearer"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/men/Card11_084.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set11.men;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 4
+ * Type: Minion • Man
+ * Strength: 10
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Skirmish: Remove (3) to make this minion damage +1.
+ */
+public class Card11_084 extends AbstractMinion {
+    public Card11_084() {
+        super(4, 10, 2, 4, Race.MAN, Culture.MEN, "Harad Standard-bearer");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 3)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveTwilightEffect(3));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new KeywordModifier(self, self, Keyword.DAMAGE, 1), Phase.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
