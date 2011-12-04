27d348ef1390d8e594d631bf8ae39f27c9ba6c0e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 18:52:07 +0000

    "Shadowfax"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gandalf/Card11_040.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set11.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPreventCardEffect;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.common.PossessionClass;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.AbstractPreventableCardEffect;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Possession • Mount
+ * Resistance: +2
+ * Game Text: Bearer must be Gandalf. Each time Gandalf is about to take a wound, you may add a burden to prevent that.
+ */
+public class Card11_040 extends AbstractAttachableFPPossession {
+    public Card11_040() {
+        super(2, 0, 0, Culture.GANDALF, PossessionClass.MOUNT, "Shadowfax", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.gandalf;
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new ResistanceModifier(self, Filters.hasAttached(self), 2));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalBeforeTriggers(String playerId, LotroGame game, Effect effect, PhysicalCard self) {
+        if (TriggerConditions.isGettingWounded(effect, game, Filters.gandalf)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new AddBurdenEffect(self, 1));
+            action.appendEffect(
+                    new ChooseAndPreventCardEffect(self, (AbstractPreventableCardEffect) effect, playerId, "Choose Gandalf", Filters.gandalf));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
