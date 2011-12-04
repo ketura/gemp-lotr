241f0f2a323012d7a26191d1cd799bb6fed59a1e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 18:08:38 +0000

    "Erland"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gandalf/Card11_030.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set11.gandalf;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPutCardFromDiscardIntoHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 5
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: To play, spot a [GANDALF] companion. Skirmish: Exert Erland to take a [GANDALF] skirmish event from your
+ * discard pile into hand.
+ */
+public class Card11_030 extends AbstractCompanion {
+    public Card11_030() {
+        super(2, 5, 3, 6, Culture.GANDALF, Race.MAN, null, "Erland", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.GANDALF, CardType.COMPANION);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ChooseAndPutCardFromDiscardIntoHandEffect(action, playerId, 1, 1, Culture.GANDALF, CardType.EVENT, Keyword.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
