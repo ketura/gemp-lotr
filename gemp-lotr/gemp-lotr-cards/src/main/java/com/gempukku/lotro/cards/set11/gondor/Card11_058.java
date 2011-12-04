33c23f7bdf63d889da29244dd3630f5d6629f0b9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 22:24:23 +0000

    "Bow of Minas Tirith"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gondor/Card11_058.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set11.gondor;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Possession • Ranged Weapon
+ * Strength: +1
+ * Game Text: Bearer must be a [GONDOR] Man. At the start of the archery phase, if bearer is at a battleground site,
+ * you may exert him or her to make the fellowship archery total +1.
+ */
+public class Card11_058 extends AbstractAttachableFPPossession {
+    public Card11_058() {
+        super(1, 1, 0, Culture.GONDOR, PossessionClass.RANGED_WEAPON, "Bow of Minas Tirith");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.GONDOR, Race.MAN);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.ARCHERY)
+                && PlayConditions.location(game, Keyword.BATTLEGROUND)
+                && PlayConditions.canExert(self, game, Filters.hasAttached(self))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self.getAttachedTo()));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new ArcheryTotalModifier(self, Side.FREE_PEOPLE, 1), Phase.ARCHERY));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
