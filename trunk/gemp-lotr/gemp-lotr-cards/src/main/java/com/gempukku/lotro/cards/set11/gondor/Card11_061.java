5151bb8c124ebb33b07668eb92f1cb9a9011bb54
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 22:46:09 +0000

    "Houses of Healing"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gondor/Card11_061.java
@@ -0,0 +1,80 @@
+package com.gempukku.lotro.cards.set11.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddTokenEffect;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.RemoveTokenEffect;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: When you play this condition, spot a [GONDOR] Man to add 3 [GONDOR] tokens here. Fellowship: Add (2)
+ * and either discard this condition or remove a [GONDOR] token from here to heal a [GONDOR] Man.
+ */
+public class Card11_061 extends AbstractPermanent {
+    public Card11_061() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.GONDOR, Zone.SUPPORT, "Houses of Healing", true);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)
+                && PlayConditions.canSpot(game, Culture.GONDOR, Race.MAN)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddTokenEffect(self, self, Token.GONDOR, 3));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new AddTwilightEffect(self, 2));
+            List<Effect> possibleCosts = new LinkedList<Effect>();
+            possibleCosts.add(
+                    new RemoveTokenEffect(self, self, Token.GONDOR) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Remove a GONDOR token from here";
+                        }
+                    });
+            possibleCosts.add(
+                    new SelfDiscardEffect(self) {
+                        @Override
+                        public String getText(LotroGame game) {
+                            return "Discard this condition";
+                        }
+                    });
+            action.appendCost(
+                    new ChoiceEffect(action, playerId, possibleCosts));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, Culture.GONDOR, Race.MAN));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
