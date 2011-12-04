640245b13a0cc371e11f4d8efd80fa0cca69335a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 23:41:26 +0000

    "Elevated Fire"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/men/Card11_078.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set11.men;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 7
+ * Type: Event • Archery
+ * Game Text: Toil 2. (For each [MEN] character you exert when playing this, its twilight cost is -2.) Spot a [MEN]
+ * minion to make the minion archery total +3.
+ */
+public class Card11_078 extends AbstractEvent {
+    public Card11_078() {
+        super(Side.SHADOW, 7, Culture.MEN, "Elevated Fire", Phase.ARCHERY);
+        addKeyword(Keyword.TOIL, 2);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.MEN, CardType.MINION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new AddUntilEndOfPhaseModifierEffect(
+                        new ArcheryTotalModifier(self, Side.SHADOW, 3), Phase.ARCHERY));
+        return action;
+    }
+}
