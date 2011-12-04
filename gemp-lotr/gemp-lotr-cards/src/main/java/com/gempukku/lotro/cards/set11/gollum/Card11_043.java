745ac35d8769b6c0f9576afe937e294b86a81d53
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 18:59:28 +0000

    "Horribly Strong"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gollum/Card11_043.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set11.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 2
+ * Type: Event • Shadow
+ * Game Text: Discard Gollum from play to wound an unbound companion who has resistance 6 or less.
+ */
+public class Card11_043 extends AbstractEvent {
+    public Card11_043() {
+        super(Side.SHADOW, 2, Culture.GOLLUM, "Horribly Strong", Phase.SHADOW);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canDiscardFromPlay(self, game, Filters.gollum);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Filters.gollum));
+        action.appendEffect(
+                new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, Filters.unboundCompanion, Filters.maxResistance(6)));
+        return action;
+    }
+}
