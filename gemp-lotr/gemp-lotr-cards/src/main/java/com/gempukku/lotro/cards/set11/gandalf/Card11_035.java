d96d8333fe2e16dcc6380d2b51aa778a0eb93631
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 18:22:37 +0000

    "Glamdring"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gandalf/Card11_035.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set11.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.PossessionClass;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be Gandalf. Each time Gandalf wins a skirmish, you may remove a burden.
+ */
+public class Card11_035 extends AbstractAttachableFPPossession {
+    public Card11_035() {
+        super(2, 2, 0, Culture.GANDALF, PossessionClass.HAND_WEAPON, "Glamdring", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.gandalf;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.gandalf)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new RemoveBurdenEffect(playerId, self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
