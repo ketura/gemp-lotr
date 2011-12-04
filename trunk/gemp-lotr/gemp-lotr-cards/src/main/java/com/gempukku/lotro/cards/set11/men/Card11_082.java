5952cf5738ace9ddacbb08fb46f7152b337677f4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 23:53:14 +0000

    "Footman of Dunland"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/men/Card11_082.java
@@ -0,0 +1,40 @@
+package com.gempukku.lotro.cards.set11.men;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 3
+ * Type: Minion • Man
+ * Strength: 7
+ * Vitality: 3
+ * Site: 4
+ * Game Text: The fellowship's current site gains plains.
+ */
+public class Card11_082 extends AbstractMinion {
+    public Card11_082() {
+        super(3, 7, 3, 4, Race.MAN, Culture.MEN, "Footman of Dunland");
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new KeywordModifier(self,
+                new Filter() {
+                    @Override
+                    public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                        return physicalCard == gameState.getCurrentSite();
+                    }
+                }, Keyword.PLAINS);
+    }
+}
