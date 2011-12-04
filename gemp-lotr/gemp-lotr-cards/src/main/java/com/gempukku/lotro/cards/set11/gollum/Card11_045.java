578ea5f539c46561ad568558355af06ecfb2cf0b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-04 19:05:38 +0000

    "Led Stray"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/gollum/Card11_045.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set11.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.PlaySiteEffect;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 0
+ * Type: Event • Shadow
+ * Game Text: Spot Gollum to add (3) and exchange one of your sites on the adventure path with another site from your
+ * adventure deck.
+ */
+public class Card11_045 extends AbstractEvent {
+    public Card11_045() {
+        super(Side.SHADOW, 0, Culture.GOLLUM, "Led Stray", Phase.SHADOW);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.gollum);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new AddTwilightEffect(self, 3));
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose your site", Zone.ADVENTURE_PATH, CardType.SITE, Filters.owner(playerId)) {
+                    @Override
+                    protected void cardSelected(LotroGame game, PhysicalCard card) {
+                        action.appendEffect(
+                                new PlaySiteEffect(action, playerId, null, card.getBlueprint().getSiteNumber()));
+                    }
+                });
+        return action;
+    }
+}
