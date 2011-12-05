14625407c721ee4af53f70022cd0879dca0a4edc
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-05 16:17:00 +0000

    - "Morgul Brute" and "Morgul Destroyer" optional "play" triggers now cannot be prevented by choosing to wound the Ring-bearer if the Ring-bearer cannot take a wound.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/wraith/Card7_188.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/wraith/Card7_188.java
@@ -10,7 +10,6 @@ import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.common.Race;
-import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
@@ -43,10 +42,10 @@ public class Card7_188 extends AbstractMinion {
     }
 
     @Override
-    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, final LotroGame game, EffectResult effectResult, final PhysicalCard self) {
         if (TriggerConditions.played(game, effectResult, self)
                 && PlayConditions.canSpot(game, Race.NAZGUL)) {
-            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendEffect(
                     new PreventableEffect(action,
                             new AddBurdenEffect(self, 1),
@@ -54,7 +53,7 @@ public class Card7_188 extends AbstractMinion {
                             new PreventableEffect.PreventionCost() {
                                 @Override
                                 public Effect createPreventionCostForPlayer(SubAction subAction, String playerId) {
-                                    return new WoundCharactersEffect(self, Filters.ringBearer) {
+                                    return new WoundCharactersEffect(self, game.getGameState().getRingBearer(playerId)) {
                                         @Override
                                         public String getText(LotroGame game) {
                                             return "Wound Ring-bearer";
