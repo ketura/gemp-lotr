cd54a460c5d183e116ef1f71144a011b360918bf
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-05 02:22:34 +0000

    "Conquered Halls"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/orc/Card11_112.java
@@ -0,0 +1,31 @@
+package com.gempukku.lotro.cards.set11.orc;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 1
+ * Type: Event • Skirmish
+ * Game Text: Make an [ORC] minion strength +1 for each underground site you can spot.
+ */
+public class Card11_112 extends AbstractEvent {
+    public Card11_112() {
+        super(Side.SHADOW, 1, Culture.ORC, "Conquered Halls", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, new CountSpottableEvaluator(CardType.SITE, Keyword.UNDERGROUND), Culture.ORC, CardType.MINION));
+        return action;
+    }
+}
