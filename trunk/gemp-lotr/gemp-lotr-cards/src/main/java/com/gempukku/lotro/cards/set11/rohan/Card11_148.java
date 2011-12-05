d5801eb3c5f598683b334bda6ed1ee3fe450e666
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-05 14:54:18 +0000

    "Hrothlac"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/rohan/Card11_148.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.set11.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPreventCardEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 5
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Response: If a [ROHAN] companion who has resistance 4 or more is about to take a wound, exert Hrothlac
+ * to prevent that.
+ */
+public class Card11_148 extends AbstractCompanion {
+    public Card11_148() {
+        super(2, 5, 3, 6, Culture.ROHAN, Race.MAN, null, "Hrothlac", true);
+    }
+
+    @Override
+    public List<? extends ActivateCardAction> getOptionalInPlayBeforeActions(String playerId, LotroGame game, Effect effect, PhysicalCard self) {
+        if (TriggerConditions.isGettingWounded(effect, game, Culture.ROHAN, CardType.COMPANION, Filters.minResistance(4))
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ChooseAndPreventCardEffect(self, (WoundCharactersEffect) effect, playerId, "Choose a companion", Culture.ROHAN, CardType.COMPANION, Filters.minResistance(4)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
