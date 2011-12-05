817fe2f27bed5152f8c0eea01976491e4e2a92e5
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-05 02:32:38 +0000

    "Denizen of the Black Pit"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/orc/Card11_117.java
@@ -0,0 +1,32 @@
+package com.gempukku.lotro.cards.set11.orc;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 4
+ * Type: Minion • Orc
+ * Strength: 10
+ * Vitality: 3
+ * Site: 4
+ * Game Text: While this minion is at an underground site, it is strength +2.
+ */
+public class Card11_117 extends AbstractMinion {
+    public Card11_117() {
+        super(4, 10, 3, 4, Race.ORC, Culture.ORC, "Denizen of the Black Pit");
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new LocationCondition(Keyword.UNDERGROUND), 2);
+    }
+}
