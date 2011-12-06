e6fb440c4935aa274327e8fd3bd7de768bbf54bf
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 14:37:52 +0000

    "Isengard Siege Bow"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/VitalityModifier.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/VitalityModifier.java
@@ -1,6 +1,6 @@
 package com.gempukku.lotro.cards.modifiers;
 
-import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.AbstractModifier;
@@ -10,7 +10,7 @@ import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 public class VitalityModifier extends AbstractModifier {
     private int _modifier;
 
-    public VitalityModifier(PhysicalCard source, Filter affectFilter, int modifier) {
+    public VitalityModifier(PhysicalCard source, Filterable affectFilter, int modifier) {
         super(source, "Vitality " + ((modifier < 0) ? modifier : ("+" + modifier)), affectFilter, ModifierEffect.VITALITY_MODIFIER);
         _modifier = modifier;
     }
