adc13025ef511f221a7406cc3963a3cdcaf24646
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 12:27:53 +0000

    "Army of Uruk-hai"

--- trunk/gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Culture.java
+++ trunk/gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Culture.java
@@ -2,7 +2,7 @@ package com.gempukku.lotro.common;
 
 public enum Culture implements Filterable {
     DWARVEN("Dwarven"), ELVEN("Elven"), GANDALF("Gandalf"), GOLLUM("Gollum"), GONDOR("Gondor"), ROHAN("Rohan"), SHIRE("Shire"),
-    DUNLAND("Dunland"), ISENGARD("Isengard"), MEN("Men"), MORIA("Moria"), ORC("Orc"), RAIDER("Raider"), SAURON("Sauron"), WRAITH("Wraith");
+    DUNLAND("Dunland"), ISENGARD("Isengard"), MEN("Men"), MORIA("Moria"), ORC("Orc"), RAIDER("Raider"), SAURON("Sauron"), URUK_HAI("Uruk-hai"), WRAITH("Wraith");
 
     private String _humanReadable;
 
