ed3a7199ccdc7f7e1cdc8e67bbc7116c30ce78da
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 22:44:44 +0000

    "Buckland Homestead"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_230.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 0
+ * Type: Site
+ * Game Text: Dwelling. Shadow: Play a Nazgul to discard a condition from play.
+ */
+public class Card11_230 extends AbstractNewSite {
+    public Card11_230() {
+        super("Buckland Homestead", 0, Direction.LEFT);
+        addKeyword(Keyword.DWELLING);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.SHADOW, self)
+                && PlayConditions.canPlayFromHand(playerId, game, Race.NAZGUL)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game, Race.NAZGUL));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.CONDITION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
