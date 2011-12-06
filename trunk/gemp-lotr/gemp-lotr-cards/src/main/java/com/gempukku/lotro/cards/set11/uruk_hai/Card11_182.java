885e84ebb24621d4a58073848c7eb213b801e835
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 12:49:46 +0000

    "Devastation"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/uruk_hai/Card11_182.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set11.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractResponseEvent;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 1
+ * Type: Event • Response
+ * Game Text: If your [URUK-HAI] minion wins a skirmish, discard a condition from play.
+ */
+public class Card11_182 extends AbstractResponseEvent {
+    public Card11_182() {
+        super(Side.SHADOW, 1, Culture.URUK_HAI, "Devastation");
+    }
+
+    @Override
+    public List<PlayEventAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.owner(playerId), Culture.URUK_HAI, CardType.MINION)) {
+            PlayEventAction action = new PlayEventAction(self);
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.CONDITION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
