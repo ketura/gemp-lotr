214db2f913d9cc9b09f686502f79af0bf4d059d6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 22:53:32 +0000

    "Crags of Emyn Muil"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_234.java
@@ -0,0 +1,40 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 0
+ * Type: Site
+ * Game Text: Mountain. If this site is in region 2 when the fellowship moves from it during the regroup phase, wound
+ * each companion.
+ */
+public class Card11_234 extends AbstractNewSite {
+    public Card11_234() {
+        super("Crags of Emyn Muil", 0, Direction.LEFT);
+        addKeyword(Keyword.MOUNTAIN);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.movesFrom(game, effectResult, self, Filters.region(2))) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new WoundCharactersEffect(self, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
