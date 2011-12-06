05d2ee60eefc60ca9efa841df1aa44a80c836c19
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 23:25:05 +0000

    "Harrowdale"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_243.java
@@ -0,0 +1,69 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.modifiers.RemoveKeywordModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.UnrespondableEffect;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 3
+ * Type: Site
+ * Game Text: Plains. Until the regroup phase, each minion skirmishing a [ROHAN] companion loses fierce and cannot gain
+ * fierce.
+ */
+public class Card11_243 extends AbstractNewSite {
+    public Card11_243() {
+        super("Harrowdale", 3, Direction.RIGHT);
+        addKeyword(Keyword.PLAINS);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        Set<Integer> minionsMarked = (Set<Integer>) self.getData();
+        if (minionsMarked == null) {
+            minionsMarked = new HashSet<Integer>();
+            self.storeData(minionsMarked);
+        }
+
+        Set<PhysicalCard> toLose = new HashSet<PhysicalCard>();
+        final Set<Integer> toLoseInts = new HashSet<Integer>();
+        for (PhysicalCard minion : Filters.filterActive(game.getGameState(), game.getModifiersQuerying(), Filters.inSkirmishAgainst(Culture.ROHAN, CardType.COMPANION))) {
+            if (!minionsMarked.contains(minion.getCardId())) {
+                toLose.add(minion);
+                toLoseInts.add(minion.getCardId());
+            }
+        }
+
+        if (toLose.size() > 0) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            final Set<Integer> minionsMarked1 = minionsMarked;
+            action.appendEffect(
+                    new UnrespondableEffect() {
+                        @Override
+                        protected void doPlayEffect(LotroGame game) {
+                            minionsMarked1.addAll(toLoseInts);
+                        }
+                    });
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new RemoveKeywordModifier(self, Filters.in(toLose), Keyword.FIERCE), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
