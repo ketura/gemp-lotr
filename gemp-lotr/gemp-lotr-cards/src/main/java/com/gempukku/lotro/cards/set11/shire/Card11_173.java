83b1707b6a6566842c42772dda2ac23af6345a6a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 12:14:21 +0000

    "Sting"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/shire/Card11_173.java
@@ -0,0 +1,76 @@
+package com.gempukku.lotro.cards.set11.shire;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be Bilbo, Frodo, or Sam. Maneuver: If bearer is Bilbo or Frodo, exert him to make another
+ * companion resistance +2 until the regroup phase. If bearer is Sam, each time he wins a skirmish, you may remove
+ * a burden.
+ */
+public class Card11_173 extends AbstractAttachableFPPossession {
+    public Card11_173() {
+        super(1, 2, 0, Culture.SHIRE, PossessionClass.HAND_WEAPON, "Sting", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.or(Filters.name("Bilbo"), Filters.frodo, Filters.sam);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.MANEUVER, self)
+                && Filters.or(Filters.name("Bilbo"), Filters.frodo).accepts(game.getGameState(), game.getModifiersQuerying(), self.getAttachedTo())
+                && PlayConditions.canExert(self, game, self.getAttachedTo())) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self.getAttachedTo()));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a companion", CardType.COMPANION, Filters.not(self.getAttachedTo())) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new ResistanceModifier(self, card, 2), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.hasAttached(self), Filters.sam)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new RemoveBurdenEffect(playerId, self, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
