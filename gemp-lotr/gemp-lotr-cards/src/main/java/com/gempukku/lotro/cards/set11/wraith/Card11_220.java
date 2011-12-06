f2c7cee7cd4acdc202269a2d5807dee986f030d7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 17:37:10 +0000

    "Ulaire Cantea"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/wraith/Card11_220.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set11.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 5
+ * Type: Minion • Nazgul
+ * Strength: 10
+ * Vitality: 3
+ * Site: 3
+ * Game Text: While Ulaire Cantea is at a forest or dwelling site, he is strength +2 and fierce.
+ */
+public class Card11_220 extends AbstractMinion {
+    public Card11_220() {
+        super(5, 10, 3, 3, Race.NAZGUL, Culture.WRAITH, "Ulaire Cantea", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, self, new LocationCondition(Filters.or(Keyword.FOREST, Keyword.DWELLING)), 2));
+        modifiers.add(
+                new KeywordModifier(self, self, new LocationCondition(Filters.or(Keyword.FOREST, Keyword.DWELLING)), Keyword.FIERCE, 1));
+        return modifiers;
+    }
+}
