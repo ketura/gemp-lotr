488eda3599ff7f6e0b77d383766542a230fc2fa2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 14:26:55 +0000

    "Hounding Uruk"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/uruk_hai/Card11_188.java
@@ -0,0 +1,35 @@
+package com.gempukku.lotro.cards.set11.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: Shadows
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 2
+ * Type: Minion • Uruk-Hai
+ * Strength: 5
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. While you can spot an exhausted companion, this minion is strength +3.
+ */
+public class Card11_188 extends AbstractMinion {
+    public Card11_188() {
+        super(2, 5, 2, 5, Race.URUK_HAI, Culture.URUK_HAI, "Hounding Uruk");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new SpotCondition(CardType.COMPANION, Filters.exhausted), 3);
+    }
+}
