66ee2e825393661dc9597a853ee333fcfccfb65b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-06 22:42:33 +0000

    "Barazinbar"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_229.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 2
+ * Type: Site
+ * Game Text: Mountain. At the start of your fellowship phase, you may exert a Dwarf to play a [DWARF] possession from
+ * your draw deck.
+ */
+public class Card11_229 extends AbstractNewSite {
+    public Card11_229() {
+        super("Barazinbar", 2, Direction.LEFT);
+        addKeyword(Keyword.MOUNTAIN);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.FELLOWSHIP)
+                && playerId.equals(game.getGameState().getCurrentPlayerId())
+                && PlayConditions.canExert(self, game, Race.DWARF)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Race.DWARF));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Culture.DWARVEN, CardType.POSSESSION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
