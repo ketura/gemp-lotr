b42ef494cdcebee41ac874fd5a72aa530506a063
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-07 00:35:02 +0000

    "Rohan Uplands"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_257.java
@@ -0,0 +1,30 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.PhaseCondition;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 1
+ * Type: Site
+ * Game Text: Plains. During the archery phase, [URUK] minions cannot take wounds.
+ */
+public class Card11_257 extends AbstractNewSite {
+    public Card11_257() {
+        super("Rohan Uplands", 1, Direction.RIGHT);
+        addKeyword(Keyword.PLAINS);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new CantTakeWoundsModifier(self, new PhaseCondition(Phase.ARCHERY), Filters.and(Culture.URUK_HAI, CardType.MINION));
+    }
+}
