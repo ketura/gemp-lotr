cdf288a108d890dc9564448c02cc2e7b03907517
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-07 00:45:29 +0000

    "Watch-tower of Cirith Ungol"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_262.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.DiscardCardAtRandomFromHandEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 1
+ * Type: Site
+ * Game Text: Mountain. Maneuver: Exert your [ORC] minion to discard a card at random from the Free Peoples
+ * player's hand.
+ */
+public class Card11_262 extends AbstractNewSite {
+    public Card11_262() {
+        super("Watch-tower of Cirith Ungol", 1, Direction.RIGHT);
+        addKeyword(Keyword.MOUNTAIN);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.MANEUVER, self)
+                && PlayConditions.canExert(self, game, Culture.ORC, CardType.MINION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Culture.ORC, CardType.MINION));
+            action.appendEffect(
+                    new DiscardCardAtRandomFromHandEffect(self, game.getGameState().getCurrentPlayerId(), true));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
