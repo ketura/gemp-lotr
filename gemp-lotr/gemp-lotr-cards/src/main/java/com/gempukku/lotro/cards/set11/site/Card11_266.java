c179e0758ab1147c1a2cfdaef785a5d44c316e97
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-07 00:52:25 +0000

    "Woody-End"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_266.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set11.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Shadows
+ * Twilight Cost: 0
+ * Type: Site
+ * Game Text: Forest. Shadow: Remove a burden to play a Nazgul from your discard pile.
+ */
+public class Card11_266 extends AbstractNewSite {
+    public Card11_266() {
+        super("Woody-End", 0, Direction.LEFT);
+        addKeyword(Keyword.FOREST);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.SHADOW, self)
+                && PlayConditions.canRemoveBurdens(game, self, 1)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Race.NAZGUL)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveBurdenEffect(playerId, self, 1));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game, Race.NAZGUL));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
