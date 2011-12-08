4cad6cd4ceffcc86359f2dcf987897a8120aab46
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-08 17:47:39 +0000

    "Seclusion"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/elven/Card12_023.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set12.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Black Rider
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 3
+ * Type: Event • Maneuver
+ * Game Text: Toil 2. (For each [ELVEN] character you exert when playing this, its twilight cost is -2) Spot an Elf
+ * companion to discard a condition from play.
+ */
+public class Card12_023 extends AbstractEvent {
+    public Card12_023() {
+        super(Side.FREE_PEOPLE, 3, Culture.ELVEN, "Seclusion", Phase.MANEUVER);
+        addKeyword(Keyword.TOIL, 2);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Race.ELF, CardType.COMPANION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.CONDITION));
+        return action;
+    }
+}
