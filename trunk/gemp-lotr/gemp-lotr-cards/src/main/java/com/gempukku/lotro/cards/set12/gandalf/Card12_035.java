bc13ea24689166134dc6922fa716aa1ce1ad2da2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-09 12:28:57 +0000

    "Watch and Wait"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/gandalf/Card12_035.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set12.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Condition
+ * Game Text: To play, spot a [GANDALF] companion. Bearer must be a companion (except the Ring-bearer).
+ * Limit 1 per bearer. Each time bearer wins a skirmish, you may remove a burden.
+ */
+public class Card12_035 extends AbstractAttachable {
+    public Card12_035() {
+        super(Side.FREE_PEOPLE, CardType.CONDITION, 2, Culture.GANDALF, null, "Watch and Wait");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, Filter additionalAttachmentFilter, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, additionalAttachmentFilter, twilightModifier)
+                && PlayConditions.canSpot(game, Culture.GANDALF, CardType.COMPANION);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(CardType.COMPANION, Filters.not(Filters.hasAttached(Filters.name(getName())), Filters.ringBearer));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, self.getAttachedTo())) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new RemoveBurdenEffect(playerId, self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
