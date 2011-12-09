79aeda062ab436d858085ec80521ebd64c2c2c22
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-09 11:41:55 +0000

    "Discoveries"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/PutCardFromDeckOnTopOfDeckEffect.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.effects;
+
+import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.timing.AbstractEffect;
+
+import java.util.Collections;
+
+public class PutCardFromDeckOnTopOfDeckEffect extends AbstractEffect {
+    private PhysicalCard _source;
+    private PhysicalCard _physicalCard;
+
+    public PutCardFromDeckOnTopOfDeckEffect(PhysicalCard source, PhysicalCard physicalCard) {
+        _physicalCard = physicalCard;
+        _source = source;
+    }
+
+    @Override
+    public boolean isPlayableInFull(LotroGame game) {
+        return _physicalCard.getZone() == Zone.DECK;
+    }
+
+    @Override
+    public String getText(LotroGame game) {
+        return "Put " + GameUtils.getCardLink(_physicalCard) + " on the top of draw deck";
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+
+    @Override
+    protected FullEffectResult playEffectReturningResult(LotroGame game) {
+        if (isPlayableInFull(game)) {
+            GameState gameState = game.getGameState();
+            gameState.removeCardsFromZone(_source.getOwner(), Collections.singleton(_physicalCard));
+            gameState.putCardOnTopOfDeck(_physicalCard);
+            return new FullEffectResult(true, true);
+        }
+        return new FullEffectResult(false, false);
+    }
+}
---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/gandalf/Card12_026.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set12.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ForEachYouSpotEffect;
+import com.gempukku.lotro.cards.effects.ReorderTopCardsOfDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Black Rider
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 7
+ * Type: Event • Fellowship
+ * Game Text: Toil 3. (For each [GANDALF] character you exert when playing this, its twilight cost is -3)
+ * Spot a [GANDALF] wizard and X other companions to examine the top X cards of your draw deck. Replace those cards
+ * in any order.
+ */
+public class Card12_026 extends AbstractEvent {
+    public Card12_026() {
+        super(Side.FREE_PEOPLE, 7, Culture.GANDALF, "Discoveries", Phase.FELLOWSHIP);
+        addKeyword(Keyword.TOIL, 3);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.GANDALF, Race.WIZARD);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ForEachYouSpotEffect(playerId, CardType.COMPANION, Filters.not(Culture.GANDALF, Race.WIZARD)) {
+                    @Override
+                    protected void spottedCards(int spotCount) {
+                        action.appendEffect(
+                                new ReorderTopCardsOfDeckEffect(action playerId, spotCount));
+                    }
+                });
+        return action;
+    }
+}
