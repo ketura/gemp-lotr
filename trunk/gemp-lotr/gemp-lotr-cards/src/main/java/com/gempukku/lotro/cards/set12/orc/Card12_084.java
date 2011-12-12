58627d274b30282b75cf8d92afe24c67ce228c37
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-12 12:59:56 +0000

    "Bloodstained Field"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/orc/Card12_084.java
@@ -0,0 +1,37 @@
+package com.gempukku.lotro.cards.set12.orc;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 1
+ * Type: Event • Archery
+ * Game Text: Spot an [ORC] lurker to wound an Elf.
+ */
+public class Card12_084 extends AbstractEvent {
+    public Card12_084() {
+        super(Side.SHADOW, 1, Culture.ORC, "Bloodstained Field", Phase.ARCHERY);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.ORC, Keyword.LURKER);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, Race.ELF));
+        return action;
+    }
+}
