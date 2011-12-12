14d901fab9294cca405fa454168a9d37ae1f4d5e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-12 12:11:57 +0000

    "Last Days"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/men/Card12_071.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set12.men;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 0
+ * Type: Condition • Support Area
+ * Game Text: Each time a [MEN] minion is killed or discarded from play (except during the regroup phase), you may play
+ * a [MEN] minion. Its twilight cost is -2, and it is fierce and strength +2 until the regroup phase.
+ */
+public class Card12_071 extends AbstractPermanent {
+    public Card12_071() {
+        super(Side.SHADOW, 0, CardType.CONDITION, Culture.MEN, Zone.SUPPORT, "Last Days", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if ((TriggerConditions.forEachKilled(game, effectResult, CardType.MINION, Culture.MEN)
+                || TriggerConditions.forEachDiscardedFromPlay(game, effectResult, CardType.MINION, Culture.MEN))
+                && !PlayConditions.isPhase(game, Phase.REGROUP)
+                && PlayConditions.canPlayFromHand(playerId, game, -2, Culture.MEN, CardType.MINION)) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game, -2, Culture.MEN, CardType.MINION) {
+                        @Override
+                        protected void afterCardPlayed(PhysicalCard cardPlayed) {
+                            action.appendEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new StrengthModifier(self, cardPlayed, 2), Phase.REGROUP));
+                            action.appendEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new KeywordModifier(self, cardPlayed, Keyword.FIERCE), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
