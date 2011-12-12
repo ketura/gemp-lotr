802403395f7afddee1dd1bb5366ecfb0ffba7ccc
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-12 12:17:48 +0000

    "The Mouth of Sauron"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/men/Card12_073.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set12.men;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 3
+ * Type: Minion • Man
+ * Strength: 9
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Maneuver: Exert The Mouth of Sauron to play a [MEN] condition or [MEN] possession from your draw deck.
+ */
+public class Card12_073 extends AbstractMinion {
+    public Card12_073() {
+        super(3, 9, 3, 5, Race.MAN, Culture.MEN, "The Mouth of Sauron", true);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.MANEUVER, self, 0)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Culture.MEN, Filters.or(CardType.CONDITION, CardType.POSSESSION)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
