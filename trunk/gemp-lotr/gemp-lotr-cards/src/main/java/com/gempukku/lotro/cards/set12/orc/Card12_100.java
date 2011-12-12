750994a14df9c12cfd197ddf98ef701855af672f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-12 17:28:25 +0000

    "Rallying Orc"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/orc/Card12_100.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/orc/Card12_100.java
@@ -1,10 +1,12 @@
 package com.gempukku.lotro.cards.set12.orc;
 
 import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
@@ -21,25 +23,25 @@ import java.util.List;
  * Culture: Orc
  * Twilight Cost: 3
  * Type: Minion • Orc
- * Strength: 7
+ * Strength: 9
  * Vitality: 2
  * Site: 4
- * Game Text: When you play this minion, you may make the Free Peoples player exert a companion for each companion
- * over 4.
+ * Game Text: Lurker. (Skirmishes involving lurker minions must be resolved after any others.) Each time you play
+ * another [ORC] lurker at a battleground or underground site, you may make the Free Peoples player exert a companion.
  */
-public class Card12_099 extends AbstractMinion {
-    public Card12_099() {
-        super(3, 7, 2, 4, Race.ORC, Culture.ORC, "Pitiless Orc");
+public class Card12_100 extends AbstractMinion {
+    public Card12_100() {
+        super(3, 9, 2, 4, Race.ORC, Culture.ORC, "Rallying Orc", true);
+        addKeyword(Keyword.LURKER);
     }
 
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.played(game, effectResult, self)) {
+        if (TriggerConditions.played(game, effectResult, Filters.not(self), Keyword.LURKER)
+                && PlayConditions.location(game, Filters.or(Keyword.BATTLEGROUND, Keyword.UNDERGROUND))) {
             OptionalTriggerAction action = new OptionalTriggerAction(self);
-            int count = Math.max(0, Filters.countActive(game.getGameState(), game.getModifiersQuerying(), CardType.COMPANION) - 4);
-            for (int i = 0; i < count; i++)
-                action.appendEffect(
-                        new ChooseAndExertCharactersEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, CardType.COMPANION));
+            action.appendEffect(
+                    new ChooseAndExertCharactersEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, CardType.COMPANION));
             return Collections.singletonList(action);
         }
         return null;
