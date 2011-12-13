dd11b7d68e050abb183a2ad4fcf0c21d0fdfa9cc
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-13 00:13:39 +0000

    FotR block boosters, starters and league.

--- gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/packs/LeagueStarterBox.java
+++ gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/packs/LeagueStarterBox.java
@@ -12,12 +12,23 @@ public class LeagueStarterBox implements PackBox {
     @Override
     public List<CardCollection.Item> openPack() {
         List<CardCollection.Item> result = new LinkedList<CardCollection.Item>();
-        if (_random.nextBoolean()) {
+        int starter = _random.nextInt(6);
+        if (starter == 0) {
             result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 1, "FotR - Gandalf Starter"));
-        } else {
+        } else if (starter == 1) {
             result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 1, "FotR - Aragorn Starter"));
+        } else if (starter == 2) {
+            result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 1, "MoM - Gandalf Starter"));
+        } else if (starter == 3) {
+            result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 1, "MoM - Gimli Starter"));
+        } else if (starter == 4) {
+            result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 1, "RotEL - Boromir Starter"));
+        } else if (starter == 5) {
+            result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 1, "RotEL - Legolas Starter"));
         }
         result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 2, "FotR - Booster"));
+        result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 2, "MoM - Booster"));
+        result.add(new CardCollection.Item(CardCollection.Item.Type.PACK, 2, "RotEL - Booster"));
         return result;
     }
 }
