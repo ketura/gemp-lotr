b09a2fd9264b833997abf305c8f8e22eb65974f0
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-13 13:08:22 +0000

    "Aldred"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/rohan/Card12_107.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set12.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 1
+ * Type: Companion • Man
+ * Strength: 5
+ * Vitality: 3
+ * Resistance: 5
+ * Game Text: At the start of each skirmish involving Aldred, you may discard a possession borne by a minion he
+ * is skirmishing.
+ */
+public class Card12_107 extends AbstractCompanion {
+    public Card12_107() {
+        super(1, 5, 3, 5, Culture.ROHAN, Race.MAN, null, "Aldred", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.SKIRMISH)
+                && Filters.inSkirmish.accepts(game.getGameState(), game.getModifiersQuerying(), self)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.POSSESSION, Filters.attachedTo(CardType.MINION, Filters.inSkirmishAgainst(self))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
