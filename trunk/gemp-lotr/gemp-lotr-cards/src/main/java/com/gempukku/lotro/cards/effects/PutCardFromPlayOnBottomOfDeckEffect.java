22aadbc60ab5656890aa79a88f01416426dbc093
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-13 23:13:29 +0000

    "Merciless Uruk"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/PutCardFromPlayOnBottomOfDeckEffect.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.effects;
+
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.timing.AbstractEffect;
+
+import java.util.Collections;
+
+public class PutCardFromPlayOnBottomOfDeckEffect extends AbstractEffect {
+    private PhysicalCard _physicalCard;
+
+    public PutCardFromPlayOnBottomOfDeckEffect(PhysicalCard physicalCard) {
+        _physicalCard = physicalCard;
+    }
+
+    @Override
+    public boolean isPlayableInFull(LotroGame game) {
+        return _physicalCard.getZone().isInPlay();
+    }
+
+    @Override
+    protected FullEffectResult playEffectReturningResult(LotroGame game) {
+        if (isPlayableInFull(game)) {
+            GameState gameState = game.getGameState();
+            gameState.sendMessage(_physicalCard.getOwner() + " puts " + GameUtils.getCardLink(_physicalCard) + " from play on the bottom of deck");
+            gameState.removeCardsFromZone(_physicalCard.getOwner(), Collections.singleton(_physicalCard));
+            gameState.putCardOnBottomOfDeck(_physicalCard);
+            return new FullEffectResult(true, true);
+        }
+        return new FullEffectResult(false, false);
+    }
+
+    @Override
+    public String getText(LotroGame game) {
+        return "Put " + GameUtils.getCardLink(_physicalCard) + " from play on bottom of deck";
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+}
