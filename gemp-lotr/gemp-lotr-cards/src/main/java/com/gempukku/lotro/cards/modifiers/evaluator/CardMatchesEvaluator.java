364e82b2e66fb263dc8bd1b2c0c42959a37187b8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-13 13:15:43 +0000

    "Challenging the Orc-host"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/evaluator/CardMatchesEvaluator.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/evaluator/CardMatchesEvaluator.java
@@ -5,21 +5,28 @@ import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.evaluator.ConstantEvaluator;
 import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 
 public class CardMatchesEvaluator implements Evaluator {
     private Filterable[] _filters;
-    private int _matches;
+    private Evaluator _matches;
     private int _default;
 
-    public CardMatchesEvaluator(int defaultValue, int matches, Filterable... filters) {
+    public CardMatchesEvaluator(int defaultValue, Evaluator matches, Filterable... filters) {
         _default = defaultValue;
         _matches = matches;
         _filters = filters;
     }
 
+    public CardMatchesEvaluator(int defaultValue, int matches, Filterable... filters) {
+        _default = defaultValue;
+        _matches = new ConstantEvaluator(matches);
+        _filters = filters;
+    }
+
     @Override
     public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
-        return Filters.and(_filters).accepts(gameState, modifiersQuerying, self) ? _matches : _default;
+        return Filters.and(_filters).accepts(gameState, modifiersQuerying, self) ? _matches.evaluateExpression(gameState, modifiersQuerying, self) : _default;
     }
 }
