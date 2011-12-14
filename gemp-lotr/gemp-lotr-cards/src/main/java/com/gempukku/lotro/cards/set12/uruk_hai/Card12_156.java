6af36385341532dbc18c884009ec413cbb112070
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 16:39:28 +0000

    "Uruk-hai Guard"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/uruk_hai/Card12_156.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set12.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.modifiers.PreventMinionBeingAssignedToCharacterModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 11
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. Assignment: Exert this minion and spot a companion to prevent the Free Peoples player from
+ * assigning that companion to this minion.
+ */
+public class Card12_156 extends AbstractMinion {
+    public Card12_156() {
+        super(4, 11, 2, 5, Race.URUK_HAI, Culture.URUK_HAI, "Uruk-hai Guard");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.ASSIGNMENT, self, 0)
+                && PlayConditions.canSelfExert(self, game)
+                && PlayConditions.canSpot(game, CardType.COMPANION)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendCost(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a companion", CardType.COMPANION) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new PreventMinionBeingAssignedToCharacterModifier(self, Side.FREE_PEOPLE, card, self), Phase.ASSIGNMENT));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
