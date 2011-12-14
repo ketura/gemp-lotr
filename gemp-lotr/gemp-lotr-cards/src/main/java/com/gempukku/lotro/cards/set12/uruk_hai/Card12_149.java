03ed803b5e23d19f25380520e76d0e2d17736e65
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 15:31:53 +0000

    "Uruk Common"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/uruk_hai/Card12_149.java
@@ -0,0 +1,57 @@
+package com.gempukku.lotro.cards.set12.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 11
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Damage +1. Each time this minion is assigned to a skirmish, if it is not at a battleground site, remove
+ * (2) or discard this minion.
+ */
+public class Card12_149 extends AbstractMinion {
+    public Card12_149() {
+        super(4, 11, 3, 5, Race.URUK_HAI, Culture.URUK_HAI, "Uruk Common");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.assignedAgainst(game, effectResult, null, Filters.any, self)
+                && !PlayConditions.location(game, Keyword.BATTLEGROUND)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            List<Effect> possibleEffects = new LinkedList<Effect>();
+            possibleEffects.add(
+                    new RemoveTwilightEffect(2));
+            possibleEffects.add(
+                    new SelfDiscardEffect(self));
+            action.appendEffect(
+                    new ChoiceEffect(action, self.getOwner(), possibleEffects));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
