3c7b75cdcafc66c314e7aaec62364a3337241049
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 20:25:29 +0000

    "Ulaire Lemenya"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/wraith/Card12_176.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set12.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.PossessionClass;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 4
+ * Type: Minion • Nazgul
+ * Strength: 9
+ * Vitality: 2
+ * Site: 3
+ * Game Text: When you play Ulaire Lemenya, you may play a [WRAITH] mount from your discard pile.
+ */
+public class Card12_176 extends AbstractMinion {
+    public Card12_176() {
+        super(4, 9, 2, 3, Race.NAZGUL, Culture.WRAITH, "Ulaire Lemenya", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Culture.WRAITH, PossessionClass.MOUNT)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game, Culture.WRAITH, PossessionClass.MOUNT));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
