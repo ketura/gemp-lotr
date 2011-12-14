c2ba6aa2bd9cf9d9735ec71eaad6491eeda9cd59
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 15:06:04 +0000

    "Shingle in a Storm"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/uruk_hai/Card12_145.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set12.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAssignCharacterToMinionEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 0
+ * Type: Condition • Support Area
+ * Game Text: While you can spot 6 companions, each [URUK-HAI] minion is damage +1. Assignment: Discard this condition
+ * to assign an [URUK-HAI] minion to a companion who has resistance 2 or less.
+ */
+public class Card12_145 extends AbstractPermanent {
+    public Card12_145() {
+        super(Side.SHADOW, 0, CardType.CONDITION, Culture.URUK_HAI, Zone.SUPPORT, "Shingle in a Storm");
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new KeywordModifier(self, Filters.and(Culture.URUK_HAI, CardType.MINION), new SpotCondition(6, CardType.COMPANION), Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.ASSIGNMENT, self, 0)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose an URUK-HAI minion", Culture.URUK_HAI, CardType.MINION, Filters.canBeAssignedToSkirmishByEffect(Side.SHADOW)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new ChooseAndAssignCharacterToMinionEffect(action, playerId, card, CardType.COMPANION, Filters.maxResistance(2)));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
