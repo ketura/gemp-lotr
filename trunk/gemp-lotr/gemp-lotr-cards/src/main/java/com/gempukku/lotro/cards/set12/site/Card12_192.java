760702f40266ed929f630ed7c042c7111397bf90
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 23:45:08 +0000

    "Slopes of Orodruin"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/site/Card12_192.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set12.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Twilight Cost: 1
+ * Type: Site
+ * Game Text: Mountain. Shadow: Remove 2 and discard 2 Free Peoples cards from hand to draw a card.
+ */
+public class Card12_192 extends AbstractNewSite {
+    public Card12_192() {
+        super("Slopes of Orodruin", 1, Direction.RIGHT);
+        addKeyword(Keyword.MOUNTAIN);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.SHADOW, self)
+                && game.getGameState().getTwilightPool() >= 2
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Side.FREE_PEOPLE)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveTwilightEffect(2));
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2, Side.FREE_PEOPLE));
+            action.appendEffect(
+                    new DrawCardsEffect(playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
