3b2212c2b0e3ed736661f93df9d6d9716bdb9d21
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 20:12:41 +0000

    "Ulaire Cantea"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/wraith/Card12_174.java
@@ -0,0 +1,61 @@
+package com.gempukku.lotro.cards.set12.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAssignCharacterToMinionEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.KillEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 5
+ * Type: Minion • Nazgul
+ * Strength: 10
+ * Vitality: 3
+ * Site: 3
+ * Game Text: Fierce. Assignment: Assign Ulaire Cantea to a companion who has resistance 0. Skirmish: Spot 6 companions
+ * and another [WRAITH] card to kill a companion Ulaire Cantea is skirmishing.
+ */
+public class Card12_174 extends AbstractMinion {
+    public Card12_174() {
+        super(5, 10, 3, 3, Race.NAZGUL, Culture.WRAITH, "Ulaire Cantea", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.ASSIGNMENT, self, 0)
+                && PlayConditions.canBeAssignedToSkirmishByEffect(self, game, self)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new ChooseAndAssignCharacterToMinionEffect(action, playerId, self, CardType.COMPANION, Filters.maxResistance(0)));
+            return Collections.singletonList(action);
+        }
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSpot(game, 6, CardType.COMPANION)
+                && PlayConditions.canSpot(game, Filters.not(self), Culture.WRAITH)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a companion", CardType.COMPANION, Filters.inSkirmishAgainst(self)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new KillEffect(card, KillEffect.Cause.CARD_EFFECT));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
