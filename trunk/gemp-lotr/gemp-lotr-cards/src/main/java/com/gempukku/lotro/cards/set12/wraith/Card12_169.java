075d6874f64d9f416fbbb14961c68229b905191f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 17:32:30 +0000

    "Sauron's Gaze"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/wraith/Card12_169.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set12.wraith;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.TransferPermanentEffect;
+import com.gempukku.lotro.cards.modifiers.RemoveSpecialAbilitiesModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: Skirmish: Transfer this condition from your support area to a character skirmishing a Nazgul. The Free
+ * Peoples player cannot use bearer's special abilities.
+ */
+public class Card12_169 extends AbstractPermanent {
+    public Card12_169() {
+        super(Side.SHADOW, 1, CardType.CONDITION, Culture.WRAITH, Zone.SUPPORT, "Sauron's Gaze");
+    }
+
+    @Override
+    public List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && self.getZone() == Zone.SUPPORT
+                && Filters.filter(game.getGameState().getSkirmish().getShadowCharacters(), game.getGameState(), game.getModifiersQuerying(), Race.NAZGUL).size() > 0
+                && game.getGameState().getSkirmish().getFellowshipCharacter() != null) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendEffect(
+                    new TransferPermanentEffect(self, game.getGameState().getSkirmish().getFellowshipCharacter()));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new RemoveSpecialAbilitiesModifier(self, Filters.hasAttached(self));
+    }
+}
