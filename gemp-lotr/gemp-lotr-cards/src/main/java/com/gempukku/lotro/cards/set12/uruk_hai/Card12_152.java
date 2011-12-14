c27cad981101eea49ada9325b87f682e04872e60
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 16:26:22 +0000

    "Uruk Dominator"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/uruk_hai/Card12_152.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set12.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ForEachYouSpotEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 8
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Damage +1. Skirmish: Exert this minion to make it strength +1 for each [URUK-HAI] Uruk-hai you spot (or +2
+ * for each if you can spot a companion who has resistance 3 or less).
+ */
+public class Card12_152 extends AbstractMinion {
+    public Card12_152() {
+        super(4, 8, 3, 5, Race.URUK_HAI, Culture.URUK_HAI, "Uruk Dominator", true);
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, final LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSelfExert(self, game)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ForEachYouSpotEffect(playerId, Culture.URUK_HAI, Race.URUK_HAI) {
+                        @Override
+                        protected void spottedCards(int spotCount) {
+                            int bonus = PlayConditions.canSpot(game, CardType.COMPANION, Filters.maxResistance(3)) ? 2 * spotCount : spotCount;
+                            action.appendEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, self, bonus), Phase.SKIRMISH));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
