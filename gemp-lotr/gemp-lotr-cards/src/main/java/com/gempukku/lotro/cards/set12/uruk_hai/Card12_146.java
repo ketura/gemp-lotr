7e663b6865f0cc45a636c043b262d66e140408f9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-14 15:16:30 +0000

    "Strange Device"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set12/uruk_hai/Card12_146.java
@@ -0,0 +1,64 @@
+package com.gempukku.lotro.cards.set12.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Black Rider
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 1
+ * Type: Event • Shadow
+ * Game Text: Spot an [URUK-HAI] minion to draw 3 cards. Then discard 2 cards from hand or discard an [URUK-HAI] minion
+ * from hand.
+ */
+public class Card12_146 extends AbstractEvent {
+    public Card12_146() {
+        super(Side.SHADOW, 1, Culture.URUK_HAI, "Strange Device", Phase.SHADOW);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.URUK_HAI, CardType.MINION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new DrawCardsEffect(playerId, 3));
+        List<Effect> possibleEffects = new LinkedList<Effect>();
+        possibleEffects.add(
+                new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2) {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return "Discard 2 cards from hand";
+                    }
+                });
+        possibleEffects.add(
+                new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, Culture.URUK_HAI, CardType.MINION) {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return "Discard an URUK-HAI minion";
+                    }
+                });
+        action.appendEffect(
+                new ChoiceEffect(action, playerId, possibleEffects));
+        return action;
+    }
+}
