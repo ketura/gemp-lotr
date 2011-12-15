8036f23827fca2426e5179704b5fc5ac046e136f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-15 12:13:35 +0000

    - "Legolas Bow" now correctly allows to heal when Legolas bearing it is exerted to play an event or condition.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/elven/Card11_023.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/elven/Card11_023.java
@@ -1,23 +1,22 @@
 package com.gempukku.lotro.cards.set11.elven;
 
 import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
-import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.effects.CheckLimitEffect;
 import com.gempukku.lotro.common.*;
-import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.game.PhysicalCard;
-import com.gempukku.lotro.logic.modifiers.Modifier;
-import com.gempukku.lotro.logic.modifiers.KeywordModifier;
-import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.timing.EffectResult;
-import com.gempukku.lotro.logic.timing.Action;
-import com.gempukku.lotro.logic.timing.results.ExertResult;
-import com.gempukku.lotro.logic.effects.HealCharactersEffect;
 import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.ExertResult;
 
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Set: Shadows
@@ -46,13 +45,13 @@ public class Card11_023 extends AbstractAttachableFPPossession {
 
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.forEachExerted(game, effectResult, self)
+        if (TriggerConditions.forEachExerted(game, effectResult, self.getAttachedTo())
                 && self.getAttachedTo().getBlueprint().getName().equals("Legolas")) {
             ExertResult exertResult = (ExertResult) effectResult;
             if (exertResult.getAction() != null && exertResult.getAction().getType() == Action.Type.PLAY_CARD) {
                 PhysicalCard playedCard = exertResult.getAction().getActionSource();
                 if (playedCard != null && Filters.and(Culture.ELVEN, Filters.or(CardType.CONDITION, CardType.EVENT)).accepts(game.getGameState(), game.getModifiersQuerying(), playedCard)) {
-                    OptionalTriggerAction action= new OptionalTriggerAction(self);
+                    OptionalTriggerAction action = new OptionalTriggerAction(self);
                     action.appendEffect(
                             new CheckLimitEffect(action, self, 1,
                                     new HealCharactersEffect(self, self.getAttachedTo())));
