fbad295fd859646602fd74f0f1ce47aa7b1965e6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-16 17:20:43 +0000

    "Arwen"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/elven/Card13_009.java
@@ -0,0 +1,67 @@
+package com.gempukku.lotro.cards.set13.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.NegativeEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 2
+ * Type: Companion • Elf
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 7
+ * Game Text: While the fellowship is at a forest site, each of your other [ELVEN] companions is resistance +1.
+ * Skirmish: Exert Arwen to make a minion she is skirmishing strength -1 for each other companion assigned to a skirmish.
+ */
+public class Card13_009 extends AbstractCompanion {
+    public Card13_009() {
+        super(2, 6, 3, 7, Culture.ELVEN, Race.ELF, null, "Arwen", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new ResistanceModifier(self, Filters.and(Filters.not(self), Culture.ELVEN, CardType.COMPANION), new LocationCondition(Keyword.FOREST), 1);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a minion", CardType.MINION, Filters.inSkirmishAgainst(self)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, card, null,
+                                                    new NegativeEvaluator(
+                                                            new CountActiveEvaluator(Filters.not(self), CardType.COMPANION, Filters.assignedToSkirmish))), Phase.SKIRMISH));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
