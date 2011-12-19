853adc2e32db084a89d683554e945838d9dfb5ec
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-19 11:09:07 +0000

    "Gimli"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set0/dwarven/Card0_062.java
@@ -0,0 +1,24 @@
+package com.gempukku.lotro.cards.set0.dwarven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * Set: Promotional
+ * Side: Free
+ * Culture: Dwarven
+ * Twilight Cost: 2
+ * Type: Companion • Dwarf
+ * Strength: 7
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Damage +1.
+ */
+public class Card0_062 extends AbstractCompanion {
+    public Card0_062() {
+        super(2, 7, 3, 6, Culture.DWARVEN, Race.DWARF, null, "Gimli", true);
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+}
