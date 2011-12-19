9f062a5616c0e4bdf27d5b8e003839320195f85e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-19 11:11:48 +0000

    "Gandalf"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set0/gandalf/Card0_064.java
@@ -0,0 +1,21 @@
+package com.gempukku.lotro.cards.set0.gandalf;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * Set: Promotional
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 4
+ * Type: Companion • Wizard
+ * Strength: 8
+ * Vitality: 4
+ * Resistance: 6
+ */
+public class Card0_064 extends AbstractCompanion {
+    public Card0_064() {
+        super(4, 8, 4, 6, Culture.GANDALF, Race.WIZARD, null, "Gandalf", true);
+    }
+}
