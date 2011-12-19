69512e44da2ed9565141dc01969f3260c61179d6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-19 10:57:04 +0000

    "Radagast's Staff"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set0/gandalf/Card0_057.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set0.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Promotional
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 1
+ * Type: Artifact • Staff
+ * Strength: +1
+ * Vitality: +1
+ * Game Text: Bearer must be Radagast. Each time the fellowship moves during the regroup phase, you may draw 2 cards.
+ */
+public class Card0_057 extends AbstractAttachableFPPossession {
+    public Card0_057() {
+        super(1, 1, 1, Culture.GANDALF, CardType.ARTIFACT, PossessionClass.STAFF, "Radagast's Staff", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.name("Radagast");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.moves(game, effectResult)
+                && PlayConditions.isPhase(game, Phase.REGROUP)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new DrawCardsEffect(playerId, 2));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
