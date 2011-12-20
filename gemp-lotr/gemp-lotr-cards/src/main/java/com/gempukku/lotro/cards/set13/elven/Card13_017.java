a3ec40766a6bd2ca959dbde769343e64c6a14a0c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-20 01:20:07 +0000

    "Kindreds Estranged"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/elven/Card13_017.java
@@ -0,0 +1,56 @@
+package com.gempukku.lotro.cards.set13.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.RemovePlayedEventFromGameEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCardsFromDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 2
+ * Type: Event • Skirmish
+ * Game Text: You may remove from the game 4 other [ELVEN] cards in your discard pile to play this event from your
+ * discard pile. Then remove this event from the game. Make a minion skirmishing your Elf at a forest or river site
+ * strength -3.
+ */
+public class Card13_017 extends AbstractEvent {
+    public Card13_017() {
+        super(Side.FREE_PEOPLE, 2, Culture.ELVEN, "Kindreds Estranged", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        if (PlayConditions.location(game, Filters.or(Keyword.FOREST, Keyword.RIVER)))
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, -3, CardType.MINION, Filters.inSkirmishAgainst(Race.ELF, Filters.owner(playerId))));
+        return action;
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActionsFromDiscard(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.isPhase(game, Phase.SKIRMISH)
+                && PlayConditions.canPlayFromDiscard(playerId, game, self)
+                && PlayConditions.canRemoveFromDiscardToPlay(self, game, playerId, 4, Culture.ELVEN)) {
+            final PlayEventAction action = getPlayCardAction(playerId, game, self, 0, false);
+            action.appendCost(
+                    new ChooseAndRemoveCardsFromDiscardEffect(action, self, playerId, 4, 4, Culture.ELVEN));
+            action.appendEffect(
+                    new RemovePlayedEventFromGameEffect(action));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
