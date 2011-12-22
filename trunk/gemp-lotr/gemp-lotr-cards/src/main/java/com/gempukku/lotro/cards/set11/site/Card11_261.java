1478642b87880ffee83e99d931849c7f8d96e84f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-22 03:42:01 +0000

    - "Valley of the Silverlode" should not correctly allow to heal every Hobbit companion.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_261.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set11/site/Card11_261.java
@@ -5,12 +5,14 @@ import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -31,10 +33,13 @@ public class Card11_261 extends AbstractNewSite {
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
         if (TriggerConditions.movesTo(game, effectResult, self)
                 && playerId.equals(game.getGameState().getCurrentPlayerId())) {
-            OptionalTriggerAction action = new OptionalTriggerAction(self);
-            action.appendEffect(
-                    new HealCharactersEffect(self, Race.HOBBIT, CardType.COMPANION));
-            return Collections.singletonList(action);
+            Collection<PhysicalCard> hobbitCompanions = Filters.filterActive(game.getGameState(), game.getModifiersQuerying(), Race.HOBBIT, CardType.COMPANION);
+            if (hobbitCompanions.size() > 0) {
+                OptionalTriggerAction action = new OptionalTriggerAction(self);
+                action.appendEffect(
+                        new ChooseAndHealCharactersEffect(action, playerId, 0, hobbitCompanions.size(), Filters.in(hobbitCompanions)));
+                return Collections.singletonList(action);
+            }
         }
         return null;
     }
