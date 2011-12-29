d1ea568fde6fb0d4c3cb2ccaf78675770139a893
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-29 06:47:18 +0000

    "The Flame of Anor"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gandalf/Card13_031.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set13.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 1
+ * Type: Event • Skirmish
+ * Game Text: Spell. Spot 3 [GANDALF] companions to make a minion skirmishing a companion strength -3.
+ */
+public class Card13_031 extends AbstractEvent {
+    public Card13_031() {
+        super(Side.FREE_PEOPLE, 1, Culture.GANDALF, "The Flame of Anor", Phase.SKIRMISH);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, 3, Culture.GANDALF, CardType.COMPANION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, -3, CardType.MINION, Filters.inSkirmishAgainst(CardType.COMPANION)));
+        return action;
+    }
+}
