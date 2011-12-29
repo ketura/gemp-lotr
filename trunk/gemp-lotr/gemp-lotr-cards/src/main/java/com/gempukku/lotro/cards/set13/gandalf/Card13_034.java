e36a2d1fda6859239f181a9b9b42c683904917ef
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2011-12-29 06:58:26 +0000

    "Look to My Coming"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gandalf/Card13_034.java
@@ -0,0 +1,63 @@
+package com.gempukku.lotro.cards.set13.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.RemovePlayedEventFromGameEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCardsFromDiscardEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Event • Skirmish
+ * Game Text: You may remove from the game 4 other [GANDALF] cards in your discard pile to play this event from your
+ * discard pile. Then remove this event from the game. Spot Gandalf to make your [GANDALF] companion strength +3.
+ */
+public class Card13_034 extends AbstractEvent {
+    public Card13_034() {
+        super(Side.FREE_PEOPLE, 2, Culture.GANDALF, "Look to My Coming", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.gandalf);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 3, Filters.owner(playerId), Culture.GANDALF, CardType.COMPANION));
+        return action;
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActionsFromDiscard(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.isPhase(game, Phase.SKIRMISH)
+                && PlayConditions.canPlayFromDiscard(playerId, game, self)
+                && PlayConditions.canRemoveFromDiscardToPlay(self, game, playerId, 4, Culture.GANDALF)) {
+            final PlayEventAction action = getPlayCardAction(playerId, game, self, 0, false);
+            action.appendCost(
+                    new ChooseAndRemoveCardsFromDiscardEffect(action, self, playerId, 4, 4, Culture.GANDALF));
+            action.appendEffect(
+                    new RemovePlayedEventFromGameEffect(action));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
