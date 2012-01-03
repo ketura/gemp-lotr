4529a2b51cbe8d40a57aa1a325a13a648fe6bff6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-03 11:07:32 +0000

    "Cunningly Hidden"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gollum/Card13_045.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gollum/Card13_045.java
@@ -3,9 +3,8 @@ package com.gempukku.lotro.cards.set13.gollum;
 import com.gempukku.lotro.cards.AbstractEvent;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
-import com.gempukku.lotro.cards.effects.RemovePlayedEventFromGameEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCardsFromDiscardEffect;
+import com.gempukku.lotro.cards.effects.discount.RemoveCardsFromDiscardDiscountEffect;
 import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Phase;
@@ -13,10 +12,6 @@ import com.gempukku.lotro.common.Side;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.timing.Action;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Set: Bloodlines
@@ -34,6 +29,8 @@ public class Card13_045 extends AbstractEvent {
 
     @Override
     public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        if (PlayConditions.canRemoveFromDiscardToPlay(self, game, playerId, 4, Culture.GOLLUM))
+            twilightModifier -= 1000;
         return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
                 && PlayConditions.canSpot(game, Filters.gollum);
     }
@@ -41,23 +38,10 @@ public class Card13_045 extends AbstractEvent {
     @Override
     public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
         PlayEventAction action = new PlayEventAction(self);
+        action.setDiscountEffect(
+                new RemoveCardsFromDiscardDiscountEffect(self, playerId, 4, Culture.GOLLUM));
         action.appendEffect(
                 new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 2, CardType.MINION));
         return action;
-    }
-
-    @Override
-    public List<? extends Action> getPhaseActionsFromDiscard(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.isPhase(game, Phase.SKIRMISH)
-                && PlayConditions.canPlayFromDiscard(playerId, game, self)
-                && PlayConditions.canRemoveFromDiscardToPlay(self, game, playerId, 4, Culture.GOLLUM)) {
-            final PlayEventAction action = getPlayCardAction(playerId, game, self, 0, false);
-            action.appendCost(
-                    new ChooseAndRemoveCardsFromDiscardEffect(action, self, playerId, 4, 4, Culture.GOLLUM));
-            action.appendEffect(
-                    new RemovePlayedEventFromGameEffect(action));
-            return Collections.singletonList(action);
-        }
-        return null;
     }
 }
