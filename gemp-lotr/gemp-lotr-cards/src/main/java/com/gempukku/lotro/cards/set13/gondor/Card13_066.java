1ba2565c0bc6568f3e8c13f6cd6da570383cd807
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-03 17:05:10 +0000

    "Faramir"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gondor/Card13_066.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set13.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 7
+ * Vitality: 3
+ * Resistance: 7
+ * Game Text: While you can spot Boromir, Faramir is twilight cost -1. While you can spot Denethor, Faramir is twilight
+ * cost -1. While Faramir bears a [GONDOR] possession, he is defender +1.
+ */
+public class Card13_066 extends AbstractCompanion {
+    public Card13_066() {
+        super(3, 7, 3, 7, Culture.GONDOR, Race.MAN, null, "Faramir", true);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        int modifier = 0;
+        if (Filters.canSpot(gameState, modifiersQuerying, Filters.boromir))
+            modifier--;
+        if (Filters.canSpot(gameState, modifiersQuerying, Filters.name("Denethor")))
+            modifier--;
+        return modifier;
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new KeywordModifier(self, Filters.and(self, Filters.hasAttached(Culture.GONDOR, CardType.POSSESSION)), Keyword.DEFENDER, 1);
+    }
+}
