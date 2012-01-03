cb2af07e521af406624a48aa71515d9b13733a3b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-03 12:00:41 +0000

    "Naked Waste"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gollum/Card13_053.java
@@ -0,0 +1,60 @@
+package com.gempukku.lotro.cards.set13.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.RemovePlayedEventFromGameEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCardsFromDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Gollum
+ * Twilight Cost: 2
+ * Type: Event • Skirmish
+ * Game Text: You may remove from the game 4 other [GOLLUM] cards in your discard pile to play this event from your
+ * discard pile. Then remove this event from the game. Spot Smeagol to make a Ring-bound companion strength +3.
+ */
+public class Card13_053 extends AbstractEvent {
+    public Card13_053() {
+        super(Side.FREE_PEOPLE, 2, Culture.GOLLUM, "Naked Waste", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.smeagol);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 3, CardType.COMPANION, Keyword.RING_BOUND));
+        return action;
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActionsFromDiscard(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.isPhase(game, Phase.SKIRMISH)
+                && PlayConditions.canPlayFromDiscard(playerId, game, self)
+                && PlayConditions.canRemoveFromDiscardToPlay(self, game, playerId, 4, Culture.GOLLUM)) {
+            final PlayEventAction action = getPlayCardAction(playerId, game, self, 0, false);
+            action.appendCost(
+                    new ChooseAndRemoveCardsFromDiscardEffect(action, self, playerId, 4, 4, Culture.GOLLUM));
+            action.appendEffect(
+                    new RemovePlayedEventFromGameEffect(action));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
