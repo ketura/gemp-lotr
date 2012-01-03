876aca01e629ca9050918337213c932c851ee462
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-03 10:46:46 +0000

    "Cunningly Hidden"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gollum/Card13_045.java
@@ -0,0 +1,63 @@
+package com.gempukku.lotro.cards.set13.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.RemovePlayedEventFromGameEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndRemoveCardsFromDiscardEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 2
+ * Type: Event • Skirmish
+ * Game Text: You may remove from the game 4 [GOLLUM] cards in your discard pile instead of paying the twilight cost for
+ * this card. Spot Gollum to make a minion strength +2.
+ */
+public class Card13_045 extends AbstractEvent {
+    public Card13_045() {
+        super(Side.SHADOW, 2, Culture.GOLLUM, "Cunningly Hidden", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.gollum);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 2, CardType.MINION));
+        return action;
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActionsFromDiscard(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.isPhase(game, Phase.SKIRMISH)
+                && PlayConditions.canPlayFromDiscard(playerId, game, self)
+                && PlayConditions.canRemoveFromDiscardToPlay(self, game, playerId, 4, Culture.GOLLUM)) {
+            final PlayEventAction action = getPlayCardAction(playerId, game, self, 0, false);
+            action.appendCost(
+                    new ChooseAndRemoveCardsFromDiscardEffect(action, self, playerId, 4, 4, Culture.GOLLUM));
+            action.appendEffect(
+                    new RemovePlayedEventFromGameEffect(action));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
