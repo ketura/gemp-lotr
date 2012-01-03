ceceea7d75ade8b43bfd972787f6650e3d197b0f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-03 12:28:19 +0000

    "Softly Up Behind"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/AbstractLotroCardBlueprint.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/AbstractLotroCardBlueprint.java
@@ -107,6 +107,11 @@ public abstract class AbstractLotroCardBlueprint implements LotroCardBlueprint {
         return null;
     }
 
+    @Override
+    public List<? extends Modifier> getInDiscardModifiers(LotroGame game, PhysicalCard self) {
+        return null;
+    }
+
     public Modifier getAlwaysOnModifier(PhysicalCard self) {
         return null;
     }
