c5a0d7dfc6de9b48f04f1ef9ebca88d47599748d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-07 15:03:04 +0000

    "Stewards' Legacy"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/gondor/Card13_075.java
@@ -0,0 +1,73 @@
+package com.gempukku.lotro.cards.set13.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.RemoveSpecialAbilitiesModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.ExertResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Condition • Support Area
+ * Game Text: Each time Denethor exerts, he gains muster until the end of the regroup phase. Each time Boromir exerts,
+ * you may exert a minion. Each time Faramir exerts, you may spot a minion to prevent it from using special abilites
+ * until the regroup phase.
+ */
+public class Card13_075 extends AbstractPermanent {
+    public Card13_075() {
+        super(Side.FREE_PEOPLE, 2, CardType.CONDITION, Culture.GONDOR, Zone.SUPPORT, "Stewards' Legacy", true);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.forEachExerted(game, effectResult, Filters.name("Denethor"))) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new KeywordModifier(self, ((ExertResult) effectResult).getExertedCard(), Keyword.MUSTER), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (TriggerConditions.forEachExerted(game, effectResult, Filters.boromir)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.MINION));
+            return Collections.singletonList(action);
+        }
+        if (TriggerConditions.forEachExerted(game, effectResult, Filters.name("Faramir"))) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a minion", CardType.MINION) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new AddUntilStartOfPhaseModifierEffect(
+                                            new RemoveSpecialAbilitiesModifier(self, card), Phase.REGROUP));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
