39caa5cf31bd10aaae1602fa1080702faec94b0f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-08 21:55:16 +0000

    "Bring Down the Wall"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/men/Card13_082.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set13.men;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: Bloodlines
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 1
+ * Type: Event • Skirmish
+ * Game Text: Spot a companion to make a [MEN] minion strength +1 for each wound on that companion.
+ */
+public class Card13_082 extends AbstractEvent {
+    public Card13_082() {
+        super(Side.SHADOW, 1, Culture.MEN, "Bring Down the Wall", Phase.SKIRMISH);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, CardType.COMPANION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseActiveCardEffect(self, playerId, "Choose a companion", CardType.COMPANION) {
+                    @Override
+                    protected void cardSelected(LotroGame game, PhysicalCard companion) {
+                        int wounds = game.getGameState().getWounds(companion);
+                        action.appendEffect(
+                                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, wounds, Culture.MEN, CardType.MINION));
+                    }
+                });
+        return action;
+    }
+}
