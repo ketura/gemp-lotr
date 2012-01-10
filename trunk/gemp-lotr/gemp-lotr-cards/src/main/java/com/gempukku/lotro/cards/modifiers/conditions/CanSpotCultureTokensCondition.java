76a79b66217c1b636cf0968a10f42be6ddf42102
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-10 16:05:49 +0000

    "Southron Murderer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/conditions/CanSpotCultureTokensCondition.java
@@ -0,0 +1,33 @@
+package com.gempukku.lotro.cards.modifiers.conditions;
+
+import com.gempukku.lotro.common.Token;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+import java.util.Map;
+
+public class CanSpotCultureTokensCondition implements Condition {
+    private int _count;
+
+    public CanSpotCultureTokensCondition(int count) {
+        _count = count;
+    }
+
+    @Override
+    public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+        int count = 0;
+        for (PhysicalCard physicalCard : Filters.filterActive(gameState, modifiersQuerying, Filters.hasAnyCultureTokens(1))) {
+            for (Map.Entry<Token, Integer> tokenCountEntry : gameState.getTokens(physicalCard).entrySet()) {
+                if (tokenCountEntry.getKey().getCulture() != null) {
+                    count += tokenCountEntry.getValue();
+                    if (count >= _count)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
