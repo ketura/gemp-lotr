87e34e6412c79e0604e9aa1c6d9331acbab4209b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-10 11:34:17 +0000

    Fixing condition merges to ignore null conditions.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/conditions/OrCondition.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/conditions/OrCondition.java
@@ -14,7 +14,7 @@ public class OrCondition implements Condition {
     @Override
     public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
         for (Condition condition : _conditions) {
-            if (condition.isFullfilled(gameState, modifiersQuerying))
+            if (condition != null && condition.isFullfilled(gameState, modifiersQuerying))
                 return true;
         }
 
