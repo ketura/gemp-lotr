52ac2eb158af4a0ccf545bdf9e54d58db87bef04
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-10 17:00:05 +0000

    "Isengard Informant"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/orc/Card13_110.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set13.orc;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 2
+ * Type: Minion • Orc
+ * Strength: 7
+ * Vitality: 2
+ * Site: 4
+ * Game Text: Regroup: Exert this minion to play an [ORC] Troll from your discard pile.
+ */
+public class Card13_110 extends AbstractMinion {
+    public Card13_110() {
+        super(2, 7, 2, 4, Race.ORC, Culture.ORC, "Isengard Informant");
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.REGROUP, self, 0)
+                && PlayConditions.canSelfExert(self, game)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Culture.ORC, Race.TROLL)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game, Culture.ORC, Race.TROLL));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
