f1fe2061c2fb7510a6f3314af00d2b9bb898c9ee
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-11 15:13:04 +0000

    "Freely Across Our Land"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/rohan/Card13_127.java
@@ -0,0 +1,79 @@
+package com.gempukku.lotro.cards.set13.rohan;
+
+import com.gempukku.lotro.cards.*;
+import com.gempukku.lotro.cards.effects.AddTokenEffect;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.RemoveTokenEffect;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseArbitraryCardsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: When you play this, add a [ROHAN] token here. Regroup: Discard this from play or remove a token from here
+ * to play a [ROHAN] possession from your discard pile on your [ROHAN] companion.
+ */
+public class Card13_127 extends AbstractPermanent {
+    public Card13_127() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.ROHAN, Zone.SUPPORT, "Freely Across Our Land", true);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddTokenEffect(self, self, Token.ROHAN));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Culture.ROHAN, CardType.POSSESSION, ExtraFilters.attachableTo(game, Culture.ROHAN, CardType.COMPANION))) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            List<Effect> possibleCosts = new LinkedList<Effect>();
+            possibleCosts.add(
+                    new SelfDiscardEffect(self));
+            possibleCosts.add(
+                    new RemoveTokenEffect(self, self, Token.ROHAN));
+            action.appendCost(
+                    new ChoiceEffect(action, playerId, possibleCosts));
+            action.appendEffect(
+                    new ChooseArbitraryCardsEffect(playerId, "Choose card to play", game.getGameState().getDiscard(playerId),
+                            Filters.and(
+                                    Culture.ROHAN, CardType.POSSESSION,
+                                    ExtraFilters.attachableTo(game, Culture.ROHAN, CardType.COMPANION)), 1, 1) {
+                        @Override
+                        protected void cardsSelected(LotroGame game, Collection<PhysicalCard> selectedCards) {
+                            if (selectedCards.size() > 0) {
+                                PhysicalCard selectedCard = selectedCards.iterator().next();
+                                game.getActionsEnvironment().addActionToStack(((AbstractAttachable) selectedCard.getBlueprint()).getPlayCardAction(playerId, game, selectedCard, Filters.and(Culture.ROHAN, CardType.COMPANION), 0));
+                            }
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+
+        return null;
+    }
+}
