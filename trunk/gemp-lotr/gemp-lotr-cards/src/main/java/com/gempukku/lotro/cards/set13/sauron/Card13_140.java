1c9419b9f4e165ebc8c3542392815bb90b16b79b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-11 17:19:39 +0000

    "Sauron"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/sauron/Card13_140.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set13.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.effects.DiscountEffect;
+import com.gempukku.lotro.cards.effects.discount.ExertCharactersDiscountEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.timing.Action;
+
+/**
+ * Set: Bloodlines
+ * Side: Shadow
+ * Culture: Sauron
+ * Twilight Cost: 16
+ * Type: Minion • Maia
+ * Strength: 24
+ * Vitality: 5
+ * Site: 6
+ * Game Text: Damage +1. Fierce. When you play Sauron, you may exert any number of minions. For each minion you exert,
+ * Sauron is twilight cost -X, where X is the current region number.
+ */
+public class Card13_140 extends AbstractMinion {
+    public Card13_140() {
+        super(16, 24, 5, 6, Race.MAIA, Culture.SAURON, "Sauron", true);
+        addKeyword(Keyword.DAMAGE, 1);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    protected int getPotentialExtraPaymentDiscount(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.countActive(game.getGameState(), game.getModifiersQuerying(), CardType.MINION, Filters.canExert(self)) * GameUtils.getRegion(game.getGameState());
+    }
+
+    @Override
+    protected DiscountEffect getDiscountEffect(Action action, String playerId, LotroGame game, PhysicalCard self) {
+        return new ExertCharactersDiscountEffect(action, self, playerId, GameUtils.getRegion(game.getGameState()), CardType.MINION);
+    }
+}
