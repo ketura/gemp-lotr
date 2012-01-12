c43c5968bc5f4e587d3626aae428eed4453fb385
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-12 14:38:49 +0000

    "Daddy Twofoot"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/shire/Card13_144.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set13.shire;
+
+import com.gempukku.lotro.cards.AbstractFollower;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPutCardFromDiscardIntoHandEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Follower
+ * Resistance: +1
+ * Game Text: Aid - Add a burden. (At the start of the maneuver phase, you may add a burden to transfer this to
+ * a companion.) Each time you transfer this to a companion, you may take a [SHIRE] event into hand from your
+ * discard pile.
+ */
+public class Card13_144 extends AbstractFollower {
+    public Card13_144() {
+        super(Side.FREE_PEOPLE, 1, 0, 0, 1, Culture.SHIRE, "Daddy Twofoot", true);
+    }
+
+    @Override
+    protected boolean canPayAidCost(LotroGame game, PhysicalCard self) {
+        return true;
+    }
+
+    @Override
+    protected Effect getAidCost(LotroGame game, PhysicalCard self) {
+        return new AddBurdenEffect(self, 1);
+    }
+
+    @Override
+    protected List<OptionalTriggerAction> getExtraOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.transferredCard(game, effectResult, self, null, CardType.COMPANION)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndPutCardFromDiscardIntoHandEffect(action, playerId, 1, 1, Culture.SHIRE, CardType.EVENT));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
