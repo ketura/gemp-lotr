823bd3c7389eb09406bee32015645133f3337f40
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-16 16:25:34 +0000

    "Grimbeorn"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set14/gandalf/Card14_006.java
@@ -0,0 +1,79 @@
+package com.gempukku.lotro.cards.set14.gandalf;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Expanded Middle-earth
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 4
+ * Vitality: 5
+ * Resistance: 6
+ * Game Text: At the start of each turn, heal Grimbeorn. At the start of the maneuver phase, you may exert Grimbeorn
+ * twice and discard a minion from hand to make Grimbeorn strength +X until the regroup phase, where X is that minion's
+ * strength.
+ */
+public class Card14_006 extends AbstractCompanion {
+    public Card14_006() {
+        super(3, 4, 5, 6, Culture.GANDALF, Race.MAN, null, "Grimbeorn", true);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (effectResult.getType() == EffectResult.Type.START_OF_TURN) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new HealCharactersEffect(self, self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.MANEUVER)
+                && PlayConditions.canSelfExert(self, 2, game)
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, CardType.MINION)) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, CardType.MINION) {
+                        @Override
+                        protected void cardsBeingDiscardedCallback(Collection<PhysicalCard> cardsBeingDiscarded) {
+                            for (PhysicalCard physicalCard : cardsBeingDiscarded) {
+                                action.appendEffect(
+                                        new AddUntilStartOfPhaseModifierEffect(
+                                                new StrengthModifier(self, self, physicalCard.getBlueprint().getStrength()), Phase.REGROUP));
+                            }
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
