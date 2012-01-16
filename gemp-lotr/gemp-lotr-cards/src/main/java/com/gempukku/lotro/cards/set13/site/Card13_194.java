48f73ce353cedd9b1f6a598fc49b11b16cb6935b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-16 11:43:42 +0000

    "Redhorn Pass"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/site/Card13_194.java
@@ -0,0 +1,19 @@
+package com.gempukku.lotro.cards.set13.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.common.Keyword;
+
+/**
+ * Set: Bloodlines
+ * Twilight Cost: 2
+ * Type: Site
+ * Game Text: Mountain. Culture tokens cannot be added, removed, or reinforced.
+ */
+public class Card13_194 extends AbstractNewSite {
+    public Card13_194() {
+        super("Redhorn Pass", 2, Direction.LEFT);
+        addKeyword(Keyword.MOUNTAIN);
+    }
+
+    // TODO - make this card work
+}
