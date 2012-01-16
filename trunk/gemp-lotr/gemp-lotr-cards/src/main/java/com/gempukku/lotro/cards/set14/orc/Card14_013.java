18407a96e4712e93a2c1031bb1b2c59c33e86563
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-16 16:58:17 +0000

    "Horror of Harad"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set14/orc/Card14_013.java
@@ -0,0 +1,29 @@
+package com.gempukku.lotro.cards.set14.orc;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * Set: Expanded Middle-earth
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 4
+ * Type: Minion • Half•troll
+ * Strength: 9
+ * Vitality: 3
+ * Site: 4
+ * Game Text: Ambush (1). Damage +1. Fierce. Lurker. Muster. Toil 2.
+ */
+public class Card14_013 extends AbstractMinion {
+    public Card14_013() {
+        super(4, 9, 3, 4, Race.HALF_TROLL, Culture.ORC, "Horror of Harad", true);
+        addKeyword(Keyword.AMBUSH, 1);
+        addKeyword(Keyword.DAMAGE, 1);
+        addKeyword(Keyword.FIERCE);
+        addKeyword(Keyword.LURKER);
+        addKeyword(Keyword.MUSTER);
+        addKeyword(Keyword.TOIL, 2);
+    }
+}
