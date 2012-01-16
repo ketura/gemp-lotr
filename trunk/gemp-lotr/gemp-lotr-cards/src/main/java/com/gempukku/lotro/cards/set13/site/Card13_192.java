11441e36842725d53b44cb184b0a25a9ef084e7f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-16 11:40:14 +0000

    "The Great Gates"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/site/Card13_192.java
@@ -0,0 +1,37 @@
+package com.gempukku.lotro.cards.set13.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: Bloodlines
+ * Twilight Cost: 0
+ * Type: Site
+ * Game Text: Battleground. While a player can spot more companions than minions, each minion is strength +1.
+ */
+public class Card13_192 extends AbstractNewSite {
+    public Card13_192() {
+        super("The Great Gates", 0, Direction.LEFT);
+        addKeyword(Keyword.BATTLEGROUND);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, CardType.MINION,
+                new Condition() {
+                    @Override
+                    public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                        return Filters.countSpottable(gameState, modifiersQuerying, CardType.COMPANION)
+                                > Filters.countSpottable(gameState, modifiersQuerying, CardType.MINION);
+                    }
+                }, 1);
+    }
+}
