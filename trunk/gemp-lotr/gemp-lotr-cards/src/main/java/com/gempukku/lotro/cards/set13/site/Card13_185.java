ddaad2d031f55a362a36715838fa1b35b112e2f7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-16 11:02:06 +0000

    "Abandoned Mine Shaft"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set13/site/Card13_185.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set13.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Bloodlines
+ * Twilight Cost: 0
+ * Type: Site
+ * Game Text: Underground. If this site is not in region 1 when the fellowship moves from here during the regroup phase,
+ * add a burden.
+ */
+public class Card13_185 extends AbstractNewSite {
+    public Card13_185() {
+        super("Abandoned Mine Shaft", 0, Direction.LEFT);
+        addKeyword(Keyword.UNDERGROUND);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.movesFrom(game, effectResult, self, Filters.not(Filters.region(1)))
+                && PlayConditions.isPhase(game, Phase.REGROUP)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddBurdenEffect(self, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
