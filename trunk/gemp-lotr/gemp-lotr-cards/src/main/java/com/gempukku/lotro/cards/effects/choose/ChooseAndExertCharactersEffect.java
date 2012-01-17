fa9c1c7487b8410b68678cd37c64cb50a429a264
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-17 12:38:41 +0000

    "Legolas"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/choose/ChooseAndExertCharactersEffect.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/choose/ChooseAndExertCharactersEffect.java
@@ -16,17 +16,17 @@ import java.util.Collection;
 
 public class ChooseAndExertCharactersEffect extends ChooseActiveCardsEffect {
     private Action _action;
-    private int _count;
+    private int _times;
     private SubAction _resultSubAction;
 
     public ChooseAndExertCharactersEffect(Action action, String playerId, int minimum, int maximum, Filterable... filters) {
         this(action, playerId, minimum, maximum, 1, filters);
     }
 
-    public ChooseAndExertCharactersEffect(Action action, String playerId, int minimum, int maximum, int count, Filterable... filters) {
+    public ChooseAndExertCharactersEffect(Action action, String playerId, int minimum, int maximum, int times, Filterable... filters) {
         super(action.getActionSource(), playerId, "Choose characters to exert", minimum, maximum, filters);
         _action = action;
-        _count = count;
+        _times = times;
     }
 
     @Override
@@ -35,7 +35,7 @@ public class ChooseAndExertCharactersEffect extends ChooseActiveCardsEffect {
             @Override
             public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
                 return modifiersQuerying.canBeExerted(gameState, _action.getActionSource(), physicalCard)
-                        && modifiersQuerying.getVitality(gameState, physicalCard) > _count;
+                        && modifiersQuerying.getVitality(gameState, physicalCard) > _times;
             }
         };
     }
@@ -43,7 +43,7 @@ public class ChooseAndExertCharactersEffect extends ChooseActiveCardsEffect {
     @Override
     protected final void cardsSelected(LotroGame game, Collection<PhysicalCard> characters) {
         _resultSubAction = new SubAction(_action);
-        for (int i = 0; i < _count; i++) {
+        for (int i = 0; i < _times; i++) {
             _resultSubAction.appendEffect(new ExertCharactersEffect(_action, _action.getActionSource(), Filters.in(characters)));
         }
         game.getActionsEnvironment().addActionToStack(_resultSubAction);
