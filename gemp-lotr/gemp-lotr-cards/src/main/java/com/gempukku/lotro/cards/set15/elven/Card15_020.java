514c5ce5d35aa2d425456ef241ad1d3e93baf0ce
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-17 12:50:08 +0000

    "Lorien's Blessing"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/elven/Card15_020.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set15.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Elven
+ * Twilight Cost: 0
+ * Type: Event • Archery
+ * Game Text: Spot an [ELVEN] follower to make the minion archery total -3.
+ */
+public class Card15_020 extends AbstractEvent {
+    public Card15_020() {
+        super(Side.FREE_PEOPLE, 0, Culture.ELVEN, "Lorien's Blessing", Phase.ARCHERY);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.ELVEN, CardType.FOLLOWER);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new AddUntilEndOfPhaseModifierEffect(
+                        new ArcheryTotalModifier(self, Side.SHADOW, -3), Phase.ARCHERY));
+        return action;
+    }
+}
