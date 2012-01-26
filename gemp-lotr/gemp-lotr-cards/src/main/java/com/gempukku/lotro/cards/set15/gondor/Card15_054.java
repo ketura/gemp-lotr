ea6fea0603d18ff14d54da0cda8386cb07848f88
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-26 13:23:46 +0000

    "Aragorn"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/gondor/Card15_054.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set15.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 4
+ * Type: Companion • Man
+ * Strength: 8
+ * Vitality: 4
+ * Resistance: 8
+ * Game Text: Ranger. Hunter 1. (While skirmishing a non-hunter character, this character is strength +1.)
+ * Skirmish: Exert Aragorn to make a hunter companion strength +1.
+ */
+public class Card15_054 extends AbstractCompanion {
+    public Card15_054() {
+        super(4, 8, 4, 8, Culture.GONDOR, Race.MAN, null, "Aragorn", true);
+        addKeyword(Keyword.RANGER);
+        addKeyword(Keyword.HUNTER, 1);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 1, CardType.COMPANION, Keyword.HUNTER));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
