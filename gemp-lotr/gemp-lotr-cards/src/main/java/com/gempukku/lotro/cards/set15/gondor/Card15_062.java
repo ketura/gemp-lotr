9b7e7a9b5845e52d571b842c7105ad873d0b34c7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-26 16:28:45 +0000

    "Ithilien Blade"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/gondor/Card15_062.java
@@ -0,0 +1,61 @@
+package com.gempukku.lotro.cards.set15.gondor;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be a [GONDOR] Man. While you can spot a roaming minion, bearer is strength +1.
+ * Maneuver: Exert bearer twice to discard a roaming minion.
+ */
+public class Card15_062 extends AbstractAttachableFPPossession {
+    public Card15_062() {
+        super(1, 2, 0, Culture.GONDOR, PossessionClass.HAND_WEAPON, "Ithilien Blade");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.GONDOR, Race.MAN);
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.hasAttached(self), new SpotCondition(CardType.MINION, Keyword.ROAMING), 1));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.MANEUVER, self)
+                && PlayConditions.canExert(self, game, 2, self.getAttachedTo())) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self.getAttachedTo()));
+            action.appendCost(
+                    new ExertCharactersEffect(self, self.getAttachedTo()));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.MINION, Keyword.ROAMING));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
