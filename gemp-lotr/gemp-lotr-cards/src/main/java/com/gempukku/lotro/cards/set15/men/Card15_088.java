5007f84101e0309987d18e14a1f216fdbb22e313
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-27 16:13:24 +0000

    "Pursuing Horde"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/men/Card15_088.java
@@ -0,0 +1,61 @@
+package com.gempukku.lotro.cards.set15.men;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 0
+ * Type: Condition
+ * Resistance: -1
+ * Game Text: To play, spot a [MEN] Man. Bearer must be a companion (except the Ring-bearer). Limit 1 per companion.
+ * Each time the fellowship moves, add (1).
+ */
+public class Card15_088 extends AbstractAttachable {
+    public Card15_088() {
+        super(Side.SHADOW, CardType.CONDITION, 0, Culture.MEN, null, "Pursuing Horde");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, Filter additionalAttachmentFilter, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, additionalAttachmentFilter, twilightModifier)
+                && PlayConditions.canSpot(game, Culture.MEN, Race.MAN);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(CardType.COMPANION, Filters.not(Filters.ringBearer), Filters.not(Filters.hasAttached(Filters.name(getName()))));
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new ResistanceModifier(self, Filters.hasAttached(self), -1);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.moves(game, effectResult)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddTwilightEffect(self, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
