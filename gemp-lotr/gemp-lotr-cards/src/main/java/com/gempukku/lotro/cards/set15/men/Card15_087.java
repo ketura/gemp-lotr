e6b64ff4734de5a1eb750e41c6d673bbf36e829a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-27 16:10:48 +0000

    "Primitive Savage"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/men/Card15_087.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set15.men;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.TakeControlOfASiteEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.PossessionClass;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 5
+ * Type: Minion • Man
+ * Strength: 13
+ * Vitality: 3
+ * Site: 4
+ * Game Text: While you can spot a companion of strength 12 or more, this minion is strength +5. Each time this minion
+ * wins a skirmish, if he bears a hand weapon, you may take control of a site.
+ */
+public class Card15_087 extends AbstractMinion {
+    public Card15_087() {
+        super(5, 13, 3, 4, Race.MAN, Culture.MEN, "Primitive Savage", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new SpotCondition(CardType.COMPANION, Filters.not(Filters.lessStrengthThan(12))), 5);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, self, Filters.hasAttached(PossessionClass.HAND_WEAPON))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new TakeControlOfASiteEffect(self, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
