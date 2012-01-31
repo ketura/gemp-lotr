d0048561a2ca5327e4fea76d3227f9978562e777
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-31 14:13:16 +0000

    "Black Land Chieftain"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/orc/Card15_099.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set15.orc;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 5
+ * Type: Minion • Orc
+ * Strength: 12
+ * Vitality: 3
+ * Site: 4
+ * Game Text: To play, spot an [ORC] minion. When you play Black Land Chieftain, you may add a threat for each
+ * Free Peoples possession and each Free Peoples artifact you can spot.
+ */
+public class Card15_099 extends AbstractMinion {
+    public Card15_099() {
+        super(5, 12, 3, 4, Race.ORC, Culture.ORC, "Black Land Chieftain", true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.ORC, CardType.MINION);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new AddThreatsEffect(playerId, self, new CountSpottableEvaluator(Side.FREE_PEOPLE, Filters.or(CardType.POSSESSION, CardType.ARTIFACT))));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
