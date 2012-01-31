00dc2fabe6070b56979f6fa1aa1de9531b525f0b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-01-31 16:05:56 +0000

    "Black Land Runner"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/orc/Card15_103.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set15.orc;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Condition;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Orc
+ * Twilight Cost: 4
+ * Type: Minion • Orc
+ * Strength: 9
+ * Vitality: 3
+ * Site: 4
+ * Game Text: To play, spot an [ORC] Orc. While you can spot more sites on the adventure path than minions, this minion
+ * is strength +5.
+ */
+public class Card15_103 extends AbstractMinion {
+    public Card15_103() {
+        super(4, 9, 3, 4, Race.ORC, Culture.ORC, "Black Land Runner");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.ORC, Race.ORC);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self,
+                new Condition() {
+                    @Override
+                    public boolean isFullfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                        return Filters.countSpottable(gameState, modifiersQuerying, CardType.SITE, Zone.ADVENTURE_PATH)
+                                > Filters.countSpottable(gameState, modifiersQuerying, CardType.MINION);
+                    }
+                }, 5);
+    }
+}
