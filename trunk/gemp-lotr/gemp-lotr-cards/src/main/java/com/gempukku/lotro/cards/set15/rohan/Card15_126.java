107fcce17ac87fb34394511fd9e8391efa35cd8a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-03 14:29:20 +0000

    "Gamling"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/rohan/Card15_126.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set15.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 2
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Valiant. Hunter 1. (While skirmishing a non-hunter character, this character is strength +1.)
+ * To play, spot a [ROHAN] Man. While you can spot 3 hunters, Gamling is strength +3.
+ */
+public class Card15_126 extends AbstractCompanion {
+    public Card15_126() {
+        super(2, 6, 3, 6, Culture.ROHAN, Race.MAN, null, "Gamling", true);
+        addKeyword(Keyword.VALIANT);
+        addKeyword(Keyword.HUNTER, 1);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.ROHAN, Race.MAN);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new SpotCondition(3, Keyword.HUNTER), 3);
+    }
+}
