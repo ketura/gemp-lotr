b880bd433ad6f3680c59fef0975e8a1f500565b2
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-03 14:17:42 +0000

    "Eomer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/rohan/Card15_123.java
@@ -0,0 +1,63 @@
+package com.gempukku.lotro.cards.set15.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 7
+ * Vitality: 3
+ * Resistance: 7
+ * Game Text: Valiant. Hunter 2. (While skirmishing a non-hunter character, this character is strength +2.)
+ * While you can spot a [ROHAN] Man, Eomer’s twilight cost is -1. Skirmish: Discard a [ROHAN] card from hand
+ * to make Eomer strength +2.
+ */
+public class Card15_123 extends AbstractCompanion {
+    public Card15_123() {
+        super(3, 7, 3, 7, Culture.ROHAN, Race.MAN, null, "Eomer", true);
+        addKeyword(Keyword.VALIANT);
+        addKeyword(Keyword.HUNTER, 2);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        if (Filters.canSpot(gameState, modifiersQuerying, Culture.ROHAN, Race.MAN))
+            return -1;
+        return 0;
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, Culture.ROHAN)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, Culture.ROHAN));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new StrengthModifier(self, self, 2), Phase.SKIRMISH));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
