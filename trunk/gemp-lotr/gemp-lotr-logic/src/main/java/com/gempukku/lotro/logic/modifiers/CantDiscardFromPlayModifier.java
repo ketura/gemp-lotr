0808fd025692d7e2b4fe447076ed4d04f52089d7
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-03 14:35:32 +0000

    "Haleth"

--- trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/CantDiscardFromPlayModifier.java
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/CantDiscardFromPlayModifier.java
@@ -1,20 +1,26 @@
 package com.gempukku.lotro.logic.modifiers;
 
-import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 
 public class CantDiscardFromPlayModifier extends AbstractModifier {
-    private Filter _sourceFilter;
+    private Filterable _sourceFilter;
 
-    public CantDiscardFromPlayModifier(PhysicalCard source, String text, Filter affectFilter, Filter sourceFilter) {
+    public CantDiscardFromPlayModifier(PhysicalCard source, String text, Filterable affectFilter, Filterable sourceFilter) {
         super(source, text, affectFilter, ModifierEffect.DISCARD_FROM_PLAY_MODIFIER);
+        _sourceFilter = sourceFilter;
+    }
+
+    public CantDiscardFromPlayModifier(PhysicalCard source, String text, Condition condition, Filterable affectFilter, Filterable sourceFilter) {
+        super(source, text, affectFilter, condition, ModifierEffect.DISCARD_FROM_PLAY_MODIFIER);
         _sourceFilter = sourceFilter;
     }
 
     @Override
     public boolean canBeDiscardedFromPlay(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard card, PhysicalCard source) {
-        if (_sourceFilter.accepts(gameState, modifiersQuerying, source))
+        if (Filters.and(_sourceFilter).accepts(gameState, modifiersQuerying, source))
             return false;
         return true;
     }
