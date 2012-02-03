1380b964f6605164ab2c11f2e8ecba8c3554ff0c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-03 17:13:01 +0000

    "Rohirrim Soldier"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/rohan/Card15_138.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set15.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Valiant. While you can spot a [ROHAN] Man, this companion’s twilight cost is -1.
+ * This companion is strength +1 for each hunter minion you can spot.
+ */
+public class Card15_138 extends AbstractCompanion {
+    public Card15_138() {
+        super(3, 6, 3, 6, Culture.ROHAN, Race.MAN, null, "Rohirrim Soldier");
+        addKeyword(Keyword.VALIANT);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        if (Filters.canSpot(gameState, modifiersQuerying, Culture.ROHAN, Race.MAN))
+            return -1;
+        return 0;
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null, new CountSpottableEvaluator(CardType.MINION, Keyword.HUNTER));
+    }
+}
