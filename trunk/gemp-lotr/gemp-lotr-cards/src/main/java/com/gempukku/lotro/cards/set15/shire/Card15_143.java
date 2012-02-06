369f58314c73e1f70b9f580b92f68573325b40b9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-06 15:06:17 +0000

    "Community Living"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/shire/Card15_143.java
@@ -0,0 +1,37 @@
+package com.gempukku.lotro.cards.set15.shire;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: The Hunters
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 0
+ * Type: Event • Fellowship
+ * Game Text: Spot a Hobbit to play a [SHIRE] follower from your draw deck.
+ */
+public class Card15_143 extends AbstractEvent {
+    public Card15_143() {
+        super(Side.FREE_PEOPLE, 0, Culture.SHIRE, "Community Living", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Race.HOBBIT);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndPlayCardFromDeckEffect(playerId, Culture.SHIRE, CardType.FOLLOWER));
+        return action;
+    }
+}
