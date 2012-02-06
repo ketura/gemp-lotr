ed905bcca28577ffc8b179b4c1bbc985bd90a0f4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-06 16:11:13 +0000

    "Advancing Horde"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/uruk_hai/Card15_155.java
@@ -0,0 +1,34 @@
+package com.gempukku.lotro.cards.set15.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 6
+ * Type: Minion • Uruk-Hai
+ * Strength: 14
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Damage +1. This minion is strength +1 for each site you control.
+ */
+public class Card15_155 extends AbstractMinion {
+    public Card15_155() {
+        super(6, 14, 3, 5, Race.URUK_HAI, Culture.URUK_HAI, "Advancing Horde");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null, new CountActiveEvaluator(Filters.siteControlled(self.getOwner())));
+    }
+}
