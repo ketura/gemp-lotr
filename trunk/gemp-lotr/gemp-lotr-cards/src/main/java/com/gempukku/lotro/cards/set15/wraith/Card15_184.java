3d283780909c2216ef307ba62d070b3ffba6d920
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-07 14:30:56 +0000

    "Úlairë Attëa"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/wraith/Card15_184.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set15.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 6
+ * Type: Minion • Nazgul
+ * Strength: 12
+ * Vitality: 3
+ * Site: 3
+ * Game Text: Fierce. Ulaire Attea is strength +1 for every 3 twilight tokens you can spot.
+ */
+public class Card15_184 extends AbstractMinion {
+    public Card15_184() {
+        super(6, 12, 3, 3, Race.NAZGUL, Culture.WRAITH, "Úlairë Attëa", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null,
+                new Evaluator() {
+                    @Override
+                    public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+                        return gameState.getTwilightPool() / 3;
+                    }
+                });
+    }
+}
