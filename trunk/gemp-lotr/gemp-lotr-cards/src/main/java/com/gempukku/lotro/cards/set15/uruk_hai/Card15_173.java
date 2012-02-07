201265548b62e1e31e62235cad634d9801e815b8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-07 12:15:43 +0000

    "Ugluk's Sword"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/uruk_hai/Card15_173.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set15.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.NotCondition;
+import com.gempukku.lotro.cards.modifiers.conditions.PhaseCondition;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 2
+ * Type: Possession • Hand Weapon
+ * Strength: +1
+ * Game Text: Bearer must be an [URUK-HAI] Uruk-hai. While you control 2 or more sites, bearer can not take wounds
+ * (except during skirmish phases). If bearer is Ugluk, he is damage +1.
+ */
+public class Card15_173 extends AbstractAttachable {
+    public Card15_173() {
+        super(Side.SHADOW, CardType.POSSESSION, 2, Culture.URUK_HAI, PossessionClass.HAND_WEAPON, "Ugluk's Sword", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.URUK_HAI, Race.URUK_HAI);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, Filters.hasAttached(self), 1));
+        modifiers.add(
+                new CantTakeWoundsModifier(self, new NotCondition(new PhaseCondition(Phase.SKIRMISH)), Filters.hasAttached(self)));
+        modifiers.add(
+                new KeywordModifier(self, Filters.and(Filters.hasAttached(self), Filters.name("Ugluk")), Keyword.DAMAGE, 1));
+        return modifiers;
+    }
+}
