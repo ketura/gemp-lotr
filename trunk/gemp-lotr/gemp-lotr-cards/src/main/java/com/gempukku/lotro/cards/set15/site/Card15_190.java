25877f330d4da7f114eeea34998e6d9a091fd5c8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-07 14:46:56 +0000

    "East Wall of Rohan"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/site/Card15_190.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set15.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Twilight Cost: 3
+ * Type: Site
+ * Game Text: At the start of the Fellowship phase, you may exert two hunter companions to play a hunter companion
+ * from your draw deck.
+ */
+public class Card15_190 extends AbstractNewSite {
+    public Card15_190() {
+        super("East Wall of Rohan", 3, Direction.RIGHT);
+    }
+
+    @Override
+    public List<ActivateCardAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.FELLOWSHIP)
+                && PlayConditions.canExert(self, game, 1, 2, CardType.COMPANION, Keyword.HUNTER)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, CardType.COMPANION, Keyword.HUNTER));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, CardType.COMPANION, Keyword.HUNTER));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
