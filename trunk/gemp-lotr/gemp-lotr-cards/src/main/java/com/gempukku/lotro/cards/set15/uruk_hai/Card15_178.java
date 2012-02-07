14743b2cb19e338be9141d203e3f557b7fc03240
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-07 12:46:48 +0000

    "Uruk Village Stormer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/uruk_hai/Card15_178.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set15.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.TakeControlOfASiteEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: The Hunters
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 10
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. While you can spot a companion of strength 12 or more, this minion is strength +4.
+ * Each time a companion or ally is killed during a skirmish involving an [URUK-HAI] minion, you may take control
+ * of a site.
+ */
+public class Card15_178 extends AbstractMinion {
+    public Card15_178() {
+        super(4, 10, 2, 5, Race.URUK_HAI, Culture.URUK_HAI, "Uruk Village Stormer");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new SpotCondition(CardType.COMPANION, Filters.not(Filters.lessStrengthThan(12))), 4);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.forEachKilled(game, effectResult, Filters.or(CardType.COMPANION, CardType.ALLY))
+                && PlayConditions.canSpot(game, Culture.URUK_HAI, CardType.MINION, Filters.inSkirmish)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new TakeControlOfASiteEffect(self, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
