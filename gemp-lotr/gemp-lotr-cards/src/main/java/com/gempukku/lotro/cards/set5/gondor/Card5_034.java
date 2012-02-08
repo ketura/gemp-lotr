0131e2f6631a525167c64299c59ceaa07549c4ea
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-08 23:42:07 +0000

    - "Fall Back" should not discard attached cards.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gondor/Card5_034.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set5/gondor/Card5_034.java
@@ -30,7 +30,7 @@ public class Card5_034 extends AbstractEvent {
                     @Override
                     protected void cardSelected(LotroGame game, PhysicalCard card) {
                         action.insertEffect(
-                                new DiscardCardsFromPlayEffect(self, Filters.and(Side.SHADOW, Filters.attachedTo(self))));
+                                new DiscardCardsFromPlayEffect(self, Filters.and(Side.SHADOW, Filters.attachedTo(card))));
                     }
                 });
         return action;
