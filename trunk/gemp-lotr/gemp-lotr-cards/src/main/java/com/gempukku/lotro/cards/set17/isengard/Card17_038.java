fcaec646e4e29b3571153f7a09e9f595feb628db
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-10 14:28:23 +0000

    "Saruman"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/isengard/Card17_038.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set17.isengard;
+
+import com.gempukku.lotro.cards.AbstractFollower;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Shadow
+ * Culture: Isengard
+ * Twilight Cost: 1
+ * Type: Follower
+ * Strength: +2
+ * Game Text: Aid - (1). (At the start of the maneuver phase, you may remove (1) to transfer this to a minion.)
+ * Each time bearer wins a skirmish, you may exert a companion.
+ */
+public class Card17_038 extends AbstractFollower {
+    public Card17_038() {
+        super(Side.SHADOW, 1, 2, 0, 0, Culture.ISENGARD, "Saruman", true);
+    }
+
+    @Override
+    protected boolean canPayAidCost(LotroGame game, PhysicalCard self) {
+        return game.getGameState().getTwilightPool() >= 1;
+    }
+
+    @Override
+    protected Effect getAidCost(LotroGame game, PhysicalCard self) {
+        return new RemoveTwilightEffect(1);
+    }
+
+    @Override
+    protected List<OptionalTriggerAction> getExtraOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.hasAttached(self))) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
