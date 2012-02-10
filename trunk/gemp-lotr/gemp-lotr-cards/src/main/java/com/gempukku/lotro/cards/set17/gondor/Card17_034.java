5c3cdca9b5fdbb9f9b23ca9497d82dc728ba2431
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-10 12:47:17 +0000

    "Spirit of the White Tree"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/gondor/Card17_034.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set17.gondor;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.cards.modifiers.OpponentsCantPlayPhaseEventsOrSpecialAbilitiesModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 1
+ * Type: Condition
+ * Game Text: Bearer must be a [GONDOR] companion. An opponent can not play skirmish events or use skirmish special
+ * abilities during skirmishes involving bearer. Each time a Shadow player adds a burden, discard this condition.
+ */
+public class Card17_034 extends AbstractAttachable {
+    public Card17_034() {
+        super(Side.FREE_PEOPLE, CardType.CONDITION, 1, Culture.GONDOR, null, "Spirit of the White Tree");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.GONDOR, CardType.COMPANION);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new OpponentsCantPlayPhaseEventsOrSpecialAbilitiesModifier(self,
+                new SpotCondition(Filters.hasAttached(self), Filters.inSkirmish), self.getOwner(), Phase.SKIRMISH);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.addedBurden(game, effectResult, Side.SHADOW)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new SelfDiscardEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
