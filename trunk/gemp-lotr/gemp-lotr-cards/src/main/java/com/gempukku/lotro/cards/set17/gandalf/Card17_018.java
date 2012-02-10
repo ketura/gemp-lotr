26c21018d4847216a7326ed8c0780a81e95bdd27
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-10 11:39:10 +0000

    "Glamdring"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/gandalf/Card17_018.java
@@ -0,0 +1,60 @@
+package com.gempukku.lotro.cards.set17.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPreventCardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Effect;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 2
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be Gandalf. Bearer is damage +1. Each time a companion is about to take a wound, you may
+ * discard a companion from hand to prevent that.
+ */
+public class Card17_018 extends AbstractAttachableFPPossession {
+    public Card17_018() {
+        super(2, 2, 0, Culture.GANDALF, PossessionClass.HAND_WEAPON, "Glamdring", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.gandalf;
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new KeywordModifier(self, Filters.hasAttached(self), Keyword.DAMAGE, 1));
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalBeforeTriggers(String playerId, LotroGame game, Effect effect, PhysicalCard self) {
+        if (TriggerConditions.isGettingWounded(effect, game, CardType.COMPANION)
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, CardType.COMPANION)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, CardType.COMPANION));
+            action.appendEffect(
+                    new ChooseAndPreventCardEffect(self, (WoundCharactersEffect) effect, playerId, "Choose a companion", CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
