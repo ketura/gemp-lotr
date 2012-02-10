ccc92710bc599874cb788390a474c0583c39303e
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-10 16:50:45 +0000

    "Grima's Dagger"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/men/Card17_044.java
@@ -0,0 +1,68 @@
+package com.gempukku.lotro.cards.set17.men;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.CancelEventEffect;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountCulturesEvaluator;
+import com.gempukku.lotro.cards.results.PlayEventResult;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +1
+ * Game Text: Bearer must be a [MEN] Man. If bearer is Grima, he is strength +1 for each Free Peoples culture
+ * you can spot over 2. Response: If a skirmish event is played, discard this possession to cancel that event.
+ */
+public class Card17_044 extends AbstractAttachable {
+    public Card17_044() {
+        super(Side.SHADOW, CardType.CONDITION, 1, Culture.MEN, PossessionClass.HAND_WEAPON, "Grima's Dagger", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.MEN, Race.MAN);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, Filters.hasAttached(self), 1));
+        modifiers.add(
+                new StrengthModifier(self, Filters.and(Filters.name("Grima"), Filters.hasAttached(self)), null,
+                        new CountCulturesEvaluator(2, Side.FREE_PEOPLE)));
+        return modifiers;
+    }
+
+    @Override
+    public List<? extends Action> getOptionalInPlayAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, CardType.EVENT, Keyword.SKIRMISH)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfDiscardEffect(self));
+            action.appendEffect(
+                    new CancelEventEffect(self, (PlayEventResult) effectResult));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
