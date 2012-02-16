f60631124834047a20a7b5cbd5dce1ae46dcbc11
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-16 08:20:52 +0000

    "Theoden"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/rohan/Card17_102.java
@@ -0,0 +1,59 @@
+package com.gempukku.lotro.cards.set17.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.LiberateASiteEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 7
+ * Vitality: 3
+ * Resistance: 7
+ * Game Text: While the Ringbearer is assigned to a skirmish, each [ROHAN] companion is strength +1. Regroup: Spot 2
+ * [ROHAN] followers and exert Theoden to liberate a site.
+ */
+public class Card17_102 extends AbstractCompanion {
+    public Card17_102() {
+        super(3, 7, 3, 7, Culture.ROHAN, Race.MAN, null, "Theoden", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, Filters.and(Culture.ROHAN, CardType.COMPANION),
+                new SpotCondition(Filters.ringBearer, Filters.assignedToSkirmish), 1);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self)
+                && PlayConditions.canSpot(game, 2, Culture.ROHAN, CardType.FOLLOWER)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(self));
+            action.appendEffect(
+                    new LiberateASiteEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
