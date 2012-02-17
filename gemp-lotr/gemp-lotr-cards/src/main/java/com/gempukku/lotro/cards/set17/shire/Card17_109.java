626793c599aeba1096b31a7c01980c56f6e0bcac
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-17 10:12:53 +0000

    "Pippin"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/shire/Card17_109.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set17.shire;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.modifiers.RaceSpotModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Companion • Hobbit
+ * Strength: 3
+ * Vitality: 4
+ * Resistance: 8
+ * Game Text: Add 1 to the number of Ents you can spot. Maneuver: Discard 2 pipeweed cards from play to remove a burden.
+ */
+public class Card17_109 extends AbstractCompanion {
+    public Card17_109() {
+        super(1, 3, 4, 8, Culture.SHIRE, Race.HOBBIT, null, "Pippin", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new RaceSpotModifier(self, Race.ENT);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.MANEUVER, self)
+                && PlayConditions.canDiscardFromPlay(self, game, 2, Keyword.PIPEWEED)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 2, 2, Keyword.PIPEWEED));
+            action.appendEffect(
+                    new RemoveBurdenEffect(playerId, self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
