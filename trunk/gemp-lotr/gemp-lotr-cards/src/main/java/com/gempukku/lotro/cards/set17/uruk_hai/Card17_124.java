8a09f352d053d8840fe39396a2e921413d1d0764
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-22 17:52:42 +0000

    "White Hand Destroyer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set17/uruk_hai/Card17_124.java
@@ -0,0 +1,36 @@
+package com.gempukku.lotro.cards.set17.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.MultiplyEvaluator;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * Set: Rise of Saruman
+ * Side: Shadow
+ * Culture: Uruk-hai
+ * Twilight Cost: 5
+ * Type: Minion • Uruk-Hai
+ * Strength: 10
+ * Vitality: 3
+ * Site: 5
+ * Game Text: Fierce. This minion gains hunter 3 for each site you control.
+ */
+public class Card17_124 extends AbstractMinion {
+    public Card17_124() {
+        super(5, 10, 3, 5, Race.URUK_HAI, Culture.URUK_HAI, "White Hand Destroyer", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new KeywordModifier(self, self, null, Keyword.HUNTER,
+                new MultiplyEvaluator(3, new CountActiveEvaluator(Filters.siteControlled(self.getOwner()))));
+    }
+}
