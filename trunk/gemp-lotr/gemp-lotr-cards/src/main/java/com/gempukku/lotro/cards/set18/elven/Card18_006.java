ab12c008ba45ff12b897be2f0a115a56ffd254ad
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-02-29 14:15:49 +0000

    "Gil-galad"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/elven/Card18_006.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/elven/Card18_006.java
@@ -3,8 +3,11 @@ package com.gempukku.lotro.cards.set18.elven;
 import com.gempukku.lotro.cards.AbstractEvent;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.ChoiceEffect;
 import com.gempukku.lotro.cards.effects.PutCardsFromDeckOnTopOfDrawDeckEffect;
 import com.gempukku.lotro.cards.effects.ShuffleDeckEffect;
+import com.gempukku.lotro.cards.effects.SpotEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Phase;
@@ -13,10 +16,12 @@ import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.effects.ChooseArbitraryCardsEffect;
+import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.UnrespondableEffect;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -43,6 +48,24 @@ public class Card18_006 extends AbstractEvent {
     @Override
     public PlayEventAction getPlayCardAction(final String playerId, LotroGame game, final PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
         final PlayEventAction action = new PlayEventAction(self);
+        List<Effect> possibleCosts = new LinkedList<Effect>();
+        possibleCosts.add(
+                new SpotEffect(1, Filters.arwen) {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return "Spot Arwen";
+                    }
+                });
+        possibleCosts.add(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Culture.ELVEN, CardType.COMPANION) {
+                    @Override
+                    public String getText(LotroGame game) {
+                        return "Exert ELVEN companion";
+                    }
+                });
+        action.appendCost(
+                new ChoiceEffect(action, playerId, possibleCosts));
+
         action.appendEffect(
                 new UnrespondableEffect() {
                     @Override
