dc08141141766f3658be5d3179e3e61f8d670c1b
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-06 16:19:20 +0000

    "Set Up"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gollum/Card18_033.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set18.gollum;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndReturnCardsToHandEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Shadow
+ * Culture: Gollum
+ * Twilight Cost: 2
+ * Type: Event • Shadow
+ * Game Text: Spot Gollum or Smeagol to return a follower to its owner's hand.
+ */
+public class Card18_033 extends AbstractEvent {
+    public Card18_033() {
+        super(Side.SHADOW, 2, Culture.GOLLUM, "Set Up", Phase.SHADOW);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.gollumOrSmeagol);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndReturnCardsToHandEffect(action, playerId, 1, 1, CardType.FOLLOWER));
+        return action;
+    }
+}
