6f408b1a3c4250c124cdcda4ff82c6c0eb23784f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-06 15:58:16 +0000

    "Ship of Smoke"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gandalf/Card18_027.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set18.gandalf;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.PutCardsFromDeckIntoHandDiscardRestEffect;
+import com.gempukku.lotro.cards.effects.RevealTopCardsOfDrawDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 0
+ * Type: Condition • Support Area
+ * Game Text: Pipeweed. Each time you play a pipeweed card, you may spot Gandalf to reveal the top 3 cards of your draw
+ * deck. Take all Free Peoples cards revealed into hand and discard the remaining cards.
+ */
+public class Card18_027 extends AbstractPermanent {
+    public Card18_027() {
+        super(Side.FREE_PEOPLE, 0, CardType.CONDITION, Culture.GANDALF, Zone.SUPPORT, "Ship of Smoke", true);
+        addKeyword(Keyword.PIPEWEED);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(final String playerId, final LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, Filters.owner(playerId), Keyword.PIPEWEED)
+                && PlayConditions.canSpot(game, Filters.gandalf)) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new RevealTopCardsOfDrawDeckEffect(self, playerId, 3) {
+                        @Override
+                        protected void cardsRevealed(List<PhysicalCard> cards) {
+                            action.appendEffect(
+                                    new PutCardsFromDeckIntoHandDiscardRestEffect(action, self, playerId, cards, Side.FREE_PEOPLE));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
