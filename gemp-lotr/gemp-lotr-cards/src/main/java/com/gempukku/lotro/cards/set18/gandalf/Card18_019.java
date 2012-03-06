b7e30fca2a4ba87c8fb23a0292497981d78448e6
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-06 11:07:27 +0000

    "Drawn to Full Height"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gandalf/Card18_019.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set18.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.CancelStrengthBonusTargetModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Gandalf
+ * Twilight Cost: 0
+ * Type: Event • Skirmish
+ * Game Text: Spell. Spot a [GANDALF] Wizard to make a minion lose all strength bonuses from possessions.
+ */
+public class Card18_019 extends AbstractEvent {
+    public Card18_019() {
+        super(Side.FREE_PEOPLE, 0, Culture.GANDALF, "Drawn to Full Height", Phase.SKIRMISH);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.GANDALF, Race.WIZARD);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose a minion", CardType.MINION) {
+                    @Override
+                    protected void cardSelected(LotroGame game, PhysicalCard card) {
+                        action.appendEffect(
+                                new AddUntilEndOfPhaseModifierEffect(
+                                        new CancelStrengthBonusTargetModifier(self, card, CardType.POSSESSION), Phase.SKIRMISH));
+                    }
+                });
+        return action;
+    }
+}
