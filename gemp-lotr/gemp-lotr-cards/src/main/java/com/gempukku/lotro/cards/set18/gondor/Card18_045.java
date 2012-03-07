7a9f7e3e696496289964b8053898fa13cff7de1d
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-07 11:03:56 +0000

    "Dunadan's Bow"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gondor/Card18_045.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set18.gondor;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 2
+ * Type: Possession • Ranged Weapon
+ * Strength: +1
+ * Game Text: Bearer must be a [GONDOR] ranger. Archery: Discard 2 [GONDOR] cards from hand to make the fellowship
+ * archery total +1.
+ */
+public class Card18_045 extends AbstractAttachableFPPossession {
+    public Card18_045() {
+        super(2, 1, 0, Culture.GONDOR, PossessionClass.RANGED_WEAPON, "Dunadan's Bow");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.GONDOR, Keyword.RANGER);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.ARCHERY, self)
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Culture.GONDOR)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2, Culture.GONDOR));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new ArcheryTotalModifier(self, Side.FREE_PEOPLE, 1), Phase.ARCHERY));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
