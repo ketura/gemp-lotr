72f92a6b41447331eff85d4ecb5b9464541acac1
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-07 12:21:35 +0000

    "Corsair Grappling Hook"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/men/Card18_060.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/men/Card18_060.java
@@ -1,6 +1,6 @@
 package com.gempukku.lotro.cards.set18.men;
 
-import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.AbstractAttachable;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
 import com.gempukku.lotro.cards.effects.ReinforceTokenEffect;
@@ -11,6 +11,8 @@ import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
 import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.timing.Action;
 
 import java.util.Collections;
@@ -26,9 +28,9 @@ import java.util.List;
  * Game Text: Bearer must be a [MEN] minion. Skirmish: Remove (3) to reinforce a [MEN] token and make bearer fierce
  * until the regroup phase.
  */
-public class Card18_060 extends AbstractAttachableFPPossession {
+public class Card18_060 extends AbstractAttachable {
     public Card18_060() {
-        super(1, 2, 0, Culture.MEN, PossessionClass.HAND_WEAPON, "Corsair Boarding Axe");
+        super(Side.SHADOW, CardType.POSSESSION, 1, Culture.MEN, PossessionClass.HAND_WEAPON, "Corsair Boarding Axe");
     }
 
     @Override
@@ -37,7 +39,12 @@ public class Card18_060 extends AbstractAttachableFPPossession {
     }
 
     @Override
-    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, Filters.hasAttached(self), 2);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
         if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 3)) {
             ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
