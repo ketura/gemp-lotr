7378a30f2d61ceccd3efd8d43c904571d3f8f148
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-07 15:39:37 +0000

    "Treachery"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/men/Card18_076.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set18.men;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.RemoveGameTextModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 3
+ * Type: Event • Shadow
+ * Game Text: Spot Grima to exert an unbound companion. That companion cannot use his or her game text until
+ * the regroup phase.
+ */
+public class Card18_076 extends AbstractEvent {
+    public Card18_076() {
+        super(Side.SHADOW, 3, Culture.MEN, "Treachery", Phase.SHADOW);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.name("Grima"));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.unboundCompanion) {
+                    @Override
+                    protected void forEachCardExertedCallback(PhysicalCard character) {
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new RemoveGameTextModifier(self, character), Phase.REGROUP));
+                    }
+                });
+        return action;
+    }
+}
