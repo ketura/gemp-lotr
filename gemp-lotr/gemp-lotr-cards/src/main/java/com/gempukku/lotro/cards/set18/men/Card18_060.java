4689792d7ab4c99dfca1917dcb4c4045f56aff48
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-07 12:11:27 +0000

    "Corsair Boarding Axe"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/men/Card18_060.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set18.men;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ReinforceTokenEffect;
+import com.gempukku.lotro.cards.effects.RemoveTwilightEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Shadow
+ * Culture: Men
+ * Twilight Cost: 1
+ * Type: Possession • Hand Weapon
+ * Strength: +2
+ * Game Text: Bearer must be a [MEN] minion. Skirmish: Remove (3) to reinforce a [MEN] token and make bearer fierce
+ * until the regroup phase.
+ */
+public class Card18_060 extends AbstractAttachableFPPossession {
+    public Card18_060() {
+        super(1, 2, 0, Culture.MEN, PossessionClass.HAND_WEAPON, "Corsair Boarding Axe");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.MEN, CardType.MINION);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 3)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveTwilightEffect(3));
+            action.appendEffect(
+                    new ReinforceTokenEffect(self, playerId, Token.MEN));
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new KeywordModifier(self, self.getAttachedTo(), Keyword.FIERCE), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
