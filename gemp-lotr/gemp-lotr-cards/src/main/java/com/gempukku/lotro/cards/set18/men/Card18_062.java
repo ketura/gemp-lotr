b96a7fc33d78ff14e6147f3266b9a7ed89911452
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-07 12:21:35 +0000

    "Corsair Grappling Hook"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/men/Card18_062.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/men/Card18_062.java
@@ -3,19 +3,18 @@ package com.gempukku.lotro.cards.set18.men;
 import com.gempukku.lotro.cards.AbstractAttachable;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.ReinforceTokenEffect;
 import com.gempukku.lotro.cards.effects.TakeControlOfASiteEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.modifiers.KeywordModifier;
 import com.gempukku.lotro.logic.modifiers.Modifier;
 import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -23,14 +22,14 @@ import java.util.List;
  * Side: Shadow
  * Culture: Men
  * Twilight Cost: 2
- * Type: Possession • Ranged Weapon
+ * Type: Possession • Hand Weapon
  * Strength: +1
- * Game Text: Bearer must be a [MEN] minion. Bearer is an archer. Each time a companion is killed during
- * the archery phase, you may control a site.
+ * Game Text: Bearer must be a [MEN] minion. When you play this possession you may reinforce a [MEN] token twice.
+ * Each time a companion is killed during a skirmish phase, you may control a site.
  */
-public class Card18_061 extends AbstractAttachable {
-    public Card18_061() {
-        super(Side.SHADOW, CardType.POSSESSION, 2, Culture.MEN, PossessionClass.RANGED_WEAPON, "Corsair Bow");
+public class Card18_062 extends AbstractAttachable {
+    public Card18_062() {
+        super(Side.SHADOW, CardType.POSSESSION, 2, Culture.MEN, PossessionClass.HAND_WEAPON, "Corsair Grappling Hook");
     }
 
     @Override
@@ -39,19 +38,22 @@ public class Card18_061 extends AbstractAttachable {
     }
 
     @Override
-    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(
-                new StrengthModifier(self, Filters.hasAttached(self), 1));
-        modifiers.add(
-                new KeywordModifier(self, Filters.hasAttached(self), Keyword.ARCHER));
-        return modifiers;
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, Filters.hasAttached(self), 1);
     }
 
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ReinforceTokenEffect(self, playerId, Token.MEN));
+            action.appendEffect(
+                    new ReinforceTokenEffect(self, playerId, Token.MEN));
+            return Collections.singletonList(action);
+        }
         if (TriggerConditions.forEachKilled(game, effectResult, CardType.COMPANION)
-                && PlayConditions.isPhase(game, Phase.ARCHERY)) {
+                && PlayConditions.isPhase(game, Phase.SKIRMISH)) {
             OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendEffect(
                     new TakeControlOfASiteEffect(self, playerId));
