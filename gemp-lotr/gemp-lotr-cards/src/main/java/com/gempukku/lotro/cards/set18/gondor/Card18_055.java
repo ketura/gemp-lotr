725c91b6d03528b433c1c5af4a772b5d45d1a2cb
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-07 11:55:19 +0000

    "Ranger of the North"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gondor/Card18_055.java
@@ -0,0 +1,63 @@
+package com.gempukku.lotro.cards.set18.gondor;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Gondor
+ * Twilight Cost: 0
+ * Type: Condition
+ * Game Text: Bearer must be a [GONDOR] Man. Maneuver: If the fellowship is in region 1 or region 2, you may exert
+ * bearer to make him or her defender +1 until the regroup phase. Regroup: Discard this to heal a companion.
+ */
+public class Card18_055 extends AbstractAttachable {
+    public Card18_055() {
+        super(Side.FREE_PEOPLE, CardType.CONDITION, 0, Culture.GONDOR, null, "Ranger of the North", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.GONDOR, Race.MAN);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.MANEUVER, self)
+                && PlayConditions.location(game, Filters.or(Filters.region(1), Filters.region(2)))
+                && PlayConditions.canExert(self, game, self.getAttachedTo())) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self.getAttachedTo()));
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new KeywordModifier(self, self.getAttachedTo(), Keyword.DEFENDER, 1), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfDiscardEffect(self));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, 1, 1, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
