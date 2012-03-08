2789cf51e6c52d97efdb1e05f84a7b78546e6a4f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 15:06:15 +0000

    "Eomer's Bow"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/rohan/Card18_095.java
@@ -0,0 +1,64 @@
+package com.gempukku.lotro.cards.set18.rohan;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseOpponentEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 1
+ * Type: Possession • Ranged Weapon
+ * Game Text: Bearer must be a [ROHAN] Man. If bearer is mounted, he or she is strength +1. Archery: If bearer is Eomer,
+ * exert him to choose a Shadow player who must exert one of his or her minions.
+ */
+public class Card18_095 extends AbstractAttachableFPPossession {
+    public Card18_095() {
+        super(1, 0, 0, Culture.ROHAN, PossessionClass.RANGED_WEAPON, "Eomer's Bow", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.ROHAN, Race.MAN);
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.and(Filters.hasAttached(self), Filters.mounted), 1));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.ARCHERY, self)
+                && PlayConditions.canExert(self, game, self.getAttachedTo(), Filters.name("Eomer"))) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ExertCharactersEffect(self, self.getAttachedTo()));
+            action.appendEffect(
+                    new ChooseOpponentEffect(playerId) {
+                        @Override
+                        protected void opponentChosen(String opponentId) {
+                            action.appendEffect(
+                                    new ChooseAndExertCharactersEffect(action, opponentId, 1, 1, Filters.owner(opponentId), CardType.MINION));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
