135ef795da9b63a1e421b9bd9df9eb05eec50ad4
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 15:49:33 +0000

    "Theoden"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/rohan/Card18_105.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set18.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 3
+ * Type: Companion • Man
+ * Strength: 7
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: While you can spot Gandalf, Theoden is resistance +2. While you can spot Eomer, Theoden is damage +1.
+ * While you can spot 3 mounted [ROHAN] companions, Theoden is strength +2.
+ */
+public class Card18_105 extends AbstractCompanion {
+    public Card18_105() {
+        super(3, 7, 3, 6, Culture.ROHAN, Race.MAN, null, "Theoden", true);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new ResistanceModifier(self, self, new SpotCondition(Filters.gandalf), 2));
+        modifiers.add(
+                new KeywordModifier(self, self, new SpotCondition(Filters.name("Eomer")), Keyword.DAMAGE, 1));
+        modifiers.add(
+                new StrengthModifier(self, self, new SpotCondition(3, Culture.ROHAN, CardType.COMPANION, Filters.mounted), 2));
+        return modifiers;
+    }
+}
