ea02f158f75fd01eb132c6fd8d8e1eb2b2bc5522
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 15:52:52 +0000

    "A Dragon's Tale"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/shire/Card18_106.java
@@ -0,0 +1,58 @@
+package com.gempukku.lotro.cards.set18.shire;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseOpponentEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 1
+ * Type: Condition • Support Area
+ * Game Text: Tale. To play, spot a Hobbit. Maneuver: Discard a tale from play to choose a Shadow player who must exert
+ * one of his or her minions.
+ */
+public class Card18_106 extends AbstractPermanent {
+    public Card18_106() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.SHIRE, Zone.SUPPORT, "A Dragon's Tale");
+        addKeyword(Keyword.TALE);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Race.HOBBIT);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.MANEUVER, self)
+                && PlayConditions.canDiscardFromPlay(self, game, Keyword.TALE)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Keyword.TALE));
+            action.appendEffect(
+                    new ChooseOpponentEffect(playerId) {
+                        @Override
+                        protected void opponentChosen(String opponentId) {
+                            action.appendEffect(
+                                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.owner(playerId), CardType.MINION));
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
