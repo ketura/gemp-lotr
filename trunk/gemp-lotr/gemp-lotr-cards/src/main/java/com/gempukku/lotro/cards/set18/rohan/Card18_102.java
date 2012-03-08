36833a9bd538b095a54bcc7b8b498ebf49f9d245
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 15:34:33 +0000

    "Rohirrim Diadem"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/rohan/Card18_102.java
@@ -0,0 +1,60 @@
+package com.gempukku.lotro.cards.set18.rohan;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Free
+ * Culture: Rohan
+ * Twilight Cost: 2
+ * Type: Condition • Support Area
+ * Game Text: To play, spot Theoden. Skirmish: Spot a [ROHAN] Man bearing a possession and discard a [ROHAN] possession
+ * from hand to make that Man strength +2.
+ */
+public class Card18_102 extends AbstractPermanent {
+    public Card18_102() {
+        super(Side.FREE_PEOPLE, 2, CardType.CONDITION, Culture.ROHAN, Zone.SUPPORT, "Rohirrim Diadem");
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.name("Theoden"));
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canSpot(game, Culture.ROHAN, Race.MAN, Filters.hasAttached(CardType.POSSESSION))
+                && PlayConditions.canDiscardFromHand(game, playerId, 1, Culture.ROHAN, CardType.POSSESSION)) {
+            final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseActiveCardEffect(self, playerId, "Choose a ROHAN Man bearing a possession", Culture.ROHAN, Race.MAN, Filters.hasAttached(CardType.POSSESSION)) {
+                        @Override
+                        protected void cardSelected(LotroGame game, PhysicalCard card) {
+                            action.appendEffect(
+                                    new AddUntilEndOfPhaseModifierEffect(
+                                            new StrengthModifier(self, card, 2), Phase.SKIRMISH));
+                        }
+                    });
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, Culture.ROHAN, CardType.POSSESSION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
