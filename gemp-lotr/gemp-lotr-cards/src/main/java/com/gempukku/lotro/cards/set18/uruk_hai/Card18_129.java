8ef12ee072d886ac3e2fee00eaedb2fe4051868c
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 17:22:20 +0000

    "White Hand Sieger"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/uruk_hai/Card18_129.java
@@ -0,0 +1,36 @@
+package com.gempukku.lotro.cards.set18.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Shadow
+ * Culture: Uruk-Hai
+ * Twilight Cost: 0
+ * Type: Minion • Uruk-Hai
+ * Strength: 7
+ * Vitality: 1
+ * Site: 5
+ * Game Text: Damage +1. To play, spot an [URUK-HAI] minion and you must control a site.
+ */
+public class Card18_129 extends AbstractMinion {
+    public Card18_129() {
+        super(0, 7, 1, 5, Race.URUK_HAI, Culture.URUK_HAI, "White Hand Sieger");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Culture.URUK_HAI, CardType.MINION)
+                && PlayConditions.canSpot(game, Filters.siteControlled(playerId));
+    }
+}
