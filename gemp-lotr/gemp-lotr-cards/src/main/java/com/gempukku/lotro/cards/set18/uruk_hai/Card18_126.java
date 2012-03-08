e9b7a142c8a7cae889ee642f62c986f9ae5bb82f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 17:17:52 +0000

    "White Hand Marchers"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/uruk_hai/Card18_126.java
@@ -0,0 +1,35 @@
+package com.gempukku.lotro.cards.set18.uruk_hai;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * Set: Treachery & Deceit
+ * Side: Shadow
+ * Culture: Uruk-Hai
+ * Twilight Cost: 4
+ * Type: Minion • Uruk-Hai
+ * Strength: 9
+ * Vitality: 2
+ * Site: 5
+ * Game Text: Damage +1. This minion is strength +1 for each other [URUK-HAI] minion you can spot.
+ */
+public class Card18_126 extends AbstractMinion {
+    public Card18_126() {
+        super(4, 9, 2, 5, Race.URUK_HAI, Culture.URUK_HAI, "White Hand Marchers", true);
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null, new CountActiveEvaluator(Filters.not(self), Culture.URUK_HAI, CardType.MINION));
+    }
+}
