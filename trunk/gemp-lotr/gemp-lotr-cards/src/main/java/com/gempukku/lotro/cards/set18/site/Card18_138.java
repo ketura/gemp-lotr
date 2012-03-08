15efb39d79cc9e492e0f7d0633f90d7fa1f50a48
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-08 17:55:32 +0000

    "Sirannon Ruins"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/site/Card18_138.java
@@ -0,0 +1,17 @@
+package com.gempukku.lotro.cards.set18.site;
+
+import com.gempukku.lotro.cards.AbstractNewSite;
+import com.gempukku.lotro.common.Keyword;
+
+/**
+ * Set: Treachery & Deceit
+ * Twilight Cost: 4
+ * Type: Site
+ * Game Text: River.
+ */
+public class Card18_138 extends AbstractNewSite {
+    public Card18_138() {
+        super("Sirannon Ruins", 4, Direction.RIGHT);
+        addKeyword(Keyword.RIVER);
+    }
+}
