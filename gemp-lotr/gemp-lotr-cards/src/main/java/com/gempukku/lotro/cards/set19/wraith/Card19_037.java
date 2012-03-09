a08d79845ab8cf929be05331a34bc8d9268198ef
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-09 12:30:24 +0000

    "Úlairë Lemenya"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set19/wraith/Card19_037.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set19.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+
+/**
+ * Set: Ages End
+ * Side: Shadow
+ * Culture: Wraith
+ * Twilight Cost: 4
+ * Type: Minion • Nazgul
+ * Strength: 9
+ * Vitality: 2
+ * Site: 3
+ * Game Text: Fierce. Ulaire Lemenya is strength +1 for each [WRAITH] possession in your discard pile.
+ */
+public class Card19_037 extends AbstractMinion {
+    public Card19_037() {
+        super(4, 9, 2, 3, Race.NAZGUL, Culture.WRAITH, "Úlairë Lemenya", true);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(final PhysicalCard self) {
+        return new StrengthModifier(self, self, null,
+                new Evaluator() {
+                    @Override
+                    public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+                        return Filters.filter(gameState.getDiscard(self.getOwner()), gameState, modifiersQuerying, Culture.WRAITH, CardType.POSSESSION).size();
+                    }
+                });
+    }
+}
