4f1bbff13126b48d15e9f77eb9a26af90333f196
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-09 12:18:27 +0000

    "Sam"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set19/shire/Card19_032.java
@@ -0,0 +1,63 @@
+package com.gempukku.lotro.cards.set19.shire;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.MakeRingBearerEffect;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Ages End
+ * Side: Free
+ * Culture: Shire
+ * Twilight Cost: 2
+ * Type: Companion • Hobbit
+ * Strength: 3
+ * Vitality: 4
+ * Resistance: 9
+ * Game Text: Fellowship. Ring-bound. While Sam bears The One Ring, he is resistance -4. While you can spot
+ * 3 [SHIRE] companions, Sam is strength +2. Response: If Frodo dies, make Sam the Ring-bearer.
+ */
+public class Card19_032 extends AbstractCompanion {
+    public Card19_032() {
+        super(2, 3, 4, 9, Culture.SHIRE, Race.HOBBIT, null, "Sam", true);
+        addKeyword(Keyword.FELLOWSHIP);
+        addKeyword(Keyword.RING_BOUND);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new ResistanceModifier(self, Filters.and(self, Filters.hasAttached(CardType.THE_ONE_RING)), -4));
+        modifiers.add(
+                new StrengthModifier(self, self, new SpotCondition(3, Culture.SHIRE, CardType.COMPANION), 2));
+        return modifiers;
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.forEachKilled(game, effectResult, Filters.frodo, Filters.ringBearer)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(new MakeRingBearerEffect(self));
+            return Collections.singletonList(action);
+        }
+
+        return null;
+    }
+}
