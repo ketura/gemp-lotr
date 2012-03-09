ea8b4e7e536b81ee01182cc4dff696c6dcd43250
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-09 10:06:48 +0000

    "Gimli"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set19/dwarven/Card19_002.java
@@ -0,0 +1,47 @@
+package com.gempukku.lotro.cards.set19.dwarven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Ages End
+ * Side: Free
+ * Culture: Dwarven
+ * Twilight Cost: 2
+ * Type: Companion • Dwarf
+ * Strength: 6
+ * Vitality: 3
+ * Resistance: 6
+ * Game Text: Damage +1. Fellowship. Each time you play a [DWARVEN] possession, you may draw a card.
+ */
+public class Card19_002 extends AbstractCompanion {
+    public Card19_002() {
+        super(2, 6, 3, 6, Culture.DWARVEN, Race.DWARF, null, "Gimli", true);
+        addKeyword(Keyword.DAMAGE, 1);
+        addKeyword(Keyword.FELLOWSHIP);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, Filters.owner(playerId), Culture.DWARVEN, CardType.POSSESSION)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new DrawCardsEffect(action, playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
