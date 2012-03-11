98fcbe420c69eea361bb4725b676af9bc8fdffc3
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-11 17:21:36 +0000

    - "Urgency" now has the activated ability to discard itself to add +1 strength.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set19/isengard/Card19_017.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set19/isengard/Card19_017.java
@@ -1,13 +1,18 @@
 package com.gempukku.lotro.cards.set19.isengard;
 
 import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.timing.Action;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -35,6 +40,20 @@ public class Card19_017 extends AbstractPermanent {
             int count = Filters.countActive(game.getGameState(), game.getModifiersQuerying(), Race.ENT);
             action.appendEffect(
                     new AddTwilightEffect(self, 2 * count));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfDiscardEffect(self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 1, CardType.MINION, Filters.not(Culture.WRAITH)));
             return Collections.singletonList(action);
         }
         return null;
