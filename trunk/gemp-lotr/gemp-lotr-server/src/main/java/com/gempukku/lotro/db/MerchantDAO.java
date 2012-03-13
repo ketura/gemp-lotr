20344d616de0d8a022e745bc04126e21eb08f591
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-13 12:29:33 +0000

    Modifying merchant.

--- trunk/gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/db/MerchantDAO.java
+++ trunk/gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/db/MerchantDAO.java
@@ -5,15 +5,21 @@ import java.util.Date;
 public interface MerchantDAO {
     public Transaction getLastTransaction(String blueprintId);
 
-    public void addTransaction(String blueprintId, float price, Date date);
+    public void addTransaction(String blueprintId, float price, Date date, TransactionType transactionType);
+
+    public enum TransactionType {
+        SELL, BUY
+    }
 
     public static class Transaction {
         private float _price;
         private Date _date;
+        private TransactionType _transactionType;
 
-        public Transaction(Date date, float price) {
+        public Transaction(Date date, float price, TransactionType transactionType) {
             _date = date;
             _price = price;
+            _transactionType = transactionType;
         }
 
         public Date getDate() {
@@ -22,6 +28,10 @@ public interface MerchantDAO {
 
         public float getPrice() {
             return _price;
+        }
+
+        public TransactionType getTransactionType() {
+            return _transactionType;
         }
     }
 }
