93b1f9fbc70f8aed4f3bda769eb5272feabb91c9
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-03-14 12:12:40 +0000

    Merchant look&feel.

--- gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/merchant/ParametrizedMerchantTest.java
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/merchant/ParametrizedMerchantTest.java
@@ -55,6 +55,23 @@ public class ParametrizedMerchantTest {
         }
     }
 
+
+    //    @Test
+    public void plotPriceEasingFromSetup() {
+        Date setupDate = new Date(0);
+
+        MerchantDAO merchantDao = new MockMerchantDAO();
+
+        ParametrizedMerchant merchant = new ParametrizedMerchant(new LotroCardBlueprintLibrary());
+        merchant.setMerchantDao(merchantDao);
+        merchant.setMerchantSetupDate(setupDate);
+
+        long hour = 1000 * 60 * 60;
+
+        for (long time = 0; time < hour * 24 * 35L; time += hour * 2)
+            System.out.println(time / hour + "," + ((int) merchant.getCardSellPrice("1_1", new Date(time))) + "," + ((int) merchant.getCardBuyPrice("1_1", new Date(time))));
+    }
+
     private void assertEqualsMoreOrLess(int expected, int given) {
         assertTrue("Expected " + expected + " more or less, but received " + given, given <= expected + 1 && given >= expected - 1);
     }
