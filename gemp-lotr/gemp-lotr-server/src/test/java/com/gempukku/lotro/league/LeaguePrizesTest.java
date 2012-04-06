b1fe104b6388ebbea58df1f0ca66ea486fbca11f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-04-06 14:48:36 +0000

    League prizes test.

--- gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/league/LeaguePrizesTest.java
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/league/LeaguePrizesTest.java
@@ -14,4 +14,13 @@ public class LeaguePrizesTest {
             System.out.println(stringIntegerEntry.getKey() + ": " + stringIntegerEntry.getValue());
         }
     }
+
+    @Test
+    public void testLeaguePrize() {
+        LeaguePrizes leaguePrizes = new LeaguePrizes();
+        CardCollection prize = leaguePrizes.getPrizeForLeague(1, 100, 2f, "fotr_block");
+        for (Map.Entry<String, Integer> stringIntegerEntry : prize.getAll().entrySet()) {
+            System.out.println(stringIntegerEntry.getKey() + ": " + stringIntegerEntry.getValue());
+        }
+    }
 }
