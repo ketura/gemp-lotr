cffc754e86bb37aba57d214888d5ae56e402b8f8
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-05-14 15:19:37 +0000

    - "The Mirror of Galadriel, Dangerous Guide" should no longer freeze the game, if top 3 cards are shadow only.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/elven/Card15_022.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set15/elven/Card15_022.java
@@ -8,21 +8,15 @@ import com.gempukku.lotro.cards.effects.PutCardsFromDeckBeneathDrawDeckEffect;
 import com.gempukku.lotro.cards.effects.RevealTopCardsOfDrawDeckEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.common.*;
-import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.decisions.ArbitraryCardsSelectionDecision;
-import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
+import com.gempukku.lotro.logic.effects.ChooseArbitraryCardsEffect;
 import com.gempukku.lotro.logic.effects.DrawOneCardEffect;
-import com.gempukku.lotro.logic.effects.PlayoutDecisionEffect;
 import com.gempukku.lotro.logic.effects.PreventEffect;
 import com.gempukku.lotro.logic.timing.Effect;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Set: The Hunters
@@ -59,23 +53,20 @@ public class Card15_022 extends AbstractPermanent {
                         @Override
                         protected void cardsRevealed(final List<PhysicalCard> revealedCards) {
                             action.appendEffect(
-                                    new PlayoutDecisionEffect(playerId,
-                                            new ArbitraryCardsSelectionDecision(1, "Choose card to put into your hand", revealedCards,
-                                                    Filters.filter(revealedCards, game.getGameState(), game.getModifiersQuerying(), Side.FREE_PEOPLE, Side.FREE_PEOPLE), 1, 1) {
-                                                @Override
-                                                public void decisionMade(String result) throws DecisionResultInvalidException {
-                                                    final List<PhysicalCard> selectedCardsByResponse = getSelectedCardsByResponse(result);
-                                                    Set<PhysicalCard> cardsToPutOnBottom = new HashSet<PhysicalCard>(revealedCards);
-                                                    for (PhysicalCard physicalCard : selectedCardsByResponse) {
-                                                        cardsToPutOnBottom.remove(physicalCard);
-                                                        action.appendEffect(
-                                                                new PutCardFromDeckIntoHandEffect(physicalCard));
-                                                    }
+                                    new ChooseArbitraryCardsEffect(playerId, "Choose card to put into your hand", revealedCards, Side.FREE_PEOPLE, 1, 1) {
+                                        @Override
+                                        protected void cardsSelected(LotroGame game, Collection<PhysicalCard> selectedCards) {
+                                            Set<PhysicalCard> cardsToPutOnBottom = new HashSet<PhysicalCard>(revealedCards);
+                                            for (PhysicalCard physicalCard : selectedCards) {
+                                                cardsToPutOnBottom.remove(physicalCard);
+                                                action.appendEffect(
+                                                        new PutCardFromDeckIntoHandEffect(physicalCard));
+                                            }
 
-                                                    action.appendEffect(
-                                                            new PutCardsFromDeckBeneathDrawDeckEffect(action, self, playerId, cardsToPutOnBottom));
-                                                }
-                                            }));
+                                            action.appendEffect(
+                                                    new PutCardsFromDeckBeneathDrawDeckEffect(action, self, playerId, cardsToPutOnBottom));
+                                        }
+                                    });
                         }
                     });
             return Collections.singletonList(action);
