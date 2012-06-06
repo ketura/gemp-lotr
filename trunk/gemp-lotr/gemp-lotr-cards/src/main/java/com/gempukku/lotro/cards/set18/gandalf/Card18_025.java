1b44a8ed68a71f30315d0d56df16ca53b162d256
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-06-06 17:15:28 +0000

    - "Perspective" now correctly adds threats.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gandalf/Card18_025.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set18/gandalf/Card18_025.java
@@ -8,6 +8,7 @@ import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
 import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
 import com.gempukku.lotro.logic.timing.Action;
 
@@ -34,6 +35,8 @@ public class Card18_025 extends AbstractPermanent {
                 && PlayConditions.canAddThreat(game, self, 2)
                 && PlayConditions.canSpot(game, Race.WIZARD)) {
             final ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new AddThreatsEffect(playerId, self, 2));
             action.appendEffect(
                     new ChooseActiveCardEffect(self, playerId, "Choose a minion", CardType.MINION) {
                         @Override
