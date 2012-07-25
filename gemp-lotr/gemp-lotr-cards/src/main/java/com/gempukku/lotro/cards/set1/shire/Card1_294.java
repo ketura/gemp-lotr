8d98501b939d39428dc80f3d80e1d952963fe96f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2012-07-25 10:37:14 +0000

    - "Hobbit Appetite" allows to heal Hobbit companions only.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/shire/Card1_294.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/shire/Card1_294.java
@@ -2,10 +2,7 @@ package com.gempukku.lotro.cards.set1.shire;
 
 import com.gempukku.lotro.cards.AbstractOldEvent;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Phase;
-import com.gempukku.lotro.common.Race;
-import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
@@ -43,7 +40,7 @@ public class Card1_294 extends AbstractOldEvent {
                                 final int twilight = getValidatedResult(result);
                                 action.appendCost(new AddTwilightEffect(self, twilight));
                                 action.appendEffect(
-                                        new ChooseAndHealCharactersEffect(action, playerId, 1, 1, twilight, Race.HOBBIT));
+                                        new ChooseAndHealCharactersEffect(action, playerId, 1, 1, twilight, CardType.COMPANION, Race.HOBBIT));
                             }
                         }));
         return action;
