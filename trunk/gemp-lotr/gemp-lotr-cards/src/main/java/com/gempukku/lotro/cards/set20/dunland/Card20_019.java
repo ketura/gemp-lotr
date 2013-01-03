d0fe9911168a569df8e1f404559994ea7cb79e27
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-03 15:14:28 +0000

    "Hillman Warrior"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_019.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set20.dunland;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.CantTakeWoundsModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.AndCondition;
+import com.gempukku.lotro.cards.modifiers.conditions.NotCondition;
+import com.gempukku.lotro.cards.modifiers.conditions.PhaseCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 3
+ * Hillman Warrior
+ * Dunland	Minion • Man
+ * 9	1	3
+ * While a [Dunland] Man is stacked on a site you control, this minion is strength +3, damage +1 and may only
+ * take wounds during skirmishes.
+ */
+public class Card20_019 extends AbstractMinion {
+    public Card20_019() {
+        super(3, 9, 1, 3, Race.MAN, Culture.DUNLAND, "Hillman Warrior");
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        SpotCondition condition = new SpotCondition(Filters.siteControlled(self.getOwner()), Filters.hasStacked(Culture.DUNLAND, Race.MAN));
+        modifiers.add(
+                new StrengthModifier(self, self, condition, 3));
+        modifiers.add(
+                new KeywordModifier(self, self, condition, Keyword.DAMAGE, 1));
+        modifiers.add(
+                new CantTakeWoundsModifier(self, new AndCondition(condition, new NotCondition(new PhaseCondition(Phase.SKIRMISH))), self));
+        return modifiers;
+    }
+}
