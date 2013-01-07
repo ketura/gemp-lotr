956d1f2c1ecb18e7c5699d3665be82bbb48c9570
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-07 15:13:42 +0000

    "Legolas", "Son of Thranduil"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_090.java
@@ -0,0 +1,32 @@
+package com.gempukku.lotro.cards.set20.elven;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * 2
+ * •Legolas, Son of Thranduil
+ * Elven	Companion • Elf
+ * 6	3	8
+ * Archer.
+ * While Legolas is at a river or forest, add 1 to the fellowship archery total.
+ */
+public class Card20_090 extends AbstractCompanion {
+    public Card20_090() {
+        super(2, 6, 3, 8, Culture.ELVEN, Race.ELF, null, "Legolas", "Son of Thranduil", true);
+        addKeyword(Keyword.ARCHER);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new ArcheryTotalModifier(self, Side.FREE_PEOPLE, new LocationCondition(Filters.or(Keyword.RIVER, Keyword.FOREST)), 1);
+    }
+}
