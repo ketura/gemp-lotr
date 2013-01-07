90cfe99caeeb8692597157d7a13e4793b7e5ebbd
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-07 16:51:07 +0000

    "Silent Scouts"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_103.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set20.elven;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.RevealTopCardsOfDrawDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1
+ * •Silent Scouts
+ * Elven	Condition • Support Area
+ * Each time the fellowship moves during the regroup phase, you may reveal the top card of your draw deck. If it is
+ * an [Elven] card, wound a minion.
+ */
+public class Card20_103 extends AbstractPermanent {
+    public Card20_103() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.ELVEN, Zone.SUPPORT, "Silent Scouts", null, true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(final String playerId, final LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.moves(game, effectResult)
+                && PlayConditions.isPhase(game, Phase.REGROUP)) {
+            final OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new RevealTopCardsOfDrawDeckEffect(self, playerId, 1) {
+                        @Override
+                        protected void cardsRevealed(List<PhysicalCard> revealedCards) {
+                            for (PhysicalCard revealedCard : revealedCards) {
+                                if (Filters.and(Culture.ELVEN).accepts(game.getGameState(), game.getModifiersQuerying(), revealedCard))
+                                    action.appendEffect(
+                                            new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, CardType.MINION));
+                            }
+                        }
+                    });
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
