19aede362c9a7909f0419693fe8c27a243524212
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-07 16:57:55 +0000

    "We Could Have Shot Him in the Dark"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_106.java
@@ -0,0 +1,38 @@
+package com.gempukku.lotro.cards.set20.elven;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * 2
+ * We Could Have Shot Him in the Dark
+ * Elven	Event • Archery
+ * Spot 2 Elf Archers to make the fellowship archery total +1 (or +2 if at a forest).
+ */
+public class Card20_106 extends AbstractEvent {
+    public Card20_106() {
+        super(Side.FREE_PEOPLE, 2, Culture.ELVEN, "We Could Have Shot Him in the Dark", Phase.ARCHERY);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, 2, Race.ELF, Keyword.ARCHER);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        int count = PlayConditions.location(game, Keyword.FOREST)?2:1;
+        action.appendEffect(
+                new AddUntilEndOfPhaseModifierEffect(
+                        new ArcheryTotalModifier(self, Side.FREE_PEOPLE, null, count), Phase.ARCHERY));
+        return action;
+    }
+}
