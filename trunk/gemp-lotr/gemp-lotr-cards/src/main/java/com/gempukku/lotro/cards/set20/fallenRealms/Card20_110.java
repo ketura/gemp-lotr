f420aebe09acf4ae35ad5f80e86e46084b405550
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-07 17:23:18 +0000

    "Easterling Beserker"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_110.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set20.fallenRealms;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+
+/**
+ * 4
+ * Easterling Beserker
+ * Fallen Realms	Minion • Man
+ * 2	2	4
+ * Easterling. Toil 1.
+ * This minion is strength +1 for each wound token you can spot.
+ */
+public class Card20_110 extends AbstractMinion {
+    public Card20_110() {
+        super(4, 2, 2, 4, Race.MAN, Culture.FALLEN_REALMS, "Easterling Beserker");
+        addKeyword(Keyword.EASTERLING);
+        addKeyword(Keyword.TOIL, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null,
+                new Evaluator() {
+                    @Override
+                    public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+                        int count = 0;
+                        for (PhysicalCard physicalCard : Filters.filterActive(gameState, modifiersQuerying, Filters.character, Filters.wounded)) {
+                            count+=gameState.getWounds(physicalCard);
+                        }
+                        return count;
+                    }
+                });
+    }
+}
