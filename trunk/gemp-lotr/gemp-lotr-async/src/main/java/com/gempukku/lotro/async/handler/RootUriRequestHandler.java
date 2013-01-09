8a06b0349644375ccff3c379638bc90bc89d7528
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-09 16:40:01 +0000

    Added ServerStatsRequestHandler.

--- trunk/gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/RootUriRequestHandler.java
+++ trunk/gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/RootUriRequestHandler.java
@@ -26,6 +26,7 @@ public class RootUriRequestHandler implements UriRequestHandler {
     private RegisterRequestHandler _registerRequestHandler;
     private ReplayRequestHandler _replayRequestHandler;
     private GameHistoryRequestHandler _gameHistoryRequestHandler;
+    private ServerStatsRequestHandler _serverStatsRequestHandler;
 
     public RootUriRequestHandler(Map<Type, Object> context) {
         _hallRequestHandler = new HallRequestHandler(context);
@@ -42,6 +43,7 @@ public class RootUriRequestHandler implements UriRequestHandler {
         _registerRequestHandler = new RegisterRequestHandler(context);
         _replayRequestHandler = new ReplayRequestHandler(context);
         _gameHistoryRequestHandler = new GameHistoryRequestHandler(context);
+        _serverStatsRequestHandler = new ServerStatsRequestHandler(context);
     }
 
     @Override
@@ -62,6 +64,8 @@ public class RootUriRequestHandler implements UriRequestHandler {
             _replayRequestHandler.handleRequest(uri.substring(_serverContextPath.length()+6), request, context, responseWriter, e);
         } else if (uri.startsWith(_serverContextPath+"gameHistory")) {
             _gameHistoryRequestHandler.handleRequest(uri.substring(_serverContextPath.length()+11), request, context, responseWriter, e);
+        } else if (uri.startsWith(_serverContextPath+"stats")) {
+            _serverStatsRequestHandler.handleRequest(uri.substring(_serverContextPath.length()+5), request, context, responseWriter, e);
         } else if (uri.startsWith(_serverContextPath+"admin")) {
             _adminRequestHandler.handleRequest(uri.substring(_serverContextPath.length()+5), request, context, responseWriter, e);
         } else if (uri.startsWith(_serverContextPath + "chat")) {
