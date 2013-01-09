a5efeac059d5b5ea05d5a8083f569d4d4b533866
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-09 16:36:36 +0000

    Added GameHistoryRequestHandler.

---
+++ gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/GameHistoryRequestHandler.java
@@ -0,0 +1,88 @@
+package com.gempukku.lotro.async.handler;
+
+import com.gempukku.lotro.async.ResponseWriter;
+import com.gempukku.lotro.db.vo.GameHistoryEntry;
+import com.gempukku.lotro.game.GameHistoryService;
+import com.gempukku.lotro.game.Player;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.QueryStringDecoder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+public class GameHistoryRequestHandler extends LotroServerRequestHandler implements UriRequestHandler {
+    private GameHistoryService _gameHistoryService;
+
+    public GameHistoryRequestHandler(Map<Type, Object> context) {
+        super(context);
+
+        _gameHistoryService = extractObject(context, GameHistoryService.class);
+    }
+
+    @Override
+    public void handleRequest(String uri, HttpRequest request, Map<Type, Object> context, ResponseWriter responseWriter, MessageEvent e) throws Exception {
+        if (uri.equals("") && request.getMethod() == HttpMethod.GET) {
+            QueryStringDecoder queryDecoder = new QueryStringDecoder(request.getUri());
+            String participantId = getQueryParameterSafely(queryDecoder, "participantId");
+            int start = Integer.parseInt(getQueryParameterSafely(queryDecoder, "start"));
+            int count = Integer.parseInt(getQueryParameterSafely(queryDecoder, "count"));
+
+            if (start < 0 || count < 1 || count > 100)
+                throw new WebApplicationException(Response.Status.BAD_REQUEST);
+
+            Player resourceOwner = getResourceOwnerSafely(request, participantId);
+
+            final List<GameHistoryEntry> playerGameHistory = _gameHistoryService.getGameHistoryForPlayer(resourceOwner, start, count);
+            int recordCount = _gameHistoryService.getGameHistoryForPlayerCount(resourceOwner);
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document doc = documentBuilder.newDocument();
+            Element gameHistory = doc.createElement("gameHistory");
+            gameHistory.setAttribute("count", String.valueOf(recordCount));
+            gameHistory.setAttribute("playerId", resourceOwner.getName());
+
+            for (GameHistoryEntry gameHistoryEntry : playerGameHistory) {
+                Element historyEntry = doc.createElement("historyEntry");
+                historyEntry.setAttribute("winner", gameHistoryEntry.getWinner());
+                historyEntry.setAttribute("loser", gameHistoryEntry.getLoser());
+
+                historyEntry.setAttribute("winReason", gameHistoryEntry.getWinReason());
+                historyEntry.setAttribute("loseReason", gameHistoryEntry.getLoseReason());
+
+                historyEntry.setAttribute("formatName", gameHistoryEntry.getFormatName());
+                String tournament = gameHistoryEntry.getTournament();
+                if (tournament != null)
+                    historyEntry.setAttribute("tournament", tournament);
+
+                if (gameHistoryEntry.getWinner().equals(resourceOwner.getName()) && gameHistoryEntry.getWinnerRecording() != null) {
+                    historyEntry.setAttribute("gameRecordingId", gameHistoryEntry.getWinnerRecording());
+                    historyEntry.setAttribute("deckName", gameHistoryEntry.getWinnerDeckName());
+                } else if (gameHistoryEntry.getLoser().equals(resourceOwner.getName()) && gameHistoryEntry.getLoserRecording() != null) {
+                    historyEntry.setAttribute("gameRecordingId", gameHistoryEntry.getLoserRecording());
+                    historyEntry.setAttribute("deckName", gameHistoryEntry.getLoserDeckName());
+                }
+
+                historyEntry.setAttribute("startTime", String.valueOf(gameHistoryEntry.getStartTime().getTime()));
+                historyEntry.setAttribute("endTime", String.valueOf(gameHistoryEntry.getEndTime().getTime()));
+
+                gameHistory.appendChild(historyEntry);
+            }
+
+            doc.appendChild(gameHistory);
+
+            responseWriter.writeResponse(doc);
+        } else {
+            responseWriter.writeError(404);
+        }
+    }
+}
