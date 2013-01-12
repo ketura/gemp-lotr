b470ed1c70c3721f86c8462695fcd67f0cb22b13
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-12 00:08:05 +0000

    More control over Threading model of the server.

--- trunk/gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/LotroAsyncServer.java
+++ trunk/gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/LotroAsyncServer.java
@@ -7,13 +7,18 @@ import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class LotroAsyncServer {
     public static void main(String[] server) {
         ChannelFactory factory =
                 new NioServerSocketChannelFactory(
                         Executors.newCachedThreadPool(),
-                        Executors.newFixedThreadPool(10));
+                        new ThreadPoolExecutor(10, Integer.MAX_VALUE,
+                                60L, TimeUnit.SECONDS,
+                                new SynchronousQueue<Runnable>()));
 
 
         ServerBootstrap bootstrap = new ServerBootstrap(factory);
