1ef011e66d2f20f12d02860e9843e4713980da66
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-12 13:29:53 +0000

    Moving ETags a layer higher (to make it faster and do no File operations).

--- gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/WebRequestHandler.java
+++ gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/WebRequestHandler.java
@@ -2,21 +2,31 @@ package com.gempukku.lotro.async.handler;
 
 import com.gempukku.lotro.async.ResponseWriter;
 import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.IF_NONE_MATCH;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 
 import java.io.File;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.Map;
 
 public class WebRequestHandler implements UriRequestHandler {
     private String _root;
+    private String _uniqueEtag;
 
     public WebRequestHandler(String root) {
         _root = root;
+        _uniqueEtag = "\"" + String.valueOf(System.currentTimeMillis()) + "\"";
     }
 
     @Override
     public void handleRequest(String uri, HttpRequest request, Map<Type, Object> context, ResponseWriter responseWriter, MessageEvent e) throws Exception {
+        if (clientHasCurrentVersion(request)) {
+            responseWriter.writeError(304);
+            return;
+        }
+
         if (uri.equals(""))
             uri = "index.html";
 
@@ -30,8 +40,21 @@ public class WebRequestHandler implements UriRequestHandler {
             File file = new File(_root + uri);
             if (!file.getCanonicalPath().startsWith(_root))
                 responseWriter.writeError(403);
-            else
-                responseWriter.writeFile(file);
+            else {
+                responseWriter.writeFile(file, Collections.singletonMap(HttpHeaders.Names.ETAG, _uniqueEtag));
+            }
         }
+    }
+
+    private boolean clientHasCurrentVersion(HttpRequest request) {
+        String ifNoneMatch = request.getHeader(IF_NONE_MATCH);
+        if (ifNoneMatch != null) {
+            String[] clientKnownVersions = ifNoneMatch.split(",");
+            for (String clientKnownVersion : clientKnownVersions) {
+                if (clientKnownVersion.trim().equals(_uniqueEtag))
+                    return true;
+            }
+        }
+        return false;
     }
 }
