6d9968f6a8f1bedd7e90a1a52b1efc6daeb1385f
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-14 22:39:23 +0000

    Adding missing weekly/singup rewards.

--- gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/HallRequestHandler.java
+++ gemp-lotr/gemp-lotr-async/src/main/java/com/gempukku/lotro/async/handler/HallRequestHandler.java
@@ -420,6 +420,7 @@ public class HallRequestHandler extends LotroServerRequestHandler implements Uri
         int channelNumber = Integer.parseInt(getFormParameterSafely(postDecoder, "channelNumber"));
 
         Player resourceOwner = getResourceOwnerSafely(request, participantId);
+        processLoginReward(request, resourceOwner.getName());
 
         HallUpdateLongPollingResource polledResource = new HallUpdateLongPollingResource(request, resourceOwner, channelNumber, responseWriter);
         if (polledResource.isChanged())
