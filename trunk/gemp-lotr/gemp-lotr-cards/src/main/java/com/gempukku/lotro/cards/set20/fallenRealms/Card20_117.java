b420947e6bd62eec7552ac850f568f1a94eb4a4f
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-24 13:41:40 +0000

    "Easterling Recruit"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_117.java
@@ -0,0 +1,33 @@
+package com.gempukku.lotro.cards.set20.fallenRealms;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * 3
+ * Easterling Recruit
+ * Fallen Realms	Minion • Man
+ * 6	2	4
+ * Easterling. Toil 1.
+ * This minion is strength +1 for each wounded minion.
+ */
+public class Card20_117 extends AbstractMinion {
+    public Card20_117() {
+        super(3, 6, 2, 4, Race.MAN, Culture.FALLEN_REALMS, "Easterling Recruit");
+        addKeyword(Keyword.EASTERLING);
+        addKeyword(Keyword.TOIL, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null, new CountActiveEvaluator(CardType.MINION, Filters.wounded));
+    }
+}
