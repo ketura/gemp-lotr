f54ffc8db91ecf4de8b1613692bb906efe388a3a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-24 15:17:44 +0000

    "Barliman Butterbur", "Jovial Innkeep"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gandalf/Card20_152.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set20.gandalf;
+
+import com.gempukku.lotro.cards.AbstractAlly;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 0
+ * •Barliman Butterbur, Jovial Innkeep
+ * Gandalf	Ally • Man • Bree
+ * 1	2
+ * To play, spot Gandalf.
+ * Fellowship or Regroup: If the fellowship is in region 1 or region 2, exert Barliman Butterbur to heal a companion.
+ */
+public class Card20_152 extends AbstractAlly {
+    public Card20_152() {
+        super(0, null, 0, 1, 2, Race.MAN, Culture.GANDALF, "Barliman Butterbur", "Jovial Innkeep", true);
+        addKeyword(Keyword.BREE);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Filters.gandalf);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if ((PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)
+                || PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self))
+            && PlayConditions.location(game, Filters.or(Filters.region(1), Filters.region(2)))
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
