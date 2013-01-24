3b8ac72f10f9d38e623f9e3a4ac1d4b3cbe18250
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-24 14:26:37 +0000

    "Red Rage"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_133.java
@@ -0,0 +1,34 @@
+package com.gempukku.lotro.cards.set20.fallenRealms;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.ForEachTwilightEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.LimitEvaluator;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * 2
+ * Red Rage
+ * Fallen Realms	Event • Skirmish
+ * Make a Southron strength +1 for each twilight token (limit +6).
+ */
+public class Card20_133 extends AbstractEvent {
+    public Card20_133() {
+        super(Side.SHADOW, 2, Culture.FALLEN_REALMS, "Red Rage", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId,
+                        new LimitEvaluator(new ForEachTwilightEvaluator(), 6), Keyword.SOUTHRON));
+        return action;
+    }
+}
