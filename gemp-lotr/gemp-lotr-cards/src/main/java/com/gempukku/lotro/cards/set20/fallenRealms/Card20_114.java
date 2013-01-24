bf774f20be925c5bba10ee3566c3e028ebf6df34
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-24 13:35:57 +0000

    "Easterling Detachment"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_114.java
@@ -0,0 +1,22 @@
+package com.gempukku.lotro.cards.set20.fallenRealms;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+
+/**
+ * 6
+ * Easterling Detachment
+ * Fallen Realms	Minion • Man
+ * 13	3	4
+ * Easterling. Fierce. Enduring.
+ */
+public class Card20_114 extends AbstractMinion {
+    public Card20_114() {
+        super(6, 13, 3, 4, Race.MAN, Culture.FALLEN_REALMS, "Easterling Detachment");
+        addKeyword(Keyword.EASTERLING);
+        addKeyword(Keyword.FIERCE);
+        addKeyword(Keyword.ENDURING);
+    }
+}
