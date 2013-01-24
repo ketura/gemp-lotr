fb32fe76c507627a8d7bbf7ba8dd1e2008468670
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-24 15:36:42 +0000

    "Gandalf", "Wisest of the Istari"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gandalf/Card20_157.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set20.gandalf;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 4
+ * •Gandalf, Wisest of the Istari
+ * Gandalf	Companion • Wizard
+ * 7	4	7
+ * Each time you play a [Gandalf] spell, you may exert Gandalf to draw a card.
+ */
+public class Card20_157 extends AbstractCompanion {
+    public Card20_157() {
+        super(4, 7, 4, 7, Culture.GANDALF, Race.WIZARD, null, "Gandalf", "Wisest of the Istari", true);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, Culture.GANDALF, Keyword.SPELL)
+                && PlayConditions.canSelfExert(self, game)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new DrawCardsEffect(action, playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
