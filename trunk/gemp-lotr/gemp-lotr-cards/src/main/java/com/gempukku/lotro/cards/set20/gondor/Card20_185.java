473e9fc8d669dd16b39bf08a0506f060b5b49cae
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-25 14:09:50 +0000

    "Boromir", "Son of Denethor"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gondor/Card20_185.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set20.gondor;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 3
+ * •Boromir, Son of Denethor
+ * Gondor	Companion • Man
+ * 7	3	5
+ * Skirmish: Exert Boromir to make a Hobbit strength +3.
+ */
+public class Card20_185 extends AbstractCompanion {
+    public Card20_185() {
+        super(3, 7, 3, 5, Culture.GONDOR, Race.MAN, null, "Boromir", "Son of Denethor", true);
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+            && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 3, Race.HOBBIT));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
