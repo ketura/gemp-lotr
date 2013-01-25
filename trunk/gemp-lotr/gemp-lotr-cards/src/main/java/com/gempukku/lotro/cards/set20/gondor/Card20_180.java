0d8d2e56267bb8d788fb672e2a9af6a20da45e88
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-25 13:49:04 +0000

    "Arrows of the North"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gondor/Card20_180.java
@@ -0,0 +1,38 @@
+package com.gempukku.lotro.cards.set20.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+
+/**
+ * 3
+ * Arrows of the North
+ * Gondor	Event • Archery
+ * Exert 2 [Gondor] rangers to wound each roaming minion.
+ */
+public class Card20_180 extends AbstractEvent {
+    public Card20_180() {
+        super(Side.FREE_PEOPLE, 3, Culture.GONDOR, "Arrows of the North", Phase.ARCHERY);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canExert(self, game, 1, 2, Culture.GONDOR, Keyword.RANGER);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 2, 2, 1, Culture.GONDOR, Keyword.RANGER));
+        action.appendEffect(
+                new WoundCharactersEffect(self, CardType.MINION, Keyword.ROAMING));
+        return action;
+    }
+}
