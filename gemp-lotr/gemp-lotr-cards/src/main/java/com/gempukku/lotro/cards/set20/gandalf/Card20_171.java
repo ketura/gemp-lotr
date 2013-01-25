72a2397db8e130ae9883f545f19b42978d7baddc
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-25 11:23:04 +0000

    "A Wizard is Never Late"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gandalf/Card20_171.java
@@ -0,0 +1,31 @@
+package com.gempukku.lotro.cards.set20.gandalf;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * 1
+ * A Wizard is Never Late
+ * Gandalf	Event • Fellowship
+ * Play a [Gandalf] character from your draw deck.
+ */
+public class Card20_171 extends AbstractEvent {
+    public Card20_171() {
+        super(Side.FREE_PEOPLE, 1, Culture.GANDALF, "A Wizard is Never Late", Phase.FELLOWSHIP);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndPlayCardFromDeckEffect(playerId, Culture.GANDALF, Filters.character));
+        return action;
+    }
+}
