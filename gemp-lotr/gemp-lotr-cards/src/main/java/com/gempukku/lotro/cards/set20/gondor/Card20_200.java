04900395e261709eb44c9702bf908369ac14f2cc
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-29 16:42:42 +0000

    "Native Cover"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gondor/Card20_200.java
@@ -0,0 +1,35 @@
+package com.gempukku.lotro.cards.set20.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.modifiers.ArcheryTotalModifier;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * 1
+ * •Native Cover
+ * Gondor	Condition • Support Area
+ * To play, spot a ranger. While the fellowship is at a site from your adventure deck, the minion archery total is -2.
+ */
+public class Card20_200 extends AbstractPermanent {
+    public Card20_200() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.GONDOR, Zone.SUPPORT, "Native Cover", null, true);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, Keyword.RANGER);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new ArcheryTotalModifier(self, Side.SHADOW,
+                new LocationCondition(Filters.owner(self.getOwner())), -2);
+    }
+}
