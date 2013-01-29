712bb0faa4db9f2faf546559d7c46b53fc11ee07
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-29 17:57:20 +0000

    "Orthanc Retainer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_212.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set20.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.CancelActivatedEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.ActivateCardResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 2
+ * Orthanc Retainer
+ * Isengard	Minion • Orc
+ * 5	3	4
+ * Lurker.
+ * Response: If a special ability is used in a skirmish involving an Uruk-hai, exert this minion to prevent that special ability.
+ */
+public class Card20_212 extends AbstractMinion {
+    public Card20_212() {
+        super(2, 5, 3, 4, Race.ORC, Culture.ISENGARD, "Orthanc Retainer");
+        addKeyword(Keyword.LURKER);
+    }
+
+    @Override
+    public List<? extends ActivateCardAction> getOptionalInPlayAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.activated(game, effectResult, Filters.any)
+                && PlayConditions.isActive(game, Race.URUK_HAI, Filters.inSkirmish)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new CancelActivatedEffect(self, (ActivateCardResult) effectResult));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
