7f56e790b118698ac0e160f2f301f6e6876d77b9
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-29 16:39:17 +0000

    "Legacy of Numenor"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gondor/Card20_198.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set20.gondor;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.CharacterWonSkirmishResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1
+ * •Legacy of Numenor
+ * Gondor	Condition • Support Area
+ * Each time a [Gondor] ranger wins a skirmish involving a roaming minion, heal that ranger.
+ * Discard this condition if a [Gondor] ranger loses a skirmish.
+ */
+public class Card20_198 extends AbstractPermanent {
+    public Card20_198() {
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.GONDOR, Zone.SUPPORT, "Legacy of Numenor", null, true);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.winsSkirmish(game, effectResult, Culture.GONDOR, Keyword.RANGER)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new HealCharactersEffect(self, ((CharacterWonSkirmishResult) effectResult).getWinner()));
+            return Collections.singletonList(action);
+        }
+        if (TriggerConditions.losesSkirmish(game, effectResult, Culture.GONDOR, Keyword.RANGER)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new SelfDiscardEffect(self));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
