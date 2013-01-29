0fd6ed5bca6701ebc4aa129ef3c8198077966a1f
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-29 16:54:03 +0000

    "Rangers of the North"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/gondor/Card20_205.java
@@ -0,0 +1,33 @@
+package com.gempukku.lotro.cards.set20.gondor;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+
+/**
+ * 0
+ * Rangers of the North
+ * Gondor	Event • Skirmish
+ * Make a [Gondor] ranger strength +1 for each site from your adventure deck in the current region.
+ */
+public class Card20_205 extends AbstractEvent {
+    public Card20_205() {
+        super(Side.FREE_PEOPLE, 0, Culture.GONDOR, "Rangers of the North", Phase.SKIRMISH);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendEffect(
+                new ChooseAndAddUntilEOPStrengthBonusEffect(
+                        action, self, playerId,
+                        new CountActiveEvaluator(CardType.SITE, Filters.owner(playerId), Filters.region(GameUtils.getRegion(game.getGameState()))), Culture.GONDOR, Keyword.RANGER));
+        return action;
+    }
+}
