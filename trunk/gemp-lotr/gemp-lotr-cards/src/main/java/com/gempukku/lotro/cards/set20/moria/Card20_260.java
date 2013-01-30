c4667f91f99d3b9b5d7d01738de3c3adddddacf3
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 16:40:21 +0000

    "Goblin Grudgebearer"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_260.java
@@ -0,0 +1,27 @@
+package com.gempukku.lotro.cards.set20.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountSpottableEvaluator;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * 3
+ * Goblin Grudgebearer
+ * Moria	Minion • Goblin
+ * 8	1	4
+ * This minion is strength +1 for each Dwarf you can spot.
+ */
+public class Card20_260 extends AbstractMinion {
+    public Card20_260() {
+        super(3, 8, 1, 4, Race.GOBLIN, Culture.MORIA, "Goblin Grudgebearer");
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, null, new CountSpottableEvaluator(Race.DWARF));
+    }
+}
