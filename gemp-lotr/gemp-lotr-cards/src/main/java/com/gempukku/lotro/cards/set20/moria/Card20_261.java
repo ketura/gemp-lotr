51744a643c1c3b14c03b07c49691152a3de598e2
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 16:44:05 +0000

    "Goblin Howler"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_261.java
@@ -0,0 +1,43 @@
+package com.gempukku.lotro.cards.set20.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.PlayCardResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 2
+ * Goblin Howler
+ * Moria	Minion • Goblin
+ * 5	2	4
+ * Each time you play a [Moria] minion from a [Moria] condition, you may make this minion strength +1 until the regroup phase.
+ */
+public class Card20_261 extends AbstractMinion{
+    public Card20_261() {
+        super(2, 5, 2, 4, Race.GOBLIN, Culture.MORIA, "Goblin Howler");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.playedFromZone(game, effectResult, Zone.STACKED, Culture.MORIA, CardType.MINION)
+                && Filters.and(Culture.MORIA, CardType.CONDITION).accepts(game.getGameState(), game.getModifiersQuerying(), ((PlayCardResult) effectResult).getAttachedOrStackedPlayedFrom())) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            PhysicalCard playedCard = ((PlayCardResult) effectResult).getPlayedCard();
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new StrengthModifier(self, playedCard, 1), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
