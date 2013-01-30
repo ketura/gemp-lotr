94ddb4ec799503340c82a00653726e718f4ab821
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 10:52:33 +0000

    "Vile Blood"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_216.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set20.isengard;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.modifiers.VitalityModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1
+ * •Vile Blood
+ * Isengard	Condition • Minion
+ * +1
+ * Bearer must be an [Isengard] orc. While you have initiative, you may play this card from your discard pile.
+ */
+public class Card20_216 extends AbstractAttachable {
+    public Card20_216() {
+        super(Side.SHADOW, CardType.CONDITION, 1, Culture.ISENGARD, null, "Vile Blood", null, true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.ISENGARD, Race.ORC);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new VitalityModifier(self, Filters.hasAttached(self), 1);
+    }
+
+
+    @Override
+    public List<? extends Action> getPhaseActionsFromDiscard(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.isPhase(game, Phase.SHADOW)
+                && PlayConditions.hasInitiative(game, Side.SHADOW)
+                && PlayConditions.canPlayFromDiscard(playerId, game, self)) {
+            return Collections.singletonList(getPlayCardAction(playerId, game, self, 0, false));
+        }
+        return null;
+    }
+}
