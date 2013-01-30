d99f2195dbe477b4a342d455298337ea65dfb3a6
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 14:29:04 +0000

    "Treachery Runs Deep"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_244.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_244.java
@@ -3,38 +3,41 @@ package com.gempukku.lotro.cards.set20.isengard;
 import com.gempukku.lotro.cards.AbstractResponseEvent;
 import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Side;
-import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.results.WoundResult;
 
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 2
- * Wizard’s Wrath
+ * Treachery Runs Deep
  * Isengard	Event • Response
  * Spell.
- * If Saruman wins a skirmish, wound an unbound companion.
+ * If a companion takes a wound, spot Saruman to wound that companion again.
  */
-public class Card20_243 extends AbstractResponseEvent {
-    public Card20_243() {
-        super(Side.SHADOW, 2, Culture.ISENGARD, "Wizard's Wrath");
+public class Card20_244 extends AbstractResponseEvent {
+    public Card20_244() {
+        super(Side.SHADOW, 2, Culture.ISENGARD, "Treachery Runs Deep");
         addKeyword(Keyword.SPELL);
     }
 
     @Override
     public List<PlayEventAction> getOptionalAfterActions(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.saruman)
-                && checkPlayRequirements(playerId, game, self, 0, 0, false, false)) {
+        if (TriggerConditions.forEachWounded(game, effectResult, CardType.COMPANION)) {
             PlayEventAction action = new PlayEventAction(self);
+            PhysicalCard woundedCompanion = ((WoundResult) effectResult).getWoundedCard();
+            action.setText("Wound "+ GameUtils.getCardLink(woundedCompanion));
             action.appendEffect(
-                    new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, Filters.unboundCompanion));
+                    new WoundCharactersEffect(self, woundedCompanion));
             return Collections.singletonList(action);
         }
         return null;
