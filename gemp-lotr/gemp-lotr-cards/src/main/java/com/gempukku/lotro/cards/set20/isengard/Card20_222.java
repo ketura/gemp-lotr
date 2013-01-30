eeb4697d5910c768b627a91b034b2652cdd948f6
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 11:11:24 +0000

    "Uruk Brute"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_222.java
@@ -0,0 +1,30 @@
+package com.gempukku.lotro.cards.set20.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * 4
+ * Uruk Brute
+ * Isengard	Minion • Uruk-hai
+ * 8	2	5
+ * Damage +1.
+ * While at a battleground, this minion is strength +2.
+ */
+public class Card20_222 extends AbstractMinion {
+    public Card20_222() {
+        super(4, 8, 2, 5, Race.URUK_HAI, Culture.ISENGARD, "Uruk Brute");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new LocationCondition(Keyword.BATTLEGROUND), 2);
+    }
+}
