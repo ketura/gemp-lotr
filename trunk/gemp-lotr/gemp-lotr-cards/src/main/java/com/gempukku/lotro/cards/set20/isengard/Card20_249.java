314f962039b90558b7a2756d0690b42c790506c6
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 14:45:27 +0000

    "One Ill Turn Deserves Another"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_249.java
@@ -0,0 +1,49 @@
+package com.gempukku.lotro.cards.set20.isengard;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.RemoveSpecialAbilitiesModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+
+/**
+ * 1
+ * One Ill Turn Deserves Another
+ * Isengard	Event • Maneuver
+ * Spell.
+ * Exert Saruman to exert a companion. That companion cannot use special abilities until the regroup phase.
+ */
+public class Card20_249 extends AbstractEvent {
+    public Card20_249() {
+        super(Side.SHADOW, 1, Culture.ISENGARD, "One Ill Turn Deserves Another", Phase.MANEUVER);
+        addKeyword(Keyword.SPELL);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canExert(self, game, Filters.saruman);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.saruman));
+        action.appendEffect(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.COMPANION) {
+                    @Override
+                    protected void forEachCardExertedCallback(PhysicalCard character) {
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new RemoveSpecialAbilitiesModifier(self, character), Phase.REGROUP));
+                    }
+                });
+        return action;
+    }
+}
