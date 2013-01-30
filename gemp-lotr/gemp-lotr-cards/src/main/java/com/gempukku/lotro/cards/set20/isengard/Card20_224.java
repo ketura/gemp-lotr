7ced46133ec38700c728fd30a8ce6355d406d8bc
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-01-30 11:13:43 +0000

    "Relentless Uruk"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_224.java
@@ -0,0 +1,30 @@
+package com.gempukku.lotro.cards.set20.isengard;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.conditions.LocationCondition;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * 4
+ * Relentless Uruk
+ * Isengard	Minion • Uruk-hai
+ * 8	2	5
+ * Damage +1.
+ * While at a battleground, this minion is fierce.
+ */
+public class Card20_224 extends AbstractMinion {
+    public Card20_224() {
+        super(4, 8, 2, 5, Race.URUK_HAI, Culture.ISENGARD, "Relentless Uruk");
+        addKeyword(Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new KeywordModifier(self, self, new LocationCondition(Keyword.BATTLEGROUND), Keyword.FIERCE, 1);
+    }
+}
