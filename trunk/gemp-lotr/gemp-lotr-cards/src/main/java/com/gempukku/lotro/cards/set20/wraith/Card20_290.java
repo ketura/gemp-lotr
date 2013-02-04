3b87c8b2fd4b08a95710de1a6a47d0d176901579
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-04 18:04:45 +0000

    "Faltering Courage"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_290.java
@@ -0,0 +1,67 @@
+package com.gempukku.lotro.cards.set20.wraith;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.cards.modifiers.evaluator.ForEachBurdenEvaluator;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1
+ * Faltering Courage
+ * Ringwraith	Condition • Companion
+ * -1
+ * Twilight.
+ * To play, spot a Nazgul.
+ * Limit 1 per companion.
+ * Skirmish: Discard this condition to make a Nazgul skirmishing bearer strength +1 for each burden you can spot.
+ */
+public class Card20_290 extends AbstractAttachable {
+    public Card20_290() {
+        super(Side.SHADOW, CardType.CONDITION, 1, Culture.WRAITH, null, "Faltering Courage");
+        addKeyword(Keyword.TWILIGHT);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(CardType.COMPANION, Filters.not(Filters.hasAttached(Filters.name(getName()))));
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, Filter additionalAttachmentFilter, int twilightModifier) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, additionalAttachmentFilter, twilightModifier)
+                && PlayConditions.canSpot(game, Race.NAZGUL);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new ResistanceModifier(self, Filters.hasAttached(self), -1);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSelfDiscard(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfDiscardEffect(self));
+            action.appendEffect(
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId,
+                            new ForEachBurdenEvaluator(), Race.NAZGUL, Filters.inSkirmishAgainst(self.getAttachedTo())));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
