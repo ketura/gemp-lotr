3a859f68aaa9b7b149397f2b864ced6b612b4fad
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-04 12:10:48 +0000

    "Goblin Patroller"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_263.java
@@ -0,0 +1,29 @@
+package com.gempukku.lotro.cards.set20.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountStackedEvaluator;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+/**
+ * 5
+ * Goblin Patroller
+ * Moria	Minion • Goblin
+ * 11	3	4
+ * This minion's twilight cost is -1 for each card stacked on a [Moria] condition.
+ */
+public class Card20_263 extends AbstractMinion {
+    public Card20_263() {
+        super(5, 11, 3, 4, Race.GOBLIN, Culture.MORIA, "Goblin Patroller");
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        return -(new CountStackedEvaluator(Filters.and(Culture.MORIA, CardType.CONDITION), Filters.any).evaluateExpression(gameState, modifiersQuerying, self));
+    }
+}
