3722f9e5a58963cfce14d3e4ef3a14abfe827e6a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-04 12:51:20 +0000

    "Goblin Runner"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_268.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set20.moria;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.effects.AddTwilightEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1
+ * Goblin Runner
+ * Moria	Minion • Goblin
+ * 5	1	4
+ * When you play this minion, you may spot another [Moria] Goblin to add (2).
+ */
+public class Card20_268 extends AbstractMinion {
+    public Card20_268() {
+        super(1, 5, 1, 4, Race.GOBLIN, Culture.MORIA, "Goblin Runner");
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)
+                && PlayConditions.canSpot(game, Filters.not(self), Culture.MORIA, Race.GOBLIN)) {
+            OptionalTriggerAction action =new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new AddTwilightEffect(self, 2));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
