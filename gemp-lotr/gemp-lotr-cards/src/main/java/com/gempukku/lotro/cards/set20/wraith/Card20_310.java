0f109eb35c86d58c4cc2a9d9ae64bc8353999ada
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-05 16:26:02 +0000

    Names.otsea, "Twilight Menace"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_310.java
@@ -0,0 +1,27 @@
+package com.gempukku.lotro.cards.set20.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * 4
+ * •Ulaire Otsea, Twilight Menace
+ * Ringwraith	Minion • Nazgul
+ * 9	3	3
+ * Twilight. Each companion bearing a [Ringwraith] condition is resistance -2.
+ */
+public class Card20_310 extends AbstractMinion {
+    public Card20_310() {
+        super(4, 9, 3, 3, Race.NAZGUL, Culture.WRAITH, Names.otsea, "Twilight Menace", true);
+        addKeyword(Keyword.TWILIGHT);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new ResistanceModifier(self, Filters.and(CardType.COMPANION, Filters.hasAttached(Culture.WRAITH, CardType.CONDITION)), -2);
+    }
+}
