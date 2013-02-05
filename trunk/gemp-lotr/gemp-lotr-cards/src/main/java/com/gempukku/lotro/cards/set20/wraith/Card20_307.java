201d9ac0f345dc9e3ebe6ac09300294f45dcfa7a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-05 16:08:30 +0000

    Names.lemenya, "Twilight Spectre"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_307.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set20.wraith;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.RemoveBurdenEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 4
+ * •Ulaire Lemenya, Twilight Spectre
+ * Ringwraith	Minion • Nazgul
+ * 9	3	3
+ * Twilight. Regroup: Remove a burden to discard a Free Peoples condition
+ */
+public class Card20_307 extends AbstractMinion {
+    public Card20_307() {
+        super(4, 9, 3, 3, Race.NAZGUL, Culture.WRAITH, Names.lemenya, "Twilight Spectre", true);
+        addKeyword(Keyword.TWILIGHT);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.REGROUP, self, 0)
+                && PlayConditions.canRemoveBurdens(game, self, 1)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveBurdenEffect(playerId, self, 1));
+            action.appendEffect(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Side.FREE_PEOPLE, CardType.CONDITION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
