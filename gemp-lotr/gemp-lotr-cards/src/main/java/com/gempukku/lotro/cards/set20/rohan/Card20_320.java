da517f1414f04f27354afea41f02c32c369d6e9a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-05 17:51:37 +0000

    Names.eowyn, "Noblewoman of Rohan"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_320.java
@@ -0,0 +1,48 @@
+package com.gempukku.lotro.cards.set20.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 2
+ * •Eowyn, Noblewoman of Rohan
+ * Rohan	Companion • Man
+ * 6	3	7
+ * While in your starting fellowship, Eowyn's twilight cost is -1.
+ * Skirmish: Discard a [Rohan] fortification to heal a companion.
+ */
+public class Card20_320 extends AbstractCompanion {
+    public Card20_320() {
+        super(2, 6, 3, 7, Culture.ROHAN, Race.MAN, null, Names.eowyn, "Noblewoman of Rohan", true);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        return (gameState.getCurrentPhase() == Phase.PLAY_STARTING_FELLOWSHIP)?-1:0;
+    }
+
+    @Override
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.canDiscardFromPlay(self, game, Culture.ROHAN, Keyword.FORTIFICATION)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, Culture.ROHAN, Keyword.FORTIFICATION));
+            action.appendEffect(
+                    new ChooseAndHealCharactersEffect(action, playerId, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
