edf3f1f78e905a1052818ec58ceee63fc4c345f5
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-06 18:04:00 +0000

    Names.theoden, "Renewed in Spirit"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_345.java
@@ -0,0 +1,29 @@
+package com.gempukku.lotro.cards.set20.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Names;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * 3
+ * •Theoden, Renewed in Spirit
+ * Rohan	Companion • Man
+ * 7	3	5
+ * While you can spot Gandalf, Theoden is strength +2.
+ */
+public class Card20_345 extends AbstractCompanion {
+    public Card20_345() {
+        super(3, 7, 3, 5, Culture.ROHAN, Race.MAN, null, Names.theoden, "Renewed in Spirit", true);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new StrengthModifier(self, self, new SpotCondition(Filters.gandalf), 2);
+    }
+}
