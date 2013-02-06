8848c1792fde33a2286872baeb5bf93984af17fa
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-06 17:56:25 +0000

    "Rohirrim Villager"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_342.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_342.java
@@ -2,13 +2,13 @@ package com.gempukku.lotro.cards.set20.rohan;
 
 import com.gempukku.lotro.cards.AbstractAlly;
 import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
 import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
 import com.gempukku.lotro.logic.timing.Action;
 
 import java.util.Collections;
@@ -16,29 +16,29 @@ import java.util.List;
 
 /**
  * 1
- * Rohirrim Peasant
+ * Rohirrim Villager
  * Rohan	Ally • Man • Edoras
  * 4	2
  * Villager.
- * Skirmish: while you have initiaitve, you may discard a card from hand to make a [rohan] man strength + 1
+ * Fellowship: If you have initiative, discard 2 cards from hand to heal a [Rohan] Man.
  */
-public class Card20_339 extends AbstractAlly {
-    public Card20_339() {
-        super(1, null, 0, 4, 2, Race.MAN, Culture.ROHAN, "Rohirrim Peasant");
+public class Card20_342 extends AbstractAlly {
+    public Card20_342() {
+        super(1, null, 0, 4, 2, Race.MAN, Culture.ROHAN, "Rohirrim Villager");
         addKeyword(Keyword.VILLAGER);
         addKeyword(Keyword.EDORAS);
     }
 
     @Override
     protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)
                 && PlayConditions.hasInitiative(game, Side.FREE_PEOPLE)
-                && PlayConditions.canDiscardFromHand(game, playerId, 1, Filters.any)) {
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)) {
             ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
-                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, Filters.any));
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2, Filters.any));
             action.appendEffect(
-                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 1, Culture.ROHAN, Race.MAN));
+                    new ChooseAndHealCharactersEffect(action, playerId, Culture.ROHAN, Race.MAN));
             return Collections.singletonList(action);
         }
         return null;
