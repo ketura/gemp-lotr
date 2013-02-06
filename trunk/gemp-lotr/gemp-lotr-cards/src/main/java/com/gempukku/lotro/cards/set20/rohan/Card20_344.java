f2acbffd4e5d0ab4db5434c69513b11a996a5065
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-06 18:02:38 +0000

    "Keeper of the Golden Hall"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_344.java
@@ -0,0 +1,33 @@
+package com.gempukku.lotro.cards.set20.rohan;
+
+import com.gempukku.lotro.cards.AbstractCompanion;
+import com.gempukku.lotro.cards.modifiers.OpponentsCantUseSpecialAbilitiesModifier;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Names;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.modifiers.GameHasCondition;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * 3
+ * •Theoden, Keeper of the Golden Hall
+ * Rohan	Companion • Man
+ * 7	3	5
+ * Valiant.
+ * While Theoden is mounted, an opponent may not use skirmish special abilities during skirmishes involving Theoden.
+ */
+public class Card20_344 extends AbstractCompanion {
+    public Card20_344() {
+        super(3, 7, 3, 5, Culture.ROHAN, Race.MAN, null, Names.theoden, "Keeper of the Golden Hall", true);
+        addKeyword(Keyword.VALIANT);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(PhysicalCard self) {
+        return new OpponentsCantUseSpecialAbilitiesModifier(self,
+                new GameHasCondition(self, Filters.mounted, Filters.inSkirmish), self.getOwner());
+    }
+}
