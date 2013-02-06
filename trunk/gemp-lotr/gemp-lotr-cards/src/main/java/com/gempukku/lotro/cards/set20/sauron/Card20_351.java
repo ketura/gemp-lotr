7d1f172f11dfd3bb4ffd720392398b64b9d62004
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-06 18:29:39 +0000

    "Blade of Gorgoroth"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_351.java
@@ -0,0 +1,45 @@
+package com.gempukku.lotro.cards.set20.sauron;
+
+import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.modifiers.conditions.NotCondition;
+import com.gempukku.lotro.cards.modifiers.conditions.SpotCulturesCondition;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.KeywordModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 1
+ * Blade of Gorgoroth
+ * Sauron	Possession • Hand weapon
+ * 2
+ * Bearer must be a [Sauron] Orc.
+ * While you cannot spot 3 Free Peoples cultures, bearer is damage +1.
+ */
+public class Card20_351 extends AbstractAttachable {
+    public Card20_351() {
+        super(Side.SHADOW, CardType.POSSESSION, 1, Culture.SAURON, PossessionClass.HAND_WEAPON, "Blade of Gorgoroth");
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.and(Culture.SAURON, Race.ORC);
+    }
+
+    @Override
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new StrengthModifier(self, Filters.hasAttached(self), 2));
+        modifiers.add(
+                new KeywordModifier(self, Filters.hasAttached(self),
+                        new NotCondition(new SpotCulturesCondition(3, Side.FREE_PEOPLE)), Keyword.DAMAGE, 1));
+        return modifiers;
+    }
+}
