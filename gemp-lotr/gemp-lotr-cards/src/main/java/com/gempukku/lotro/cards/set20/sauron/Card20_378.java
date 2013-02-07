e9986ff538353bf1ecf831333faec2685db981af
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-07 17:44:05 +0000

    "Troll of Morannon"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_378.java
@@ -0,0 +1,54 @@
+package com.gempukku.lotro.cards.set20.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddBurdenEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 5
+ * Troll of Morannon
+ * Sauron	Minion • Troll
+ * 14	4	6
+ * Damage +1. Fierce.
+ * This minion's twilight cost is +1 for each Free Peoples culture you can spot.
+ * When you play this minion in region 3, add a burden.
+ */
+public class Card20_378 extends AbstractMinion {
+    public Card20_378() {
+        super(5, 14, 4, 6, Race.TROLL, Culture.SAURON, "Troll of Morannon");
+        addKeyword(Keyword.DAMAGE, 1);
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
+        return GameUtils.getSpottableFPCulturesCount(gameState, modifiersQuerying, self.getOwner());
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)
+            && PlayConditions.location(game, Filters.region(3))) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddBurdenEffect(self.getOwner(), self, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
