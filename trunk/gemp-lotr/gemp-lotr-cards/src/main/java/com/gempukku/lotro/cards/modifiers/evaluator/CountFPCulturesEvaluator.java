7f9901ba6229de4d29669824e93be96ffb9c7104
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-07 13:31:45 +0000

    "Orc Ravener"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/modifiers/evaluator/CountFPCulturesEvaluator.java
@@ -0,0 +1,20 @@
+package com.gempukku.lotro.cards.modifiers.evaluator;
+
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+
+public class CountFPCulturesEvaluator implements Evaluator {
+    private String _playerId;
+
+    public CountFPCulturesEvaluator(String playerId) {
+        _playerId = playerId;
+    }
+
+    @Override
+    public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+        return GameUtils.getSpottableFPCulturesCount(gameState, modifiersQuerying, _playerId);
+    }
+}
