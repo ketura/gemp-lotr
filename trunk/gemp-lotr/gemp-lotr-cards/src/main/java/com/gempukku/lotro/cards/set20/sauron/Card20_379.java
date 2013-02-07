7ac731ff9f87053a819166cd73d694a0cbec2518
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-07 17:46:01 +0000

    "Vicious Olog-hai"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_379.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.set20.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 8
+ * Vicious Olog-hai
+ * Sauron	Minion • Troll
+ * 15	4	6
+ * Fierce.
+ * Regroup: Exert this minion to add a threat.
+ */
+public class Card20_379 extends AbstractMinion {
+    public Card20_379() {
+        super(8, 15, 4, 6, Race.TROLL, Culture.SAURON, "Vicious Olog-hai");
+        addKeyword(Keyword.FIERCE);
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.REGROUP, self, 0)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new AddThreatsEffect(playerId, self, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
