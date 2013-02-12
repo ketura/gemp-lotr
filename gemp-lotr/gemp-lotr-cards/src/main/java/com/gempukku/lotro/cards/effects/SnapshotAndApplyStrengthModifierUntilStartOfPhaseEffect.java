1075420e45f55dbd476150bb39aee8408ba4287a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-12 10:23:55 +0000

    - "Eregion's Trails" now makes a snapshot of affected cards upon being played and does not apply to any other cards.

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/effects/SnapshotAndApplyStrengthModifierUntilStartOfPhaseEffect.java
@@ -0,0 +1,51 @@
+package com.gempukku.lotro.cards.effects;
+
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+import com.gempukku.lotro.logic.timing.AbstractEffect;
+
+public class SnapshotAndApplyStrengthModifierUntilStartOfPhaseEffect extends AbstractEffect {
+    private PhysicalCard _source;
+    private Filterable[] _filters;
+    private Evaluator _evaluator;
+    private Phase _phase;
+
+    public SnapshotAndApplyStrengthModifierUntilStartOfPhaseEffect(PhysicalCard source, Evaluator evaluator, Phase phase, Filterable... filter) {
+        _source = source;
+        _evaluator = evaluator;
+        _phase = phase;
+        _filters = filter;
+    }
+
+    @Override
+    public String getText(LotroGame game) {
+        return "Apply strength modifier";
+    }
+
+    @Override
+    protected FullEffectResult playEffectReturningResult(LotroGame game) {
+        for (PhysicalCard physicalCard : Filters.filterActive(game.getGameState(), game.getModifiersQuerying(), _filters)) {
+            final int modifier = _evaluator.evaluateExpression(game.getGameState(), game.getModifiersQuerying(), physicalCard);
+            if (modifier != 0)
+                game.getModifiersEnvironment().addUntilStartOfPhaseModifier(
+                        new StrengthModifier(_source, physicalCard, modifier), _phase);
+        }
+
+        return new FullEffectResult(true);
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+
+    @Override
+    public boolean isPlayableInFull(LotroGame game) {
+        return true;
+    }
+}
