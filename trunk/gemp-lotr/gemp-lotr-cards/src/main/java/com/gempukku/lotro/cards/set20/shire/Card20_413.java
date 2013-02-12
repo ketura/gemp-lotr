8a2ddde1e2c20c7d584dfba31cc34dbe9b5675aa
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-12 17:36:39 +0000

    "Sting", "Baggins Birthright"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/shire/Card20_413.java
@@ -0,0 +1,41 @@
+package com.gempukku.lotro.cards.set20.shire;
+
+import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.common.PossessionClass;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1
+ * •Sting, Baggins Birthright
+ * Shire	Possession • Hand Weapon
+ * 2
+ * Bearer must be Frodo.
+ * While skirmishing a minion with strength of 8 or less, Frodo is strength +2.
+ */
+public class Card20_413 extends AbstractAttachableFPPossession {
+    public Card20_413() {
+        super(1, 2, 0, Culture.SHIRE, PossessionClass.HAND_WEAPON, "Sting", "Baggins Birthright", true);
+    }
+
+    @Override
+    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+        return Filters.frodo;
+    }
+
+    @Override
+    protected List<? extends Modifier> getNonBasicStatsModifiers(PhysicalCard self) {
+        return Collections.singletonList(
+                new StrengthModifier(self, Filters.and(Filters.hasAttached(self),
+                        Filters.inSkirmishAgainst(CardType.MINION, Filters.lessStrengthThan(9))), 2);
+    }
+}
