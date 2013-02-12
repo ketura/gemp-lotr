9c6eb8a40f05c82b468412401792b0db9bf56544
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-12 12:31:55 +0000

    "The Great Eye"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_360.java
@@ -0,0 +1,70 @@
+package com.gempukku.lotro.cards.set20.sauron;
+
+import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
+import com.gempukku.lotro.cards.modifiers.FPCulturesSpotCountModifier;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountActiveEvaluator;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 4
+ * •The Great Eye
+ * Sauron	Minion
+ * 8	4	6
+ * Cunning.
+ * When you play this minion, spot another [Sauron] minion to add a threat for each companion over 4.
+ * Shadow: Exert The Great Eye twice to make the number of Free Peoples cultures you can spot -1 until the regroup phase.
+ */
+public class Card20_360 extends AbstractMinion {
+    public Card20_360() {
+        super(4, 8, 4, 6, null, Culture.SAURON, "The Great Eye", null, true);
+        addKeyword(Keyword.CUNNING);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)
+                && PlayConditions.canSpot(game, Filters.not(self), Culture.SAURON, CardType.MINION)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddThreatsEffect(self.getOwner(), self,
+                            new CountActiveEvaluator(4, null, CardType.COMPANION)));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
+                && PlayConditions.canSelfExert(self, 2, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new FPCulturesSpotCountModifier(self, playerId, -1), Phase.REGROUP));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
