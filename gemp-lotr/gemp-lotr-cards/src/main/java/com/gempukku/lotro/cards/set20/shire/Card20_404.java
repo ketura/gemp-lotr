347a9a884727a0bb8bbd8594a4f52fe96a6c58c3
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-12 17:11:06 +0000

    "A Promise"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/shire/Card20_404.java
@@ -0,0 +1,34 @@
+package com.gempukku.lotro.cards.set20.shire;
+
+import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.modifiers.conditions.AndCondition;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+/**
+ * 0
+ * A Promise
+ * Shire	Condition • Support Area
+ * While you can spot Frodo and Sam and neither are exhausted, each are strength +1.
+ */
+public class Card20_404 extends AbstractPermanent {
+    public Card20_404() {
+        super(Side.FREE_PEOPLE, 0, CardType.CONDITION, Culture.SHIRE, Zone.SUPPORT, "A Promise");
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
+        return new StrengthModifier(self, Filters.or(Filters.frodo, Filters.sam),
+                new AndCondition(
+                        new SpotCondition(Filters.frodo, Filters.not(Filters.exhausted)),
+                        new SpotCondition(Filters.sam, Filters.not(Filters.exhausted))), 1);
+    }
+}
