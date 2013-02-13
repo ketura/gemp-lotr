8000c61558ca7071eb63348c3523a76ba4fa3f30
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 14:11:19 +0000

    "Dimholt Road"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_453.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set20.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.ExertCharactersEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Dimholt Road
+ * 7	6
+ * Mountain.
+ * If you cannot spot Aragorn when the fellowship moves to Dimholt Road, each companion must exert.
+ */
+public class Card20_453 extends AbstractSite {
+    public Card20_453() {
+        super("Dimholt Road", Block.SECOND_ED, 7, 6, null);
+        addKeyword(Keyword.MOUNTAIN);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.movesTo(game, effectResult, self)
+                && !PlayConditions.canSpot(game, Filters.aragorn)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new ExertCharactersEffect(action, self, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
