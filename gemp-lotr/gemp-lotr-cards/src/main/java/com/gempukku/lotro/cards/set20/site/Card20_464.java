fcd14ef3089cd3a01e10539a9602e8598c8b2995
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 14:49:44 +0000

    "The Black Gate"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_464.java
@@ -0,0 +1,29 @@
+package com.gempukku.lotro.cards.set20.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.CantPlayCardsModifier;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+
+/**
+ * The Black Gate
+ * 9	9
+ * Battleground.
+ * Free People's events may not be played.
+ */
+public class Card20_464 extends AbstractSite {
+    public Card20_464() {
+        super("The Black Gate", Block.SECOND_ED, 9, 9, null);
+        addKeyword(Keyword.BATTLEGROUND);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
+        return new CantPlayCardsModifier(self, Side.FREE_PEOPLE, CardType.EVENT);
+    }
+}
