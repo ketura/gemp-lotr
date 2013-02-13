145c665797e597cc89c346c597d7640064e50413
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 16:37:40 +0000

    Controlling 5 sites now wins you a game.

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/rules/WinConditionRule.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/rules/WinConditionRule.java
@@ -1,9 +1,12 @@
 package com.gempukku.lotro.logic.timing.rules;
 
+import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.AbstractActionProxy;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.game.state.actions.DefaultActionsEnvironment;
+import com.gempukku.lotro.logic.GameUtils;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.modifiers.ModifierFlag;
 import com.gempukku.lotro.logic.timing.EffectResult;
@@ -32,7 +35,12 @@ public class WinConditionRule {
                                 && game.getGameState().getCurrentSiteNumber() == 9
                                 && !game.getModifiersQuerying().hasFlagActive(game.getGameState(), ModifierFlag.WIN_CHECK_AFTER_SHADOW_RECONCILE))
                             game.playerWon(game.getGameState().getCurrentPlayerId(), "Surviving to Regroup phase on site 9");
-
+                        else {
+                            for (String opponent : GameUtils.getOpponents(game, game.getGameState().getCurrentPlayerId())) {
+                                if (Filters.countActive(game.getGameState(), game.getModifiersQuerying(), CardType.SITE, Filters.siteControlled(opponent))>=5)
+                                    game.playerWon(opponent, "Controls 5 sites");
+                            }
+                        }
                         return null;
                     }
                 });
