85276c087372e99a1658ad011cc24073cd08229d
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 12:37:37 +0000

    "Henneth Annun"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_447.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set20.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Henneth Annun
+ * 6	3
+ * Sanctuary.
+ * When the fellowship moves from Henneth Annun, heal each ring-bound companion.
+ */
+public class Card20_447 extends AbstractSite {
+    public Card20_447() {
+        super("Henneth Annun", Block.SECOND_ED, 6, 3, null);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.movesFrom(game, effectResult, self)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new HealCharactersEffect(self, CardType.COMPANION, Keyword.RING_BOUND));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
