4e5645cf241764172da7ed5e07759ed621212a0b
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 14:12:25 +0000

    "Eastern Ithilien"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_454.java
@@ -0,0 +1,30 @@
+package com.gempukku.lotro.cards.set20.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.modifiers.ResistanceModifier;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.SpotCondition;
+
+/**
+ * Eastern Ithilien
+ * 7	6
+ * Forest.
+ * While you can spot a Nazgul, each companion is resistance -1.
+ */
+public class Card20_454 extends AbstractSite {
+    public Card20_454() {
+        super("Eastern Ithilien", Block.SECOND_ED, 7, 6, null);
+        addKeyword(Keyword.FOREST);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
+        return new ResistanceModifier(self, CardType.COMPANION, new SpotCondition(Race.NAZGUL), -1);
+    }
+}
