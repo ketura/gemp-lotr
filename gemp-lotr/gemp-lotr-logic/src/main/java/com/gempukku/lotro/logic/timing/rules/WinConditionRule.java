9aad83ee04b70c5da522216b4d19d3388959cddc
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 16:44:49 +0000

    Controlling 5 sites now wins you a game, defined per format.

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/rules/WinConditionRule.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/rules/WinConditionRule.java
@@ -35,7 +35,8 @@ public class WinConditionRule {
                                 && game.getGameState().getCurrentSiteNumber() == 9
                                 && !game.getModifiersQuerying().hasFlagActive(game.getGameState(), ModifierFlag.WIN_CHECK_AFTER_SHADOW_RECONCILE))
                             game.playerWon(game.getGameState().getCurrentPlayerId(), "Surviving to Regroup phase on site 9");
-                        else if (game.getFormat().winOnControlling5Sites()) {
+                        else if (game.getFormat().winOnControlling5Sites()
+                                && effectResults.getType() == EffectResult.Type.TAKE_CONTROL_OF_SITE) {
                             for (String opponent : GameUtils.getOpponents(game, game.getGameState().getCurrentPlayerId())) {
                                 if (Filters.countActive(game.getGameState(), game.getModifiersQuerying(), CardType.SITE, Filters.siteControlled(opponent))>=5)
                                     game.playerWon(opponent, "Controls 5 sites");
