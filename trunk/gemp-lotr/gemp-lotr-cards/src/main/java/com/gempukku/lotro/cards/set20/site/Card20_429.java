1d42028b088999107151d67c7adc805051b44cfc
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 11:54:38 +0000

    "Ford of Bruinen"

---
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_429.java
@@ -0,0 +1,55 @@
+package com.gempukku.lotro.cards.set20.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.TwilightCostModifier;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.List;
+
+/**
+ * Ford of Bruinen
+ * 3	0
+ * River. Sanctuary.
+ * The twilight cost of the first Nazgul played at Ford of Bruinen each turn is -5.
+ */
+public class Card20_429 extends AbstractSite {
+    public Card20_429() {
+        super("Ford of Bruinen", Block.SECOND_ED, 3, 0, null);
+        addKeyword(Keyword.RIVER);
+        addKeyword(Keyword.SANCTUARY);
+    }
+
+    @Override
+    public Modifier getAlwaysOnModifier(LotroGame game, final PhysicalCard self) {
+        return new TwilightCostModifier(self,
+                Filters.and(
+                        Race.NAZGUL,
+                        new Filter() {
+                            @Override
+                            public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                                return modifiersQuerying.getUntilEndOfTurnLimitCounter(self).getUsedLimit() < 1;
+                            }
+                        }), -5);
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, Race.NAZGUL)
+                && PlayConditions.location(game, self))
+            game.getModifiersQuerying().getUntilEndOfTurnLimitCounter(self).incrementToLimit(1, 1);
+        return null;
+    }
+}
