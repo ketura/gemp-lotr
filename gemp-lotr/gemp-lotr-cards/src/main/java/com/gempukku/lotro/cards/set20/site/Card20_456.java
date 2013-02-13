e341cf6a049268b790ae7ee43ae82992a503f6a0
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-13 14:15:25 +0000

    "Orthanc Pinnacle"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_456.java
@@ -0,0 +1,39 @@
+package com.gempukku.lotro.cards.set20.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Orthanc Pinnacle
+ * 7	6
+ * Each time a companion takes a wound in a skirmish involving Saruman, the Shadow player may exert a companion.
+ */
+public class Card20_456 extends AbstractSite {
+    public Card20_456() {
+        super("Orthanc Pinnacle", Block.SECOND_ED, 7, 6, null);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.forEachWounded(game, effectResult, CardType.COMPANION, Filters.inSkirmishAgainst(Filters.saruman))
+                && GameUtils.isShadow(game, playerId)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, CardType.COMPANION));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
