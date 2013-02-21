7a626f98c9d08e33af36b407034f14889f00b599
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-21 12:03:37 +0000

    "Troll of Gorgoroth"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_377.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_377.java
@@ -18,17 +18,17 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * 5
+ * 4
  * Troll of Gorgoroth
  * Sauron	Minion • Troll
- * 14	4	6
+ * 12	3	6
  * Fierce.
  * This minion's twilight cost is +1 for each Free Peoples culture you can spot.
  * When you play this minion in region 3, you may discard a Free Peoples condition.
  */
 public class Card20_377 extends AbstractMinion {
     public Card20_377() {
-        super(5, 14, 4, 6, Race.TROLL, Culture.SAURON, "Troll of Gorgoroth");
+        super(4, 12, 3, 6, Race.TROLL, Culture.SAURON, "Troll of Gorgoroth");
         addKeyword(Keyword.FIERCE);
     }
 
