fce4e90969db0c94822f95be34aad3e04882d3ab
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-21 11:43:31 +0000

    "Horn of Helm Hammerhand"

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_332.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_332.java
@@ -4,19 +4,18 @@ import com.gempukku.lotro.cards.AbstractPermanent;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.actions.PlayPermanentAction;
-import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
 import com.gempukku.lotro.cards.effects.ChoiceEffect;
+import com.gempukku.lotro.cards.effects.SnapshotAndApplyStrengthModifierUntilStartOfPhaseEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
-import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.modifiers.evaluator.ConstantEvaluator;
 import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,7 +25,7 @@ import java.util.List;
  * •Horn of Helm Hammerhand
  * Rohan	Artifact • Support Area
  * To play exert Theoden or two Valiant [Rohan] Men.
- * Each time you play a [Rohan] fortification, each mounted [Rohan] man is strength +1 until the regroup phase.
+ * Each time you play a [Rohan] fortification, each mounted companion is strength +1 until the regroup phase.
  */
 public class Card20_332 extends AbstractPermanent {
     public Card20_332() {
@@ -66,10 +65,9 @@ public class Card20_332 extends AbstractPermanent {
     public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
         if (TriggerConditions.played(game, effectResult, Filters.owner(self.getOwner()), Culture.ROHAN, Keyword.FORTIFICATION)) {
             RequiredTriggerAction action = new RequiredTriggerAction(self);
-            final Collection<PhysicalCard> affectedCards = Filters.filterActive(game.getGameState(), game.getModifiersQuerying(), Culture.ROHAN, Race.MAN, Filters.mounted);
             action.appendEffect(
-                    new AddUntilStartOfPhaseModifierEffect(
-                            new StrengthModifier(self, Filters.in(affectedCards), 1), Phase.REGROUP));
+                    new SnapshotAndApplyStrengthModifierUntilStartOfPhaseEffect(
+                            self, new ConstantEvaluator(1), Phase.REGROUP, Filters.mounted, CardType.COMPANION));
             return Collections.singletonList(action);
         }
         return null;
