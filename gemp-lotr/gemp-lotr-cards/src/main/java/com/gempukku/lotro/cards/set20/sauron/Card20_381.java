839456df1aab9dd3266baff0707fdf7e62fb8f96
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-21 12:07:48 +0000

    "Fires Raged Unchecked"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_381.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_381.java
@@ -1,46 +1,39 @@
 package com.gempukku.lotro.cards.set20.sauron;
 
 import com.gempukku.lotro.cards.AbstractPermanent;
-import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.actions.PlayPermanentAction;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.MinionSiteNumberModifier;
 import com.gempukku.lotro.cards.modifiers.conditions.MinThreatCondition;
+import com.gempukku.lotro.cards.modifiers.evaluator.ForEachThreatEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.MultiplyEvaluator;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.modifiers.Modifier;
-import com.gempukku.lotro.logic.modifiers.RemoveKeywordModifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 1
- * You Are Being Tracked
+ * Fires Raged Unchecked
  * Sauron	Condition • Support Area
- * To play exert a [Sauron] minion.
- * While you can spot three threats, [Sauron] minions are not roaming.
+ * The site number of each [Sauron] Orc is -1 for each threat.
+ * While you can spot 3 threats, each [Sauron] Orc that is not roaming is strength + 1.
  */
 public class Card20_381 extends AbstractPermanent {
     public Card20_381() {
-        super(Side.SHADOW, 1, CardType.CONDITION, Culture.SAURON, Zone.SUPPORT, "You Are Being Tracked");
+        super(Side.SHADOW, 1, CardType.CONDITION, Culture.SAURON, Zone.SUPPORT, "Fires Raged Unchecked");
     }
 
     @Override
-    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
-        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
-                && PlayConditions.canExert(self, game, Culture.SAURON, CardType.MINION);
-    }
-
-    @Override
-    public PlayPermanentAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
-        final PlayPermanentAction action = super.getPlayCardAction(playerId, game, self, twilightModifier, ignoreRoamingPenalty);
-        action.appendCost(
-                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Culture.SAURON, CardType.MINION));
-        return action;
-    }
-
-    @Override
-    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new RemoveKeywordModifier(self, Filters.and(Culture.SAURON, CardType.MINION),
-                new MinThreatCondition(3), Keyword.ROAMING);
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(
+                new MinionSiteNumberModifier(self, Filters.and(Culture.SAURON, Race.ORC), null, new MultiplyEvaluator(-1, new ForEachThreatEvaluator())));
+        modifiers.add(
+                new StrengthModifier(self, Filters.and(Culture.SAURON, Race.ORC, Filters.not(Keyword.ROAMING)), new MinThreatCondition(3), 1));
+        return modifiers;
     }
 }
