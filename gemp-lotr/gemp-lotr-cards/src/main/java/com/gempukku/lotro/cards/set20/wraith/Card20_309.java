892a1cfa2b977ae73b693a3fac0a94b2aa3300c3
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-02-21 11:41:38 +0000

    Names.nertea, "Twilight Fiend"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_309.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_309.java
@@ -1,22 +1,26 @@
 package com.gempukku.lotro.cards.set20.wraith;
 
 import com.gempukku.lotro.cards.AbstractMinion;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.SelfExertEffect;
 import com.gempukku.lotro.cards.modifiers.FpSkirmishResistanceStrengthOverrideModifier;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Keyword;
-import com.gempukku.lotro.common.Names;
-import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 4
  * •Ulaire Nertea, Twilight Fiend
  * Ringwraith	Minion • Nazgul
  * 9	3	3
- * Twilight. Characters skirmishing Ulaire Nertea use resistance instead of strength to resolve skirmishes.
+ * Twilight. Exert Nertea to make characters skirmishing him use resistance instead of strength to resolve skirmishes.
  */
 public class Card20_309 extends AbstractMinion {
     public Card20_309() {
@@ -25,7 +29,17 @@ public class Card20_309 extends AbstractMinion {
     }
 
     @Override
-    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new FpSkirmishResistanceStrengthOverrideModifier(self, Filters.and(Filters.character, Filters.inSkirmishAgainst(self)), null);
+    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SKIRMISH, self, 0)
+                && PlayConditions.canSelfExert(self, game)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new SelfExertEffect(action, self));
+            action.appendEffect(
+                    new AddUntilEndOfPhaseModifierEffect(
+                            new FpSkirmishResistanceStrengthOverrideModifier(self, Filters.and(Filters.character, Filters.inSkirmishAgainst(self)), null)));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
