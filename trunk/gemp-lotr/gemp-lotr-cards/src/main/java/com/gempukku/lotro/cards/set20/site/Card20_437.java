5299f9b054967ff791dbde6aa8de664bf1931d40
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-12 11:19:21 +0000

    - "Moria Caverns" now allows to play a Goblin from discard for discarding two cards, instead of forcing FP player to perform a next move (per card text).

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_437.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/site/Card20_437.java
@@ -2,8 +2,7 @@ package com.gempukku.lotro.cards.set20.site;
 
 import com.gempukku.lotro.cards.AbstractSite;
 import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
 import com.gempukku.lotro.common.Block;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Phase;
@@ -12,8 +11,7 @@ import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
-import com.gempukku.lotro.logic.modifiers.ModifierFlag;
-import com.gempukku.lotro.logic.modifiers.SpecialFlagModifier;
+import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
 import com.gempukku.lotro.logic.timing.Action;
 
 import java.util.Collections;
@@ -33,15 +31,14 @@ public class Card20_437 extends AbstractSite {
 
     @Override
     public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseSiteDuringPhase(game, Phase.REGROUP, self)
-                && PlayConditions.canExert(self, game, Filters.owner(playerId), Race.GOBLIN)
-                && PlayConditions.canSpot(game, Filters.unboundCompanion, Race.HOBBIT)) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.SHADOW, self)
+                && PlayConditions.canDiscardFromHand(game, playerId, 2, Filters.any)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Race.GOBLIN)) {
             ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
-                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.owner(playerId), Race.GOBLIN));
+                    new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 2));
             action.appendEffect(
-                    new AddUntilEndOfPhaseModifierEffect(
-                            new SpecialFlagModifier(self, ModifierFlag.HAS_TO_MOVE_IF_POSSIBLE)));
+                    new ChooseAndPlayCardFromDiscardEffect(playerId, game, Race.GOBLIN));
             return Collections.singletonList(action);
         }
         return null;
