0f89cf0fea9baced10ac91a7bcddd6eb939f70aa
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-12 11:52:05 +0000

    - Corrected the images assignments of Arwen (switched the two), also "Arwen, Bane of the Black Riders" became "Arwen, Lady of Imladris" with completely different text (per card text).

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_073.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_073.java
@@ -1,17 +1,16 @@
 package com.gempukku.lotro.cards.set20.elven;
 
 import com.gempukku.lotro.cards.AbstractCompanion;
-import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
-import com.gempukku.lotro.cards.effects.RevealTopCardsOfDrawDeckEffect;
-import com.gempukku.lotro.cards.effects.SelfExertEffect;
-import com.gempukku.lotro.common.*;
-import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.cards.effects.ForEachYouSpotEffect;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Race;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
-import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
+import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -28,36 +27,21 @@ import java.util.List;
  */
 public class Card20_073 extends AbstractCompanion {
     public Card20_073() {
-        super(2, 6, 3, 8, Culture.ELVEN, Race.ELF, null, "Arwen", "Bane of the Black Riders", true);
+        super(2, 6, 3, 8, Culture.ELVEN, Race.ELF, null, "Arwen", "Lady of Imladris", true);
         addKeyword(Keyword.RANGER);
     }
 
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(final String playerId, final LotroGame game, EffectResult effectResult, final PhysicalCard self) {
-        if (TriggerConditions.startOfPhase(game, effectResult, Phase.REGROUP)
-                && PlayConditions.canSelfExert(self, game)) {
+        if (TriggerConditions.moves(game, effectResult)) {
             final OptionalTriggerAction action = new OptionalTriggerAction(self);
-            action.appendCost(
-                    new SelfExertEffect(action, self));
             action.appendEffect(
-                    new RevealTopCardsOfDrawDeckEffect(self, playerId, 1) {
+                    new ForEachYouSpotEffect(playerId, Keyword.RIVENDELL, CardType.ALLY) {
                         @Override
-                        protected void cardsRevealed(List<PhysicalCard> revealedCards) {
-                            for (final PhysicalCard revealedCard : revealedCards) {
-                                if (Filters.and(Culture.ELVEN).accepts(game.getGameState(), game.getModifiersQuerying(), revealedCard)) {
-                                    action.appendEffect(
-                                            new ChooseActiveCardEffect(self, playerId, "Choose a minion to wound", Filters.canTakeWounds(self, 1), CardType.MINION) {
-                                                @Override
-                                                protected void cardSelected(LotroGame game, PhysicalCard minion) {
-                                                    action.appendEffect(
-                                                            new WoundCharactersEffect(self, revealedCard));
-                                                    if (Filters.and(Race.NAZGUL).accepts(game.getGameState(), game.getModifiersQuerying(), minion))
-                                                        action.appendEffect(
-                                                                new WoundCharactersEffect(self, revealedCard));
-                                                }
-                                            });
-                                }
-                            }
+                        protected void spottedCards(int spotCount) {
+                            for (int i=0; i<spotCount; i++)
+                                action.appendEffect(
+                                        new ChooseAndWoundCharactersEffect(action, playerId, 1, 1, CardType.MINION));
                         }
                     });
             return Collections.singletonList(action);
