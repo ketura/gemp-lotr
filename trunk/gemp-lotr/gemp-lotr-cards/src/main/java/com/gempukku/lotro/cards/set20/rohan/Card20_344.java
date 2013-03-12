6913572ebdc74c0d898b860c3403811071dfde6a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-12 11:27:23 +0000

    - "Theoden, Keeper of the Golden Hall" now only prevents skirmish special abilities from being played not any special abilities.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_344.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/rohan/Card20_344.java
@@ -1,11 +1,8 @@
 package com.gempukku.lotro.cards.set20.rohan;
 
 import com.gempukku.lotro.cards.AbstractCompanion;
-import com.gempukku.lotro.cards.modifiers.OpponentsCantUseSpecialAbilitiesModifier;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Keyword;
-import com.gempukku.lotro.common.Names;
-import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.cards.modifiers.OpponentsCantUsePhaseSpecialAbilitiesModifier;
+import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
@@ -28,7 +25,7 @@ public class Card20_344 extends AbstractCompanion {
 
     @Override
     public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new OpponentsCantUseSpecialAbilitiesModifier(self,
-                new GameHasCondition(self, Filters.mounted, Filters.inSkirmish), self.getOwner());
+        return new OpponentsCantUsePhaseSpecialAbilitiesModifier(self,
+                new GameHasCondition(self, Filters.mounted, Filters.inSkirmish), Phase.SKIRMISH, self.getOwner());
     }
 }
