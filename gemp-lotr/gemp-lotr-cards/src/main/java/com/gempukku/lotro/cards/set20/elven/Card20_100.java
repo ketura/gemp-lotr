54c0c6d24d742e6988e775419c581eaf9a9fd1a4
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-14 10:53:26 +0000

    - "Peering Forward" now adds the strength penalty till Regroup, rather than end of phase.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_100.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_100.java
@@ -2,15 +2,14 @@ package com.gempukku.lotro.cards.set20.elven;
 
 import com.gempukku.lotro.cards.AbstractPermanent;
 import com.gempukku.lotro.cards.TriggerConditions;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
-import com.gempukku.lotro.common.CardType;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Side;
-import com.gempukku.lotro.common.Zone;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.timing.EffectResult;
 import com.gempukku.lotro.logic.timing.results.RevealCardFromTopOfDeckResult;
 
@@ -29,14 +28,21 @@ public class Card20_100 extends AbstractPermanent {
     }
 
     @Override
-    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, final PhysicalCard self) {
         if (TriggerConditions.revealedCardsFromTopOfDeck(effectResult, self.getOwner())) {
             RevealCardFromTopOfDeckResult revealedCardsResult = (RevealCardFromTopOfDeckResult) effectResult;
             List<RequiredTriggerAction> actions = new LinkedList<RequiredTriggerAction>();
             for (PhysicalCard revealedElvenCard : Filters.filter(revealedCardsResult.getRevealedCards(), game.getGameState(), game.getModifiersQuerying(), Culture.ELVEN)) {
-                RequiredTriggerAction action = new RequiredTriggerAction(self);
+                final RequiredTriggerAction action = new RequiredTriggerAction(self);
                 action.appendEffect(
-                        new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, self.getOwner(), -1, CardType.MINION));
+                        new ChooseActiveCardEffect(self, self.getOwner(), "Choose a minion for strength -1 penalty", CardType.MINION) {
+                            @Override
+                            protected void cardSelected(LotroGame game, PhysicalCard card) {
+                                action.appendEffect(
+                                        new AddUntilStartOfPhaseModifierEffect(
+                                                new StrengthModifier(self, card, -1), Phase.REGROUP));
+                            }
+                        });
                 actions.add(action);
             }
             return actions;
