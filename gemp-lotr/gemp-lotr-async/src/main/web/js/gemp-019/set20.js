24315700df64189a239254d1fc391469225e929d
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-15 17:52:28 +0000

    20_017

--- gemp-lotr/gemp-lotr-async/src/main/web/js/gemp-019/set20.js
+++ gemp-lotr/gemp-lotr-async/src/main/web/js/gemp-019/set20.js
@@ -15,7 +15,7 @@ var set20 = {
     '20_14': 'http://lotrtcg.org/coreset/dunland/clanofthehills(r1).png',
     '20_15': 'http://lotrtcg.org/coreset/dunland/hillmantribe(r1).png',
     '20_16': 'http://lotrtcg.org/coreset/dunland/hillmantorchbearer.png',
-    '20_17': 'http://lotrtcg.org/coreset/dunland/hillmanprimitive.png',
+    '20_17': 'http://lotrtcg.org/coreset/dunland/hillmanprimitive(r1).png',
     '20_18': 'http://lotrtcg.org/coreset/dunland/hillmanpillager.png',
     '20_19': 'http://lotrtcg.org/coreset/dunland/hillmanwarrior.png',
     '20_20': 'http://lotrtcg.org/coreset/dunland/frecaos.png',
--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_017.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_017.java
@@ -20,24 +20,25 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * 1
+ * 2
  * Hillman Primitive
- * Dunland	Minion • Man
- * 6	1	3
- * Shadow: If stacked on a site you control, play Hillman Primitive to take a [Dunland] possession
- * from your discard pile into hand.
- * Regroup: Spot Saruman or another [Dunland] Man to stack Hillman Primitive on a site you control.
+ * Minion • Man
+ * 7	1	3
+ * Shadow: If stacked on a site you control, play this minion to take a [Dunland] possession from your discard pile
+ * into hand.
+ * Regroup: Spot Saruman or another [Dunland] Man to stack this minion on a site you control.
+ * http://lotrtcg.org/coreset/dunland/hillmanprimitive(r1).png
  */
 public class Card20_017 extends AbstractMinion {
     public Card20_017() {
-        super(1, 6, 1, 3, Race.MAN, Culture.DUNLAND, "Hillman Primitive");
+        super(2, 7, 1, 3, Race.MAN, Culture.DUNLAND, "Hillman Primitive");
     }
 
     @Override
     public List<? extends Action> getPhaseActionsFromStacked(String playerId, LotroGame game, PhysicalCard self) {
         if (PlayConditions.canUseStackedShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
                 && PlayConditions.stackedOn(self, game, Filters.siteControlled(self.getOwner()))
-                && checkPlayRequirements(playerId, game, self, 0, -1, false, false)) {
+                && checkPlayRequirements(playerId, game, self, 0, 0, false, false)) {
             ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
                     new ChooseAndPlayCardFromStackedEffect(playerId, self.getStackedOn(), self));
