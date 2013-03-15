2fccdda849e470b23ca042e8974afaee5fcfc34e
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-15 17:44:28 +0000

    20_008

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_008.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_008.java
@@ -13,36 +13,32 @@ import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
 import com.gempukku.lotro.logic.effects.ChooseAndDiscardCardsFromHandEffect;
+import com.gempukku.lotro.logic.modifiers.GameHasCondition;
 import com.gempukku.lotro.logic.modifiers.KeywordModifier;
 import com.gempukku.lotro.logic.modifiers.Modifier;
-import com.gempukku.lotro.logic.modifiers.SpotCondition;
-import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 5
- * • Dunlending Horde
- * Dunland	Minion • Man
- * 12	2	3
- * While you control a site, Dunlending Horde is fierce.
- * When you play Dunlending Horde, you may discard a [Dunland] card from hand. If you do, Dunlending Horde
- * is strength +2 and damage +1 until the regroup phase.
+ * 4
+ * Dunlending Horde
+ * Minion • Man
+ * 10	2	3
+ * While you control a site, this minion is fierce.
+ * When you play this minion, you may discard a [Dunland] card from hand. If you do, this minion is damage +1
+ * until the regroup phase.
+ * http://lotrtcg.org/coreset/dunland/dunlendinghorde(r1).png
  */
 public class Card20_008 extends AbstractMinion {
     public Card20_008() {
-        super(5, 12, 2, 3, Race.MAN, Culture.DUNLAND, "Dunlending Horde");
+        super(5, 10, 2, 3, Race.MAN, Culture.DUNLAND, "Dunlending Horde");
     }
 
     @Override
-    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(
-                new KeywordModifier(self, self, new SpotCondition(Filters.siteControlled(self.getOwner())), Keyword.FIERCE, 1));
-        return modifiers;
+    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
+        return new KeywordModifier(self, self, new GameHasCondition(Filters.siteControlled(self.getOwner())), Keyword.FIERCE, 1);
     }
 
     @Override
@@ -52,9 +48,6 @@ public class Card20_008 extends AbstractMinion {
             OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendCost(
                     new ChooseAndDiscardCardsFromHandEffect(action, playerId, false, 1, Culture.DUNLAND));
-            action.appendEffect(
-                    new AddUntilStartOfPhaseModifierEffect(
-                            new StrengthModifier(self, self, 2), Phase.REGROUP));
             action.appendEffect(
                     new AddUntilStartOfPhaseModifierEffect(
                             new KeywordModifier(self, self, Keyword.DAMAGE, 1), Phase.REGROUP));
