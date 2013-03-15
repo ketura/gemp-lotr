ee4f65c705dabeafd7e2f81541e630fd78ac283a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-15 18:09:47 +0000

    20_057

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dwarven/Card20_057.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dwarven/Card20_057.java
@@ -2,9 +2,9 @@ package com.gempukku.lotro.cards.set20.dwarven;
 
 import com.gempukku.lotro.cards.AbstractCompanion;
 import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfPhaseModifierEffect;
 import com.gempukku.lotro.cards.effects.SelfExertEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
-import com.gempukku.lotro.cards.modifiers.evaluator.CardMatchesEvaluator;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Phase;
@@ -14,26 +14,24 @@ import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
-import com.gempukku.lotro.logic.modifiers.Modifier;
-import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
-import com.gempukku.lotro.logic.modifiers.SpotCondition;
-import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+import com.gempukku.lotro.logic.modifiers.*;
 
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 3
- * •Gloin, Gimli's Father
- * Dwarven	Companion • Dwarf
+ * •Gloin, Venerable Dwarf
+ * Companion • Dwarf
  * 5	4	6
  * Damage +1.
- * While you can spot Gimli, Gloin is strength +2 and twilight cost -1.
- * Skirmish: Exert Gloin to make another Dwarf strength +2 (or strength +3 if that Dwarf is Gimli).
+ * While you can spot Gimli, Gloin is strength +1 and his twilight cost is -1.
+ * Skirmish: Exert Gloin to make another Dwarf strength +2 (and damage +1 if that Dwarf is Gimli).
+ * http://lotrtcg.org/coreset/dwarven/gloinvd(r1).png
  */
 public class Card20_057 extends AbstractCompanion {
     public Card20_057() {
-        super(3, 5, 4, 6, Culture.DWARVEN, Race.DWARF, null, "Gloin", "Gimli's Father", true);
+        super(3, 5, 4, 6, Culture.DWARVEN, Race.DWARF, null, "Gloin", "Venerable Dwarf", true);
         addKeyword(Keyword.DAMAGE, 1);
     }
 
@@ -46,18 +44,27 @@ public class Card20_057 extends AbstractCompanion {
 
     @Override
     public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new StrengthModifier(self, self, new SpotCondition(Filters.gimli), 2);
+        return new StrengthModifier(self, self, new SpotCondition(Filters.gimli), 1);
     }
 
     @Override
-    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+    protected List<ActivateCardAction> getExtraInPlayPhaseActions(String playerId, final LotroGame game, final PhysicalCard self) {
         if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
                 && PlayConditions.canSelfExert(self, game)) {
-            ActivateCardAction action = new ActivateCardAction(self);
+            final ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
                     new SelfExertEffect(action, self));
             action.appendEffect(
-                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, new CardMatchesEvaluator(2, 3, Filters.gimli), Race.DWARF));
+                    new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, 2, Race.DWARF) {
+                        @Override
+                        protected void selectedCharacterCallback(PhysicalCard selectedCharacter) {
+                            if (Filters.gimli.accepts(game.getGameState(), game.getModifiersQuerying(), selectedCharacter)) {
+                                action.appendEffect(
+                                        new AddUntilEndOfPhaseModifierEffect(
+                                                new KeywordModifier(self, selectedCharacter, Keyword.DAMAGE, 1)));
+                            }
+                        }
+                    });
             return Collections.singletonList(action);
         }
         return null;
