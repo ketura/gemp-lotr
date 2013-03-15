31e38b9264dc998b87fd30d006f6b5726a9aad4a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-15 18:03:53 +0000

    20_048

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dwarven/Card20_048.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dwarven/Card20_048.java
@@ -13,14 +13,16 @@ import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 
 /**
- * 2
- * •Dwarven Vigor
- * Dwarven	Condition • Support Area
- * While there is a [Dwarven] card stacked on this condition, each Dwarf is strength + 1 for each vitality he or she has.
+ * 1
+ * Dwarven Vigor
+ * Condition • Support Area
+ * While there is a [Dwarven] card stacked on this condition, each Dwarf is strength +1 for each point of vitality
+ * over 3 that he has.
+ * http://lotrtcg.org/coreset/dwarven/dwarvenvigor(r1).png
  */
 public class Card20_048 extends AbstractPermanent {
     public Card20_048() {
-        super(Side.FREE_PEOPLE, 2, CardType.CONDITION, Culture.DWARVEN, Zone.SUPPORT, "Dwarven Vigor", null, true);
+        super(Side.FREE_PEOPLE, 1, CardType.CONDITION, Culture.DWARVEN, Zone.SUPPORT, "Dwarven Vigor");
     }
 
     @Override
@@ -34,7 +36,7 @@ public class Card20_048 extends AbstractPermanent {
                 }, new Evaluator() {
                     @Override
                     public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
-                        return modifiersQuerying.getVitality(gameState, cardAffected);
+                        return Math.max(0, modifiersQuerying.getVitality(gameState, cardAffected)-3);
                     }
                 });
     }
