1ea24490b444298c8a98de5da6a517edc69c715a
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 11:32:59 +0000

    20_369

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_369.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_369.java
@@ -3,7 +3,9 @@ package com.gempukku.lotro.cards.set20.sauron;
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
@@ -19,18 +21,21 @@ import java.util.List;
 /**
  * 3
  * Orc Savage
- * Sauron	Minion • Orc
+ * Minion • Orc
  * 9	3	6
- * Each time this minion wins a skirmish, you may remove a threat to wound an unbound companion.
+ * Lurker.
+ * Each time a [Sauron] minion wins a skirmish, you may remove a threat to wound an unbound companion.
+ * http://lotrtcg.org/coreset/sauron/orcsavage(r1).png
  */
 public class Card20_369 extends AbstractMinion {
     public Card20_369() {
         super(3, 9, 3, 6, Race.ORC, Culture.SAURON, "Orc Savage");
+        addKeyword(Keyword.LURKER);
     }
 
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.winsSkirmish(game, effectResult, self)
+        if (TriggerConditions.winsSkirmish(game, effectResult, Culture.SAURON, CardType.MINION)
                 && PlayConditions.canRemoveThreat(game, self, 1)) {
             OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendCost(
