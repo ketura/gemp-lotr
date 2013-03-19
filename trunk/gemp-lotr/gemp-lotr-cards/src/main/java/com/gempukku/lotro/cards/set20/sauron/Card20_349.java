9862a15864bc2bfa64d7dbf49c24f70e1a0bc3cf
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 11:23:11 +0000

    20_349

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_349.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_349.java
@@ -2,39 +2,79 @@ package com.gempukku.lotro.cards.set20.sauron;
 
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.effects.SelfExertEffect;
-import com.gempukku.lotro.cards.modifiers.conditions.CanSpotFPCulturesCondition;
-import com.gempukku.lotro.cards.modifiers.conditions.NotCondition;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
+import com.gempukku.lotro.logic.decisions.MultipleChoiceAwaitingDecision;
 import com.gempukku.lotro.logic.effects.ChooseAndWoundCharactersEffect;
+import com.gempukku.lotro.logic.effects.PlayoutDecisionEffect;
+import com.gempukku.lotro.logic.modifiers.GameHasCondition;
 import com.gempukku.lotro.logic.modifiers.KeywordModifier;
 import com.gempukku.lotro.logic.modifiers.Modifier;
 import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
- * 4
+ * 5
  * •Barad-Dur Captain
- * Sauron	Minion • Orc
- * 10	3	6
- * While you cannot spot 3 Free Peoples cultures, this minion is damage +1.
+ * Minion • Orc
+ * 11	3	6
+ * When you play this minion, name a Free Peoples culture.
+ * While skirmishing a character of the named culture, this minion is damage +1.
  * Skirmish: Exert Barad-Dur Captain to wound a character it is skirmishing.
+ * http://lotrtcg.org/coreset/sauron/baraddurcaptain(r1).png
  */
 public class Card20_349 extends AbstractMinion {
     public Card20_349() {
-        super(5, 13, 3, 6, Race.ORC, Culture.SAURON, "Barad-Dur Captain", null, true);
+        super(5, 11, 3, 6, Race.ORC, Culture.SAURON, "Barad-Dur Captain", null, true);
     }
 
     @Override
     public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
         return new KeywordModifier(self, self,
-                new NotCondition(new CanSpotFPCulturesCondition(self.getOwner(), 3)), Keyword.DAMAGE, 1);
+                new GameHasCondition(self, Filters.inSkirmishAgainst((Culture) self.getWhileInZoneData())), Keyword.DAMAGE, 1);
+    }
+
+    @Override
+    public String getDisplayableInformation(PhysicalCard self) {
+        if (self.getWhileInZoneData() != null)
+            return "Chosen culture is " + ((Culture) self.getWhileInZoneData()).getHumanReadable();
+        return null;
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)) {
+            if (TriggerConditions.played(game, effectResult, self)) {
+                RequiredTriggerAction action = new RequiredTriggerAction(self);
+
+                final Set<String> possibleCultures = new LinkedHashSet<String>();
+                for (Culture culture : Culture.values())
+                    if (culture.isFP())
+                        possibleCultures.add(culture.getHumanReadable());
+
+                action.appendEffect(
+                        new PlayoutDecisionEffect(self.getOwner(),
+                                new MultipleChoiceAwaitingDecision(1, "Choose a culture", possibleCultures.toArray(new String[possibleCultures.size()])) {
+                                    @Override
+                                    protected void validDecisionMade(int index, String result) {
+                                        self.setWhileInZoneData(Culture.findCultureByHumanReadable(result));
+                                    }
+                                }));
+                return Collections.singletonList(action);
+            }
+        }
+        return null;
     }
 
     @Override
