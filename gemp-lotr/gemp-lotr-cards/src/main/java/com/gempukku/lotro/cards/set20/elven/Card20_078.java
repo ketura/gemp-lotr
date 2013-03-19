1237f372afc88a30e974bcd40aead5c7050326ac
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 09:37:35 +0000

    20_078

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_078.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_078.java
@@ -21,9 +21,10 @@ import java.util.List;
 /**
  * 3
  * Coordinated Strike
- * Elven	Condition • Support Area
- * The twilight cost of this condition is -1 for each unwounded [Elven] archer you can spot.
- * Archery: Exert two [Elven] archers and discard this condition to make the fellowship archery total +2.
+ * Condition • Support Area
+ * The twilight cost of this condition is -1 for each unwounded [Elven] archer companion you can spot.
+ * Archery: Exert two [Elven] archer companions and discard this condition to make the fellowship archery total +2.
+ * http://lotrtcg.org/coreset/elven/coordinatedstrike(r1).png
  */
 public class Card20_078 extends AbstractPermanent {
     public Card20_078() {
@@ -32,17 +33,17 @@ public class Card20_078 extends AbstractPermanent {
 
     @Override
     public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
-        return -Filters.countActive(gameState, modifiersQuerying, Culture.ELVEN, Keyword.ARCHER, Filters.unwounded);
+        return -Filters.countActive(gameState, modifiersQuerying, Culture.ELVEN, CardType.COMPANION, Keyword.ARCHER, Filters.unwounded);
     }
 
     @Override
     protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
         if (PlayConditions.canUseFPCardDuringPhase(game, Phase.ARCHERY, self)
-                && PlayConditions.canExert(self,game, 1, 2, Culture.ELVEN, Keyword.ARCHER)
+                && PlayConditions.canExert(self,game, 1, 2, Culture.ELVEN, CardType.COMPANION, Keyword.ARCHER)
                 && PlayConditions.canSelfDiscard(self, game)) {
             ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
-                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, 1, Culture.ELVEN, Keyword.ARCHER));
+                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, 1, Culture.ELVEN, CardType.COMPANION, Keyword.ARCHER));
             action.appendCost(
                     new SelfDiscardEffect(self));
             action.appendEffect(
