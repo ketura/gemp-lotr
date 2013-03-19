73d7a3c44dc9f0bccb2587f7327cfc868c6aa727
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 09:50:05 +0000

    20_138

--- gemp-lotr/gemp-lotr-async/src/main/web/js/gemp-019/set20.js
+++ gemp-lotr/gemp-lotr-async/src/main/web/js/gemp-019/set20.js
@@ -136,7 +136,7 @@ var set20 = {
     '20_135': 'http://lotrtcg.org/coreset/fallenrealms/resolveofrhun(r1).png',
     '20_136': 'http://lotrtcg.org/coreset/fallenrealms/southronbandit.png',
     '20_137': 'http://lotrtcg.org/coreset/fallenrealms/southrondeadeye(r1).png',
-    '20_138': 'http://lotrtcg.org/coreset/fallenrealms/southronnomad.png',
+    '20_138': 'http://lotrtcg.org/coreset/fallenrealms/southronnomad(r1).png',
     '20_139': 'http://lotrtcg.org/coreset/fallenrealms/southronpackmaster.png',
     '20_140': 'http://lotrtcg.org/coreset/fallenrealms/southronrogue.png',
     '20_141': 'http://lotrtcg.org/coreset/fallenrealms/southronsharpshooter.png',
--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_138.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_138.java
@@ -2,59 +2,41 @@ package com.gempukku.lotro.cards.set20.fallenRealms;
 
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.TriggerConditions;
-import com.gempukku.lotro.cards.effects.ChoiceEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
-import com.gempukku.lotro.common.Side;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 2
  * Southron Nomad
- * Fallen Realms	Minion • Man
- * 4	2	4
+ * Minion • Man
+ * 5	2	4
  * Southron. Ambush (1).
- * When you play this minion, you may exert an unbound companion (or 2 Free Peoples men).
+ * When you play this minion, the Free Peoples player must exert an unbound companion.
+ * http://lotrtcg.org/coreset/fallenrealms/southronnomad(r1).png
  */
 public class Card20_138 extends AbstractMinion {
     public Card20_138() {
-        super(2, 4, 2, 4, Race.MAN, Culture.FALLEN_REALMS, "Southron Nomad");
+        super(2, 5, 2, 4, Race.MAN, Culture.FALLEN_REALMS, "Southron Nomad");
         addKeyword(Keyword.SOUTHRON);
         addKeyword(Keyword.AMBUSH, 1);
     }
 
     @Override
-    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
         if (TriggerConditions.played(game, effectResult, self)) {
-            OptionalTriggerAction action = new OptionalTriggerAction(self);
-            List<Effect> possibleEffects = new LinkedList<Effect>();
-            possibleEffects.add(
-                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.unboundCompanion) {
-                        @Override
-                        public String getText(LotroGame game) {
-                            return "Exert an unbound companion";
-                        }
-                    });
-            possibleEffects.add(
-                    new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Side.FREE_PEOPLE, Race.MAN) {
-                        @Override
-                        public String getText(LotroGame game) {
-                            return "Exert 2 Free Peoples men";
-                        }
-                    });
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
             action.appendEffect(
-                    new ChoiceEffect(action, playerId, possibleEffects));
+                    new ChooseAndExertCharactersEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, Filters.unboundCompanion));
             return Collections.singletonList(action);
         }
         return null;
