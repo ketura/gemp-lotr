d41cc508f60fd9fbac7cfc7720c28642c913ab05
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 11:44:36 +0000

    20_376

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_376.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_376.java
@@ -2,16 +2,17 @@ package com.gempukku.lotro.cards.set20.sauron;
 
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.modifiers.evaluator.CountFPCulturesEvaluator;
+import com.gempukku.lotro.cards.modifiers.evaluator.NegativeEvaluator;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
 import com.gempukku.lotro.game.PhysicalCard;
-import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.GameUtils;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.effects.AddThreatsEffect;
-import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -20,20 +21,22 @@ import java.util.List;
 /**
  * 4
  * Threatening Olog-hai
- * Sauron	Minion • Troll
- * 12	3	6
+ * Minion • Troll
+ * 14	3	6
  * Fierce.
- * This minion's twilight cost is +1 for each Free Peoples culture you can spot. Each time this minion wins a skirmish, add a threat.
+ * This minion is strength -1 for each Free Peoples culture you can spot.
+ * Each time this minion wins a skirmish, add a threat.
+ * http://lotrtcg.org/coreset/sauron/threateningologhai(r1).png
  */
 public class Card20_376 extends AbstractMinion {
     public Card20_376() {
-        super(4, 12, 3, 6, Race.TROLL, Culture.SAURON, "Threatening Olog-hai");
+        super(4, 14, 3, 6, Race.TROLL, Culture.SAURON, "Threatening Olog-hai");
         addKeyword(Keyword.FIERCE);
     }
 
     @Override
-    public int getTwilightCostModifier(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard self) {
-        return GameUtils.getSpottableFPCulturesCount(gameState, modifiersQuerying, self.getOwner());
+    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
+        return new StrengthModifier(self, self, null, new NegativeEvaluator(new CountFPCulturesEvaluator(self.getOwner())));
     }
 
     @Override
