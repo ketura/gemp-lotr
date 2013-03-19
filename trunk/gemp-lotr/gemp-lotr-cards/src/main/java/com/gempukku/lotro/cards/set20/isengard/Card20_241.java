51e498b766ee7dce5a03aa98003d37cf9876515e
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 10:14:32 +0000

    20_241

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_241.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/isengard/Card20_241.java
@@ -1,19 +1,22 @@
 package com.gempukku.lotro.cards.set20.isengard;
 
 import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPreventCardEffect;
 import com.gempukku.lotro.cards.modifiers.VitalityModifier;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
-import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.RemoveThreatsEffect;
+import com.gempukku.lotro.logic.effects.WoundCharactersEffect;
 import com.gempukku.lotro.logic.modifiers.KeywordModifier;
 import com.gempukku.lotro.logic.modifiers.Modifier;
-import com.gempukku.lotro.logic.modifiers.RemoveKeywordModifier;
 import com.gempukku.lotro.logic.modifiers.StrengthModifier;
-import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -22,9 +25,12 @@ import java.util.List;
 /**
  * 2
  * •Saruman’s Staff, Instrument of War
- * Isengard	Artifact • Staff
- * +1	 +1
- * Saruman loses cunning and is fierce. Each time Saruman wins a skirmish, add a threat.
+ * Artifact • Staff
+ * 2	1
+ * Bearer must be Saruman.
+ * He is fierce.
+ * Response: If Saruman is about to take a wound, remove a threat to prevent that wound.
+ * http://lotrtcg.org/coreset/isengard/sarumansstaffiow(r1).png
  */
 public class Card20_241 extends AbstractAttachable {
     public Card20_241() {
@@ -40,22 +46,23 @@ public class Card20_241 extends AbstractAttachable {
     public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(
-                new StrengthModifier(self, Filters.hasAttached(self), 1));
+                new StrengthModifier(self, Filters.hasAttached(self), 2));
         modifiers.add(
                 new VitalityModifier(self, Filters.hasAttached(self), 1));
-        modifiers.add(
-                new RemoveKeywordModifier(self, Filters.hasAttached(self), Keyword.CUNNING));
         modifiers.add(
                 new KeywordModifier(self, Filters.hasAttached(self), Keyword.FIERCE));
         return modifiers;
     }
 
     @Override
-    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.winsSkirmish(game, effectResult, Filters.saruman)) {
-            RequiredTriggerAction action = new RequiredTriggerAction(self);
+    public List<? extends Action> getOptionalInPlayBeforeActions(String playerId, LotroGame game, Effect effect, PhysicalCard self) {
+        if (TriggerConditions.isGettingWounded(effect, game, Filters.saruman)
+                && PlayConditions.canRemoveThreat(game, self, 1)) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new RemoveThreatsEffect(self, 1));
             action.appendEffect(
-                    new AddThreatsEffect(self.getOwner(), self, 1));
+                    new ChooseAndPreventCardEffect(self, (WoundCharactersEffect) effect, playerId, "Choose Saruman", Filters.saruman));
             return Collections.singletonList(action);
         }
         return null;
