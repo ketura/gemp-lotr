61fd0a88ff719a5ed94a9d1175638815118c004a
marcins78@gmail.com <marcins78@gmail.com@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 23:38:26 +0000

    - "Barad-Dur Captain" and "Orc Stalker" should no longer cancel the game.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_371.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_371.java
@@ -2,18 +2,15 @@ package com.gempukku.lotro.cards.set20.sauron;
 
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.TriggerConditions;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Keyword;
-import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.decisions.MultipleChoiceAwaitingDecision;
 import com.gempukku.lotro.logic.effects.PlayoutDecisionEffect;
-import com.gempukku.lotro.logic.modifiers.GameHasCondition;
 import com.gempukku.lotro.logic.modifiers.KeywordModifier;
-import com.gempukku.lotro.logic.modifiers.Modifier;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -45,31 +42,30 @@ public class Card20_371 extends AbstractMinion {
     @Override
     public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, final PhysicalCard self) {
         if (TriggerConditions.played(game, effectResult, self)) {
-            if (TriggerConditions.played(game, effectResult, self)) {
-                RequiredTriggerAction action = new RequiredTriggerAction(self);
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
 
-                final Set<String> possibleCultures = new LinkedHashSet<String>();
-                for (Culture culture : Culture.values())
-                    if (culture.isFP())
-                        possibleCultures.add(culture.getHumanReadable());
+            final Set<String> possibleCultures = new LinkedHashSet<String>();
+            for (Culture culture : Culture.values())
+                if (culture.isFP())
+                    possibleCultures.add(culture.getHumanReadable());
 
-                action.appendEffect(
-                        new PlayoutDecisionEffect(self.getOwner(),
-                                new MultipleChoiceAwaitingDecision(1, "Choose a culture", possibleCultures.toArray(new String[possibleCultures.size()])) {
-                                    @Override
-                                    protected void validDecisionMade(int index, String result) {
-                                        self.setWhileInZoneData(Culture.findCultureByHumanReadable(result));
-                                    }
-                                }));
-                return Collections.singletonList(action);
-            }
+            action.appendEffect(
+                    new PlayoutDecisionEffect(self.getOwner(),
+                            new MultipleChoiceAwaitingDecision(1, "Choose a culture", possibleCultures.toArray(new String[possibleCultures.size()])) {
+                                @Override
+                                protected void validDecisionMade(int index, String result) {
+                                    self.setWhileInZoneData(Culture.findCultureByHumanReadable(result));
+                                }
+                            }));
+            return Collections.singletonList(action);
+        }
+        if (TriggerConditions.assignedAgainst(game, effectResult, null, Filters.and(CardType.COMPANION, (Culture) self.getWhileInZoneData()), self)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new AddUntilStartOfPhaseModifierEffect(
+                            new KeywordModifier(self, self, Keyword.FIERCE), Phase.REGROUP));
+            return Collections.singletonList(action);
         }
         return null;
-    }
-
-    @Override
-    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new KeywordModifier(self, self,
-                new GameHasCondition(self, Filters.inSkirmishAgainst((Culture) self.getWhileInZoneData())), Keyword.FIERCE, 1);
     }
 }
