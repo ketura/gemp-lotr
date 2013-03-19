11d929a760cc1da5198384f15062f182ce2e8fca
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 11:36:20 +0000

    20_370

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_370.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/sauron/Card20_370.java
@@ -2,31 +2,47 @@ package com.gempukku.lotro.cards.set20.sauron;
 
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.modifiers.PlayersCantUseCardPhaseSpecialAbilitiesModifier;
-import com.gempukku.lotro.cards.modifiers.conditions.CanSpotFPCulturesCondition;
-import com.gempukku.lotro.cards.modifiers.conditions.NotCondition;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Phase;
-import com.gempukku.lotro.common.Race;
-import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.game.state.Skirmish;
 import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
 
 /**
  * 3
  * Orc Skulker
- * Sauron	Minion • Orc
+ * Minion • Orc
  * 9	3	6
- * While you cannot spot 3 Free Peoples cultures, characters skirmishing this minion may not use skirmish special abilities.
+ * Lurker.
+ * Companions of the same culture as the companion assigned to skirmish this minion may not use skirmish special abilities.
+ * http://lotrtcg.org/coreset/sauron/orcskulker(r1).png
  */
 public class Card20_370 extends AbstractMinion {
     public Card20_370() {
         super(3, 9, 3, 6, Race.ORC, Culture.SAURON, "Orc Skulker");
+        addKeyword(Keyword.LURKER);
     }
 
     @Override
     public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new PlayersCantUseCardPhaseSpecialAbilitiesModifier(self,
-                new NotCondition(new CanSpotFPCulturesCondition(self.getOwner(), 3)), Phase.SKIRMISH, Filters.character, Filters.inSkirmishAgainst(self));
+        return new PlayersCantUseCardPhaseSpecialAbilitiesModifier(self, Phase.SKIRMISH,
+                CardType.COMPANION,
+                new Filter() {
+                    @Override
+                    public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                        final Skirmish skirmish = gameState.getSkirmish();
+                        if (skirmish != null) {
+                            final PhysicalCard fellowshipCharacter = skirmish.getFellowshipCharacter();
+                            if (fellowshipCharacter != null)
+                                if (fellowshipCharacter.getBlueprint().getCardType() == CardType.COMPANION)
+                                    return fellowshipCharacter.getBlueprint().getCulture() == physicalCard.getBlueprint().getCulture();
+                        }
+
+                        return false;
+                    }
+                });
     }
 }
