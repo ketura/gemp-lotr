d7729e617f926132f52ec47859213233028d3aac
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-19 09:45:59 +0000

    20_116

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_116.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_116.java
@@ -2,41 +2,42 @@ package com.gempukku.lotro.cards.set20.fallenRealms;
 
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
-import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.common.Race;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
 import com.gempukku.lotro.logic.effects.AddTwilightEffect;
 import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
-import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 4
- * Easterling Guard
- * Fallen Realms	Minion • Man
+ * Easterling Pikeman
+ * Minion • Man
  * 9	2	4
  * Easterling.
- * Shadow: Heal this minion to add (1).
+ * When you play this minion, you may heal an Easterling to add (1).
+ * http://lotrtcg.org/coreset/fallenrealms/easterlingpikeman(r1).png
  */
 public class Card20_116 extends AbstractMinion {
     public Card20_116() {
-        super(4, 9, 2, 4, Race.MAN, Culture.FALLEN_REALMS, "Easterling Guard");
+        super(4, 9, 2, 4, Race.MAN, Culture.FALLEN_REALMS, "Easterling Pikeman");
         addKeyword(Keyword.EASTERLING);
     }
 
     @Override
-    protected List<? extends Action> getExtraPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseShadowCardDuringPhase(game, Phase.SHADOW, self, 0)
-            && PlayConditions.canHeal(self, game, self)) {
-            ActivateCardAction action = new ActivateCardAction(self);
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.played(game, effectResult, self)
+                && PlayConditions.canHeal(self, game, Keyword.EASTERLING)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendCost(
-                    new ChooseAndHealCharactersEffect(action, playerId, self));
+                    new ChooseAndHealCharactersEffect(action, playerId, Keyword.EASTERLING));
             action.appendEffect(
                     new AddTwilightEffect(self, 1));
             return Collections.singletonList(action);
