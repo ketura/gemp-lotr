1a099c11ef7a0b5ce2e922a3ad9c0db6f57a0532
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-25 10:21:49 +0000

    20_003

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_003.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/dunland/Card20_003.java
@@ -3,35 +3,43 @@ package com.gempukku.lotro.cards.set20.dunland;
 import com.gempukku.lotro.cards.AbstractMinion;
 import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
-import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.cards.effects.LiberateASiteEffect;
 import com.gempukku.lotro.cards.effects.TakeControlOfASiteEffect;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
-import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Race;
-import com.gempukku.lotro.common.Side;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
-import com.gempukku.lotro.logic.actions.SubAction;
-import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.timing.EffectResult;
 
 import java.util.Collections;
 import java.util.List;
 
 /**
- * 2
+ * 3
  * Dunlending Firebrand
- * Dunland	Minion • Man
- * 7 1 3
- * When you play Dunlending Firebrand, you may spot 2 other [Dunland] men to take control of a site.
- * The Free Peoples player may discard a Free Peoples condition to prevent this.
+ * Minion • Man
+ * 9	1	3
+ * When you play this minion, you may spot 2 other [Dunland] men to take control of a site.
+ * If this minion loses a skirmish, the Free Peoples player may liberate a site.
+ * http://www.lotrtcg.org/coreset/dunland/dunlendingfirebrand(r2).jpg
  */
 public class Card20_003 extends AbstractMinion {
     public Card20_003() {
-        super(2, 7, 1, 3, Race.MAN, Culture.DUNLAND, "Dunlending Firebrand");
+        super(3, 9, 1, 3, Race.MAN, Culture.DUNLAND, "Dunlending Firebrand");
+    }
+
+    @Override
+    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.losesSkirmish(game, effectResult, self)) {
+            RequiredTriggerAction action = new RequiredTriggerAction(self);
+            action.appendEffect(
+                    new LiberateASiteEffect(self, game.getGameState().getCurrentPlayerId(), null));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 
     @Override
@@ -40,14 +48,7 @@ public class Card20_003 extends AbstractMinion {
             && PlayConditions.canSpot(game, 2, Filters.not(self), Culture.DUNLAND, Race.MAN)) {
             final OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendEffect(
-                    new PreventableEffect(action, new TakeControlOfASiteEffect(self, playerId), game.getGameState().getCurrentPlayerId(),
-                            new PreventableEffect.PreventionCost() {
-                                @Override
-                                public Effect createPreventionCostForPlayer(SubAction subAction, String playerId) {
-                                    return new ChooseAndDiscardCardsFromPlayEffect(action, game.getGameState().getCurrentPlayerId(), 1, 1, Side.FREE_PEOPLE, CardType.CONDITION);
-
-                                }
-                            }));
+                    new TakeControlOfASiteEffect(self, playerId));
             return Collections.singletonList(action);
         }
         return null;
