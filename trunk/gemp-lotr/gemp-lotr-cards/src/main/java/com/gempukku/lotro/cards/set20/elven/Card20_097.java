96b3fae885a349d1eba402e13b7477f03774fa24
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-25 10:34:40 +0000

    20_097

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_097.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/elven/Card20_097.java
@@ -2,19 +2,18 @@ package com.gempukku.lotro.cards.set20.elven;
 
 import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
 import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.actions.SubCostToEffectAction;
-import com.gempukku.lotro.cards.effects.DiscardCardFromDeckEffect;
-import com.gempukku.lotro.cards.effects.RevealTopCardsOfDrawDeckEffect;
-import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.effects.PreventableEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPutCardsFromHandOnTopOfDrawDeckEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.GameUtils;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
-import com.gempukku.lotro.logic.decisions.YesNoDecision;
-import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
-import com.gempukku.lotro.logic.effects.PlayoutDecisionEffect;
+import com.gempukku.lotro.logic.actions.SubAction;
 import com.gempukku.lotro.logic.timing.Action;
+import com.gempukku.lotro.logic.timing.Effect;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,11 +21,12 @@ import java.util.List;
 /**
  * 0
  * •Narya, Elven Ring of Power
- * Elven	Artifact • Ring
+ * Artifact • Ring
  * 1
  * Bearer must be Cirdan or Gandalf.
- * Maneuver: Exert bearer to reveal the top card of your draw deck. If it is a Free Peoples card, you may discard it
- * to heal 2 companions of that card's culture.
+ * Skirmish: Stack 3 cards from hand on top of your draw deck to make a minion skirmishing an unbound companion strength -3.
+ * The Shadow player may stack 3 cards on top of his or her draw deck to prevent this.
+ * http://www.lotrtcg.org/coreset/elven/naryaerop(r2).jpg
  */
 public class Card20_097 extends AbstractAttachableFPPossession {
     public Card20_097() {
@@ -40,34 +40,31 @@ public class Card20_097 extends AbstractAttachableFPPossession {
 
     @Override
     protected List<? extends Action> getExtraInPlayPhaseActions(final String playerId, final LotroGame game, PhysicalCard self) {
-        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.MANEUVER, self)
-                && PlayConditions.canExert(self, game, Filters.hasAttached(self))) {
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.SKIRMISH, self)
+                && PlayConditions.hasCardInHand(game, playerId, 3, Filters.any)) {
             final ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
-                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.hasAttached(self)));
+                    new ChooseAndPutCardsFromHandOnTopOfDrawDeckEffect(action, playerId, 3, 3, Filters.any));
             action.appendEffect(
-                    new RevealTopCardsOfDrawDeckEffect(self, playerId, 1) {
-                        @Override
-                        protected void cardsRevealed(List<PhysicalCard> revealedCards) {
-                            for (final PhysicalCard revealedCard : revealedCards) {
-                                if (Filters.and(Side.FREE_PEOPLE).accepts(game.getGameState(), game.getModifiersQuerying(), revealedCard)) {
-                                    action.appendEffect(
-                                            new PlayoutDecisionEffect(playerId,
-                                                    new YesNoDecision("Do you want to discard that card to heal 2 companions of the card's culture?") {
-                                                        @Override
-                                                        protected void yes() {
-                                                            SubCostToEffectAction subAction = new SubCostToEffectAction(action);
-                                                            subAction.appendCost(
-                                                                    new DiscardCardFromDeckEffect(revealedCard));
-                                                            subAction.appendEffect(
-                                                                    new ChooseAndHealCharactersEffect(action, playerId, 2, 2, CardType.COMPANION, revealedCard.getBlueprint().getCulture()));
-                                                            game.getActionsEnvironment().addActionToStack(subAction);
-                                                        }
-                                                    }));
+                    new PreventableEffect(action,
+                            new ChooseAndAddUntilEOPStrengthBonusEffect(action, self, playerId, -3, CardType.MINION, Filters.inSkirmishAgainst(Filters.unboundCompanion)) {
+                                @Override
+                                public String getText(LotroGame game) {
+                                    return "Make a minion skirmishing an unbound companion strength -3";
                                 }
-                            }
-                        }
-                    });
+                            },
+                            GameUtils.getOpponents(game, playerId),
+                            new PreventableEffect.PreventionCost() {
+                                @Override
+                                public Effect createPreventionCostForPlayer(SubAction subAction, String playerId) {
+                                    return new ChooseAndPutCardsFromHandOnTopOfDrawDeckEffect(action, playerId, 3, 3, Filters.any) {
+                                        @Override
+                                        public String getText(LotroGame game) {
+                                            return "Stack 3 cards from hand on top of your draw deck";
+                                        }
+                                    };
+                                }
+                            }));
             return Collections.singletonList(action);
         }
         return null;
