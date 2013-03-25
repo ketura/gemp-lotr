3988d73548b129521da7676fdca7069c739c8edd
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-25 11:19:47 +0000

    20_398

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/shire/Card20_398.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/shire/Card20_398.java
@@ -2,14 +2,14 @@ package com.gempukku.lotro.cards.set20.shire;
 
 import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
 import com.gempukku.lotro.cards.PlayConditions;
-import com.gempukku.lotro.cards.effects.ForEachYouSpotEffect;
+import com.gempukku.lotro.cards.effects.CheckPhaseLimitEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.ActivateCardAction;
-import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
+import com.gempukku.lotro.logic.effects.HealCharactersEffect;
 import com.gempukku.lotro.logic.timing.Action;
 
 import java.util.Collections;
@@ -18,9 +18,10 @@ import java.util.List;
 /**
  * 1
  * •Merry's Pipe
- * Shire	Possession • Pipe
- * Bearer must be Merry.
- * Fellowship: Discard a pipeweed possession and spot X pipes to heal X Hobbit companions.
+ * Possession • Pipe
+ * Bearer must be a Hobbit.
+ * Fellowship: If bearer is Merry, you may discard a pipeweed possession to heal every companion bearing a pipe (limit once per phase).
+ * http://lotrtcg.org/coreset/shire/merryspipe(r2).jpg
  */
 public class Card20_398 extends AbstractAttachableFPPossession {
     public Card20_398() {
@@ -29,24 +30,20 @@ public class Card20_398 extends AbstractAttachableFPPossession {
 
     @Override
     protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
-        return Filters.name("Merry");
+        return Race.HOBBIT;
     }
 
     @Override
     protected List<? extends Action> getExtraInPlayPhaseActions(final String playerId, LotroGame game, PhysicalCard self) {
         if (PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)
+                && PlayConditions.isActive(game, Filters.name("Merry"), Filters.hasAttached(self))
                 && PlayConditions.canDiscardFromPlay(self, game, CardType.POSSESSION, Keyword.PIPEWEED)) {
             final ActivateCardAction action= new ActivateCardAction(self);
             action.appendCost(
                     new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 1, 1, CardType.POSSESSION, Keyword.PIPEWEED));
-            action.appendCost(
-                    new ForEachYouSpotEffect(playerId, PossessionClass.PIPE) {
-                        @Override
-                        protected void spottedCards(int spotCount) {
-                            action.appendEffect(
-                                    new ChooseAndHealCharactersEffect(action, playerId, spotCount, spotCount, CardType.COMPANION, Race.HOBBIT));
-                        }
-                    });
+            action.appendEffect(
+                    new CheckPhaseLimitEffect(action, self, 1,
+                            new HealCharactersEffect(self, CardType.COMPANION, Filters.hasAttached(PossessionClass.PIPE))));
             return Collections.singletonList(action);
         }
         return null;
