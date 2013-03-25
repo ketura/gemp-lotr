c356516deb51ffe02d14e006cc1047280a24e5b7
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-25 11:22:44 +0000

    20_403

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/shire/Card20_403.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/shire/Card20_403.java
@@ -2,6 +2,7 @@ package com.gempukku.lotro.cards.set20.shire;
 
 import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
 import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.CheckPhaseLimitEffect;
 import com.gempukku.lotro.cards.effects.ForEachYouSpotEffect;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
 import com.gempukku.lotro.common.*;
@@ -18,9 +19,10 @@ import java.util.List;
 /**
  * 1
  * •Pippin's Pipe
- * Shire	Possession • Pipe
- * Bearer must be Pippin.
- * Fellowship: Discard a pipeweed possesion and spot X pipes to remove X threats.
+ * Possession • Pipe
+ * Bearer must be a Hobbit.
+ * Regroup: If bearer is Pippin, discard a pipeweed possession and spot X pipes to remove X threats (limit once per phase).
+ * http://lotrtcg.org/coreset/shire/pippinspipe(r2).jpg
  */
 public class Card20_403 extends AbstractAttachableFPPossession {
     public Card20_403() {
@@ -29,12 +31,13 @@ public class Card20_403 extends AbstractAttachableFPPossession {
 
     @Override
     protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
-        return Filters.name("Pippin");
+        return Race.HOBBIT;
     }
 
     @Override
     protected List<? extends Action> getExtraInPlayPhaseActions(String playerId, LotroGame game, final PhysicalCard self) {
-        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.FELLOWSHIP, self)
+        if (PlayConditions.canUseFPCardDuringPhase(game, Phase.REGROUP, self)
+                && PlayConditions.isActive(game, Filters.name("Pippin"), Filters.hasAttached(self))
                 && PlayConditions.canDiscardFromPlay(self, game, CardType.POSSESSION, Keyword.PIPEWEED)) {
             final ActivateCardAction action = new ActivateCardAction(self);
             action.appendCost(
@@ -44,7 +47,8 @@ public class Card20_403 extends AbstractAttachableFPPossession {
                         @Override
                         protected void spottedCards(int spotCount) {
                             action.appendEffect(
-                                    new RemoveThreatsEffect(self, spotCount));
+                                    new CheckPhaseLimitEffect(action, self, 1,
+                                            new RemoveThreatsEffect(self, spotCount)));
                         }
                     });
             return Collections.singletonList(action);
