e8941092728b20ea9e33a2dea9ddc7d473f46802
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-03-25 11:36:25 +0000

    - "Black Beast of Angmar, Death in Flight" now correctly applies its strength and vitality modifier.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_284.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/wraith/Card20_284.java
@@ -1,6 +1,7 @@
 package com.gempukku.lotro.cards.set20.wraith;
 
 import com.gempukku.lotro.cards.AbstractAttachable;
+import com.gempukku.lotro.cards.modifiers.VitalityModifier;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
@@ -8,6 +9,10 @@ import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.modifiers.CantTakeWoundsModifier;
 import com.gempukku.lotro.logic.modifiers.GameHasCondition;
 import com.gempukku.lotro.logic.modifiers.Modifier;
+import com.gempukku.lotro.logic.modifiers.StrengthModifier;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 3
@@ -17,7 +22,7 @@ import com.gempukku.lotro.logic.modifiers.Modifier;
  * Bearer must be The Witch-king.
  * While the Ring-bearer has 6 or less resistance, The Witch-king may not take wounds.
  */
-public class Card20_284 extends AbstractAttachable  {
+public class Card20_284 extends AbstractAttachable {
     public Card20_284() {
         super(Side.SHADOW, CardType.POSSESSION, 3, Culture.WRAITH, PossessionClass.MOUNT, "Black Beast of Angmar", "Death in Flight", true);
     }
@@ -28,8 +33,14 @@ public class Card20_284 extends AbstractAttachable  {
     }
 
     @Override
-    public Modifier getAlwaysOnModifier(LotroGame game, PhysicalCard self) {
-        return new CantTakeWoundsModifier(self,
-                new GameHasCondition(Filters.ringBearer, Filters.maxResistance(6)), Filters.witchKing);
+    public List<? extends Modifier> getAlwaysOnModifiers(LotroGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new CantTakeWoundsModifier(self,
+                new GameHasCondition(Filters.ringBearer, Filters.maxResistance(6)), Filters.witchKing));
+        modifiers.add(
+                new StrengthModifier(self, Filters.hasAttached(self), 2));
+        modifiers.add(
+                new VitalityModifier(self, Filters.hasAttached(self), 1));
+        return modifiers;
     }
 }
