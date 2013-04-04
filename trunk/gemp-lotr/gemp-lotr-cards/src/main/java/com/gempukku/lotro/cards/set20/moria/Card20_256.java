fd97a30818e528a07183923a827a701474be307e
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-04-04 09:41:18 +0000

    - "Cave Troll of Moria, Monstrous Friend" should now correctly count the number of cards you can discard from hand to play it, when figuring out, if it is playable.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_256.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_256.java
@@ -31,7 +31,7 @@ public class Card20_256 extends AbstractMinion {
 
     @Override
     protected int getPotentialExtraPaymentDiscount(String playerId, LotroGame game, PhysicalCard self) {
-        return Filters.filter(game.getGameState().getHand(playerId), game.getGameState(), game.getModifiersQuerying(), Culture.MORIA, Filters.not(self)).size();
+        return Filters.filter(game.getGameState().getHand(playerId), game.getGameState(), game.getModifiersQuerying(), Culture.MORIA, Race.GOBLIN, Filters.not(self)).size();
     }
 
     @Override
