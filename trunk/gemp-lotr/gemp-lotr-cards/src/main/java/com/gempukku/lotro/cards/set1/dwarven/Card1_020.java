5dbf57154cb399677658a607e3363e0fdfa4758e
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-04-19 14:05:27 +0000

    Some refactoring.

--- trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/dwarven/Card1_020.java
+++ trunk/gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/dwarven/Card1_020.java
@@ -1,20 +1,18 @@
 package com.gempukku.lotro.cards.set1.dwarven;
 
 import com.gempukku.lotro.cards.AbstractPermanent;
+import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.effects.SelfDiscardEffect;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.GameUtils;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.effects.DiscardCardsFromPlayEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
-import com.gempukku.lotro.logic.timing.results.CharacterWonSkirmishResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -27,28 +25,23 @@ import java.util.List;
  * this condition if a Dwarf loses a skirmish.
  */
 public class Card1_020 extends AbstractPermanent {
-    public Card1_020() {
-        super(Side.FREE_PEOPLE, 2, CardType.CONDITION, Culture.DWARVEN, Zone.SUPPORT, "Let Them Come!");
-    }
+	public Card1_020() {
+		super(Side.FREE_PEOPLE, 2, CardType.CONDITION, Culture.DWARVEN, Zone.SUPPORT, "Let Them Come!");
+	}
 
-    @Override
-    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.winsSkirmishInvolving(game, effectResult, Race.DWARF, Race.ORC)) {
-            CharacterWonSkirmishResult wonResult = (CharacterWonSkirmishResult) effectResult;
-            List<RequiredTriggerAction> actions = new LinkedList<RequiredTriggerAction>();
-            for (PhysicalCard losingOrc : Filters.filter(wonResult.getInvolving(), game.getGameState(), game.getModifiersQuerying(), Race.ORC)) {
-                RequiredTriggerAction action = new RequiredTriggerAction(self);
-                action.setText("Discard " + GameUtils.getFullName(losingOrc));
-                action.appendEffect(new DiscardCardsFromPlayEffect(self, losingOrc));
-                actions.add(action);
-            }
-            return actions;
-        }
-        if (TriggerConditions.losesSkirmish(game, effectResult, Race.DWARF)) {
-            RequiredTriggerAction action = new RequiredTriggerAction(self);
-            action.appendEffect(new SelfDiscardEffect(self));
-            return Collections.singletonList(action);
-        }
-        return null;
-    }
+	@Override
+	public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+		if (TriggerConditions.winsSkirmish(game, effectResult, Race.DWARF) && PlayConditions.isActive(game, Filters.inSkirmish, Race.ORC)) {
+			RequiredTriggerAction action = new RequiredTriggerAction(self);
+			action.appendEffect(
+							new DiscardCardsFromPlayEffect(self, Filters.inSkirmish, Race.ORC));
+			return Collections.singletonList(action);
+		}
+		if (TriggerConditions.losesSkirmish(game, effectResult, Race.DWARF)) {
+			RequiredTriggerAction action = new RequiredTriggerAction(self);
+			action.appendEffect(new SelfDiscardEffect(self));
+			return Collections.singletonList(action);
+		}
+		return null;
+	}
 }
