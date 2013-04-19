3bee9ed0f66dcd03832f8e73ce6500ed5a622aa1
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-04-19 14:05:27 +0000

    Some refactoring.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/dwarven/Card1_009.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set1/dwarven/Card1_009.java
@@ -3,20 +3,16 @@ package com.gempukku.lotro.cards.set1.dwarven;
 import com.gempukku.lotro.cards.AbstractAttachableFPPossession;
 import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.effects.DiscardTopCardFromDeckEffect;
-import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Filterable;
-import com.gempukku.lotro.common.PossessionClass;
-import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
 import com.gempukku.lotro.logic.timing.EffectResult;
-import com.gempukku.lotro.logic.timing.results.CharacterWonSkirmishResult;
+import com.gempukku.lotro.logic.timing.results.CharacterLostSkirmishResult;
 
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Set: The Fellowship of the Ring
@@ -29,36 +25,25 @@ import java.util.Set;
  * top card from his draw deck.
  */
 public class Card1_009 extends AbstractAttachableFPPossession {
-    public Card1_009() {
-        super(0, 2, 0, Culture.DWARVEN, PossessionClass.HAND_WEAPON, "Dwarven Axe");
-    }
+	public Card1_009() {
+		super(0, 2, 0, Culture.DWARVEN, PossessionClass.HAND_WEAPON, "Dwarven Axe");
+	}
 
-    @Override
-    protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
-        return Race.DWARF;
-    }
+	@Override
+	protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
+		return Race.DWARF;
+	}
 
-    @Override
-    public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (TriggerConditions.winsSkirmish(game, effectResult, self.getAttachedTo())) {
-            CharacterWonSkirmishResult skirmishResult = ((CharacterWonSkirmishResult) effectResult);
-            Set<PhysicalCard> losers = skirmishResult.getInvolving();
+	@Override
+	public List<RequiredTriggerAction> getRequiredAfterTriggers(LotroGame game, EffectResult effectResult, PhysicalCard self) {
+		if (TriggerConditions.losesSkirmishInvolving(game, effectResult, CardType.MINION, Filters.hasAttached(self))) {
+			CharacterLostSkirmishResult skirmishResult = ((CharacterLostSkirmishResult) effectResult);
+			PhysicalCard loser = skirmishResult.getLoser();
+			RequiredTriggerAction action = new RequiredTriggerAction(self);
+			action.appendEffect(new DiscardTopCardFromDeckEffect(self, loser.getOwner(), 1, true));
+			return Collections.singletonList(action);
+		}
 
-            List<RequiredTriggerAction> actions = new LinkedList<RequiredTriggerAction>();
-
-            Set<String> losingPlayers = new HashSet<String>();
-            for (PhysicalCard loser : losers)
-                losingPlayers.add(loser.getOwner());
-
-            for (String losingPlayer : losingPlayers) {
-                RequiredTriggerAction action = new RequiredTriggerAction(self);
-                action.appendEffect(new DiscardTopCardFromDeckEffect(self, losingPlayer, true));
-                actions.add(action);
-            }
-
-            return actions;
-        }
-
-        return null;
-    }
+		return null;
+	}
 }
