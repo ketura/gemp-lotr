09cf0589b49f90f6f7431c0d665c9985b778d386
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-11-20 17:31:36 +0000

    Goblin Reclaimer

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_266.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/moria/Card20_266.java
@@ -6,8 +6,8 @@ import com.gempukku.lotro.cards.TriggerConditions;
 import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDiscardEffect;
 import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
-import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
@@ -17,11 +17,12 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * 2
- * Goblin Reclaimer
- * Moria	Minion • Goblin
- * 6	2	4
- * When you play this minion at an underground site, you may play a [Moria] condition from your discard pile.
+ * ❷ Goblin Reclaimer [Mor]
+ * Minion • Goblin
+ * Strength: 6   Vitality: 2   Roaming: 4
+ * When you play this minion, you may spot another [Mor] Goblin to play a [Mor] condition from your discard pile.
+ * <p/>
+ * http://lotrtcg.org/coreset/moria/goblinreclaimer(r3).jpg
  */
 public class Card20_266 extends AbstractMinion {
     public Card20_266() {
@@ -31,8 +32,8 @@ public class Card20_266 extends AbstractMinion {
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
         if (TriggerConditions.played(game, effectResult, self)
-                && PlayConditions.location(game, Keyword.UNDERGROUND)
-            && PlayConditions.canPlayFromDiscard(playerId, game, Culture.MORIA, CardType.CONDITION)) {
+                && PlayConditions.canSpot(game, Filters.not(self), Culture.MORIA, Race.GOBLIN)
+                && PlayConditions.canPlayFromDiscard(playerId, game, Culture.MORIA, CardType.CONDITION)) {
             OptionalTriggerAction action = new OptionalTriggerAction(self);
             action.appendEffect(
                     new ChooseAndPlayCardFromDiscardEffect(playerId, game, Culture.MORIA, CardType.CONDITION));
