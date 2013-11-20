cd7556677b6345e29b0e5c0a1208db9729b6f5fb
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-11-20 16:16:52 +0000

    Easterling Grunt

--- gemp-lotr/gemp-lotr-async/src/main/web/js/gemp-019/set20.js
+++ gemp-lotr/gemp-lotr-async/src/main/web/js/gemp-019/set20.js
@@ -465,6 +465,7 @@ var set20 = {
     '20_474': 'http://lotrtcg.org/coreset/rohan/notadefeat.jpg',
     '20_475': 'http://lotrtcg.org/coreset/shire/halflingdexterity.jpg',
     '20_477': 'http://lotrtcg.org/coreset/shire/dimpleboffinbots(r1).jpg',
-    '20_478': 'http://www.lotrtcg.org/coreset/isengard/torturedandmutilated(r2).jpg',
-    '20_479': 'http://lotrtcg.org/coreset/shire/crossingthethreshold(r2).jpg'
+    '20_478': 'http://lotrtcg.org/coreset/isengard/torturedandmutilated(r2).jpg',
+    '20_479': 'http://lotrtcg.org/coreset/shire/crossingthethreshold(r2).jpg',
+    '20_480': 'http://lotrtcg.org/coreset/fallenrealms/easterlinggrunt(r3).jpg'
 }
