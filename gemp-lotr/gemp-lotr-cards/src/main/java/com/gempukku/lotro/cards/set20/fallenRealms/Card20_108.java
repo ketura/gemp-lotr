372a565680b18104257653643b47770a032d99db
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-11-20 16:00:01 +0000

    Dwarven Vigor and Called to Mordor

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_108.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set20/fallenRealms/Card20_108.java
@@ -1,40 +1,49 @@
 package com.gempukku.lotro.cards.set20.fallenRealms;
 
 import com.gempukku.lotro.cards.AbstractEvent;
-import com.gempukku.lotro.cards.PlayConditions;
 import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndAddUntilEOPStrengthBonusEffect;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
 import com.gempukku.lotro.game.state.LotroGame;
-import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.modifiers.ModifiersQuerying;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 
 /**
- * 7
- * Called to Mordor
- * Fallen Realms	Event • Shadow
- * Toil 2.
- * Spot an Easterling to draw 3 cards.
+ * ❶ Called to Mordor [Fal]
+ * Event • Skirmish
+ * Make an Easterling strength +1 for each wound on each character in his skirmish.
+ * http://lotrtcg.org/coreset/fallenrealms/calledtomordor(r3).jpg
  */
 public class Card20_108 extends AbstractEvent {
     public Card20_108() {
-        super(Side.SHADOW, 7, Culture.FALLEN_REALMS, "Called to Mordor", Phase.SHADOW);
-        addKeyword(Keyword.TOIL, 2);
-    }
-
-    @Override
-    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
-        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
-                && PlayConditions.canSpot(game, Keyword.EASTERLING);
+        super(Side.SHADOW, 1, Culture.FALLEN_REALMS, "Called to Mordor", Phase.SKIRMISH);
     }
 
     @Override
     public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
         PlayEventAction action = new PlayEventAction(self);
         action.appendEffect(
-                new DrawCardsEffect(action, playerId, 3));
+                new ChooseAndAddUntilEOPStrengthBonusEffect(
+                        action, self, playerId,
+                        new Evaluator() {
+                            @Override
+                            public int evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+                                int wounds = 0;
+                                if (Filters.inSkirmish.accepts(gameState, modifiersQuerying, cardAffected)) {
+                                    for (PhysicalCard physicalCard : Filters.filterActive(gameState, modifiersQuerying, Filters.character, Filters.inSkirmish, Filters.wounded)) {
+                                        wounds += gameState.getWounds(physicalCard);
+                                    }
+                                }
+                                return wounds;
+
+                            }
+                        }, Keyword.EASTERLING));
         return action;
     }
 }
