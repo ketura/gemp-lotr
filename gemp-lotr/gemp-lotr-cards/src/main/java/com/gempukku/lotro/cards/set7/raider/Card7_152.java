88f4ea9c703a3f11d942289cd4d2bec170655657
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2013-12-03 17:19:09 +0000

    - "Mumak Commander" no longer kills all the minions in its skirmish in addition to the companion.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/raider/Card7_152.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set7/raider/Card7_152.java
@@ -7,6 +7,7 @@ import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
+import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.RequiredTriggerAction;
@@ -14,6 +15,7 @@ import com.gempukku.lotro.logic.effects.KillEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
 import com.gempukku.lotro.logic.timing.results.CharacterWonSkirmishResult;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -46,9 +48,10 @@ public class Card7_152 extends AbstractMinion {
         if (TriggerConditions.winsSkirmishInvolving(game, effectResult, self, CardType.COMPANION)
                 && game.getGameState().getThreats() >= 6) {
             CharacterWonSkirmishResult skirmishResult = (CharacterWonSkirmishResult) effectResult;
+            final Collection<PhysicalCard> losingCompanion = Filters.filter(skirmishResult.getInvolving(), game.getGameState(), game.getModifiersQuerying(), CardType.COMPANION);
             RequiredTriggerAction action = new RequiredTriggerAction(self);
             action.appendEffect(
-                    new KillEffect(skirmishResult.getInvolving(), KillEffect.Cause.CARD_EFFECT));
+                    new KillEffect(losingCompanion, KillEffect.Cause.CARD_EFFECT));
             return Collections.singletonList(action);
         }
         return null;
