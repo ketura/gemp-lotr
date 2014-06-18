fe79080670eac78ca4fd0cffbf5998fa4543772d
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2014-06-18 14:45:49 +0000

    Hand size is based on format.

--- trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/pregame/PlayStartingFellowshipGameProcess.java
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/pregame/PlayStartingFellowshipGameProcess.java
@@ -25,7 +25,7 @@ public class PlayStartingFellowshipGameProcess implements GameProcess {
 
             _nextProcess = new PlayerPlaysStartingFellowshipGameProcess(nextPlayer, new PlayStartingFellowshipGameProcess(_playOrder, _firstPlayer));
         } else {
-            _nextProcess = new PlayersDrawEightCardsGameProcess(_firstPlayer);
+            _nextProcess = new PlayersDrawStartingHandGameProcess(_firstPlayer);
         }
     }
 
