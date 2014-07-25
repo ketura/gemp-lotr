df06f51989405ab4e789992e27b4d19266f33ba1
marcins78 <marcins78@eb7970ca-0f6f-de4b-2938-835b230b9d1f>
2014-07-25 16:12:21 +0000

    Introducing adventures.

---
+++ trunk/gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/timing/processes/turn/ai/AIPlayerAssignsMinionsGameProcess.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.logic.timing.processes.turn.ai;
+
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filter;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.SystemQueueAction;
+import com.gempukku.lotro.logic.effects.AssignmentPhaseEffect;
+import com.gempukku.lotro.logic.timing.processes.GameProcess;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class AIPlayerAssignsMinionsGameProcess implements GameProcess {
+    private Set<PhysicalCard> _leftoverMinions;
+    private GameProcess _followingGameProcess;
+
+    public AIPlayerAssignsMinionsGameProcess(Set<PhysicalCard> leftoverMinions, GameProcess followingGameProcess) {
+        _leftoverMinions = leftoverMinions;
+        _followingGameProcess = followingGameProcess;
+    }
+
+    @Override
+    public void process(LotroGame game) {
+        GameState gameState = game.getGameState();
+        Filter minionFilter = Filters.and(CardType.MINION, Filters.owner("AI"), Filters.in(_leftoverMinions));
+
+        final Set<PhysicalCard> minions = new HashSet<PhysicalCard>(Filters.filterActive(gameState, game.getModifiersQuerying(), minionFilter, Filters.assignableToSkirmish(Side.SHADOW, true, false)));
+        if (minions.size() > 0) {
+            final PhysicalCard ringBearer = game.getGameState().getRingBearer(game.getGameState().getCurrentPlayerId());
+            Map<PhysicalCard, Set<PhysicalCard>> assignments = Collections.singletonMap(ringBearer, minions);
+
+            if (game.getModifiersQuerying().isValidAssignments(game.getGameState(), Side.SHADOW, assignments)) {
+                SystemQueueAction action = new SystemQueueAction();
+                action.appendEffect(
+                        new AssignmentPhaseEffect("AI", assignments, "Shadow player assignments"));
+
+                game.getActionsEnvironment().addActionToStack(action);
+            }
+        }
+    }
+
+    @Override
+    public GameProcess getNextProcess() {
+        return _followingGameProcess;
+    }
+}
