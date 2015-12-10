91f46d005e158d1d5e4cc2bf5968dcec6ce00780
MarcinSc <marcins78@gmail.com>
2015-12-09 20:28:23 -0800

    Fixing leagues (tested). The packs based on files are picked up automatically. Updated database script to the latest code version.

--- gemp-lotr/database_script.sql
+++ gemp-lotr/database_script.sql
@@ -9,223 +9,232 @@ USE `gemp-lotr` ;
 -- Table `gemp-lotr`.`collection`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`collection` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `player_id` INT(11) NOT NULL ,
-  `collection` MEDIUMBLOB NOT NULL ,
-  `type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`player_id` INT(11) NOT NULL ,
+`collection` MEDIUMBLOB NOT NULL ,
+`type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+PRIMARY KEY (`id`) ,
+INDEX `player_collection` (`player_id` ASC, `type` ASC) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 5788
+AUTO_INCREMENT = 36167
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`deck`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`deck` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `player_id` INT(11) NOT NULL ,
-  `name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'Default' ,
-  `type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'Default' ,
-  `contents` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `player_deck` (`player_id` ASC, `name` ASC) ,
-  INDEX `player_id` (`id` ASC) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`player_id` INT(11) NOT NULL ,
+`name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'Default' ,
+`type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'Default' ,
+`contents` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+PRIMARY KEY (`id`) ,
+UNIQUE INDEX `player_deck` (`player_id` ASC, `name` ASC) ,
+INDEX `player_id` (`player_id` ASC) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 25552
+AUTO_INCREMENT = 128881
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`game_history`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`game_history` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `winner` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `loser` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `win_reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `lose_reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `win_recording_id` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `lose_recording_id` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `start_date` DECIMAL(20,0) NOT NULL ,
-  `end_date` DECIMAL(20,0) NOT NULL ,
-  `format_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `winner_deck_name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `loser_deck_name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `tournament` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`winner` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`loser` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`win_reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`lose_reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`win_recording_id` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`lose_recording_id` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`start_date` DECIMAL(20,0) NOT NULL ,
+`end_date` DECIMAL(20,0) NOT NULL ,
+`format_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`winner_deck_name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`loser_deck_name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`tournament` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+PRIMARY KEY (`id`) ,
+INDEX `end_date` (`end_date` ASC) ,
+INDEX `index3` (`winner` ASC) ,
+INDEX `index4` (`loser` ASC) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 71300
+AUTO_INCREMENT = 517160
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
 
+-- -----------------------------------------------------
+-- Table `gemp-lotr`.`ip_ban`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`ip_ban` (
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`ip` VARCHAR(255) NOT NULL ,
+`prefix` TINYINT(4) NOT NULL ,
+PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+AUTO_INCREMENT = 113
+DEFAULT CHARACTER SET = utf8;
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`league`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`league` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `class` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `parameters` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `start` INT(11) NOT NULL ,
-  `end` INT(11) NOT NULL ,
-  `status` INT(11) NOT NULL ,
-  `cost` INT(11) NOT NULL DEFAULT '0' ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`class` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`parameters` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`start` INT(11) NOT NULL ,
+`end` INT(11) NOT NULL ,
+`status` INT(11) NOT NULL ,
+`cost` INT(11) NOT NULL DEFAULT '0' ,
+PRIMARY KEY (`id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 32
+AUTO_INCREMENT = 159
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`league_match`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`league_match` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `league_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `season_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `winner` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `loser` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`league_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`season_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`winner` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`loser` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+PRIMARY KEY (`id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 17753
+AUTO_INCREMENT = 100862
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`league_participation`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`league_participation` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `league_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `player_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `join_ip` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`league_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`player_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`join_ip` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+PRIMARY KEY (`id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 4417
+AUTO_INCREMENT = 19364
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `gemp-lotr`.`merchant_data`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`merchant_data` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `blueprint_id` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `transaction_date` DATETIME NOT NULL ,
-  `transaction_price` FLOAT NOT NULL ,
-  `transaction_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `sell_count` INT(11) NOT NULL ,
-  `buy_count` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `blueprintId_UNIQUE` (`blueprint_id` ASC) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 3087
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`player`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`player` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `password` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `type` VARCHAR(5) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'u' ,
-  `last_login_reward` INT(11) NULL DEFAULT NULL ,
-  `last_ip` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `create_ip` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`name` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`password` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`type` VARCHAR(5) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'u' ,
+`last_login_reward` INT(11) NULL DEFAULT NULL ,
+`last_ip` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`create_ip` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`banned_until` DECIMAL(20,0) NULL DEFAULT NULL ,
+PRIMARY KEY (`id`) ,
+UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 3811
+AUTO_INCREMENT = 21899
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
 
+-- -----------------------------------------------------
+-- Table `gemp-lotr`.`scheduled_tournament`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`scheduled_tournament` (
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`tournament_id` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`format` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`start` DECIMAL(20,0) NOT NULL ,
+`cost` DECIMAL(10,0) NOT NULL ,
+`playoff` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`prizes` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`minimum_players` DECIMAL(3,0) NOT NULL ,
+`started` DECIMAL(1,0) NOT NULL DEFAULT '0' ,
+PRIMARY KEY (`id`) ,
+INDEX `started` (`started` ASC) )
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`tournament`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`tournament` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `tournament_id` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `start` DECIMAL(20,0) NOT NULL ,
-  `draft_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `format` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `collection` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `stage` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `pairing` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `round` INT(3) NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`tournament_id` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`start` DECIMAL(20,0) NOT NULL ,
+`draft_type` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`format` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`collection` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`stage` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`pairing` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+`round` INT(3) NULL DEFAULT NULL ,
+`prizes` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+PRIMARY KEY (`id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 54
+AUTO_INCREMENT = 1145
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`tournament_match`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`tournament_match` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `tournament_id` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `round` DECIMAL(2,0) NOT NULL ,
-  `player_one` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `player_two` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `winner` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`tournament_id` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`round` DECIMAL(2,0) NOT NULL ,
+`player_one` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`player_two` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`winner` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+PRIMARY KEY (`id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 161
+AUTO_INCREMENT = 12858
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`tournament_player`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`tournament_player` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `tournament_id` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `player` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `deck_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `deck` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `dropped` BINARY(1) NOT NULL DEFAULT '0' ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`tournament_id` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`player` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`deck_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`deck` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`dropped` BINARY(1) NOT NULL DEFAULT '0' ,
+PRIMARY KEY (`id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 437
+AUTO_INCREMENT = 9215
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
 
 -- -----------------------------------------------------
 -- Table `gemp-lotr`.`transfer`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `gemp-lotr`.`transfer` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `notify` INT(11) NOT NULL ,
-  `player` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `currency` INT(11) NOT NULL ,
-  `collection` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `transfer_date` DECIMAL(20,0) NOT NULL ,
-  `direction` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  PRIMARY KEY (`id`) )
+`id` INT(11) NOT NULL AUTO_INCREMENT ,
+`notify` INT(11) NOT NULL ,
+`player` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`currency` INT(11) NOT NULL ,
+`collection` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+`transfer_date` DECIMAL(20,0) NOT NULL ,
+`direction` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+PRIMARY KEY (`id`) ,
+INDEX `player` (`player` ASC, `notify` ASC) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 1424795
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/builder/PacksStorageBuilder.java
+++ gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/builder/PacksStorageBuilder.java
@@ -4,8 +4,6 @@ import com.gempukku.lotro.cards.CardSets;
 import com.gempukku.lotro.packs.*;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
-
 public class PacksStorageBuilder {
     private static final Logger _logger = Logger.getLogger(PacksStorageBuilder.class);
 
@@ -16,101 +14,11 @@ public class PacksStorageBuilder {
             packStorage.addPackBox("Random FotR Foil Common", new RandomFoilPack("C", new String[]{"1", "2", "3"}, cardSets));
             packStorage.addPackBox("Random FotR Foil Uncommon", new RandomFoilPack("U", new String[]{"1", "2", "3"}, cardSets));
 
-            packStorage.addPackBox("(S)FotR - Starter", new FixedPackBox("(S)FotR - Starter"));
-            packStorage.addPackBox("(S)MoM - Starter", new FixedPackBox("(S)MoM - Starter"));
-            packStorage.addPackBox("(S)RotEL - Starter", new FixedPackBox("(S)RotEL - Starter"));
-
-            packStorage.addPackBox("(S)TTT - Starter", new FixedPackBox("(S)TTT - Starter"));
-            packStorage.addPackBox("(S)BoHD - Starter", new FixedPackBox("(S)BoHD - Starter"));
-            packStorage.addPackBox("(S)EoF - Starter", new FixedPackBox("(S)EoF - Starter"));
-
-            packStorage.addPackBox("(S)SH - Starter", new FixedPackBox("(S)SH - Starter"));
-            packStorage.addPackBox("(S)BR - Starter", new FixedPackBox("(S)BR - Starter"));
-            packStorage.addPackBox("(S)BL - Starter", new FixedPackBox("(S)BL - Starter"));
-
-            packStorage.addPackBox("(S)RotK - Starter", new FixedPackBox("(S)RotK - Starter"));
-            packStorage.addPackBox("(S)SoG - Starter", new FixedPackBox("(S)SoG - Starter"));
-            packStorage.addPackBox("(S)MD - Starter", new FixedPackBox("(S)MD - Starter"));
-
             packStorage.addPackBox("(S)FotR - Tengwar", new TengwarPackBox(new int[]{1, 2, 3}, cardSets));
             packStorage.addPackBox("(S)TTT - Tengwar", new TengwarPackBox(new int[]{4, 5, 6}, cardSets));
             packStorage.addPackBox("(S)RotK - Tengwar", new TengwarPackBox(new int[]{7, 8, 10}, cardSets));
             packStorage.addPackBox("(S)SH - Tengwar", new TengwarPackBox(new int[]{11, 12, 13}, cardSets));
             packStorage.addPackBox("(S)Tengwar", new TengwarPackBox(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, cardSets));
-
-            packStorage.addPackBox("(S)Booster Choice", new FixedPackBox("(S)Booster Choice"));
-            packStorage.addPackBox("(S)Movie Booster Choice", new FixedPackBox("(S)Movie Booster Choice"));
-
-            packStorage.addPackBox("FotR - Gandalf Starter", new FixedPackBox("FotR - Gandalf Starter"));
-            packStorage.addPackBox("FotR - Aragorn Starter", new FixedPackBox("FotR - Aragorn Starter"));
-
-            packStorage.addPackBox("MoM - Gandalf Starter", new FixedPackBox("MoM - Gandalf Starter"));
-            packStorage.addPackBox("MoM - Gimli Starter", new FixedPackBox("MoM - Gimli Starter"));
-
-            packStorage.addPackBox("RotEL - Boromir Starter", new FixedPackBox("RotEL - Boromir Starter"));
-            packStorage.addPackBox("RotEL - Legolas Starter", new FixedPackBox("RotEL - Legolas Starter"));
-
-            packStorage.addPackBox("TTT - Aragorn Starter", new FixedPackBox("TTT - Aragorn Starter"));
-            packStorage.addPackBox("TTT - Theoden Starter", new FixedPackBox("TTT - Theoden Starter"));
-
-            packStorage.addPackBox("BoHD - Eowyn Starter", new FixedPackBox("BoHD - Eowyn Starter"));
-            packStorage.addPackBox("BoHD - Legolas Starter", new FixedPackBox("BoHD - Legolas Starter"));
-
-            packStorage.addPackBox("EoF - Faramir Starter", new FixedPackBox("EoF - Faramir Starter"));
-            packStorage.addPackBox("EoF - Witch-king Starter", new FixedPackBox("EoF - Witch-king Starter"));
-
-            packStorage.addPackBox("RotK - Aragorn Starter", new FixedPackBox("RotK - Aragorn Starter"));
-            packStorage.addPackBox("RotK - Eomer Starter", new FixedPackBox("RotK - Eomer Starter"));
-
-            packStorage.addPackBox("SoG - Merry Starter", new FixedPackBox("SoG - Merry Starter"));
-            packStorage.addPackBox("SoG - Pippin Starter", new FixedPackBox("SoG - Pippin Starter"));
-
-            packStorage.addPackBox("MD - Frodo Starter", new FixedPackBox("MD - Frodo Starter"));
-            packStorage.addPackBox("MD - Sam Starter", new FixedPackBox("MD - Sam Starter"));
-
-            packStorage.addPackBox("SH - Aragorn Starter", new FixedPackBox("SH - Aragorn Starter"));
-            packStorage.addPackBox("SH - Eowyn Starter", new FixedPackBox("SH - Eowyn Starter"));
-            packStorage.addPackBox("SH - Gandalf Starter", new FixedPackBox("SH - Gandalf Starter"));
-            packStorage.addPackBox("SH - Legolas Starter", new FixedPackBox("SH - Legolas Starter"));
-
-            packStorage.addPackBox("BR - Mouth Starter", new FixedPackBox("BR - Mouth Starter"));
-            packStorage.addPackBox("BR - Saruman Starter", new FixedPackBox("BR - Saruman Starter"));
-
-            packStorage.addPackBox("BL - Arwen Starter", new FixedPackBox("BL - Arwen Starter"));
-            packStorage.addPackBox("BL - Boromir Starter", new FixedPackBox("BL - Boromir Starter"));
-
-            packStorage.addPackBox("Expanded", new FixedPackBox("Expanded"));
-            packStorage.addPackBox("Wraith", new FixedPackBox("Wraith"));
-            packStorage.addPackBox("AgesEnd", new FixedPackBox("AgesEnd"));
-
-            packStorage.addPackBox("(S)Special-1-3", new FixedPackBox("(S)Special-1-3"));
-            packStorage.addPackBox("(S)Special-4-6", new FixedPackBox("(S)Special-4-6"));
-            packStorage.addPackBox("(S)Special-7-9", new FixedPackBox("(S)Special-7-9"));
-
-            packStorage.addPackBox("Special-01", new FixedPackBox("Special-01"));
-            packStorage.addPackBox("Special-02", new FixedPackBox("Special-02"));
-            packStorage.addPackBox("Special-03", new FixedPackBox("Special-03"));
-            packStorage.addPackBox("Special-04", new FixedPackBox("Special-04"));
-            packStorage.addPackBox("Special-05", new FixedPackBox("Special-05"));
-            packStorage.addPackBox("Special-06", new FixedPackBox("Special-06"));
-            packStorage.addPackBox("Special-07", new FixedPackBox("Special-07"));
-            packStorage.addPackBox("Special-08", new FixedPackBox("Special-08"));
-            packStorage.addPackBox("Special-09", new FixedPackBox("Special-09"));
-
-            packStorage.addPackBox("(S)TSSealed-S1", new FixedPackBox("(S)TSSealed-S1"));
-            packStorage.addPackBox("TSSealedS1D1", new FixedPackBox("TSSealedS1D1"));
-            packStorage.addPackBox("TSSealedS1D2", new FixedPackBox("TSSealedS1D2"));
-            packStorage.addPackBox("TSSealedS1D3", new FixedPackBox("TSSealedS1D3"));
-
-            packStorage.addPackBox("(S)TSSealed-S2", new FixedPackBox("(S)TSSealed-S2"));
-            packStorage.addPackBox("TSSealedS2D1", new FixedPackBox("TSSealedS2D1"));
-            packStorage.addPackBox("TSSealedS2D2", new FixedPackBox("TSSealedS2D2"));
-            packStorage.addPackBox("TSSealedS2D3", new FixedPackBox("TSSealedS2D3"));
-
-            packStorage.addPackBox("(S)TSSealed-S3", new FixedPackBox("(S)TSSealed-S3"));
-            packStorage.addPackBox("TSSealedS3D1", new FixedPackBox("TSSealedS3D1"));
-            packStorage.addPackBox("TSSealedS3D2", new FixedPackBox("TSSealedS3D2"));
-            packStorage.addPackBox("TSSealedS3D3", new FixedPackBox("TSSealedS3D3"));
 
             packStorage.addPackBox("FotR - Booster", new RarityPackBox(cardSets.getSetDefinitions().get("1")));
             packStorage.addPackBox("MoM - Booster", new RarityPackBox(cardSets.getSetDefinitions().get("2")));
@@ -135,9 +43,6 @@ public class PacksStorageBuilder {
             packStorage.addPackBox("REF - Booster", new ReflectionsPackBox(cardSets));
 
             return packStorage;
-        } catch (IOException exp) {
-            _logger.error("Error while creating resource", exp);
-            exp.printStackTrace();
         } catch (RuntimeException exp) {
             _logger.error("Error while creating resource", exp);
             exp.printStackTrace();
