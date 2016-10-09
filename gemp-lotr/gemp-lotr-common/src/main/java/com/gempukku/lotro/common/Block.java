9f267d02e06362e5ade5573b0bebfc507bfeb5d4
PhallenCassidy <leoplurodyn@gmail.com>
2016-10-08 20:33:13 -0400

    Added Hobbit Draft block

--- gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Block.java
+++ gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Block.java
@@ -2,7 +2,10 @@ package com.gempukku.lotro.common;
 
 public enum Block {
     FELLOWSHIP("Fellowship"), TWO_TOWERS("Towers"), KING("King"), SHADOWS("Shadows and onwards"), SPECIAL("Special"),
-    SECOND_ED("2nd edition");
+    SECOND_ED("2nd edition"),
+	
+	//Additional Hobbit Draft block
+	HOBBIT("Hobbit");
 
     private String _humanReadable;
 
