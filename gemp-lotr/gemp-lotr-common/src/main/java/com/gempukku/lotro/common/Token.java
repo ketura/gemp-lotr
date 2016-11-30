499eac50b3478a235c905557e7aacd9289c5d291
PhallenCassidy <leoplurodyn@gmail.com>
2016-11-30 13:23:56 -0500

    Fixed typo

--- gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Token.java
+++ gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Token.java
@@ -10,7 +10,7 @@ public enum Token {
     URUK_HAI(Culture.URUK_HAI), MEN(Culture.MEN), ORC(Culture.ORC),
 
 	//Additional Hobbit Draft cultures
-	ESGAROTH(Culture.ESGAROTH), GUNDABAD(Culture.GUNDABAD), MIRKWOOD(Culture.MIRKWOOD), SMAUG(Culture.SMAUG(), SPIDER(Culture.SPIDER), TROLL(Culture.TROLL);
+	ESGAROTH(Culture.ESGAROTH), GUNDABAD(Culture.GUNDABAD), MIRKWOOD(Culture.MIRKWOOD), SMAUG(Culture.SMAUG), SPIDER(Culture.SPIDER), TROLL(Culture.TROLL);
 
     private Culture _culture;
 
