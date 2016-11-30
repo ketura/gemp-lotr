27c3d2bf0cbe8e8c3da56594e8f4eb59f76bb34f
PhallenCassidy <leoplurodyn@gmail.com>
2016-11-30 12:51:32 -0500

    Main Deck: sites

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set30/site/Card30_054.java
@@ -0,0 +1,42 @@
+package com.gempukku.lotro.cards.set30.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.TriggerConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Main Deck
+ * Twilight Cost: 3
+ * Type: Site
+ * Site: 6
+ * Game Text: River. Sanctuary. At the start of the regroup phase,
+ * you may play a [ESGAROTH] or [DWARVEN] ally from your draw deck.
+ */
+public class Card30_054 extends AbstractSite {
+    public Card30_054() {
+        super("Esgaroth", Block.HOBBIT, 6, 3, Direction.RIGHT);
+        addKeyword(Keyword.RIVER);
+    }
+
+    @Override
+    public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
+        if (TriggerConditions.startOfPhase(game, effectResult, Phase.REGROUP)
+                && playerId.equals(game.getGameState().getCurrentPlayerId())) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Filters.or(Culture.ESGAROTH, Culture.DWARVEN), CardType.ALLY));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
