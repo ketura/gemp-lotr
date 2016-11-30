0485611a482e4090872d7788d05634848bc6d0d0
PhallenCassidy <leoplurodyn@gmail.com>
2016-11-30 12:51:08 -0500

    "Bag End"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set30/site/Card30_049.java
@@ -0,0 +1,44 @@
+package com.gempukku.lotro.cards.set30.site;
+
+import com.gempukku.lotro.cards.AbstractSite;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndPlayCardFromHandEffect;
+import com.gempukku.lotro.common.Block;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.ActivateCardAction;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+import com.gempukku.lotro.logic.timing.Action;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Main Deck
+ * Type: Site
+ * Site: 1
+ * Game Text: Fellowship: Play a [DWARF] follower to draw a card.
+ */
+public class Card30_049 extends AbstractSite {
+    public Card30_049() {
+        super("Bag End", Block.HOBBIT, 1, 0, Direction.RIGHT);
+    }
+
+    @Override
+    public List<? extends Action> getPhaseActions(String playerId, LotroGame game, PhysicalCard self) {
+        if (PlayConditions.canUseSiteDuringPhase(game, Phase.FELLOWSHIP, self)
+                && Filters.filter(game.getGameState().getHand(playerId), game.getGameState(), game.getModifiersQuerying(), Culture.DWARVEN, CardType.FOLLOWER, Filters.playable(game)).size() > 0) {
+            ActivateCardAction action = new ActivateCardAction(self);
+            action.appendCost(
+                    new ChooseAndPlayCardFromHandEffect(playerId, game, Filters.and(Culture.DWARVEN, CardType.FOLLOWER)));
+            action.appendEffect(
+                    new DrawCardsEffect(action, playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
