aa6fd74785f7d75063e866f10fecfb85491f3aba
PhallenCassidy <leoplurodyn@gmail.com>
2017-06-14 23:07:41 -0400

    Update Card31_027.java

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/gundabad/Card31_027.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/gundabad/Card31_027.java
@@ -53,7 +53,7 @@ public class Card31_027 extends AbstractAttachable {
 
     @Override
     protected Filterable getValidTargetFilter(String playerId, LotroGame game, PhysicalCard self) {
-        return Keyword.WARG_RIDER);
+        return Keyword.WARG_RIDER;
     }
 	
 	@Override
