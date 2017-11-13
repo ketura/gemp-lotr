86925fb256e897762945e2028ed16dea4f54e1f3
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2017-11-13 14:43:45 -0800

    - Added "Skillful Negociator"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/shire/Card31_043.java
@@ -0,0 +1,52 @@
+package com.gempukku.lotro.cards.set31.shire;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilStartOfPhaseModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.AllyParticipatesInArcheryFireAndSkirmishesModifier;
+import com.gempukku.lotro.common.CardType;
+import com.gempukku.lotro.common.Culture;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.common.Side;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.ChooseActiveCardEffect;
+
+/**
+ * Skillful Negociator [Shire]
+ Event • Maneuver
+ Twilight Cost 1
+ 'Spot an ally and exert Bilbo to allow that ally to participate in archery fire and skirmishes until the regroup phase.'
+ */
+public class Card31_043 extends AbstractEvent {
+    public Card31_043() {
+        super(Side.FREE_PEOPLE, 1, Culture.SHIRE, "Skillful Negociator", Phase.MANEUVER);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canSpot(game, CardType.ALLY)
+                && PlayConditions.canExert(self, game, Filters.name("Bilbo"));
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, final PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        final PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Filters.name("Bilbo")));
+        action.appendEffect(
+                new ChooseActiveCardEffect(self, playerId, "Choose an ally", CardType.ALLY) {
+                    @Override
+                    protected void cardSelected(LotroGame game, PhysicalCard card) {
+                        action.appendEffect(
+                                new AddUntilStartOfPhaseModifierEffect(
+                                        new AllyParticipatesInArcheryFireAndSkirmishesModifier(self, card), Phase.REGROUP));
+                    }
+                });
+        return action;
+    }
+}
