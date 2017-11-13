4a02d39399fff62b3561ee4a59bf487b289d245b
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2017-11-13 14:19:49 -0800

    - Added "Barrels"

---
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/shire/Card31_040.java
@@ -0,0 +1,50 @@
+package com.gempukku.lotro.cards.set31.shire;
+
+import com.gempukku.lotro.cards.AbstractEvent;
+import com.gempukku.lotro.cards.PlayConditions;
+import com.gempukku.lotro.cards.actions.PlayEventAction;
+import com.gempukku.lotro.cards.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndDiscardCardsFromPlayEffect;
+import com.gempukku.lotro.cards.effects.choose.ChooseAndExertCharactersEffect;
+import com.gempukku.lotro.cards.modifiers.MoveLimitModifier;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.filters.Filters;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.effects.DrawCardsEffect;
+
+/**
+ * Barrels [Shire]
+ Event • Regroup
+ Twilight Cost 1
+ 'Exert 2 [Dwarven] companions to discard up to 2 minions (except Smaug) and draw 3 cards. If the fellowship is at
+ a river, make the move limit +1 for this turn.'
+ */
+public class Card31_040 extends AbstractEvent {
+    public Card31_040() {
+        super(Side.FREE_PEOPLE, 1, Culture.SHIRE, "Barrels", Phase.REGROUP);
+    }
+
+    @Override
+    public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
+        return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
+                && PlayConditions.canExert(self, game, 1, 2, Culture.DWARVEN, CardType.COMPANION);
+    }
+
+    @Override
+    public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
+        PlayEventAction action = new PlayEventAction(self);
+        action.appendCost(
+                new ChooseAndExertCharactersEffect(action, playerId, 2, 2, Culture.DWARVEN, CardType.COMPANION));
+        action.appendEffect(
+                new ChooseAndDiscardCardsFromPlayEffect(action, playerId, 0, 2, CardType.MINION, Filters.not(Filters.name("Smaug"))));
+        action.appendEffect(
+                new DrawCardsEffect(action, playerId, 3));
+        if (game.getModifiersQuerying().hasKeyword(game.getGameState(), game.getGameState().getCurrentSite(), Keyword.RIVER)) {
+            action.appendEffect(
+                    new AddUntilEndOfTurnModifierEffect(
+                            new MoveLimitModifier(self, 1)));
+        }
+        return action;
+    }
+}
