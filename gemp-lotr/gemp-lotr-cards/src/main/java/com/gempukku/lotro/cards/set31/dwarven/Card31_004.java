8da4a2c945b67521a3c6eb48ad5b27b72f696215
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2017-11-13 17:29:12 -0800

    - "Great Barricade" now allows to exert only companions, and doesn't crash the game, if there is a Dwarven card present.

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/dwarven/Card31_004.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/dwarven/Card31_004.java
@@ -41,14 +41,14 @@ public class Card31_004 extends AbstractPermanent {
     @Override
     public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
         return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
-                && PlayConditions.canExert(self, game, Race.DWARF);
+                && PlayConditions.canExert(self, game, Race.DWARF, CardType.COMPANION);
 	}
 	
 	@Override
     public PlayPermanentAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
         PlayPermanentAction permanentAction = super.getPlayCardAction(playerId, game, self, twilightModifier, ignoreRoamingPenalty);
         permanentAction.appendCost(
-                new ChooseAndExertCharactersEffect(permanentAction, playerId, 1, 1, Race.DWARF));
+                new ChooseAndExertCharactersEffect(permanentAction, playerId, 1, 1, Race.DWARF, CardType.COMPANION));
         return permanentAction;
 	}
 
