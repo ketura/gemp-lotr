b2703b23855e0648c7d05ec3c99a11f9413f867a
PhallenCassidy <leoplurodyn@gmail.com>
2018-03-11 13:16:49 -0400

    Fix to The Hobbit "Caught in a Sack"
    Now properly unique

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/troll/Card31_065.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set31/troll/Card31_065.java
@@ -36,7 +36,7 @@ import java.util.List;
  */
 public class Card31_065 extends AbstractPermanent {
     public Card31_065() {
-        super(Side.SHADOW, 2, CardType.CONDITION, Culture.GUNDABAD, Zone.SUPPORT, "Caught in a Sack");
+        super(Side.SHADOW, 2, CardType.CONDITION, Culture.GUNDABAD, Zone.SUPPORT, "Caught in a Sack", null, true);
     }
 
     @Override
