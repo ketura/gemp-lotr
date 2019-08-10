2de864d633d7cb08a42c117cab88b8559a3b7792
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-08-10 10:16:13 -0700

    "Seeking my Counsel" now allows to play Saruman from discard

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/isengard/Card40_138.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/isengard/Card40_138.java
@@ -1,9 +1,5 @@
 package com.gempukku.lotro.cards.set40.isengard;
 
-import com.gempukku.lotro.logic.cardtype.AbstractEvent;
-import com.gempukku.lotro.logic.timing.PlayConditions;
-import com.gempukku.lotro.logic.actions.PlayEventAction;
-import com.gempukku.lotro.logic.effects.choose.ChooseAndPlayCardFromDeckEffect;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Phase;
 import com.gempukku.lotro.common.Side;
@@ -11,8 +7,18 @@ import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.GameUtils;
+import com.gempukku.lotro.logic.actions.PlayEventAction;
+import com.gempukku.lotro.logic.cardtype.AbstractEvent;
 import com.gempukku.lotro.logic.effects.AddThreatsEffect;
+import com.gempukku.lotro.logic.effects.ChoiceEffect;
+import com.gempukku.lotro.logic.effects.choose.ChooseAndPlayCardFromDeckEffect;
+import com.gempukku.lotro.logic.effects.choose.ChooseAndPlayCardFromDiscardEffect;
 import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.PlayConditions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Title: Seeking My Counsel
@@ -32,14 +38,21 @@ public class Card40_138 extends AbstractEvent {
     @Override
     public boolean checkPlayRequirements(String playerId, LotroGame game, PhysicalCard self, int withTwilightRemoved, int twilightModifier, boolean ignoreRoamingPenalty, boolean ignoreCheckingDeadPile) {
         return super.checkPlayRequirements(playerId, game, self, withTwilightRemoved, twilightModifier, ignoreRoamingPenalty, ignoreCheckingDeadPile)
-                && PlayConditions.canPlayFromDeck(playerId, game, Filters.saruman);
+                && (PlayConditions.canPlayFromDeck(playerId, game, Filters.saruman)
+                || PlayConditions.canPlayFromDiscard(playerId, game, Filters.saruman));
     }
 
     @Override
     public PlayEventAction getPlayCardAction(String playerId, LotroGame game, PhysicalCard self, int twilightModifier, boolean ignoreRoamingPenalty) {
         PlayEventAction action = new PlayEventAction(self);
-        action.appendCost(
+        List<Effect> possibleCosts = new ArrayList<Effect>(2);
+        possibleCosts.add(
                 new ChooseAndPlayCardFromDeckEffect(playerId, Filters.saruman));
+        possibleCosts.add(
+                new ChooseAndPlayCardFromDiscardEffect(playerId, game, Filters.saruman));
+        action.appendCost(
+                new ChoiceEffect(
+                        action, playerId, possibleCosts));
         action.appendEffect(
                 new AddThreatsEffect(playerId, self,
                         new Evaluator() {
