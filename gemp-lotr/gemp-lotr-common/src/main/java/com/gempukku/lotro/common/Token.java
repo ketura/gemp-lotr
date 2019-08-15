13cd360e1cb8a366a07ca9c7aee492fa1f2e0af3
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-08-14 19:26:36 -0700

    Find Token by Culture

--- gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Token.java
+++ gemp-lotr/gemp-lotr-common/src/main/java/com/gempukku/lotro/common/Token.java
@@ -9,8 +9,8 @@ public enum Token {
 
     URUK_HAI(Culture.URUK_HAI), MEN(Culture.MEN), ORC(Culture.ORC),
 
-	//Additional Hobbit Draft cultures
-	ESGAROTH(Culture.ESGAROTH), GUNDABAD(Culture.GUNDABAD), MIRKWOOD(Culture.MIRKWOOD), SMAUG(Culture.SMAUG), SPIDER(Culture.SPIDER), TROLL(Culture.TROLL);
+    //Additional Hobbit Draft cultures
+    ESGAROTH(Culture.ESGAROTH), GUNDABAD(Culture.GUNDABAD), MIRKWOOD(Culture.MIRKWOOD), SMAUG(Culture.SMAUG), SPIDER(Culture.SPIDER), TROLL(Culture.TROLL);
 
     private Culture _culture;
 
@@ -24,5 +24,13 @@ public enum Token {
 
     public Culture getCulture() {
         return _culture;
+    }
+
+    Token findTokenForCulture(Culture culture) {
+        for (Token token : Token.values()) {
+            if (token.getCulture() == culture)
+                return token;
+        }
+        return null;
     }
 }
