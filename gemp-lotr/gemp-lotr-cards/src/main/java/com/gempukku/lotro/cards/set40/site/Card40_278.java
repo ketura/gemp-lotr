2a1237c4aff7d700cd8cbdb82154d62fdaa6327c
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-08-15 12:58:12 -0700

    New card text for "Southfarthing"

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/site/Card40_278.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/site/Card40_278.java
@@ -1,17 +1,17 @@
 package com.gempukku.lotro.cards.set40.site;
 
-import com.gempukku.lotro.common.SitesBlock;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.logic.GameUtils;
 import com.gempukku.lotro.logic.cardtype.AbstractSite;
+import com.gempukku.lotro.logic.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.logic.timing.PlayConditions;
 import com.gempukku.lotro.logic.effects.choose.ChooseAndPlayCardFromDeckEffect;
-import com.gempukku.lotro.common.CardType;
-import com.gempukku.lotro.common.Keyword;
-import com.gempukku.lotro.common.PossessionClass;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
 import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.TriggerConditions;
 import com.gempukku.lotro.logic.timing.results.FinishedPlayingFellowshipResult;
 
 import java.util.Collections;
@@ -24,8 +24,7 @@ import java.util.List;
  * Site Number: 1
  * Shadow Number: 0
  * Card Number: 1U278
- * Game Text: After revealing his or her starting fellowship, the first player may play a pipe or pipeweed possession
- * from his or her draw deck for free.
+ * Game Text: At the start of your turn, you may exert a Hobbit to play a pipe or pipeweed possession from your draw deck.
  */
 public class Card40_278 extends AbstractSite {
     public Card40_278() {
@@ -34,15 +33,15 @@ public class Card40_278 extends AbstractSite {
 
     @Override
     public List<OptionalTriggerAction> getOptionalAfterTriggers(String playerId, LotroGame game, EffectResult effectResult, PhysicalCard self) {
-        if (effectResult.getType() == EffectResult.Type.FINISHED_PLAYING_FELLOWSHIP) {
-            FinishedPlayingFellowshipResult finishedPlayingFellowshipResult = (FinishedPlayingFellowshipResult) effectResult;
-            if (finishedPlayingFellowshipResult.getPlayerId().equals(game.getGameState().getPlayerOrder().getFirstPlayer())
-                    && PlayConditions.canPlayFromDeck(playerId, game, Filters.or(PossessionClass.PIPE, Filters.and(CardType.POSSESSION, Keyword.PIPEWEED)))) {
-                OptionalTriggerAction action = new OptionalTriggerAction(self);
-                action.appendEffect(
-                        new ChooseAndPlayCardFromDeckEffect(playerId, -100, Filters.or(PossessionClass.PIPE, Filters.and(CardType.POSSESSION, Keyword.PIPEWEED))));
-                return Collections.singletonList(action);
-            }
+        if (TriggerConditions.startOfTurn(game, effectResult)
+                && GameUtils.isFP(game, playerId)
+                && PlayConditions.canExert(self, game, Race.HOBBIT)) {
+            OptionalTriggerAction action = new OptionalTriggerAction(self);
+            action.appendCost(
+                    new ChooseAndExertCharactersEffect(action, playerId, 1, 1, Race.HOBBIT));
+            action.appendEffect(
+                    new ChooseAndPlayCardFromDeckEffect(playerId, Filters.or(PossessionClass.PIPE, Filters.and(CardType.POSSESSION, Keyword.PIPEWEED))));
+            return Collections.singletonList(action);
         }
         return null;
     }
