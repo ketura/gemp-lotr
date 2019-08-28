8a11f738b8ec17a21b627c434aaad7bb8ad02691
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-08-28 15:37:20 -0700

    Fixed issues after streamlining modifiers

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/CantAddBurdensModifier.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/CantAddBurdensModifier.java
@@ -1,23 +1,17 @@
 package com.gempukku.lotro.logic.modifiers;
 
 import com.gempukku.lotro.common.Filterable;
-import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 
 public class CantAddBurdensModifier extends AbstractModifier {
-    private Filter _sourceFilters;
-
     public CantAddBurdensModifier(PhysicalCard source, Condition condition, Filterable... sourceFilters) {
-        super(source, "Can't add burdens", null, condition, ModifierEffect.BURDEN_MODIFIER);
-        _sourceFilters = Filters.and(sourceFilters);
+        super(source, "Can't add burdens", Filters.and(sourceFilters), condition, ModifierEffect.BURDEN_MODIFIER);
     }
 
     @Override
     public boolean canAddBurden(LotroGame game, String performingPlayer, PhysicalCard source) {
-        if (_sourceFilters.accepts(game, source))
-            return false;
-        return true;
+        return false;
     }
 }
--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/CantRemoveThreatsModifier.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/CantRemoveThreatsModifier.java
@@ -1,23 +1,18 @@
 package com.gempukku.lotro.logic.modifiers;
 
 import com.gempukku.lotro.common.Filterable;
-import com.gempukku.lotro.filters.Filter;
 import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 
 public class CantRemoveThreatsModifier extends AbstractModifier {
-    private Filter _sourceFilters;
 
     public CantRemoveThreatsModifier(PhysicalCard source, Condition condition, Filterable... sourceFilters) {
-        super(source, "Can't remove threats", null, condition, ModifierEffect.THREAT_MODIFIER);
-        _sourceFilters = Filters.and(sourceFilters);
+        super(source, "Can't remove threats", Filters.and(sourceFilters), condition, ModifierEffect.THREAT_MODIFIER);
     }
 
     @Override
     public boolean canRemoveThreat(LotroGame game, PhysicalCard source) {
-        if (_sourceFilters.accepts(game, source))
-            return false;
-        return true;
+        return false;
     }
 }
