f39c38cb112f32e4c7333e2fb28cd6c9595b3b80
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-09-04 09:58:40 -0700

    Frodos are Ring-bound by default

--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/shire/Card40_248.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/shire/Card40_248.java
@@ -1,15 +1,15 @@
 package com.gempukku.lotro.cards.set40.shire;
 
-import com.gempukku.lotro.logic.cardtype.AbstractCompanion;
-import com.gempukku.lotro.logic.timing.TriggerConditions;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
 import com.gempukku.lotro.common.Race;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.cardtype.AbstractCompanion;
 import com.gempukku.lotro.logic.effects.HealCharactersEffect;
 import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.TriggerConditions;
 
 import java.util.Collections;
 import java.util.List;
@@ -31,7 +31,6 @@ public class Card40_248 extends AbstractCompanion {
     public Card40_248() {
         super(0, 3, 4, 10, Culture.SHIRE, Race.HOBBIT, null, "Frodo",
                 "Bane of the Sackville-Bagginses", true);
-        addKeyword(Keyword.RING_BOUND);
         addKeyword(Keyword.CAN_START_WITH_RING);
     }
 
--- gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/shire/Card40_249.java
+++ gemp-lotr/gemp-lotr-cards/src/main/java/com/gempukku/lotro/cards/set40/shire/Card40_249.java
@@ -1,11 +1,5 @@
 package com.gempukku.lotro.cards.set40.shire;
 
-import com.gempukku.lotro.logic.cardtype.AbstractCompanion;
-import com.gempukku.lotro.logic.timing.PlayConditions;
-import com.gempukku.lotro.logic.timing.TriggerConditions;
-import com.gempukku.lotro.logic.effects.ChoiceEffect;
-import com.gempukku.lotro.logic.effects.SelfExertEffect;
-import com.gempukku.lotro.logic.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.common.CardType;
 import com.gempukku.lotro.common.Culture;
 import com.gempukku.lotro.common.Keyword;
@@ -14,10 +8,16 @@ import com.gempukku.lotro.filters.Filters;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.OptionalTriggerAction;
+import com.gempukku.lotro.logic.cardtype.AbstractCompanion;
+import com.gempukku.lotro.logic.effects.ChoiceEffect;
 import com.gempukku.lotro.logic.effects.ChooseAndHealCharactersEffect;
 import com.gempukku.lotro.logic.effects.HealCharactersEffect;
+import com.gempukku.lotro.logic.effects.SelfExertEffect;
+import com.gempukku.lotro.logic.effects.choose.ChooseAndExertCharactersEffect;
 import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.EffectResult;
+import com.gempukku.lotro.logic.timing.PlayConditions;
+import com.gempukku.lotro.logic.timing.TriggerConditions;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -41,7 +41,6 @@ public class Card40_249 extends AbstractCompanion {
     public Card40_249() {
         super(0, 3, 4, 10, Culture.SHIRE, Race.HOBBIT, null, "Frodo",
                 "Extraordinarily Resilient", true);
-        addKeyword(Keyword.RING_BOUND);
         addKeyword(Keyword.CAN_START_WITH_RING);
     }
 
