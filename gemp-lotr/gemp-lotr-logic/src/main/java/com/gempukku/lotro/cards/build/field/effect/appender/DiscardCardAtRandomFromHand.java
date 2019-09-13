0c5eede8dea39f4f1414046db9f3144839123a8f
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-09-13 15:03:04 -0700

    Some FotR cards

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/DiscardCardAtRandomFromHand.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/DiscardCardAtRandomFromHand.java
@@ -1,35 +1,49 @@
 package com.gempukku.lotro.cards.build.field.effect.appender;
 
-import com.gempukku.lotro.cards.build.ActionContext;
-import com.gempukku.lotro.cards.build.CardGenerationEnvironment;
-import com.gempukku.lotro.cards.build.InvalidCardDefinitionException;
+import com.gempukku.lotro.cards.build.*;
 import com.gempukku.lotro.cards.build.field.FieldUtils;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppender;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppenderProducer;
+import com.gempukku.lotro.cards.build.field.effect.appender.resolver.PlayerResolver;
+import com.gempukku.lotro.cards.build.field.effect.appender.resolver.ValueResolver;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.actions.CostToEffectAction;
 import com.gempukku.lotro.logic.effects.DiscardCardAtRandomFromHandEffect;
 import com.gempukku.lotro.logic.timing.Effect;
 import org.json.simple.JSONObject;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DiscardCardAtRandomFromHand implements EffectAppenderProducer {
     @Override
     public EffectAppender createEffectAppender(JSONObject effectObject, CardGenerationEnvironment environment) throws InvalidCardDefinitionException {
-        FieldUtils.validateAllowedFields(effectObject, "forced");
+        FieldUtils.validateAllowedFields(effectObject, "player", "count", "forced");
 
+        final String player = FieldUtils.getString(effectObject.get("player"), "player", "you");
+        final PlayerSource playerSource = PlayerResolver.resolvePlayer(player, environment);
+        final ValueSource countSource = ValueResolver.resolveEvaluator(effectObject.get("count"), 1, environment);
         final boolean forced = FieldUtils.getBoolean(effectObject.get("forced"), "forced");
 
         return new DelayedAppender() {
             @Override
-            protected Effect createEffect(boolean cost, CostToEffectAction action, ActionContext actionContext) {
-                return new DiscardCardAtRandomFromHandEffect(actionContext.getSource(), actionContext.getPerformingPlayer(), forced);
+            protected List<Effect> createEffects(boolean cost, CostToEffectAction action, ActionContext actionContext) {
+                final String playerId = playerSource.getPlayer(actionContext);
+                final int count = countSource.getEvaluator(actionContext).evaluateExpression(actionContext.getGame(), null);
+
+                List<Effect> result = new LinkedList<>();
+                for (int i = 0; i < count; i++)
+                    result.add(new DiscardCardAtRandomFromHandEffect(actionContext.getSource(), playerId, forced));
+
+                return result;
             }
 
             @Override
             public boolean isPlayableInFull(ActionContext actionContext) {
                 final LotroGame game = actionContext.getGame();
-                final String playerId = actionContext.getPerformingPlayer();
-                return game.getGameState().getHand(playerId).size() >= 1
+                final String playerId = playerSource.getPlayer(actionContext);
+                final int count = countSource.getEvaluator(actionContext).evaluateExpression(game, null);
+                return game.getGameState().getHand(playerId).size() >= count
                         && (!forced || game.getModifiersQuerying().canDiscardCardsFromHand(game, playerId, actionContext.getSource()));
             }
         };
