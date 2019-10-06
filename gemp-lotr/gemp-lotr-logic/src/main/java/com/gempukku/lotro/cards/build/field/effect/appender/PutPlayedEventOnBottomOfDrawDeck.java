65a03988529339f39b36f13b81cadad583c6f264
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-10-06 13:30:59 -0700

    Fixing couple of bugs

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/PutPlayedEventOnBottomOfDrawDeck.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/PutPlayedEventOnBottomOfDrawDeck.java
@@ -7,9 +7,9 @@ import com.gempukku.lotro.cards.build.field.FieldUtils;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppender;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppenderProducer;
 import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.actions.PlayEventAction;
 import com.gempukku.lotro.logic.effects.PutPlayedEventOnBottomOfDeckEffect;
 import com.gempukku.lotro.logic.timing.Effect;
-import com.gempukku.lotro.logic.timing.results.PlayEventResult;
 import org.json.simple.JSONObject;
 
 public class PutPlayedEventOnBottomOfDrawDeck implements EffectAppenderProducer {
@@ -20,8 +20,7 @@ public class PutPlayedEventOnBottomOfDrawDeck implements EffectAppenderProducer 
         return new DelayedAppender() {
             @Override
             protected Effect createEffect(boolean cost, CostToEffectAction action, ActionContext actionContext) {
-                final PlayEventResult playEventResult = (PlayEventResult) actionContext.getEffectResult();
-                return new PutPlayedEventOnBottomOfDeckEffect(playEventResult.getPlayEventAction());
+                return new PutPlayedEventOnBottomOfDeckEffect((PlayEventAction) action);
             }
         };
     }
