998acd488878e6ea45cda202a88f6e4e3e3ef1b4
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2021-02-06 18:54:34 -0600

    Added some package namespaces for less haphazard individual card testing.  Added GenericCardTestHelper, which is intended to be used to make unit tests less obscure.  Altered the default AbstractAtTest format to multipath instead of movie.

---
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/cards/official/set2/Card2_013Tests.java
@@ -0,0 +1,65 @@
+package com.gempukku.lotro.cards.official.set2;
+
+import com.gempukku.lotro.cards.GenericCardTestHelper;
+import com.gempukku.lotro.common.Keyword;
+import com.gempukku.lotro.common.Phase;
+import com.gempukku.lotro.game.CardNotFoundException;
+import com.gempukku.lotro.game.PhysicalCardImpl;
+import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+// Tidings of Erebor
+public class Card2_013Tests
+{
+    protected GenericCardTestHelper GetScenario() throws CardNotFoundException, DecisionResultInvalidException {
+        return new GenericCardTestHelper(
+                new HashMap<String, String>()
+                {{
+                    put("tidings", "2_13");
+                    put("gimli", "1_13");
+
+                    put("deckcard1", "1_13");
+                    put("deckcard2", "1_13");
+                    put("deckcard3", "1_13");
+
+                }}
+        );
+    }
+
+    @Test
+    public void TidingsAbilityCanBePrevented() throws DecisionResultInvalidException, CardNotFoundException {
+        //Pre-game setup
+        GenericCardTestHelper scn = GetScenario();
+
+        PhysicalCardImpl tidings = scn.GetFreepsCard("tidings");
+        PhysicalCardImpl gimli = scn.GetFreepsCard("gimli");
+        scn.FreepsMoveCharToTable(gimli);
+        scn.FreepsMoveCardToHand(tidings);
+        scn.FreepsMoveCardToDeck("deckcard1", "deckcard2", "deckcard3");
+
+        scn.StartGame();
+        scn.FreepsSkipCurrentPhaseAction();
+
+        scn.SkipToPhase(Phase.REGROUP);
+
+
+        scn.FreepsPlayCard(tidings);
+        assertEquals(3, scn.GetTwilight());
+        assertEquals(0, scn.GetFreepsHandCount());
+        assertEquals(3, scn.GetFreepsDeckCount());
+        scn.ShadowAcceptOptionalTrigger();
+        assertEquals(0, scn.GetTwilight());
+        assertEquals(0, scn.GetFreepsHandCount());
+        assertEquals(3, scn.GetFreepsDeckCount());
+
+    }
+
+
+
+}
