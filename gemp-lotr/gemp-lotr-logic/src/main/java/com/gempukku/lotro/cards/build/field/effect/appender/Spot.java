9317b96daa4fcf69d2a68a0d03251d584c89272d
Troy Biesterfeld <troy.biesterfeld@gmail.com>
2021-06-06 19:28:17 -0500

    Add remaining Movie block balancing errata to playtesting

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/Spot.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/Spot.java
@@ -1,23 +1,53 @@
 package com.gempukku.lotro.cards.build.field.effect.appender;
 
-import com.gempukku.lotro.cards.build.CardGenerationEnvironment;
-import com.gempukku.lotro.cards.build.InvalidCardDefinitionException;
+import com.gempukku.lotro.cards.build.*;
 import com.gempukku.lotro.cards.build.field.FieldUtils;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppender;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppenderProducer;
 import com.gempukku.lotro.cards.build.field.effect.appender.resolver.CardResolver;
+import com.gempukku.lotro.cards.build.field.effect.appender.resolver.ValueResolver;
+import com.gempukku.lotro.common.Filterable;
+import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.timing.Effect;
+import com.gempukku.lotro.logic.timing.PlayConditions;
+import com.gempukku.lotro.logic.timing.UnrespondableEffect;
 import org.json.simple.JSONObject;
 
 public class Spot implements EffectAppenderProducer {
     @Override
     public EffectAppender createEffectAppender(JSONObject effectObject, CardGenerationEnvironment environment) throws InvalidCardDefinitionException {
-        FieldUtils.validateAllowedFields(effectObject, "filter", "memorize", "text");
+        FieldUtils.validateAllowedFields(effectObject, "count", "filter", "memorize", "text", "skipMemorize");
 
+        final ValueSource valueSource = ValueResolver.resolveEvaluator(effectObject.get("count"), 1, environment);
         final String filter = FieldUtils.getString(effectObject.get("filter"), "filter");
         final String memorize = FieldUtils.getString(effectObject.get("memorize"), "memorize", "_temp");
         final String text = FieldUtils.getString(effectObject.get("text"), "text", "Choose card to spot");
+        final boolean skipMemorize = FieldUtils.getBoolean(effectObject.get("skipMemorize"), "skipMemorize", false);
 
-        return CardResolver.resolveCard(
-                filter, memorize, "you", text, environment);
+        if (skipMemorize) {
+            final FilterableSource filterableSource = environment.getFilterFactory().generateFilter(filter, environment);
+
+            return new DelayedAppender() {
+                @Override
+                protected Effect createEffect(boolean cost, CostToEffectAction action, ActionContext actionContext) {
+                    return new UnrespondableEffect() {
+                        @Override
+                        protected void doPlayEffect(LotroGame game) {
+                        }
+                    };
+                }
+
+                @Override
+                public boolean isPlayableInFull(ActionContext actionContext) {
+                    final int count = valueSource.getEvaluator(actionContext).evaluateExpression(actionContext.getGame(), null);
+                    Filterable filterable = filterableSource.getFilterable(actionContext);
+                    return PlayConditions.canSpot(actionContext.getGame(), count, filterable);
+                }
+            };
+        } else {
+            return CardResolver.resolveCards(
+                    filter, valueSource, memorize, "you", text, environment);
+        }
     }
 }
