d9dc3d5f35f8c727fe721ce45ddcefd69fa34b44
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2021-12-20 01:52:16 -0600

    Fixed a crash from casting the wrong object type, which was a copy-paste error from the original class.

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/trigger/ActivatedTriggerCheckerProducer.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/trigger/ActivatedTriggerCheckerProducer.java
@@ -8,6 +8,7 @@ import com.gempukku.lotro.cards.build.field.FieldUtils;
 import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.logic.timing.TriggerConditions;
+import com.gempukku.lotro.logic.timing.results.ActivateCardResult;
 import com.gempukku.lotro.logic.timing.results.PlayCardResult;
 import com.gempukku.lotro.logic.timing.results.PlayEventResult;
 import org.json.simple.JSONObject;
@@ -27,10 +28,10 @@ public class ActivatedTriggerCheckerProducer implements TriggerCheckerProducer {
                 boolean activated = TriggerConditions.activated(actionContext.getGame(), actionContext.getEffectResult(), filterable);
 
                 if (activated) {
-                    PlayCardResult playCardResult = (PlayCardResult) actionContext.getEffectResult();
+                    ActivateCardResult activateCardResult = (ActivateCardResult) actionContext.getEffectResult();
 
                     if (memorize != null) {
-                        PhysicalCard playedCard = playCardResult.getPlayedCard();
+                        PhysicalCard playedCard = activateCardResult.getSource();
                         actionContext.setCardMemory(memorize, playedCard);
                     }
                 }
