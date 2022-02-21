15a039bdf173ea27d8d26f10f27b8aa20df1ad55
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2022-02-20 23:53:45 -0600

    Various errata/V1 fixes.  Changed the IncrementPerPhaseLimit param to take a ValueResolver rather than a hard-coded int.

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/IncrementPerPhaseLimit.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/IncrementPerPhaseLimit.java
@@ -3,11 +3,14 @@ package com.gempukku.lotro.cards.build.field.effect.appender;
 import com.gempukku.lotro.cards.build.ActionContext;
 import com.gempukku.lotro.cards.build.CardGenerationEnvironment;
 import com.gempukku.lotro.cards.build.InvalidCardDefinitionException;
+import com.gempukku.lotro.cards.build.ValueSource;
 import com.gempukku.lotro.cards.build.field.FieldUtils;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppender;
 import com.gempukku.lotro.cards.build.field.effect.EffectAppenderProducer;
+import com.gempukku.lotro.cards.build.field.effect.appender.resolver.ValueResolver;
 import com.gempukku.lotro.logic.actions.CostToEffectAction;
 import com.gempukku.lotro.logic.effects.IncrementPhaseLimitEffect;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 import com.gempukku.lotro.logic.timing.Effect;
 import com.gempukku.lotro.logic.timing.PlayConditions;
 import org.json.simple.JSONObject;
@@ -17,12 +20,15 @@ public class IncrementPerPhaseLimit implements EffectAppenderProducer {
     public EffectAppender createEffectAppender(JSONObject effectObject, CardGenerationEnvironment environment) throws InvalidCardDefinitionException {
         FieldUtils.validateAllowedFields(effectObject, "limit", "perPlayer");
 
-        final int limit = FieldUtils.getInteger(effectObject.get("limit"), "limit", 1);
+        final ValueSource limitSource = ValueResolver.resolveEvaluator(effectObject.get("limit"), 1, environment);
         final boolean perPlayer = FieldUtils.getBoolean(effectObject.get("perPlayer"), "perPlayer", false);
 
         return new DelayedAppender() {
             @Override
             protected Effect createEffect(boolean cost, CostToEffectAction action, ActionContext actionContext) {
+                final Evaluator evaluator = limitSource.getEvaluator(actionContext);
+                final int limit = evaluator.evaluateExpression(actionContext.getGame(), actionContext.getSource());
+
                 if (perPlayer)
                     return new IncrementPhaseLimitEffect(actionContext.getSource(), actionContext.getPerformingPlayer() + "_", limit);
                 else
@@ -31,6 +37,9 @@ public class IncrementPerPhaseLimit implements EffectAppenderProducer {
 
             @Override
             public boolean isPlayableInFull(ActionContext actionContext) {
+                final Evaluator evaluator = limitSource.getEvaluator(actionContext);
+                final int limit = evaluator.evaluateExpression(actionContext.getGame(), actionContext.getSource());
+
                 if (perPlayer)
                     return PlayConditions.checkPhaseLimit(actionContext.getGame(), actionContext.getSource(), actionContext.getPerformingPlayer() + "_", limit);
                 else
