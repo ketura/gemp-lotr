beb5db82431c508c1c85f429a0e016edd14d23ee
Christopher Lund <lund_christopher@outlook.com>
2023-07-19 16:26:59 -0500

    Minor refactoring

--- gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/db/DeckDAO.java
+++ gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/db/DeckDAO.java
@@ -2,21 +2,20 @@ package com.gempukku.lotro.db;
 
 import com.gempukku.lotro.game.Player;
 import com.gempukku.lotro.cards.lotronly.LotroDeck;
-import com.gempukku.lotro.cards.CardDeck;
 
 import java.util.Map;
 import java.util.Set;
 
 public interface DeckDAO {
-    public LotroDeck getDeckForPlayer(Player player, String name);
+    LotroDeck getDeckForPlayer(Player player, String name);
 
-    public void saveDeckForPlayer(Player player, String name, String target_format, String notes, LotroDeck deck);
+    void saveDeckForPlayer(Player player, String name, String target_format, String notes, LotroDeck deck);
 
-    public void deleteDeckForPlayer(Player player, String name);
+    void deleteDeckForPlayer(Player player, String name);
 
-    public LotroDeck renameDeck(Player player, String oldName, String newName);
+    LotroDeck renameDeck(Player player, String oldName, String newName);
 
-    public Set<Map.Entry<String, String>> getPlayerDeckNames(Player player);
+    Set<Map.Entry<String, String>> getPlayerDeckNames(Player player);
 
-    public LotroDeck buildLotroDeckFromContents(String deckName, String contents, String target_format, String notes);
+    LotroDeck buildLotroDeckFromContents(String deckName, String contents, String target_format, String notes);
 }
